bbb2575003df95fe3510eca988723835f5e09009
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 05:03:50 +0000

    Add package optilogs/0.1.0/optilogs-0.1.0.tar.gz

---
+++ packages/optilogs/optilogs-0.1.0.tar.gz/optilogs-0.1.0/README.md
@@ -0,0 +1,10 @@
+# Optilog
+
+Optilog is a simple logging library that sends logs to a server via HTTP requests.
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install optilog.
+
+```bash
+pip install optilog
---
+++ packages/optilogs/optilogs-0.1.0.tar.gz/optilogs-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="optilogs",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+    ],
+    author="Allan PEREZ GONZALEZ",
+    author_email="aperezgo74@gmail.com",
+    description="A simple logging library to send logs to a server",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/votre-repo/optilog",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
