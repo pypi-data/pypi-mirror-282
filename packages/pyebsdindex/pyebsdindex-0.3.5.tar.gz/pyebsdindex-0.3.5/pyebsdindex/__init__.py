1a11cbb2f836057ead2d6e61279a0b158c11d359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 05:04:13 +0000

    Add package pyebsdindex/0.3.5/pyebsdindex-0.3.5.tar.gz

---
+++ packages/pyebsdindex/pyebsdindex-0.3.5.tar.gz/pyebsdindex-0.3.5/pyebsdindex/__init__.py
@@ -0,0 +1,32 @@
+__author__ = "Dave Rowenhorst"
+__author_email__ = ""
+# Initial committer first, then sorted by line contributions
+__credits__ = [
+    "Dave Rowenhorst",
+    "Håkon Wiik Ånes",
+]
+__description__ = "Python based tool for Radon based EBSD indexing"
+__name__ = "pyebsdindex"
+__version__ = "0.3.5"
+
+
+# Try to import only once - also will perform check that at least one GPU is found.
+try:
+    _pyopencl_installed = False
+    import pyopencl
+    from pyebsdindex.opencl import openclparam
+    testcl = openclparam.OpenClParam()
+    try:
+        gpu = testcl.get_gpu()
+        if len(gpu) > 0:
+            _pyopencl_installed = True
+    except:
+        raise ImportError('pyopencl could not find GPU')
+except ImportError:
+    _pyopencl_installed = False
+
+try:
+    import ray
+    _ray_installed = True
+except ImportError:
+    _ray_installed = False
