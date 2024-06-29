348f63f7d46bcc5031fb921ab1900ffbebe696d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 03:04:48 +0000

    Add package relationalai/0.3.9/relationalai-0.3.9.tar.gz

---
+++ packages/relationalai/relationalai-0.3.9.tar.gz/relationalai-0.3.9/src/relationalai/std/__init__.py
@@ -0,0 +1,13 @@
+from ..dsl import alias, rel, Vars
+from . import graphs, aggregates, strings, math, dates
+
+__all__ = [
+    "aggregates",
+    "alias",
+    "dates",
+    "graphs",
+    "math",
+    "rel",
+    "strings",
+    "Vars"
+]
