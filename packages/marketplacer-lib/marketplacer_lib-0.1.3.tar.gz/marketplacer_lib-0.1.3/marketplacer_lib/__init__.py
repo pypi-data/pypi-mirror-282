560ac516a5f23c8d8bdb89ba993c89f876d8780c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 05:03:28 +0000

    Add package marketplacer-lib/0.1.3/marketplacer_lib-0.1.3.tar.gz

---
+++ packages/marketplacer-lib/marketplacer_lib-0.1.3.tar.gz/marketplacer_lib-0.1.3/marketplacer_lib/__init__.py
@@ -0,0 +1,9 @@
+from .rest_client import MarketplacerRestClient, RestVariant, RestVariantAttributes
+from .gql_client import MarketplacerGQLClient
+
+__all__ = [
+    "MarketplacerRestClient",
+    "RestVariant",
+    "RestVariantAttributes",
+    "MarketplacerGQLClient",
+]
