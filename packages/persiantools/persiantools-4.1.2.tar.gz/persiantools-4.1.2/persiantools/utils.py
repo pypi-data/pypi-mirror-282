8551837966d3cb5585b2d64451d49173e3b4919a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 04:04:18 +0000

    Add package persiantools/4.1.2/persiantools-4.1.2.tar.gz

---
+++ packages/persiantools/persiantools-4.1.2.tar.gz/persiantools-4.1.2/persiantools/utils.py
@@ -0,0 +1,26 @@
+import re
+from typing import Dict
+
+
+def replace(string: str, dictionary: Dict[str, str]) -> str:
+    """
+    Replace occurrences of keys in the dictionary with their corresponding values in the given string.
+
+    Args:
+    string (str): The string to perform replacements on.
+    dictionary (Dict[str, str]): A dictionary where the keys are the substrings to be replaced and the values are the replacements.
+
+    Returns:
+    str: The modified string with replacements made.
+
+    Raises:
+    TypeError: If the input string is not of type str.
+    """
+    if not isinstance(string, str):
+        raise TypeError("Input must be of type str")
+
+    if not dictionary:
+        return string
+
+    pattern = re.compile("|".join(re.escape(key) for key in dictionary.keys()))
+    return pattern.sub(lambda x: dictionary[x.group()], string)
