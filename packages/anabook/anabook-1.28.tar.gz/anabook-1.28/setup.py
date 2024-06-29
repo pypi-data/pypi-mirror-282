91c21686d127b3fe12fd9b144aac2f94918d885a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 05:04:07 +0000

    Add package anabook/1.28/anabook-1.28.tar.gz

---
+++ packages/anabook/anabook-1.28.tar.gz/anabook-1.28/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="anabook",
+    version="1.28",
+    packages=find_packages(),
+    description="Funciones para analisis descriptivo y utilitarios usados en el desarrollo de modelos de Aprendizaje Automatico",
+    author="Freddy Alvarado",
+    author_email="freddy.alvarado.b1@gmail.com",
+    url="https://github.com/FreddyAlvarado/anabook",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=[
+        'pandas<=2.1.3',
+        'numpy<=1.26.2',
+        'matplotlib<=3.8.2',
+        'seaborn<=0.13.0',
+        'scipy<=1.11.4'
+    ],
+)
