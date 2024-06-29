e82eecdfdc2265c12da50d0257043f0bbf24a28b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 04:04:01 +0000

    Add package dycw-utilities/0.31.3/dycw_utilities-0.31.3-py3-none-any.whl

---
+++ packages/dycw-utilities/dycw_utilities-0.31.3-py3-none-any.whl/utilities/zoneinfo.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from zoneinfo import ZoneInfo
+
+HONG_KONG = ZoneInfo("Asia/Hong_Kong")
+TOKYO = ZoneInfo("Asia/Tokyo")
+US_CENTRAL = ZoneInfo("US/Central")
+US_EASTERN = ZoneInfo("US/Eastern")
+
+
+__all__ = ["HONG_KONG", "TOKYO", "US_CENTRAL", "US_EASTERN"]
