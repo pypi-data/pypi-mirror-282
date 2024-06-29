ff8623846c6e24906f4e9c572cb7bf40e18129ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 04:04:11 +0000

    Add package serverless-llm-store/0.0.0/serverless_llm_store-0.0.0.tar.gz

---
+++ packages/serverless-llm-store/serverless_llm_store-0.0.0.tar.gz/serverless_llm_store-0.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="serverless_llm_store",
+    version="0.0.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Yao Fu",
+    author_email="fuyao3860@gmail.com",
+    description="A simple description of your package",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/ServerlessLLM/ServerlessLLM",
+)
