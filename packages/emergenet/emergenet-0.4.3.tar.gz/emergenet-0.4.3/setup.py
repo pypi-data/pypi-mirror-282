0eacd063b3c8131160405c9d3a4a6f2211f85faa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 15:03:36 +0000

    Add package emergenet/0.4.3/emergenet-0.4.3.tar.gz

---
+++ packages/emergenet/emergenet-0.4.3.tar.gz/emergenet-0.4.3/setup.py
@@ -0,0 +1,58 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+import os, shutil, gzip
+
+package_name = 'emergenet'
+example_dir = 'examples/'
+bin_dir = 'bin/'
+example_data_dir = example_dir + 'example_data/'
+
+version = {}
+with open("version.py") as fp:
+    exec(fp.read(), version)
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name=package_name,
+    author='zed.uchicago.edu',
+    author_email='ishanu@uchicago.edu',
+    version = str(version['__version__']),
+    packages=find_packages(),
+    scripts=[],
+    url='https://github.com/zeroknowledgediscovery/emergenet',
+    license='LICENSE',
+    description='Superfast Risk Estimation of Emerging Pathogens',
+    keywords=[
+        'computational biology',
+        'decision trees', 
+        'machine learning', 
+        'emerging pathogens'],
+    download_url='https://github.com/zeroknowledgediscovery/emergenet/archive/'+str(version['__version__'])+'.tar.gz',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    install_requires=[
+        "scikit-learn", 
+        "scipy", 
+        "numpy",  
+        "pandas",
+        "joblib",
+        "quasinet==0.1.54",
+        "distance",
+        "alphashape",
+        "scipy"],
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Software Development :: Libraries",
+        "Programming Language :: Python :: 3.6"],
+    include_package_data=True,
+    )
