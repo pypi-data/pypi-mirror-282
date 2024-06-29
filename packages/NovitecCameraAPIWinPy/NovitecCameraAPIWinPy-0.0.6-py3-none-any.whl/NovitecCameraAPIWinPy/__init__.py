a136f568e148e5169fed76f338e0998cf232dfc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 03:02:45 +0000

    Add package NovitecCameraAPIWinPy/0.0.6/NovitecCameraAPIWinPy-0.0.6-py3-none-any.whl

---
+++ packages/NovitecCameraAPIWinPy/NovitecCameraAPIWinPy-0.0.6-py3-none-any.whl/NovitecCameraAPIWinPy/__init__.py
@@ -0,0 +1,5 @@
+from .NovitecCameraAPIWinPy import NovitecCamera
+from .NovitecCameraAPIWinPy import CError
+from .NovitecCameraAPIWinPy import CImage
+from .NovitecCameraAPIWinPy import DiscoverDeviceInfo
+from .NovitecCameraAPIWinPy import CallBackDisconnect
