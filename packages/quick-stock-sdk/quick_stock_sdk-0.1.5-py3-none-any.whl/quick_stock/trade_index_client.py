40814b44bd5f55dec0dbe7f899a2779366e3835a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 15:02:54 +0000

    Add package quick-stock-sdk/0.1.5/quick_stock_sdk-0.1.5-py3-none-any.whl

---
+++ packages/quick-stock-sdk/quick_stock_sdk-0.1.5-py3-none-any.whl/quick_stock/trade_index_client.py
@@ -0,0 +1,41 @@
+import datetime
+from quick_stock.d import getDClient
+from quick_stock.remote.index import TradeIndex
+
+class TradeIndexClient:
+    dClient = None
+
+    def __init__(self):
+        self.dClient = getDClient()
+
+    def get_all_index(self):
+        """
+        :return: list - string
+        """
+        df = self.dClient.select("select * from mc_index_basic")
+        if df.empty or datetime.date.today().day == 1:
+            df = TradeIndex().get_all()
+            self.dClient.save(df, 'mc_index_basic')
+        return df
+
+    def minute(self, code):
+        return TradeIndex().minute(code)
+
+    def minute30(self, code, start=None, end=None):
+        return TradeIndex().minute30(code,start,end)
+
+    def minute60(self, code, start=None, end=None):
+        return TradeIndex().minute60(code,start,end)
+
+    def daily(self, code, start=None, end=None):
+        return TradeIndex().daily(code,start,end)
+
+    def weekly(self, code, start=None, end=None):
+        return TradeIndex().weekly(code,start,end)
+
+    def monthly(self, code, start=None, end=None):
+        return TradeIndex().monthly(code,start,end)
+
+if __name__ == "__main__":
+    print(TradeIndexClient().daily("0.000001"))
+
