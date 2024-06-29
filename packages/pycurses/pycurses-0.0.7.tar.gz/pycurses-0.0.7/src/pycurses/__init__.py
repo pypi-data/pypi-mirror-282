ffac51e004959e650c0bb1801c04ea1fc5b59907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 03:04:10 +0000

    Add package pycurses/0.0.7/pycurses-0.0.7.tar.gz

---
+++ packages/pycurses/pycurses-0.0.7.tar.gz/pycurses-0.0.7/src/pycurses/__init__.py
@@ -0,0 +1,18 @@
+""" Python package for making terminal UIs """
+
+from setuptools import setup
+setup()
+
+from colors import CursesColors
+from list_view import ListView
+import windows
+import layout
+import mainwindow
+import popup
+
+
+__version__ = "0.0.7"
+
+
+
+
