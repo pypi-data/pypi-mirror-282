67d3245c0a2a717352e1982149f0989d5761ca46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 03:04:16 +0000

    Add package marshmallowqa.py/0.2.1/marshmallowqa_py-0.2.1-py3-none-any.whl

---
+++ packages/marshmallowqa.py/marshmallowqa_py-0.2.1-py3-none-any.whl/marshmallowqa/__init__.py
@@ -0,0 +1,11 @@
+from .cookie import MarshmallowCookie, retrieve_cookies
+from .marshmallow import Marshmallow, Message, MessageDetail, User
+
+__all__ = [
+    "Marshmallow",
+    "Message",
+    "MessageDetail",
+    "MarshmallowCookie",
+    "User",
+    "retrieve_cookies",
+]
