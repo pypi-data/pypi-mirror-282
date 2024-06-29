5bae30944d281aa994417d3d742e5c82d8a341e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 03:04:51 +0000

    Add package nautobot-secrets-providers/3.0.0/nautobot_secrets_providers-3.0.0.tar.gz

---
+++ packages/nautobot-secrets-providers/nautobot_secrets_providers-3.0.0.tar.gz/nautobot_secrets_providers-3.0.0/nautobot_secrets_providers/urls.py
@@ -0,0 +1,12 @@
+"""Django urlpatterns declaration for nautobot_secrets_providers app."""
+
+from django.urls import path
+
+from nautobot_secrets_providers import views
+
+
+app_name = "nautobot_secrets_providers"
+
+urlpatterns = [
+    path("", views.SecretsProvidersHomeView.as_view(), name="home"),
+]
