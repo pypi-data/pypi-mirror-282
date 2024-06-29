0fccadc15d7519c7ca34da48a51651c34d7fb25d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 15:02:54 +0000

    Add package reactivedataflow/0.1.3/reactivedataflow-0.1.3.tar.gz

---
+++ packages/reactivedataflow/reactivedataflow-0.1.3.tar.gz/reactivedataflow-0.1.3/reactivedataflow/execution_graph.py
@@ -0,0 +1,48 @@
+# Copyright (c) 2024 Microsoft Corporation.
+"""The reactivedataflow Library."""
+
+from typing import Any
+
+import reactivex as rx
+
+from .errors import OutputNotFoundError
+from .model import Output
+from .nodes.execution_node import Node
+
+
+class ExecutionGraph:
+    """A live execution graph."""
+
+    _nodes: dict[str, Node]
+    _outputs: dict[str, Output]
+
+    def __init__(self, nodes: dict[str, Node], outputs: dict[str, Output]):
+        """Initialize the execution graph.
+
+        Args:
+            nodes: The nodes in the graph.
+            outputs: The outputs of the graph.
+        """
+        self._nodes = nodes
+        self._outputs = outputs
+
+    def dispose(self) -> None:
+        """Dispose of all nodes."""
+        for node in self._nodes.values():
+            node.dispose()
+
+    def output(self, name: str) -> rx.Observable[Any]:
+        """Read the output of a node."""
+        output = self._outputs.get(name)
+        if output is None:
+            raise OutputNotFoundError(name)
+        node = self._nodes[output.node or output.name]
+        return node.output(output.port)
+
+    def output_value(self, name: str) -> rx.Observable[Any]:
+        """Read the output of a node."""
+        output = self._outputs.get(name)
+        if output is None:
+            raise OutputNotFoundError(name)
+        node = self._nodes[output.node or output.name]
+        return node.output_value(output.port)
