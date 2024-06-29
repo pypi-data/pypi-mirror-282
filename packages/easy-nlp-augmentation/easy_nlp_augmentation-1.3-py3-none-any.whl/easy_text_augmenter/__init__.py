1f33ed4f803bf998d95a32fbd6a446d9cf35899a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 15:03:54 +0000

    Add package easy-nlp-augmentation/1.3/easy_nlp_augmentation-1.3-py3-none-any.whl

---
+++ packages/easy-nlp-augmentation/easy_nlp_augmentation-1.3-py3-none-any.whl/easy_text_augmenter/__init__.py
@@ -0,0 +1,4 @@
+from .augment import augment_random_word, augment_random_character, augment_word_bert
+from .info import info
+
+__all__ = ['augment_random_word', 'augment_random_character', 'augment_word_bert', 'info']
