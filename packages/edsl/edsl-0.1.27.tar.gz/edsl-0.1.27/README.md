147723b753feec8dcd808742f73ba7b6b8ec82e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 03:04:29 +0000

    Add package edsl/0.1.27/edsl-0.1.27.tar.gz

---
+++ packages/edsl/edsl-0.1.27.tar.gz/edsl-0.1.27/README.md
@@ -0,0 +1,29 @@
+# Expected Parrot Domain-Specific Language 
+<p align="center">
+  <img src="https://github.com/expectedparrot/edsl/blob/main/static/logo.png?raw=true" alt="edsl.png" width="100"/>
+</p>
+
+The Expected Parrot Domain-Specific Language (EDSL) package lets you conduct computational social science and market research with AI. Use it to design surveys and experiments, simulate responses with large language models, and perform data labeling and other research tasks. EDSL comes with built-in methods for analyzing, visualizing and sharing your results. 
+
+## 🔗 Links
+- PyPI: https://pypi.org/project/edsl/
+- Documentation: https://docs.expectedparrot.com
+- Getting started: https://docs.expectedparrot.com/en/latest/starter_tutorial.html
+- Discord: https://discord.com/invite/mxAYkjfy9m
+
+
+## 💡 Contributions, Feature Requests & Bugs
+Interested in contributing? Want us to add a new feature? Found a nasty bug that you would like us to squash? Please send us an email at info@expectedparrot.com or message us at our Discord server.
+
+
+## 💻 Getting started
+EDSL is compatible with Python 3.9 - 3.12.
+```
+pip install edsl
+```
+
+- Read the docs at http://docs.expectedparrot.com.
+- See https://docs.expectedparrot.com/en/latest/starter_tutorial.html for examples and tutorials.
+
+## 🔧 Dependencies
+API keys for LLMs that you want to use, stored in a `.env` file
---
+++ packages/edsl/edsl-0.1.27.tar.gz/edsl-0.1.27/edsl/language_models/repair.py
@@ -0,0 +1,103 @@
+import json
+import asyncio
+
+from edsl.utilities.utilities import clean_json
+
+
+async def async_repair(bad_json, error_message=""):
+    s = clean_json(bad_json)
+    from edsl import Model
+
+    m = Model()
+
+    try:
+        # this is the OpenAI version, but that's fine
+        valid_dict = json.loads(s)
+        success = True
+    except json.JSONDecodeError:
+        valid_dict = {}
+        success = False
+        # print("Replacing control characters didn't work. Trying with the model.")
+    else:
+        return valid_dict, success
+
+    prompt = f"""This is the output from a less capable language model.  
+    It was supposed to respond with just a JSON object with an answer to a question and some commentary, 
+    in a field called "comment" next to "answer".
+    Please repair this bad JSON: {bad_json}."""
+
+    if error_message:
+        prompt += f" Parsing error message: {error_message}"
+
+    try:
+        results = await m.async_execute_model_call(
+            prompt,
+            system_prompt="You are a helpful agent. Only return the repaired JSON, nothing else.",
+        )
+    except Exception as e:
+        return {}, False
+
+    try:
+        # this is the OpenAI version, but that's fine
+        valid_dict = json.loads(results["choices"][0]["message"]["content"])
+        success = True
+    except json.JSONDecodeError:
+        valid_dict = {}
+        success = False
+
+    return valid_dict, success
+
+
+def repair_wrapper(bad_json, error_message=""):
+    try:
+        loop = asyncio.get_event_loop()
+        if loop.is_running():
+            # Add repair as a task to the running loop
+            task = loop.create_task(async_repair(bad_json, error_message))
+            return task
+        else:
+            # Run a new event loop for repair
+            return loop.run_until_complete(async_repair(bad_json, error_message))
+    except RuntimeError:
+        # Create a new event loop if one is not already available
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        return loop.run_until_complete(async_repair(bad_json, error_message))
+
+
+def repair(bad_json, error_message=""):
+    return repair_wrapper(bad_json, error_message)
+
+
+# Example usage:
+# result, success = repair_wrapper('{"name": "John Doe", "age": 30,}')  # example bad JSON
+
+
+# def repair_wrapper(bad_json, error_message=""):
+#     loop = asyncio.get_event_loop()
+#     if loop.is_running():
+#         # Add repair as a task to the running loop
+#         task = loop.create_task(repair(bad_json, error_message))
+#         return task
+#     else:
+#         # Run a new event loop for repair
+#         return loop.run_until_complete(repair(bad_json, error_message))
+
+
+# Example usage:
+# result, success = repair_wrapper('{"name": "John Doe", "age": 30,}')  # example bad JSON
+
+
+if __name__ == "__main__":
+    bad_json = """
+    {
+      'answer': "The problematic phrase in the excerpt is \'typically\'. This word is vague and can lead to different interpretations. An alternative phrasing that would be less problematic is: 
+      'On average, how long do you cook scrambled eggs?}
+    """
+    try:
+        json.loads(bad_json)
+        print("Loaded")
+    except json.JSONDecodeError as e:
+        error_message = str(e)
+        repaired, success = repair(bad_json, error_message)
+        print(f"Repaired: {repaired}")
