ad429e76b2a730468ed84dd07f123c9ed8221fad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 04:04:03 +0000

    Add package python-115/0.0.9.7.2.3/python_115-0.0.9.7.2.3.tar.gz

---
+++ packages/python-115/python_115-0.0.9.7.2.3.tar.gz/python_115-0.0.9.7.2.3/p115/tool/__init__.py
@@ -0,0 +1,245 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+__author__ = "ChenyangGao <https://chenyanggao.github.io>"
+__all__ = [
+    "login_scan_cookie", "crack_captcha", "wish_make", "wish_answer", 
+    "wish_list", "wish_aid_list", "wish_adopt", 
+]
+
+from collections import defaultdict
+from collections.abc import Callable, Iterable
+from typing import cast
+
+from concurrenttools import thread_pool_batch
+from p115 import P115Client, check_response
+
+
+CAPTCHA_CRACK: Callable[[bytes], str]
+
+
+def login_scan_cookie(
+    client: str | P115Client, 
+    app: str = "", 
+    replace: bool = False, 
+) -> str:
+    """扫码登录 115 网盘，获取绑定到特定 app 的 cookie
+    app 至少有 23 个可用值，目前找出 13 个：
+        - web
+        - ios
+        - 115ios
+        - android
+        - 115android
+        - 115ipad
+        - tv
+        - qandroid
+        - windows
+        - mac
+        - linux
+        - wechatmini
+        - alipaymini
+    还有几个备选：
+        - bios
+        - bandroid
+        - qios（登录机制有些不同，暂时未破解）
+
+    设备列表如下：
+
+    | No.    | ssoent  | app        | description            |
+    |-------:|:--------|:-----------|:-----------------------|
+    |     01 | A1      | web        | 网页版                 |
+    |     02 | A2      | ?          | 未知: android          |
+    |     03 | A3      | ?          | 未知: iphone           |
+    |     04 | A4      | ?          | 未知: ipad             |
+    |     05 | B1      | ?          | 未知: android          |
+    |     06 | D1      | ios        | 115生活(iOS端)         |
+    |     07 | D2      | ?          | 未知: ios              |
+    |     08 | D3      | 115ios     | 115(iOS端)             |
+    |     09 | F1      | android    | 115生活(Android端)     |
+    |     10 | F2      | ?          | 未知: android          |
+    |     11 | F3      | 115android | 115(Android端)         |
+    |     12 | H1      | ipad       | 未知: ipad             |
+    |     13 | H2      | ?          | 未知: ipad             |
+    |     14 | H3      | 115ipad    | 115(iPad端)            |
+    |     15 | I1      | tv         | 115网盘(Android电视端) |
+    |     16 | M1      | qandriod   | 115管理(Android端)     |
+    |     17 | N1      | qios       | 115管理(iOS端)         |
+    |     18 | O1      | ?          | 未知: ipad             |
+    |     19 | P1      | windows    | 115生活(Windows端)     |
+    |     20 | P2      | mac        | 115生活(macOS端)       |
+    |     21 | P3      | linux      | 115生活(Linux端)       |
+    |     22 | R1      | wechatmini | 115生活(微信小程序)    |
+    |     23 | R2      | alipaymini | 115生活(支付宝小程序)  |
+    """
+    if isinstance(client, str):
+        client = P115Client(client)
+    if not app:
+        app = client.login_device()["icon"]
+    return client.login_another_app(app, replace=replace).cookies
+
+
+def crack_captcha(
+    client: str | P115Client, 
+    sample_count: int = 16, 
+    crack: None | Callable[[bytes], str] = None, 
+) -> bool:
+    """破解 115 的图片验证码。如果返回 True，则说明破解成功，否则失败。如果失败，就不妨多运行这个函数几次。
+
+    :param client: 115 客户端
+    :param sample_count: 单个文字的采样次数，共会执行 10 * sample_count 次识别
+    :param crack: 破解验证码图片，输入图片的二进制数据，输出识别的字符串
+
+    :return: 是否破解成功
+
+    你可以反复尝试，直到破解成功，代码如下
+
+        while not crack_captcha(client):
+            pass
+
+    如果你需要检测是否存在验证码，然后进行破解，代码如下
+
+        resp = client.download_url_web("a")
+        if not resp["state"] and resp["code"] == 911:
+            print("出现验证码，尝试破解")
+            while not crack_captcha(client):
+                print("破解失败，再次尝试")
+    """
+    global CAPTCHA_CRACK
+    if crack is None:
+        try:
+            crack = CAPTCHA_CRACK
+        except NameError:
+            try:
+                # https://pypi.org/project/ddddocr/
+                from ddddocr import DdddOcr
+            except ImportError:
+                from subprocess import run
+                from sys import executable
+                run([executable, "-m", "pip", "install", "-U", "ddddocr==1.4.11"], check=True)
+                from ddddocr import DdddOcr # type: ignore
+            crack = CAPTCHA_CRACK = cast(Callable[[bytes], str], DdddOcr(show_ad=False).classification)
+    if isinstance(client, str):
+        client = P115Client(client)
+    while True:
+        captcha = crack(client.captcha_code())
+        if len(captcha) == 4 and all("\u4E00" <= char <= "\u9FFF" for char in captcha):
+            break
+    ls: list[defaultdict[str, int]] = [defaultdict(int) for _ in range(10)]
+    def crack_single(i, submit):
+        try:
+            char = crack(client.captcha_single(i))
+            if len(char) == 1 and "\u4E00" <= char <= "\u9FFF":
+                ls[i][char] += 1
+            else:
+                submit(i)
+        except:
+            submit(i)
+    thread_pool_batch(crack_single, (i for i in range(10) for _ in range(sample_count)))
+    l: list[str] = [max(d, key=lambda k: d[k]) for d in ls]
+    try:
+        code = "".join(str(l.index(char)) for char in captcha)
+    except ValueError:
+        return False
+    resp = client.captcha_verify(code)
+    return resp["state"]
+
+
+def wish_make(
+    client: str | P115Client, 
+    content: str = "随便许个愿", 
+    size: int = 5, 
+) -> str:
+    """许愿树活动：创建许愿（许愿创建后需要等审核）
+    :param client: 115 客户端
+    :param content: 许愿内容
+    :param size: 答谢空间大小，单位是 GB
+
+    :return: 许愿 id
+    """
+    if isinstance(client, str):
+        client = P115Client(client)
+    return check_response(client.act_xys_wish(
+        {"rewardSpace": size, "content": content}
+    ))["data"]["xys_id"]
+
+
+def wish_answer(
+    client: str | P115Client, 
+    wish_id: str, 
+    content: str = "帮你助个愿", 
+    file_ids: int | str | Iterable[int | str] = "", 
+) -> str:
+    """许愿树活动：创建助愿（助愿创建后需要等审核）
+    :param client: 115 客户端
+    :param wish_id: 许愿 id
+    :param content: 助愿内容
+    :param file_ids: 文件在你的网盘的 id，多个用逗号 "," 隔开
+
+    :return: 助愿 id
+    """
+    if isinstance(client, str):
+        client = P115Client(client)
+    if not isinstance(file_ids, (int, str)):
+        file_ids = ",".join(map(str, file_ids))
+    check_response(client.act_xys_get_desire_info(wish_id))
+    return check_response(
+        client.act_xys_aid_desire({"id": wish_id, "content": content, "file_ids": file_ids}
+    ))["data"]["aid_id"]
+
+
+def wish_list(
+    client: str | P115Client, 
+    type: int = 0, 
+) -> list[dict]:
+    """许愿树活动：我的许愿列表
+    :param client: 115 客户端
+    :param type: 类型
+        - 0: 全部
+        - 1: 进行中
+        - 2: 已实现
+    """
+    if isinstance(client, str):
+        client = P115Client(client)
+    payload: dict = {"type": type, "limit": 1000, "page": 1}
+    ls = adds = check_response(client.act_xys_my_desire(payload))["data"]["list"]
+    while len(adds) == 1000:
+        payload["page"] += 1
+        adds = check_response(client.act_xys_my_desire(payload))["data"]["list"]
+        ls.extend(adds)
+    return ls
+
+
+def wish_aid_list(
+    client: str | P115Client, 
+    wish_id: str, 
+) -> list[dict]:
+    """许愿树活动：许愿的助愿列表
+    :param wish_id: 许愿 id
+    """
+    if isinstance(client, str):
+        client = P115Client(client)
+    payload: dict = {"id": wish_id, "limit": 1000, "page": 1}
+    ls = adds = check_response(client.act_xys_desire_aid_list(payload))["data"]["list"]
+    while len(adds) == 1000:
+        payload["page"] += 1
+        adds = check_response(client.act_xys_desire_aid_list(payload))["data"]["list"]
+        ls.extend(adds)
+    return ls
+
+
+def wish_adopt(
+    client: str | P115Client, 
+    wish_id: str, 
+    aid_id: int | str, 
+    to_cid: int = 0, 
+) -> dict:
+    """许愿树活动：采纳助愿
+    :param client: 115 客户端
+    :param wish_id: 许愿 id
+    :param aid_id: 助愿 id
+    :param to_cid: 助愿的分享文件保存到你的网盘中目录的 id
+    """
+    if isinstance(client, str):
+        client = P115Client(client)
+    return check_response(client.act_xys_adopt({"did": wish_id, "aid": aid_id, "to_cid": to_cid}))
+
