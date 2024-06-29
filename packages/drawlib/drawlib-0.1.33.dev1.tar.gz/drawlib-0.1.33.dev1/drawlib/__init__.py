3582eac20864fc5036f5a79b7b734072e32f5233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 05:04:12 +0000

    Add package drawlib/0.1.33.dev1/drawlib-0.1.33.dev1.tar.gz

---
+++ packages/drawlib/drawlib-0.1.33.dev1.tar.gz/drawlib-0.1.33.dev1/drawlib/__init__.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2024 Yuichi Ito (yuichi@yuichi.com)
+#
+# This software is licensed under the Apache License, Version 2.0.
+# For more information, please visit: https://github.com/yuichi110/drawlib
+#
+# This software is provided "as is", without warranty of any kind,
+# express or implied, including but not limited to the warranties of
+# merchantability, fitness for a particular purpose and noninfringement.
+
+"""Root package."""
+
+from typing import Final
+
+# please update here when you release new version
+VERSION = "0.1.33.dev1"
+
+# might not be changed
+LIB_NAME: Final[str] = "drawlib"
+AUTHOR: Final[str] = "Yuichi Ito"
+CONTACT: Final[str] = "yuichi@yuichi.com"
+__version__: Final[str] = VERSION
