3fa15f86efff9fed0cb3aed39e44d9741569a2fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 04:03:37 +0000

    Add package persian-pdf-converter/0.1.1/persian_pdf_converter-0.1.1.tar.gz

---
+++ packages/persian-pdf-converter/persian_pdf_converter-0.1.1.tar.gz/persian_pdf_converter-0.1.1/setup.py
@@ -0,0 +1,31 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="persian_pdf_converter",
+    version="0.1.1",
+    packages=find_packages(),
+    package_data={
+        '': ['statics/*'],  # Include all files and subdirectories inside folder1 and folder2
+    },
+    install_requires=[
+        "tqdm",
+        "pdf2image",
+        "pytesseract",
+        "Pillow",
+        "python-docx"
+    ],
+    author="mahdiramezani",
+    author_email="mahdiramezanii.official@gmail.com",
+    description="convert persian pdf to .docx",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/mahdiramezanii/persian_pdf_converter",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
