c9487e8e44ddca9f9d1e853bfb92cbc51347d916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 04:03:32 +0000

    Add package aic-utils/0.27/aic_utils-0.27.tar.gz

---
+++ packages/aic-utils/aic_utils-0.27.tar.gz/aic_utils-0.27/setup.py
@@ -0,0 +1,26 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='aic_utils',
+    version='0.27',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        # Add other dependencies here
+    ],
+    author='Dylan D',
+    author_email='dylan.doyle@jdpa.com',
+    description='A utility package for dataset ID retrieval',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/dylandoyle11/aic_utils', 
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
+
