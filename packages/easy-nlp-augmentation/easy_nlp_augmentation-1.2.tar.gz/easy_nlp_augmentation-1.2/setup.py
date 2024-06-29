6d070a3a688766166a177618458c603122ec5547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 15:03:54 +0000

    Add package easy-nlp-augmentation/1.2/easy_nlp_augmentation-1.2.tar.gz

---
+++ packages/easy-nlp-augmentation/easy_nlp_augmentation-1.2.tar.gz/easy_nlp_augmentation-1.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='easy_nlp_augmentation',
+    version='1.2',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'requests',
+        'pandas',
+        'nlpaug'
+    ],
+    entry_points={
+        'console_scripts': [
+            'text_augmenter = text_augmenter.augment:main'
+        ]
+    },
+    author='Shizuka',
+    author_email='shizuka0@proton.me',
+    description='A package for augmenting text data using NLP techniques directly in your pandas dataframe.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Shizu-ka/easy-nlp-augmentation',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
