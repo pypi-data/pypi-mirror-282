7780c65ffb233d3d889b73100d1829a0530fc90c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 05:03:19 +0000

    Add package kaxe/0.3.1/kaxe-0.3.1-py3-none-any.whl

---
+++ packages/kaxe/kaxe-0.3.1-py3-none-any.whl/kaxe/objects/__init__.py
@@ -0,0 +1,8 @@
+
+from .point import Points
+from .equation import Equation
+from .function import Function
+from .pillar import Pillars
+from .map import ColorMap
+from .parameter import ParametricEquation
+from .arrow import Arrow
