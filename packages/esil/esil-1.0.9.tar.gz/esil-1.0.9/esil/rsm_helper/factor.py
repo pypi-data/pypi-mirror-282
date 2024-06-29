31e74686f8ebcc13e35f0e4afc10245c5eeb0ff2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 03:05:10 +0000

    Add package esil/1.0.9/esil-1.0.9.tar.gz

---
+++ packages/esil/esil-1.0.9.tar.gz/esil-1.0.9/esil/rsm_helper/factor.py
@@ -0,0 +1,10 @@
+class FactorInfo:
+    def __init__(self):
+        self.Region = ""
+        self.Pollutant = ""
+        self.Source = ""
+        self.Min = 0.0
+        self.Max = 1.2
+        self.Limit = 0.2
+        self.BaseEmissionRatio = 0.0
+        self.Index = 0
