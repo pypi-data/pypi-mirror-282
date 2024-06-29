1c0563bd64644eb4045c37f89c509f43dc57079b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 14:03:14 +0000

    Add package tmdybinding/0.1.1/tmdybinding-0.1.1.tar.gz

---
+++ packages/tmdybinding/tmdybinding-0.1.1.tar.gz/tmdybinding-0.1.1/tmdybinding/__init__.py
@@ -0,0 +1,30 @@
+"""Tight-binding lattices for TMDs for the Pybinding-package."""
+from .tmd_lattice import *
+from .tmd_abstract_lattice import *
+from .parameters import *
+from .parameters import *
+
+
+def tests():
+    """Run the package tests."""
+    import pytest
+    import pathlib
+    import os
+    from pybinding.utils.misc import cd
+    import pybinding as pb
+
+    module_path = pathlib.Path(__file__).parent
+
+    args = []
+    if (module_path / 'tests').exists():
+        # tests are inside installed package -> use read-only mode
+        args.append('--failpath=' + os.getcwd() + '/failed')
+        with cd(module_path):
+            args += ['-c', str(module_path / 'tests/local.cfg'), str(module_path)]
+            error_code = pytest.main(args)
+    else:
+        # tests are in dev environment -> use development mode
+        with cd(module_path.parent):
+            error_code = pytest.main(args)
+
+    return error_code or None
