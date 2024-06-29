cdb38fcbc42f007b668b533355ebbd6bbda9e049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 15:03:52 +0000

    Add package AlphaZero-GameEnv/1.0.0.0/AlphaZero_GameEnv-1.0.0.0-py3-none-any.whl

---
+++ packages/AlphaZero-GameEnv/AlphaZero_GameEnv-1.0.0.0-py3-none-any.whl/SimpleGomoku/__init__.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append('./')
+
+# from .SelfPlay import *
+# from .MCTS import *
+# from .Train import *
+# from .Util import *
+# from .Evaluate import *
---
+++ packages/AlphaZero-GameEnv/AlphaZero_GameEnv-1.0.0.0-py3-none-any.whl/SimpleGomoku/env/__init__.py
@@ -0,0 +1,5 @@
+import sys
+sys.path.append('../')
+
+from .Gomoku import *
+from .TicTacToe import *
