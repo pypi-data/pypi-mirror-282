d4c55e7077200173a173fedf7e69afe9238348d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 04:03:09 +0000

    Add package aerosol-functions/0.0.15/aerosol-functions-0.0.15.tar.gz

---
+++ packages/aerosol-functions/aerosol-functions-0.0.15.tar.gz/aerosol-functions-0.0.15/README.md
@@ -0,0 +1,11 @@
+![logo](https://github.com/jlpl/aerosol-functions/raw/master/logo.png)
+
+A collection of tools to analyze and visualize atmospheric aerosol and ion data.
+
+## Installation
+```shell
+pip install aerosol-functions
+```
+
+## Documentation
+See [here](https://jlpl.github.io/aerosol-functions/)
---
+++ packages/aerosol-functions/aerosol-functions-0.0.15.tar.gz/aerosol-functions-0.0.15/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md","r") as fh: 
+    long_description = fh.read()
+
+setup(
+    name = "aerosol-functions",
+    version = "0.0.15",
+    description = 'Functions to analyze atmospheric aerosol data',
+    package_dir = {'':'src'},
+    packages = ['aerosol'],
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    python_requires = '>=3.9',
+    install_requires = [
+        "pandas >= 1.1.0",
+        "numpy >= 1.19.0",
+        "matplotlib >= 3.3.4",
+        "scipy >= 1.5.3",
+    ],
+    url = "https://github.com/jlpl/aerosol-functions",
+    author = "Janne Lampilahti",
+    author_email = "janne.lampilahti@helsinki.fi",
+)
