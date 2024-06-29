c95af2d5e878e1a0e32a327261a80def2637360b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 03:04:32 +0000

    Add package invenio-requests/4.5.1/invenio-requests-4.5.1.tar.gz

---
+++ packages/invenio-requests/invenio-requests-4.5.1.tar.gz/invenio-requests-4.5.1/invenio_requests/__init__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021-2024 CERN.
+# Copyright (C) 2021-2023 TU Wien.
+#
+# Invenio-Requests is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Invenio module for generic and customizable requests."""
+
+from .customizations import RequestAction
+from .ext import InvenioRequests
+from .proxies import (
+    current_event_type_registry,
+    current_events_service,
+    current_request_type_registry,
+    current_requests,
+    current_requests_resource,
+    current_requests_service,
+)
+
+__version__ = "4.5.1"
+
+__all__ = (
+    "__version__",
+    "current_event_type_registry",
+    "current_events_service",
+    "current_request_type_registry",
+    "current_requests_resource",
+    "current_requests_service",
+    "current_requests",
+    "InvenioRequests",
+    "RequestAction",
+)
