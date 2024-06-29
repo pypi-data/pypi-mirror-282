d0dd1e96bbd51a4d8797503a424e07927e019e9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 03:02:42 +0000

    Add package tofupilot/0.1.16/tofupilot-0.1.16.tar.gz

---
+++ packages/tofupilot/tofupilot-0.1.16.tar.gz/tofupilot-0.1.16/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tofupilot",
+    version="0.1.16",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "setuptools",
+        "packaging"
+    ],
+    author="Félix Berthier",
+    author_email="felix.berthier@tofupilot.com",
+    description="The official python client for the TofuPilot API",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/tofupilot/python-client",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
