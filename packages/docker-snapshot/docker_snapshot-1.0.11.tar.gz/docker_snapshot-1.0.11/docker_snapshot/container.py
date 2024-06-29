44d4bb11d4e220831fd22c61a3ddea6bdcd02ffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 03:04:47 +0000

    Add package docker-snapshot/1.0.11/docker_snapshot-1.0.11.tar.gz

---
+++ packages/docker-snapshot/docker_snapshot-1.0.11.tar.gz/docker_snapshot-1.0.11/docker_snapshot/container.py
@@ -0,0 +1,206 @@
+import io
+import re
+import shlex
+import importlib.resources as pkg_resources
+import click
+import docker
+from functools import wraps
+from rich.progress import Progress
+from docker import errors
+from docker_snapshot import images, settings
+
+
+HELPER_BASE_PATH = "/mnt/ds"
+
+client = docker.from_env()
+container = None
+
+
+def requires_helper_container(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        alloc()
+        res = f(*args, **kwargs)
+        dealloc()
+        return res
+
+    return wrapper
+
+
+class freeze_target_container(object):
+    def __init__(self):
+        pass
+
+    def __enter__(self):
+        stop(settings.get("container_name"))
+
+    def __exit__(self, type, value, traceback):
+        start(settings.get("container_name"))
+
+
+def is_target_container_running():
+    return is_running(settings.get("container_name"))
+
+
+def get_image_id():
+    namespace = settings.get("namespace")
+    return f"ds-{namespace}"
+
+
+def get_volume_id():
+    namespace = settings.get("namespace")
+    return f"ds-{namespace}"
+
+
+def get_container_id():
+    namespace = settings.get("namespace")
+    return f"ds-{namespace}"
+
+
+def build_image():
+    dockerfile = io.BytesIO(pkg_resources.read_text(images, "rsync").encode("utf-8"))
+    image, _ = client.images.build(fileobj=dockerfile, tag=get_image_id())
+    return image
+
+
+def create_volume():
+    return client.volumes.create(name=get_volume_id())
+
+
+def create_container(image, volume, volumes_from):
+    return client.containers.create(
+        image,
+        name=get_container_id(),
+        volumes={volume.name: {"bind": HELPER_BASE_PATH, "mode": "rw"}},
+        volumes_from=volumes_from,
+        working_dir=HELPER_BASE_PATH,
+        tty=True,
+        stdin_open=True,
+        auto_remove=False,
+    )
+
+
+def alloc():
+    global container
+    # Get or build image
+    try:
+        # NOTE: Blocks future versions of Dockerfile
+        image = client.images.get(get_image_id())
+    except errors.ImageNotFound:
+        image = build_image()
+
+    # Get or create volume
+    try:
+        volume = client.volumes.get(get_volume_id())
+    except errors.NotFound:
+        volume = create_volume()
+
+    # Get and/or run container
+    try:
+        container = client.containers.get(get_container_id())
+    except errors.NotFound:
+        target_container_name = settings.get("container_name")
+        if not exists(target_container_name):
+            raise Exception(
+                f"Target container with name {target_container_name} not found."
+            )
+        container = create_container(image, volume, target_container_name)
+
+    container.start()
+
+
+def dealloc():
+    container.stop(timeout=0)
+
+
+def sh(command):
+    global container
+    code, output = container.exec_run(["sh", "-c", command])
+    return output.decode("utf-8")
+
+
+def file_read(path):
+    return sh(f"cat {path} 2>/dev/null")
+
+
+def file_write(path, content):
+    return sh(f"echo {shlex.quote(content)} > {path}")
+
+
+def directory_remove(path):
+    return sh(f"rm -rf {path}")
+
+
+def directory_size(path):
+    # TODO: Replace with regex
+    size = int(sh(f"du -s {path}").split("\t")[0]) * 1024
+    return size
+
+def directory_filecount(path):
+    try: return int(sh(f"find {path} -type f | wc -l"))
+    except ValueError: return None
+
+
+def sync(source_directory, destination_path):
+    global container
+
+    response = container.exec_run(
+        [
+            "sh",
+            "-c",
+            f"rsync -aAHX --delete --info=progress2 {source_directory}/ {destination_path}",
+        ],
+        stream=True,
+        stdout=True,
+        stderr=True,
+    )
+
+    current_percentage = 0
+    with Progress() as progress:
+        task = progress.add_task("Copying files...", total=100)
+        for out in response.output:
+            percentage_string = re.search(r"\d+%", out.decode("utf-8"))
+            if not percentage_string:
+                continue
+
+            try:
+                percentage = int(percentage_string.group()[:-1])
+                if current_percentage >= percentage:
+                    continue
+            except Exception as e:
+                pass
+
+            progress.update(task, advance=percentage - current_percentage)
+            current_percentage = percentage
+        progress.update(task, completed=100)
+
+
+def stop(container_name):
+    click.echo(f"Stopping container {container_name}...")
+    try:
+        client.containers.get(container_name).stop(timeout=1)
+    except errors.NotFound:
+        raise Exception(f"Container `{container_name}` not found")
+
+
+def start(container_name):
+    click.echo(f"Starting container {container_name}...")
+    try:
+        client.containers.get(container_name).start()
+    except errors.NotFound:
+        raise Exception(f"Container `{container_name}` not found")
+
+
+def exists(container_name):
+    try:
+        client.containers.get(container_name)
+        return True
+    except errors.NotFound:
+        return False
+
+
+def is_running(container_name):
+    try:
+        return client.containers.get(container_name).status == "running"
+    except errors.NotFound:
+        return False
---
+++ packages/docker-snapshot/docker_snapshot-1.0.11.tar.gz/docker_snapshot-1.0.11/docker_snapshot/snapshot.py
@@ -0,0 +1,122 @@
+import json
+import uuid
+import hruid
+import datetime
+import dataclasses
+from docker_snapshot import container, settings
+
+
+@dataclasses.dataclass
+class Snapshot:
+    uuid: str = None
+    name: str = None
+    size: int = 0
+    file_count: int = 0
+    created: int = 0
+
+    def __post_init__(self):
+        if not self.uuid:
+            self.uuid = str(uuid.uuid4())
+        if not self.created:
+            self.created = int(datetime.datetime.now().timestamp())
+        if not self.name:
+            generator = hruid.Generator(use_number=False)
+            self.name = generator.random()
+        # NOTE: Soft "migration", will result in saved file_count after first create / remove
+        if not self.file_count:
+            self.file_count = container.directory_filecount(self.path)
+
+    @property
+    def created_when(self):
+        return datetime.datetime.fromtimestamp(self.created)
+
+    @property
+    def path(self):
+        return f"{container.HELPER_BASE_PATH}/{self.uuid}"
+
+
+# Could be made lazy-loaded
+def load_database():
+    json_string = container.file_read("db.json")
+    if not json_string:
+        return []
+
+    return list(
+        map(lambda snapshot_data: Snapshot(**snapshot_data), json.loads(json_string))
+    )
+
+
+def save_database(snapshot_list):
+    json_string = json.dumps(
+        list(map(lambda snapshot: dataclasses.asdict(snapshot), snapshot_list))
+    )
+    container.file_write("db.json", json_string)
+
+
+def snapshot_list():
+    snapshots = load_database()
+    return snapshots
+
+
+def snapshot_create(name):
+    snapshots = load_database()
+
+    existing_snapshots = list(filter(lambda s: s.name == name, snapshots))
+    if len(existing_snapshots) > 0:
+        raise Exception("A snapshot with that name already exists")
+
+    # Dummy file_count for the command to only run once
+    snapshot = Snapshot(name=name, file_count=123)
+
+    with container.freeze_target_container():
+        container.sync(settings.get("directory"), snapshot.path)
+
+    snapshot.size = container.directory_size(snapshot.path)
+    snapshot.file_count = container.directory_filecount(snapshot.path)
+
+    snapshots.append(snapshot)
+    save_database(snapshots)
+    return snapshot
+
+
+def snapshot_delete(name):
+    snapshots_before = load_database()
+
+    existing_snapshots = list(filter(lambda s: s.name == name, snapshots_before))
+    if len(existing_snapshots) > 1:
+        raise Exception("This shouldn't happen - 2 snapshots with the same name")
+
+    if len(existing_snapshots) < 1:
+        raise Exception("No snapshot found")
+
+    snapshot = existing_snapshots[0]
+
+    snapshots_after = list(filter(lambda s: s.name != name, snapshots_before))
+
+    container.directory_remove(snapshot.path)
+
+    save_database(snapshots_after)
+
+
+def snapshot_restore(name):
+    snapshots_before = load_database()
+
+    existing_snapshots = list(filter(lambda s: s.name == name, snapshots_before))
+    if len(existing_snapshots) > 1:
+        raise Exception("This shouldn't happen - 2 snapshots with the same name")
+
+    if len(existing_snapshots) < 1:
+        raise Exception("No snapshot found")
+
+    snapshot = existing_snapshots[0]
+
+    with container.freeze_target_container():
+        container.sync(snapshot.path, settings.get("directory"))
+
+
+def snapshot_present_stats():
+    path = settings.get("directory")
+    return Snapshot(
+        file_count=container.directory_filecount(path),
+        size=container.directory_size(path),
+    )
