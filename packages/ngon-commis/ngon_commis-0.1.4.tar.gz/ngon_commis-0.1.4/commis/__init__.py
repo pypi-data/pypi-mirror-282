8b8ef371fcfaae7aa0a6b0f7df0693c79a464cc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 04:03:07 +0000

    Add package ngon-commis/0.1.4/ngon_commis-0.1.4.tar.gz

---
+++ packages/ngon-commis/ngon_commis-0.1.4.tar.gz/ngon_commis-0.1.4/commis/__init__.py
@@ -0,0 +1,1 @@
+from .scripts.commis import BaseIngredient
---
+++ packages/ngon-commis/ngon_commis-0.1.4.tar.gz/ngon_commis-0.1.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ngon-commis',
+    package_name='commis',
+    version='0.1.4',
+    author='Silver',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+        "dataclasses",
+        "numpy",
+        "opencv-python",
+        "pytest",
+        "setuptools~=69.5.1",
+    ],
+)
