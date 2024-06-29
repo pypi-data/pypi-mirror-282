2c62a614bf76b3051963c0af262030880706dbad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 05:03:33 +0000

    Add package pjdev-sqlmodel/0.0.1a0/pjdev_sqlmodel-0.0.1a0.tar.gz

---
+++ packages/pjdev-sqlmodel/pjdev_sqlmodel-0.0.1a0.tar.gz/pjdev_sqlmodel-0.0.1a0/src/pjdev_sqlmodel/pjdev_db_settings.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from typing import (
+    Optional,
+)
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class SqlModelSettings(BaseSettings):
+    data_path: Path
+
+    model_config = SettingsConfigDict(
+        case_sensitive=False, extra="ignore", env_prefix="PJDEV_"
+    )
+
+
+class Context:
+    settings: Optional[SqlModelSettings] = None
+
+
+__ctx = Context()
+
+
+def init_settings(root: Path, data_directory_name: str = ".data") -> None:
+    __ctx.settings = SqlModelSettings(
+        _env_file=root / ".env", data_path=root / data_directory_name
+    )
+
+
+def get_settings() -> SqlModelSettings:
+    if __ctx.settings is None:
+        msg = "Settings are not initialized -- call init_settings()"
+        raise Exception(msg)
+    return __ctx.settings
