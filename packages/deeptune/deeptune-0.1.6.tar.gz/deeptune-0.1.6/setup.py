71b9567ffb1fafa9a4dc7436959ea697163f12cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 04:05:08 +0000

    Add package deeptune/0.1.6/deeptune-0.1.6.tar.gz

---
+++ packages/deeptune/deeptune-0.1.6.tar.gz/deeptune-0.1.6/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="deeptune",  # This should be the name of your package
+    version="0.1.6",
+    description="Python SDK for Deeptune Text-to-Speech API",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Deeptunee",
+    author_email="blair@deeptune.com",
+    url="https://github.com/yourusername/blair-wdq",  # Update this URL to your repository
+    packages=find_packages(
+        include=["deeptune", "deeptune.*"]
+    ),  # Adjust this if your package structure is different
+    install_requires=[
+        "requests",  # Add other dependencies if necessary
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires=">=3.7",
+)
