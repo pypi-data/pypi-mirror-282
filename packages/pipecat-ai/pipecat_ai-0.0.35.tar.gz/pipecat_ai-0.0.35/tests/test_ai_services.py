54696d658f9bcd3bdd9bc85ac4ee904f84950db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 04:04:22 +0000

    Add package pipecat-ai/0.0.35/pipecat_ai-0.0.35.tar.gz

---
+++ packages/pipecat-ai/pipecat_ai-0.0.35.tar.gz/pipecat_ai-0.0.35/tests/test_ai_services.py
@@ -0,0 +1,32 @@
+import unittest
+
+from typing import AsyncGenerator
+
+from pipecat.services.ai_services import AIService
+from pipecat.pipeline.frames import EndFrame, Frame, TextFrame
+
+
+class SimpleAIService(AIService):
+    async def process_frame(self, frame: Frame) -> AsyncGenerator[Frame, None]:
+        yield frame
+
+
+class TestBaseAIService(unittest.IsolatedAsyncioTestCase):
+    async def test_simple_processing(self):
+        service = SimpleAIService()
+
+        input_frames = [
+            TextFrame("hello"),
+            EndFrame()
+        ]
+
+        output_frames = []
+        for input_frame in input_frames:
+            async for output_frame in service.process_frame(input_frame):
+                output_frames.append(output_frame)
+
+        self.assertEqual(input_frames, output_frames)
+
+
+if __name__ == "__main__":
+    unittest.main()
