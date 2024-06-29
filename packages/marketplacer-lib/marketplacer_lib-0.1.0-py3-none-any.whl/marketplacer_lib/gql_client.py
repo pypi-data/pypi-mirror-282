7b87b49f2bd37c1ff04c3c684c904c8571287d13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 03:04:47 +0000

    Add package marketplacer-lib/0.1.0/marketplacer_lib-0.1.0-py3-none-any.whl

---
+++ packages/marketplacer-lib/marketplacer_lib-0.1.0-py3-none-any.whl/marketplacer_lib/gql_client.py
@@ -0,0 +1,85 @@
+import requests
+
+
+class MarketplacerGQLClient:
+
+    def __init__(self, gql_url: str, api_key: str):
+        self.gql_url = gql_url
+        self.api_key = api_key
+        self.headers = {
+            "MARKETPLACER-API-KEY": api_key,
+            "Content-Type": "application/json",
+        }
+
+    def update_advert_price(self, advert_id: int, new_price: float):
+        pass
+
+    def update_variant_stock(self, variant_id: int, new_stock: int):
+        pass
+
+    def get_variant_id_by_sku(self, sku: str) -> int:
+        pass
+
+    def get_advert_id_by_variant_id(self, variant_id: int) -> int:
+        pass
+
+    def get_mutations(self):
+
+        introspection_query = """
+            {
+                __schema {
+                    mutationType {
+                        fields {
+                            name
+                        }
+                    }
+                }
+            }
+        """
+
+        response = requests.post(
+            url=self.gql_url, json={"query": introspection_query}, headers=self.headers
+        )
+
+        if response.status_code == 200:
+            result = response.json()
+            mutations = result["data"]["__schema"]["mutationType"]["fields"]
+            return [mutation["name"] for mutation in mutations]
+        else:
+            response.raise_for_status()
+
+    def upsert_advert(self, input: dict) -> dict:
+
+        mutation = """
+            mutation($input: AdvertUpsertMutationInput!) {
+                advertUpsert(input: $input) {
+                    advert {
+                        id
+                        editablePrice
+                    }
+                    errors {
+                        field
+                        messages
+                    }
+                }
+            }
+        """
+
+        variables = {
+            "input": {
+                "clientMutationId": "python-script",
+                "advertId": str(input["id"]),
+                "attributes": {"price": str(input["price"])},
+            }
+        }
+
+        gql_request = {"query": mutation, "variables": variables}
+
+        response = requests.post(
+            url=self.gql_url, json=gql_request, headers=self.headers
+        )
+
+        if response.ok:
+            return response.json()
+        else:
+            raise response.raise_for_status()
