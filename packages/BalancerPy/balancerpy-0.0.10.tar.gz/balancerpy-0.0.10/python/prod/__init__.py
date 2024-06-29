72fe9199f0df1789894922e9818759d9cf667c36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 05:04:06 +0000

    Add package BalancerPy/0.0.10/balancerpy-0.0.10.tar.gz

---
+++ packages/BalancerPy/balancerpy-0.0.10.tar.gz/balancerpy-0.0.10/python/prod/__init__.py
@@ -0,0 +1,6 @@
+from balancerpy.erc import *
+from balancerpy.vault import *
+from balancerpy.cwpt.factory import *
+from balancerpy.cwpt.exchg import *
+from balancerpy.utils.interfaces import *
+from balancerpy.utils.data import *
---
+++ packages/BalancerPy/balancerpy-0.0.10.tar.gz/balancerpy-0.0.10/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(name='BalancerPy',
+      version='0.0.10',
+      description='Balancer Analytics with Python',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/defipy-devs/balancerpy',
+      author = "icmoore",
+      author_email = "defipy.devs@gmail.com",
+      license='MIT',
+      package_dir = {"balancerpy": "python/prod"},
+      packages=[
+          'balancerpy',
+          'balancerpy.cwpt.exchg',
+          'balancerpy.cwpt.exchg.result',
+          'balancerpy.cwpt.factory',
+          'balancerpy.erc',
+          'balancerpy.vault',
+          'balancerpy.quote',
+          'balancerpy.constants',
+          'balancerpy.utils.interfaces',
+          'balancerpy.utils.data'     
+      ],
+      zip_safe=False)
