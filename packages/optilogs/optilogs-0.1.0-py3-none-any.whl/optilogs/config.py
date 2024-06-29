9aab0fc9c0d0a3bf316f924a92222d6032517f88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 05:03:50 +0000

    Add package optilogs/0.1.0/optilogs-0.1.0-py3-none-any.whl

---
+++ packages/optilogs/optilogs-0.1.0-py3-none-any.whl/optilogs/config.py
@@ -0,0 +1,7 @@
+class OptilogConfig:
+    def __init__(self, server_ip, server_port):
+        self.server_ip = server_ip
+        self.server_port = server_port
+
+    def get_server_url(self):
+        return f"http://{self.server_ip}:{self.server_port}/log"
