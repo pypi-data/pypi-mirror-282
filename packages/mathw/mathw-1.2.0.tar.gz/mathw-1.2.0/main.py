bf4b815945a38b9d7dac0ba9bfa85221d752f593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 15:03:54 +0000

    Add package mathw/1.2.0/mathw-1.2.0.tar.gz

---
+++ packages/mathw/mathw-1.2.0.tar.gz/mathw-1.2.0/main.py
@@ -0,0 +1,10 @@
+from mathw import MathWork
+from mathw.series.utils import gen_ops
+
+if __name__ == '__main__':
+    MathWork(
+        series='Mult',  # 类别
+        pageNum=20,  # 页数
+        level=24,  # 等级
+    ).go()
+
