2cf5ed40ac772f391443386eb418117fc255c2d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 03:05:10 +0000

    Add package lbx-toolkit/0.5/lbx_toolkit-0.5.tar.gz

---
+++ packages/lbx-toolkit/lbx_toolkit-0.5.tar.gz/lbx_toolkit-0.5/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import locale
+
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_desc= fh.read()
+
+setup(
+    name='lbx_toolkit',
+    version='0.5',
+    packages=find_packages(),
+    install_requires=[
+        'psycopg2',
+        'pandas',
+        'numpy',
+        'msal',
+        'requests',
+        'selenium',
+        'webdriver-manager'
+    ],
+    license='MIT License',
+    description='Biblioteca de ferramentas LBX S/A',
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    author='Cristiano P. Ferrari',
+    author_email='boxferrari@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
