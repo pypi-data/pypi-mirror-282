f88bba53e41d3ae778e3f6db21606f05e4cffbbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 03:02:49 +0000

    Add package cxyxtools/0.0.0.4/cxyxtools-0.0.0.4.tar.gz

---
+++ packages/cxyxtools/cxyxtools-0.0.0.4.tar.gz/cxyxtools-0.0.0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cxyxtools',
+    version='0.0.0.4',
+    description='常用工具类',
+    long_description='''A longer description of your package that can span multiple lines''',
+    author='cxyx',
+    author_email='18883325829@163.com',
+    url='http://cxyx.cn',
+    packages=find_packages(),
+    install_requires=[
+        # list of your package dependencies
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
