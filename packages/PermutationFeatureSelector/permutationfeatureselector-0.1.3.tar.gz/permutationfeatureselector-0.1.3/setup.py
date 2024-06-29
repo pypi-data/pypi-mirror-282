88d486d59df3b18f21657f49a1ac2eee394c35dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 15:03:06 +0000

    Add package PermutationFeatureSelector/0.1.3/permutationfeatureselector-0.1.3.tar.gz

---
+++ packages/PermutationFeatureSelector/permutationfeatureselector-0.1.3.tar.gz/permutationfeatureselector-0.1.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='PermutationFeatureSelector',
+    version='0.1.3',
+    description='A package for calculating permutation importance and selecting features.',
+    author='Itsuki Ito',
+    author_email='itoitsuki.28@gmail.com',
+    url='https://github.com/Itsuki-2822/PermutationFeatureSelector', 
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scikit-learn',
+        'matplotlib',
+        'lightgbm'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
