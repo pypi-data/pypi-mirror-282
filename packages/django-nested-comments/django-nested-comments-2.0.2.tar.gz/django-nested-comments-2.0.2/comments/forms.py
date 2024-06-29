2c13dde1e57967b80faa41b0d20361bf62eea573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 04:04:11 +0000

    Add package django-nested-comments/2.0.2/django-nested-comments-2.0.2.tar.gz

---
+++ packages/django-nested-comments/django-nested-comments-2.0.2.tar.gz/django-nested-comments-2.0.2/comments/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.conf import settings
+
+from .models import CommentVersion
+
+import nh3
+
+class CommentVersionForm(forms.ModelForm):
+    class Meta:
+        model = CommentVersion
+        fields = ['message']
+
+    def __init__(self, *args, **kwargs):
+        self.parent_object = kwargs.pop("parent_object", None)
+        self.user = kwargs.pop("user", None)
+        super().__init__(*args, **kwargs)
+
+    def clean_message(self):
+        message = self.cleaned_data['message']
+        allowed_tags = settings.ALLOWED_TAGS if hasattr(settings, 'ALLOWED_TAGS') else nh3.ALLOWED_TAGS
+        allowed_attributes = settings.ALLOWED_ATTRIBUTES if hasattr(settings, 'ALLOWED_ATTRIBUTES') else nh3.ALLOWED_ATTRIBUTES
+        return nh3.clean(message, tags=allowed_tags, attributes=allowed_attributes, strip_comments=True, link_rel=None)
