21e26d5aa3f5edf3015d9a8c8f13dd9df601e418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 05:04:14 +0000

    Add package zeroapi/0.7.0/zeroapi-0.7.0.tar.gz

---
+++ packages/zeroapi/zeroapi-0.7.0.tar.gz/zeroapi-0.7.0/zero/utils/type_util.py
@@ -0,0 +1,172 @@
+import dataclasses
+import datetime
+import decimal
+import enum
+import typing
+import uuid
+from typing import Callable, Optional, get_origin, get_type_hints
+
+import msgspec
+
+builtin_types: typing.List = [
+    None,
+    bool,
+    int,
+    float,
+    str,
+    bytes,
+    bytearray,
+    tuple,
+    typing.Tuple,
+    list,
+    typing.List,
+    dict,
+    typing.Dict,
+    set,
+    typing.Set,
+    frozenset,
+    typing.FrozenSet,
+]
+
+std_lib_types: typing.List = [
+    datetime.datetime,
+    datetime.date,
+    datetime.time,
+    uuid.UUID,
+    decimal.Decimal,
+    enum.Enum,
+    enum.IntEnum,
+    dataclasses.dataclass,
+]
+
+typing_types: typing.List = [
+    typing.Any,
+    typing.Union,
+    typing.Optional,
+]
+
+msgspec_types: typing.List = [
+    msgspec.Struct,
+    msgspec.Raw,
+]
+
+
+allowed_types = builtin_types + std_lib_types + typing_types
+
+
+def verify_function_args(func: Callable) -> None:
+    arg_count = func.__code__.co_argcount
+    if arg_count < 1:
+        return
+    if arg_count > 1:
+        raise ValueError(
+            f"`{func.__name__}` has more than 1 args; "
+            "RPC functions can have only one arg - msg, or no arg"
+        )
+
+    arg_name = func.__code__.co_varnames[0]
+    func_arg_type = get_type_hints(func)
+    if arg_name not in func_arg_type:
+        raise TypeError(
+            f"`{func.__name__}` has no type hinting; RPC functions must have type hints"
+        )
+
+
+def verify_function_return(func: Callable) -> None:
+    return_count = func.__code__.co_argcount
+    if return_count > 1:
+        raise ValueError(
+            f"`{func.__name__}` has more than 1 return values; "
+            "RPC functions can have only one return value"
+        )
+
+    types = get_type_hints(func)
+    if not types.get("return"):
+        raise TypeError(
+            f"`{func.__name__}` has no return type hinting; RPC functions must have type hints"
+        )
+
+
+def get_function_input_class(func: Callable) -> Optional[type]:
+    arg_count = func.__code__.co_argcount
+    if arg_count == 0:
+        return None
+    if arg_count == 1:
+        arg_name = func.__code__.co_varnames[0]
+        func_arg_type = get_type_hints(func)
+        return func_arg_type[arg_name]
+
+    return None
+
+
+def get_function_return_class(func: Callable):
+    types = get_type_hints(func)
+    return types.get("return")
+
+
+def verify_function_input_type(func: Callable):
+    input_type = get_function_input_class(func)
+    if input_type in allowed_types:
+        return
+
+    origin_type = get_origin(input_type)
+    if origin_type is not None and origin_type in allowed_types:
+        return
+
+    for mtype in msgspec_types:
+        if input_type is not None and issubclass(input_type, mtype):
+            return
+
+    raise TypeError(
+        f"{func.__name__} has type {input_type} which is not allowed; "
+        "allowed types are: \n" + "\n".join([str(t) for t in allowed_types])
+    )
+
+
+def verify_function_return_type(func: Callable):
+    return_type = get_function_return_class(func)
+    if return_type in allowed_types:
+        return
+
+    origin_type = get_origin(return_type)
+    if origin_type is not None and origin_type in allowed_types:
+        return
+
+    for typ in msgspec_types:
+        if issubclass(return_type, typ):
+            return
+
+    raise TypeError(
+        f"{func.__name__} has return type {return_type} which is not allowed; "
+        "allowed types are: \n" + "\n".join([str(t) for t in allowed_types])
+    )
+
+
+def verify_allowed_type(msg, rpc_method: Optional[str] = None):
+    if not isinstance(msg, tuple(allowed_types)):
+        method_name = f"for method `{rpc_method}`" if rpc_method else ""
+        raise TypeError(
+            f"{msg} is not allowed {method_name}; allowed types are: \n"
+            + "\n".join([str(t) for t in allowed_types])
+        )
+
+
+def verify_incoming_rpc_call_input_type(
+    msg, rpc_method: str, rpc_input_type_map: dict
+):  # pragma: no cover
+    input_type = rpc_input_type_map[rpc_method]
+    if input_type is None:
+        return
+
+    if input_type in builtin_types:
+        if input_type != type(msg):
+            raise TypeError(
+                f"{msg} is not allowed for method `{rpc_method}`; allowed type: {input_type}"
+            )
+
+    origin_type = get_origin(input_type)
+    if origin_type in builtin_types:
+        if origin_type != type(msg):
+            raise TypeError(
+                f"{msg} is not allowed for method `{rpc_method}`; allowed type: {input_type}"
+            )
---
+++ packages/zeroapi/zeroapi-0.7.0.tar.gz/zeroapi-0.7.0/zero/zeromq_patterns/queue_device/client.py
@@ -0,0 +1,201 @@
+import asyncio
+import logging
+import sys
+from asyncio import Event
+from typing import Dict, Optional
+
+import zmq
+import zmq.asyncio as zmqasync
+import zmq.error as zmqerr
+
+from zero.error import ConnectionException, TimeoutException
+from zero.utils import util
+
+
+class ZeroMQClient:
+    def __init__(self, default_timeout):
+        self._address = None
+        self._default_timeout = default_timeout
+        self._context = zmq.Context.instance()
+
+        self.socket = self._context.socket(zmq.DEALER)
+        self.socket.setsockopt(zmq.LINGER, 0)  # dont buffer messages
+        self.socket.setsockopt(zmq.RCVTIMEO, default_timeout)
+        self.socket.setsockopt(zmq.SNDTIMEO, default_timeout)
+
+        self.poller = zmq.Poller()
+        self.poller.register(self.socket, zmq.POLLIN)
+
+    def connect(self, address: str) -> None:
+        self._address = address
+        self.socket.connect(address)
+        self._send(util.unique_id().encode() + b"connect" + b"")
+        self._recv()
+        logging.info("Connected to server at %s", self._address)
+
+    def request(self, message: bytes, timeout: Optional[int] = None) -> bytes:
+        _timeout = self._default_timeout if timeout is None else timeout
+
+        def _poll_data():
+            # poll is slow, need to find a better way
+            if not self._poll(_timeout):
+                raise TimeoutException(
+                    f"Timeout while sending message at {self._address}"
+                )
+
+            rcv_data = self._recv()
+
+            # first 32 bytes as response id
+            resp_id = rcv_data[:32].decode()
+
+            # the rest is response data
+            resp_data = rcv_data[32:]
+
+            return resp_id, resp_data
+
+        req_id = util.unique_id()
+        self._send(req_id.encode() + message)
+
+        resp_id, resp_data = None, None
+        # as the client is synchronous, we know that the response will be available any next poll
+        # we try to get the response until timeout because a previous call might be timed out
+        # and the response is still in the socket,
+        # so we poll until we get the response for this call
+        while resp_id != req_id:
+            resp_id, resp_data = _poll_data()
+
+        return resp_data  # type: ignore
+
+    def close(self) -> None:
+        self.socket.close()
+
+    def _send(self, message: bytes) -> None:
+        try:
+            self.socket.send(message, zmq.NOBLOCK)
+        except zmqerr.Again as exc:
+            raise ConnectionException(
+                f"Connection error for send at {self._address}"
+            ) from exc
+
+    def _poll(self, timeout: int) -> bool:
+        socks = dict(self.poller.poll(timeout))
+        return self.socket in socks
+
+    def _recv(self) -> bytes:
+        try:
+            return self.socket.recv()
+        except zmqerr.Again as exc:
+            raise ConnectionException(
+                f"Connection error for recv at {self._address}"
+            ) from exc
+
+
+class AsyncZeroMQClient:
+    def __init__(self, default_timeout: int = 2000):
+        if sys.platform == "win32":
+            # windows need special event loop policy to work with zmq
+            asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+        self._address: str = None  # type: ignore
+        self._default_timeout = default_timeout
+        self._context = zmqasync.Context.instance()
+
+        self.socket: zmqasync.Socket = self._context.socket(zmq.DEALER)
+        self.socket.setsockopt(zmq.LINGER, 0)  # dont buffer messages
+        self.socket.setsockopt(zmq.RCVTIMEO, default_timeout)
+        self.socket.setsockopt(zmq.SNDTIMEO, default_timeout)
+
+        self.poller = zmqasync.Poller()
+        self.poller.register(self.socket, zmq.POLLIN)
+
+        self._resp_map: Dict[str, bytes] = {}
+
+        # self.peer1, self.peer2 = zpipe_async(self._context)
+
+    async def connect(self, address: str) -> None:
+        self._address = address
+        self.socket.connect(address)
+        await self._send(util.unique_id().encode() + b"connect" + b"")
+        await self._recv()
+        logging.info("Connected to server at %s", self._address)
+
+    async def request(self, message: bytes, timeout: Optional[int] = None) -> bytes:
+        _timeout = self._default_timeout if timeout is None else timeout
+        expire_at = util.current_time_us() + (_timeout * 1000)
+
+        is_data = Event()
+
+        async def _poll_data():
+            # async has issue with poller, after 3-4 calls, it returns empty
+            # if not await self._poll(_timeout):
+            #     raise TimeoutException(f"Timeout while sending message at {self._address}")
+
+            resp = await self._recv()
+
+            # first 32 bytes as response id
+            resp_id = resp[:32].decode()
+
+            # the rest is response data
+            resp_data = resp[32:]
+            self._resp_map[resp_id] = resp_data
+
+            # pipe is a good way to notify the main event loop that there is a response
+            # but pipe is actually slower than sleep, because it is a zmq socket
+            # yes it uses inproc, but still slower than asyncio.sleep
+            # try:
+            #     await self.peer1.send(b"")
+            # except zmqerr.Again:
+            #     # if the pipe is full, just pass
+            #     pass
+
+            is_data.set()
+
+        req_id = util.unique_id()
+        await self._send(req_id.encode() + message)
+
+        # poll can get response of a different call
+        # so we poll until we get the response of this call or timeout
+        await _poll_data()
+
+        while req_id not in self._resp_map:
+            if util.current_time_us() > expire_at:
+                raise TimeoutException(
+                    f"Timeout while waiting for response at {self._address}"
+                )
+
+            # await asyncio.sleep(1e-6)
+            await asyncio.wait_for(is_data.wait(), timeout=_timeout)
+
+            # try:
+            #     await self.peer2.recv()
+            # except zmqerr.Again:
+            #     # if the pipe is empty, just pass
+            #     pass
+
+        resp_data = self._resp_map.pop(req_id)
+
+        return resp_data
+
+    def close(self) -> None:
+        self.socket.close()
+        self._resp_map.clear()
+
+    async def _send(self, message: bytes) -> None:
+        try:
+            await self.socket.send(message, zmq.NOBLOCK)
+        except zmqerr.Again as exc:
+            raise ConnectionException(
+                f"Connection error for send at {self._address}"
+            ) from exc
+
+    async def _poll(self, timeout: int) -> bool:
+        socks = dict(await self.poller.poll(timeout))
+        return self.socket in socks
+
+    async def _recv(self) -> bytes:
+        try:
+            return await self.socket.recv()
+        except zmqerr.Again as exc:
+            raise ConnectionException(
+                f"Connection error for recv at {self._address}"
+            ) from exc
