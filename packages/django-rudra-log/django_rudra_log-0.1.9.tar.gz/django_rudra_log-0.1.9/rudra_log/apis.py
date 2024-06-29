8d58f5ead83378766cc03474b333d949eed5dbbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 15:02:56 +0000

    Add package django-rudra-log/0.1.9/django_rudra_log-0.1.9.tar.gz

---
+++ packages/django-rudra-log/django_rudra_log-0.1.9.tar.gz/django_rudra_log-0.1.9/rudra_log/apis.py
@@ -0,0 +1,45 @@
+import requests
+from django.conf import settings
+
+from .helpers import LogSettings
+
+settings: LogSettings = getattr(settings, "LOG_SETTINGS", None)
+
+
+def post_api_log(log_data: dict, priority: bool = False):
+    url = (
+        settings.url + "/api/api-log/"
+        if not settings.url.endswith("/")
+        else settings.url + "api/api-log/"
+    )
+    headers = {
+        "content-type": "application/json",
+        "x-env-id": settings.env_key,
+    }
+    query = {"p": "1"} if priority else None
+
+    requests.post(
+        url,
+        headers=headers,
+        params=query,
+        json=log_data,
+    )
+
+
+def post_or_put_celery_log(log_data: dict, method: str):
+    url = (
+        settings.url + "/api/celery-log/"
+        if not settings.url.endswith("/")
+        else settings.url + "api/celery-log/"
+    )
+    headers = {
+        "content-type": "application/json",
+        "x-env-id": settings.env_key,
+    }
+
+    requests.request(
+        method,
+        url,
+        headers=headers,
+        json=log_data,
+    )
