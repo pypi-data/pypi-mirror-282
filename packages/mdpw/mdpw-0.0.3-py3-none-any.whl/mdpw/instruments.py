203c05e0f71feedd32078eb9b30178a398078d31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 16:03:53 +0000

    Add package mdpw/0.0.3/mdpw-0.0.3-py3-none-any.whl

---
+++ packages/mdpw/mdpw-0.0.3-py3-none-any.whl/mdpw/instruments.py
@@ -0,0 +1,162 @@
+INSTRUMENTS = {}
+#Piano:
+INSTRUMENTS['AcousticGrandPiano'] = 1 
+INSTRUMENTS['BrightAcousticPiano'] = 2 
+INSTRUMENTS['ElectricGrandPiano'] = 3 
+INSTRUMENTS['Honky-tonkPiano'] = 4 
+INSTRUMENTS['ElectricPiano1'] = 5 
+INSTRUMENTS['ElectricPiano2'] = 6 
+INSTRUMENTS['Harpsichord'] = 7 
+INSTRUMENTS['Clavinet'] = 8 
+
+#Chromatic Percussion:
+INSTRUMENTS['Celesta'] = 9 
+INSTRUMENTS['Glockenspiel'] = 10 
+INSTRUMENTS['MusicBox'] = 11 
+INSTRUMENTS['Vibraphone'] = 12 
+INSTRUMENTS['Marimba'] = 13 
+INSTRUMENTS['Xylophone'] = 14 
+INSTRUMENTS['TubularBells'] = 15 
+INSTRUMENTS['Dulcimer'] = 16 
+
+#Organ:
+INSTRUMENTS['DrawbarOrgan'] = 17 
+INSTRUMENTS['PercussiveOrgan'] = 18 
+INSTRUMENTS['RockOrgan'] = 19 
+INSTRUMENTS['ChurchOrgan'] = 20 
+INSTRUMENTS['Reed Organ'] = 21 
+INSTRUMENTS['Accordion'] = 22 
+INSTRUMENTS['Harmonica'] = 23 
+INSTRUMENTS['TangoAccordion'] = 24 
+
+#Guitar:
+INSTRUMENTS['AcousticGuitarNylon'] = 25 
+INSTRUMENTS['AcousticGuitarSteel'] = 26 
+INSTRUMENTS['ElectricGuitarJazz'] = 27 
+INSTRUMENTS['ElectricGuitarClean'] = 28 
+INSTRUMENTS['ElectricGuitarMuted'] = 29 
+INSTRUMENTS['OverdrivenGuitar'] = 30 
+INSTRUMENTS['DistortionGuitar'] = 31 
+INSTRUMENTS['GuitarHarmonics'] = 32 
+
+#Bass:
+INSTRUMENTS['AcousticBass'] = 33 
+INSTRUMENTS['ElectricBassFinger'] = 34 
+INSTRUMENTS['ElectricBassPick'] = 35 
+INSTRUMENTS['FretlessBass'] = 36 
+INSTRUMENTS['SlapBass1'] = 37 
+INSTRUMENTS['SlapBass2'] = 38 
+INSTRUMENTS['SynthBass1'] = 39 
+INSTRUMENTS['SynthBass2'] = 40 
+
+#Strings:
+INSTRUMENTS['Violin'] = 41 
+INSTRUMENTS['Viola'] = 42 
+INSTRUMENTS['Cello'] = 43 
+INSTRUMENTS['Contrabass'] = 44 
+INSTRUMENTS['TremoloStrings'] = 45 
+INSTRUMENTS['PizzicatoStrings'] = 46 
+INSTRUMENTS['OrchestralHarp'] = 47 
+INSTRUMENTS['Timpani'] = 48 
+
+#Strings (continued):
+INSTRUMENTS['StringEnsemble1'] = 49 
+INSTRUMENTS['StringEnsemble2'] = 50 
+INSTRUMENTS['SynthStrings1'] = 51 
+INSTRUMENTS['SynthStrings2'] = 52 
+INSTRUMENTS['ChoirAahs'] = 53 
+INSTRUMENTS['VoiceOohs'] = 54 
+INSTRUMENTS['SynthVoice'] = 55 
+INSTRUMENTS['OrchestraHit'] = 56 
+
+#Brass:
+INSTRUMENTS['Trumpet'] = 57 
+INSTRUMENTS['Trombone'] = 58 
+INSTRUMENTS['Tuba'] = 59 
+INSTRUMENTS['MutedTrumpet'] = 60 
+INSTRUMENTS['FrenchHorn'] = 61 
+INSTRUMENTS['BrassSection'] = 62 
+INSTRUMENTS['SynthBrass1'] = 63 
+INSTRUMENTS['SynthBrass2'] = 64 
+
+#Reed:
+INSTRUMENTS['SopranoSax'] = 65 
+INSTRUMENTS['AltoSax'] = 66 
+INSTRUMENTS['TenorSax'] = 67 
+INSTRUMENTS['BaritoneSax'] = 68 
+INSTRUMENTS['Oboe'] = 69 
+INSTRUMENTS['EnglishHorn'] = 70 
+INSTRUMENTS['Bassoon'] = 71 
+INSTRUMENTS['Clarinet'] = 72 
+
+#Pipe:
+INSTRUMENTS['Piccolo'] = 73 
+INSTRUMENTS['Flute'] = 74 
+INSTRUMENTS['Recorder'] = 75 
+INSTRUMENTS['PanFlute'] = 76 
+INSTRUMENTS['BlownBottle'] = 77 
+INSTRUMENTS['Shakuhachi'] = 78 
+INSTRUMENTS['Whistle'] = 79 
+INSTRUMENTS['Ocarina'] = 80 
+
+#Synth Lead:
+INSTRUMENTS['Lead1'] = 81 #Lead 1 (square)
+INSTRUMENTS['Lead2'] = 82 #Lead 2 (sawtooth)
+INSTRUMENTS['Lead3'] = 83 #Lead 3 (calliope)
+INSTRUMENTS['Lead4'] = 84 #Lead 4 (chiff)
+INSTRUMENTS['Lead5'] = 85 #Lead 5 (charang)
+INSTRUMENTS['Lead6'] = 86 #Lead 6 (voice)
+INSTRUMENTS['Lead7'] = 87 #Lead 7 (fifths)
+INSTRUMENTS['Lead8'] = 88 #Lead 8 (bass + lead)
+
+#Synth Pad:
+INSTRUMENTS['Pad1'] = 89 # (new age)
+INSTRUMENTS['Pad2'] = 90 # (warm)
+INSTRUMENTS['Pad3'] = 91 # (polysynth)
+INSTRUMENTS['Pad4'] = 92 # (choir)
+INSTRUMENTS['Pad5'] = 93 # (bowed)
+INSTRUMENTS['Pad6'] = 94 # (metallic)
+INSTRUMENTS['Pad7'] = 95 # (halo)
+INSTRUMENTS['Pad8'] = 96 # (sweep)
+
+#Synth Effects:
+INSTRUMENTS['FX1'] = 97 # (rain)
+INSTRUMENTS['FX2'] = 98 # (soundtrack)
+INSTRUMENTS['FX3'] = 99 # (crystal)
+INSTRUMENTS['FX4'] = 100 # (atmosphere)
+INSTRUMENTS['FX5'] = 101 # (brightness)
+INSTRUMENTS['FX6'] = 102 # (goblins)
+INSTRUMENTS['FX7'] = 103 # (echoes)
+INSTRUMENTS['FX8'] = 104  #(sci-fi)
+
+#Ethnic:
+INSTRUMENTS['Sitar'] = 105 
+INSTRUMENTS['Banjo'] = 106 
+INSTRUMENTS['Shamisen'] = 107 
+INSTRUMENTS['Koto'] = 108 
+INSTRUMENTS['Kalimba'] = 109 
+INSTRUMENTS['BagPipe'] = 110 
+INSTRUMENTS['Fiddle'] = 111 
+INSTRUMENTS['Shanai'] = 112 
+
+#Percussive:
+INSTRUMENTS['TinkleBell'] = 113 
+INSTRUMENTS['Agogo'] = 114 
+INSTRUMENTS['SteelDrums'] = 115 
+INSTRUMENTS['Woodblock'] = 116 
+INSTRUMENTS['TaikoDrum'] = 117 
+INSTRUMENTS['MelodicTom'] = 118 
+INSTRUMENTS['SynthDrum'] = 119 
+
+#Sound effects:
+INSTRUMENTS['ReverseCymbal'] = 120 
+INSTRUMENTS['GuitarFretNoise'] = 121 
+INSTRUMENTS['BreathNoise'] = 122 
+INSTRUMENTS['Seashore'] = 123 
+INSTRUMENTS['BirdTweet'] = 124 
+INSTRUMENTS['TelephoneRing'] = 125 
+INSTRUMENTS['Helicopter'] = 126 
+INSTRUMENTS['Applause'] = 127 
+INSTRUMENTS['Gunshot'] = 128 
+
+INSTRUMENTS['Drum'] = 129 
