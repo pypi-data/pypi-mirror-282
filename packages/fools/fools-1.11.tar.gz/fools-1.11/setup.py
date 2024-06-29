bc5ff1ce73429363616bc3b55fa5ba47be4d5c7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 05:03:48 +0000

    Add package fools/1.11/fools-1.11.tar.gz

---
+++ packages/fools/fools-1.11.tar.gz/fools-1.11/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+DESCRIPTION = 'An app with some funny scripts.'
+
+setup(
+    name='fools',
+    version='1.11',
+    author="walper",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click',
+        'opencv-python',
+        'pytube',
+        'moviepy',
+        'flask',
+        'pillow',
+        'moviepy'
+    ],
+    keywords=[
+        "toolsbox",
+        "tools",
+        "youtube",
+        "download",
+        "meme",
+        "walper"
+    ],
+    entry_points={
+        'console_scripts': [
+            'fools = fools.main:start',
+            'afools = fools.main:cli'
+        ],
+    },
+)
