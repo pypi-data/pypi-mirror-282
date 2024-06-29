1317f308d3651bf511431867af351b2cac04ba74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 14:03:26 +0000

    Add package python-apollo-proxy/0.1.3/python-apollo-proxy-0.1.3.tar.gz

---
+++ packages/python-apollo-proxy/python-apollo-proxy-0.1.3.tar.gz/python-apollo-proxy-0.1.3/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+# ---------------------------------------------------------------------------------------------------------
+# ProjectName:  python-apollo
+# FileName:     setup.py
+# Description:  TODO
+# Author:       GIGABYTE
+# CreateDate:   2024/04/19
+# Copyright ©2011-2024. Hunan xxxxxxx Company limited. All rights reserved.
+# ---------------------------------------------------------------------------------------------------------
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='python-apollo-proxy',
+    version='0.1.3',
+    description='This is my apollo proxy client package',
+    long_description='This is my apollo proxy client package',
+    author='ckf10000',
+    author_email='ckf10000@sina.com',
+    url='https://github.com/ckf10000/python-apollo',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
