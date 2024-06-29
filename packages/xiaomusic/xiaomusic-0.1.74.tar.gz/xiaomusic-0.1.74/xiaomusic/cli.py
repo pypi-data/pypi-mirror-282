e26d37ec8154dff98daf2b4875a521a78c675354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 03:04:09 +0000

    Add package xiaomusic/0.1.74/xiaomusic-0.1.74.tar.gz

---
+++ packages/xiaomusic/xiaomusic-0.1.74.tar.gz/xiaomusic-0.1.74/xiaomusic/cli.py
@@ -0,0 +1,57 @@
+import argparse
+import asyncio
+
+from xiaomusic.config import Config
+from xiaomusic.xiaomusic import XiaoMusic
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--hardware",
+        dest="hardware",
+        help="小爱 hardware",
+    )
+    parser.add_argument(
+        "--account",
+        dest="account",
+        help="xiaomi account",
+    )
+    parser.add_argument(
+        "--password",
+        dest="password",
+        help="xiaomi password",
+    )
+    parser.add_argument(
+        "--cookie",
+        dest="cookie",
+        help="xiaomi cookie",
+    )
+    parser.add_argument(
+        "--verbose",
+        dest="verbose",
+        action="store_true",
+        default=None,
+        help="show info",
+    )
+    parser.add_argument(
+        "--config",
+        dest="config",
+        help="config file path",
+    )
+    parser.add_argument(
+        "--ffmpeg_location",
+        dest="ffmpeg_location",
+        help="ffmpeg bin path",
+    )
+
+    options = parser.parse_args()
+    config = Config.from_options(options)
+
+    xiaomusic = XiaoMusic(config)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(xiaomusic.run_forever())
+
+
+if __name__ == "__main__":
+    main()
