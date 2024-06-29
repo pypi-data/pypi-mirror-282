10c7d492523b46463ed56fb8dd54c60510d1119a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 04:04:15 +0000

    Add package Pyrows/0.1/Pyrows-0.1.tar.gz

---
+++ packages/Pyrows/Pyrows-0.1.tar.gz/Pyrows-0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+    name= 'Pyrows',
+    version= '0.1',
+    author= 'youssef-khaled',
+    description= 'library storage and management in txt files by-rows',
+    packages = setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License"
+
+    ]
+
+)
