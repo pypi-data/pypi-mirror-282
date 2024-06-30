72a77a66a98cda113f57068b024c536afba5853b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 03:05:16 +0000

    Add package wheelio-assesement-core-lib/0.0.3/wheelio_assesement_core_lib-0.0.3.tar.gz

---
+++ packages/wheelio-assesement-core-lib/wheelio_assesement_core_lib-0.0.3.tar.gz/wheelio_assesement_core_lib-0.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wheelio-assesement-core-lib",
+    version="0.0.3",
+    packages=find_packages(),
+    install_requires=["pyjwt", "fastapi", "motor"],
+    author='Ashwin Singh',
+    author_email='ashwinsinghgr8@gmail.com',
+    description='A common package for shared utilities and models for wheel.io technical challenge',
+    long_description='Do not use this, this will go off in some time',
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/common_package',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.11',
+)
