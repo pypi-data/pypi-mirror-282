4ad7f8b2471fd280f47b5a997b54c987e5c33615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 03:05:17 +0000

    Add package googletranswheel2/4.0.0/googletranswheel2-4.0.0-py3-none-any.whl

---
+++ packages/googletranswheel2/googletranswheel2-4.0.0-py3-none-any.whl/googletranswheel2/__init__.py
@@ -0,0 +1,8 @@
+"""Free Google Translate API for Python. Translates totally free of charge."""
+
+__all__ = 'Translator',
+__version__ = '4.0.0'
+
+
+from googletranswheel2.client import Translator
+from googletranswheel2.constants import LANGCODES, LANGUAGES  # noqa
