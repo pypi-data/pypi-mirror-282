6fec110c78797c145b8a739616e5d9d626c58ab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 04:04:02 +0000

    Add package concoction/0.1.1/concoction-0.1.1-py3-none-any.whl

---
+++ packages/concoction/concoction-0.1.1-py3-none-any.whl/concoction/core.py
@@ -0,0 +1,49 @@
+import warnings
+from functools import wraps
+from typing import Any, Dict
+
+from benedict import benedict
+
+global_config: benedict = benedict()
+
+
+def set_global_config(config: Dict[str, Any]):
+    if not isinstance(config, Dict):
+        raise TypeError(
+            "Config must be of type Dict[str, Any], got {0}".format(
+                type(config)
+            )
+        )
+    for key in config.keys():
+        if not isinstance(key, str):
+            raise TypeError(
+                "All keys in config must be of type str, found {0} of type {1}".format(
+                    key, type(key)
+                )
+            )
+    global global_config
+    global_config = benedict(config)
+
+
+class Configuration:
+    def __init__(self, prefix: str):
+        self.prefix = prefix
+
+    def __call__(self, cls):
+        @wraps(cls)
+        def wrapper(*args, **kwargs):
+            if not args and not kwargs:
+                config_section = global_config.get(self.prefix, {})
+                if not config_section:
+                    warnings.warn(
+                        UserWarning(
+                            "Configuration section {0} for {1} not found".format(
+                                self.prefix, cls.__name__
+                            )
+                        ),
+                        stacklevel=2,
+                    )
+                kwargs.update(config_section)
+            return cls(*args, **kwargs)
+
+        return wrapper
