619fb2c7033d415ca80e9541c75d3084c8f0bf65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 04:03:59 +0000

    Add package par-segmentation/0.1.15/par_segmentation-0.1.15.tar.gz

---
+++ packages/par-segmentation/par_segmentation-0.1.15.tar.gz/par_segmentation-0.1.15/tests/test_de_correct.py
@@ -0,0 +1,46 @@
+import os
+
+import numpy as np
+import pytest
+
+from par_segmentation import load_image
+from par_segmentation.quantifier import ImageQuant
+
+
+class TestDeCorrectTests:
+    """
+    Making sure results from differential evolution optimiser are as expected
+    Note: if underlying algorithm is changed, or default parameters are changed, tests may fail
+
+    """
+
+    path = os.path.dirname(os.path.abspath(__file__)) + "/../scripts"
+    imgs = [
+        load_image(
+            os.path.dirname(os.path.abspath(__file__))
+            + "/../scripts/nwg338_af_corrected.tif"
+        ),
+    ]
+    rois = [
+        np.loadtxt(
+            os.path.dirname(os.path.abspath(__file__))
+            + "/../scripts/nwg338_ROI_manual.txt"
+        ),
+    ]
+
+    def test_1(self):
+        # Correct results when quantifying the image
+        np.random.seed(12345)  # <- as it uses a stochastic algorithm
+        iq = ImageQuant(img=self.imgs[0], roi=self.rois[0], method="DE", verbose=False)
+        iq.run()
+        res = iq.compile_res()
+
+        assert res.iloc[0]["Frame"] == 0
+        assert res.iloc[0]["Position"] == 0
+        assert res.iloc[0]["Membrane signal"] == pytest.approx(
+            7169.0573371446435, rel=1e-4
+        )
+        assert res.iloc[0]["Cytoplasmic signal"] == pytest.approx(
+            7201.659347334887, rel=1e-4
+        )
+        assert iq.roi[0][0, 0] == pytest.approx(181.70199811949246, rel=1e-4)
