0236feee3aa45b420224ca8e0b7d4e45578f5867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 04:04:05 +0000

    Add package tko/0.8.6/tko-0.8.6-py3-none-any.whl

---
+++ packages/tko/tko-0.8.6-py3-none-any.whl/tko/util/tfmt.py
@@ -0,0 +1,149 @@
+class Color:
+    enabled = False
+    terminal_styles = {
+        '.': '\033[0m', # Reset
+        '*': '\033[1m', # Bold
+        '/': '\033[3m', # Italic
+        '_': '\033[4m', # Underline
+        
+        'k': '\033[30m', # Black
+        'r': '\033[31m', # Red
+        'g': '\033[32m', # Green
+        'y': '\033[33m', # Yellow
+        'b': '\033[34m', # Blue
+        'm': '\033[35m', # Magenta
+        'c': '\033[36m', # Cyan
+        'w': '\033[37m', # White
+
+
+        'K': '\033[40m', # Background black
+        'W': '\033[47m', # Background white
+    }
+
+    @staticmethod
+    def ljust(text: str, width: int) -> str:
+        return text + " " * (width - Color.len(text))
+
+    @staticmethod
+    def center(text: str, width: int, filler: str) -> str:
+        before = filler * ((width - Color.len(text)) // 2)
+        after = filler * ((width - Color.len(text) + 1) // 2)
+        return before + text + after
+
+    @staticmethod
+    def remove_colors(text: str) -> str:
+        for color in Color.terminal_styles.values():
+            text = text.replace(color, "")
+        return text
+
+    @staticmethod
+    def len(text):
+        return len(Color.remove_colors(text))
+
+
+def colour(modifiers: str, text: str) -> str:
+    if not Color.enabled:
+        return text
+    
+    output = ''
+    for m in modifiers:
+        val = Color.terminal_styles.get(m, '')
+        if val != '':
+            output += val
+    output += text + Color.terminal_styles.get('.', "")
+    return output
+
+class __Symbols:
+    def __init__(self):
+        self.opening = ""
+        self.neutral = ""
+        self.success = ""
+        self.failure = ""
+        self.wrong = ""
+        self.compilation = ""
+        self.execution = ""
+        self.unequal = ""
+        self.equalbar = ""
+        self.hbar = ""
+        self.vbar = ""
+        self.whitespace = ""  # interpunct
+        self.newline = ""  # carriage return
+        self.cfill = ""
+        self.tab = ""
+        self.arrow_up = ""
+        self.check = ""  
+        self.uncheck = ""
+        self.opcheck = ""
+        self.opuncheck = ""
+
+        self.ascii = False
+        self.set_unicode()
+
+    def get_mode(self) -> str:
+        return "ASCII" if self.ascii else "UTF-8"
+
+    def set_ascii(self):
+        self.ascii = True
+
+        self.opening = "=> "
+        self.neutral = "."
+        self.success = "S"
+        self.failure = "X"
+        self.wrong = "W"
+        self.compilation = "C"
+        self.execution = "E"
+        self.unequal = "#"
+        self.equalbar = "|"
+        self.hbar = "─"
+        self.vbar = "│"
+        self.whitespace = "\u2E31"  # interpunct
+        self.newline = "\u21B5"  # carriage return
+        self.cfill = "_"
+        self.tab = "    "
+        self.arrow_up = "A"
+
+        self.check = "x"  
+        self.uncheck = "."
+        self.opcheck = "█"
+        self.opuncheck = "▒"
+
+    def set_unicode(self):
+        self.ascii = False
+
+        self.opening = "=> "
+        self.neutral = "»"
+        self.success = "✓"
+        self.failure = "✗"
+        self.wrong = "ω"
+        self.compilation = "ϲ"
+        self.execution = "ϵ"
+        self.unequal = "├"
+        self.equalbar = "│"
+        self.hbar = "─"
+        self.vbar = "│"
+        self.whitespace = "\u2E31"  # interpunct
+        self.newline = "\u21B5"  # carriage return
+        self.cfill = "_"
+        self.tab = "    "
+        self.arrow_up = "↑"
+
+        self.check = "✓"  
+        self.uncheck = "✗"
+        self.opcheck = "ⴲ"
+        self.opuncheck = "ⵔ"
+
+
+    def set_colors(self):
+        self.opening = colour("b", self.opening)
+        self.neutral = colour("b", self.neutral)
+        self.success = colour("g", self.success)
+        self.failure = colour("r", self.failure)
+        self.wrong = colour("r", self.wrong)
+        self.compilation = colour("y", self.compilation)
+        self.execution = colour("y", self.execution)
+        self.unequal = colour("r", self.unequal)
+        self.equalbar = colour("g", self.equalbar)
+
+
+symbols = __Symbols()
+
