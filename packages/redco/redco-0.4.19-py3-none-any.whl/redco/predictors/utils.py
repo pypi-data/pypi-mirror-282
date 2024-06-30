061403a04c81e75a1ebc33937b1bad10746a11e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 14:03:57 +0000

    Add package redco/0.4.19/redco-0.4.19-py3-none-any.whl

---
+++ packages/redco/redco-0.4.19-py3-none-any.whl/redco/predictors/utils.py
@@ -0,0 +1,63 @@
+#  Copyright 2021 Google LLC
+#  #
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  #
+#      https://www.apache.org/licenses/LICENSE-2.0
+#  #
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import numpy as np
+import jax
+from flax.jax_utils import unreplicate
+
+
+def add_idxes(examples):
+    assert all(['__idx__' not in example for example in examples])
+    return [{**example, '__idx__': idx} for idx, example in enumerate(examples)]
+
+
+def collate_fn_wrapper(examples, collate_fn):
+    idxes = [example.pop('__idx__') for example in examples]
+    return {**collate_fn(examples), '__idx__': np.array(idxes)}
+
+
+def pred_fn_wrapper(pred_rng, params, batch, pred_fn):
+    idxes = batch.pop('__idx__')
+    return {
+        'raw_preds': pred_fn(pred_rng=pred_rng, params=params, batch=batch),
+        '__idx__': idxes
+    }
+
+
+def pred_step(pred_rng, params, batch, pred_fn, mesh):
+    if mesh is None:
+        preds = pred_fn(pred_rng=pred_rng, params=params, batch=batch)
+        return jax.lax.all_gather(preds, axis_name='batch')
+    else:
+        return jax.vmap(lambda b: pred_fn(
+            pred_rng=pred_rng, params=params, batch=b))(batch)
+
+
+def default_output_fn(preds):
+    batch_size = jax.tree.leaves(preds)[0].shape[0]
+    assert jax.tree.all(jax.tree.map(lambda x: x.shape[0] == batch_size, preds))
+    return [jax.tree.map(lambda x: x[i], preds) for i in range(batch_size)]
+
+
+def process_batch_preds(batch_preds_with_idxes, mesh):
+    if mesh is None:
+        batch_preds_with_idxes = unreplicate(batch_preds_with_idxes)
+
+    jax.tree.map(np.asarray, batch_preds_with_idxes)
+    preds = jax.tree.map(
+        lambda t: t.reshape((-1,) + t.shape[2:]),
+        batch_preds_with_idxes['raw_preds'])
+    idxes_argsort = np.argsort(batch_preds_with_idxes['__idx__'].reshape(-1))
+
+    return jax.tree.map(lambda t: t[idxes_argsort], preds)
---
+++ packages/redco/redco-0.4.19-py3-none-any.whl/redco/trainers/utils.py
@@ -0,0 +1,80 @@
+#  Copyright 2021 Google LLC
+#  #
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  #
+#      https://www.apache.org/licenses/LICENSE-2.0
+#  #
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import jax
+import jax.numpy as jnp
+
+
+def global_norm(updates):
+    return jnp.sqrt(sum(jnp.sum(x * x) for x in jax.tree.leaves(updates)))
+
+
+def loss_and_grads(train_rng, state, batch, loss_fn):
+    def compute_loss(params):
+        return loss_fn(
+            train_rng=train_rng,
+            state=state,
+            params=params,
+            batch=batch,
+            is_training=True)
+
+    grad_fn = jax.value_and_grad(compute_loss)
+    return grad_fn(state.params)
+
+
+def train_step(train_rng, state, batch, loss_fn, lr_schedule_fn, mesh):
+    if mesh is None:
+        loss, grads = loss_and_grads(
+            train_rng=train_rng, state=state, batch=batch, loss_fn=loss_fn)
+        grads = jax.lax.pmean(grads, 'batch')
+    else:
+        loss, grads = jax.vmap(lambda b: loss_and_grads(
+            train_rng=train_rng, state=state, batch=b, loss_fn=loss_fn))(batch)
+        loss = jnp.mean(loss)
+        grads = jax.tree.map(lambda x: jnp.mean(x, axis=0), grads)
+
+    new_state = state.apply_gradients(grads=grads)
+
+    metrics = {
+        'loss': loss, 'step': state.step, 'grad_norm': global_norm(grads)
+    }
+    if lr_schedule_fn is not None:
+        metrics['lr'] = lr_schedule_fn(state.step)
+
+    if mesh is None:
+        metrics = jax.lax.pmean(metrics, axis_name='batch')
+
+    return new_state, metrics
+
+
+def eval_step(state, batch, loss_fn, mesh):
+    if mesh is None:
+        loss = loss_fn(
+            train_rng=jax.random.PRNGKey(0),
+            state=state,
+            params=state.params,
+            batch=batch,
+            is_training=False)
+        loss = jax.lax.pmean(loss, axis_name='batch')
+    else:
+        loss = jax.vmap(
+            lambda b: loss_fn(
+                train_rng=jax.random.PRNGKey(0),
+                state=state,
+                params=state.params,
+                batch=b,
+                is_training=False))(batch)
+        loss = jnp.mean(loss)
+
+    return {'loss': loss}
