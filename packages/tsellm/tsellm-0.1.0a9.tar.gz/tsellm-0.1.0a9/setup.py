d864be3d869bdadf8bc9fa6853a7db679a4a1902
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 04:03:28 +0000

    Add package tsellm/0.1.0a9/tsellm-0.1.0a9.tar.gz

---
+++ packages/tsellm/tsellm-0.1.0a9.tar.gz/tsellm-0.1.0a9/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+import os
+
+VERSION = "0.1.0a9"
+
+
+def get_long_description():
+    with open(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md"),
+        encoding="utf8",
+    ) as fp:
+        return fp.read()
+
+
+setup(
+    name="tsellm",
+    description="Interactive SQLite shell with LLM support",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    author="Florents Tselai",
+    url="https://github.com/Florents-Tselai/tsellm",
+    entry_points="""
+        [console_scripts]
+        tsellm=tsellm.cli:cli
+    """,
+    project_urls={
+        "Issues": "https://github.com/Florents-Tselai/tsellm/issues",
+        "CI": "https://github.com/Florents-Tselai/tsellm/actions",
+        "Changelog": "https://github.com/Florents-Tselai/tsellm/releases",
+    },
+    license="BSD License",
+    version=VERSION,
+    packages=["tsellm"],
+    install_requires=["llm", "setuptools", "pip"],
+    extras_require={
+        "test": [
+            "pytest",
+            "pytest-cov",
+            "black",
+            "ruff",
+            "sqlite_utils",
+            "llm-markov",
+            "llm-embed-hazo",
+        ]
+    },
+    python_requires=">=3.11",
+)
