9b5725afdcbc0ff95275de4001be2f1fffc15a7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 04:04:12 +0000

    Add package tim-odoo/0.1.0/tim_odoo-0.1.0.tar.gz

---
+++ packages/tim-odoo/tim_odoo-0.1.0.tar.gz/tim_odoo-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tim_odoo",
+    version="0.1.0",
+    description="",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Bùi Đức Tuấn",
+    author_email="buiductuan12081995@gmail.com",
+    url="https://github.com/OrcCyber/tim.git",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
