8aa36f0107aadb80ce5d52693fc91a6c1898267e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 15:03:48 +0000

    Add package medface3d/0.0.2/medface3d-0.0.2.tar.gz

---
+++ packages/medface3d/medface3d-0.0.2.tar.gz/medface3d-0.0.2/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import subprocess
+import os
+
+current_file_path = os.path.abspath(__file__)
+current_folder_path = os.path.dirname(current_file_path)
+
+with open(os.path.join(current_folder_path, 'README.md'), 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        install.run(self)
+        subprocess.check_call(['pip', 'install', '-r', os.path.join(current_folder_path, 'requirements.txt')])
+
+setup(
+    name='medface3d',
+    version='0.0.2',
+    include_package_data=True,
+    package_data={
+        'medface3D': ['data/demo/*.jpg', 'data/demo/*.png', 'data/demo/*.gif'],  # Include all jpg, png, and gif files,
+        'medface3D': ['data/sample/*.jpg', 'data/sample/*.png', 'data/sample/*.gif'],  # Include all jpg, png, and gif files
+    },
+    packages=find_packages(),
+    description= "3D face reconstruction using mediapipe 3D Face mesh",
+    long_description = long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        # List other pip dependencies here if any
+    ],
+    cmdclass={
+        'install': PostInstallCommand,
+    },
+    python_requires='>=3.7',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ]
+)
