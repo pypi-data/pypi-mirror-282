b3adcf2d5cfd619098c2ca0ac62e1350698397cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 04:04:06 +0000

    Add package spanish-dni/1.0.2/spanish_dni-1.0.2.tar.gz

---
+++ packages/spanish-dni/spanish_dni-1.0.2.tar.gz/spanish_dni-1.0.2/setup.py
@@ -0,0 +1,29 @@
+import pathlib
+
+from setuptools import setup
+
+README = (pathlib.Path(__file__).resolve().parent / "README.md").read_text()
+
+setup(
+    name="spanish-dni",
+    version="1.0.2",
+    description="Spanish DNI utilities for NIE/NIF",
+    python_requires=">=3.8",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Joan Travé",
+    author_email="jtravegordillo@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["spanish_dni"],
+    include_package_data=True,
+    install_requires=[],
+    entry_points={},
+    project_urls={
+        "Source": "https://github.com/joanTrave/spanish_dni",
+    },
+)
