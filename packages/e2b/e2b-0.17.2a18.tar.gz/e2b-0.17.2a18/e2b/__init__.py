92eb5af5b6fd3385ce538e5169a6bcfdd248f7e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 04:03:50 +0000

    Add package e2b/0.17.2a18/e2b-0.17.2a18.tar.gz

---
+++ packages/e2b/e2b-0.17.2a18.tar.gz/e2b-0.17.2a18/e2b/__init__.py
@@ -0,0 +1,36 @@
+from .api import (
+    ApiClient,
+    client,
+)
+from .connection_config import (
+    ConnectionConfig,
+    DOMAIN,
+)
+
+from .exceptions import (
+    SandboxException,
+    TimeoutException,
+    NotFoundException,
+    AuthenticationException,
+    InvalidArgumentException,
+    NotEnoughSpaceException,
+)
+from .sandbox.sandbox_api import SandboxInfo
+from .sandbox.main import Sandbox
+from .sandbox.process.process_handle import (
+    ProcessHandle,
+    ProcessResult,
+    Stderr,
+    Stdout,
+    ProcessExitException,
+)
+from .sandbox.process.main import ProcessInfo
+
+from .sandbox.filesystem.watch_handle import (
+    WatchHandle,
+    FilesystemEvent,
+    FilesystemEventType,
+)
+from .sandbox.filesystem.filesystem import EntryInfo, FileType
+
+from .sandbox.main import Sandbox
