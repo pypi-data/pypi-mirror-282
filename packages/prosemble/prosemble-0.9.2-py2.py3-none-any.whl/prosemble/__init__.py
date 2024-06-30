c5a8aa1436ce8eb9ae7c9ee91fa6ac3c12786bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 14:04:11 +0000

    Add package prosemble/0.9.2/prosemble-0.9.2-py2.py3-none-any.whl

---
+++ packages/prosemble/prosemble-0.9.2-py2.py3-none-any.whl/prosemble/__init__.py
@@ -0,0 +1,11 @@
+"""
+Prosemble package
+"""
+from . import models
+from . import core
+from . import datasets
+
+
+__author__ = """Nana Abeka Otoo"""
+__email__ = 'nanaabekaotoo@gmail.com'
+__version__ = '0.9.2'
