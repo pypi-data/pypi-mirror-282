0f9b223a263d0635a30887c095ebf89d2c491fd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 14:03:58 +0000

    Add package Sasta/0.1/Sasta-0.1.tar.gz

---
+++ packages/Sasta/Sasta-0.1.tar.gz/Sasta-0.1/Sasta/cli.py
@@ -0,0 +1,8 @@
+def main():
+    print("Profile Details:")
+    print("Name: Sasta Hari Haran R")
+    print("Email: sastahariharn0567@gmail.com")
+    print("GitHub: https://github.com/Sastahariharan567")
+
+if __name__ == "__main__":
+    main()
---
+++ packages/Sasta/Sasta-0.1.tar.gz/Sasta-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Sasta",
+    version="0.1",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'mypackage-cli=mypackage.cli:main',
+        ],
+    },
+    install_requires=[
+        # List your package dependencies here
+    ],
+    author="Sasta Hari Haran R",
+    author_email="sastahariharn0567@gmail.com",
+    description="Hi just introducing me through cmd",
+    url="https://github.com/Sastahariharan567/Sasta",
+)
