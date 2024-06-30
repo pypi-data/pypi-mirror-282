59bdca82477bdf7e564fffab6eb340187e93a6a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 03:03:27 +0000

    Add package voice-easy/0.0.8/voice_easy-0.0.8-py3-none-any.whl

---
+++ packages/voice-easy/voice_easy-0.0.8-py3-none-any.whl/voice_easy/voice_easy.py
@@ -0,0 +1,24 @@
+import sounddevice as sd
+from scipy.io.wavfile import write
+import speech_recognition as sr
+
+class Voice(object):
+
+    def __init__(self, mike=1, fs=44100) -> None:
+        self._fs = fs
+        self.r = sr.Recognizer() # 음성 인식 객체 생성
+        sd.default.samplerate = fs # 샘플링 속도
+        sd.default.channels = mike # 확인한 마이크 번호 등록
+        
+    def record(self, sec=2, language="ko-KR"):
+        myAudio = sd.rec(int(sec * self._fs), dtype='int32') # 음성 받기
+        print("명령을 말하세요")
+        sd.wait() # 음성을 받을 동안 대기
+        print("명령을 받았습니다")
+        write('tempWave.wav', self._fs, myAudio) # 음성 파일 저장
+        ## 음성을 텍스트로 변환
+        with sr.AudioFile('tempWave.wav') as source: # 음성 파일 읽어오기
+            audio = self.r.record(source) # 음성 파일을 읽어 소리 데이터 변환
+            text = self.r.recognize_google(audio, language=language) # 음성 인식(한글)
+            return text
+        return "받은 명령을 텍스트로 변환하지 못하였습니다"
