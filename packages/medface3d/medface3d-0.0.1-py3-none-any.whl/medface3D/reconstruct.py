363ffcda920056d95d4e067549719830f7d73ad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 15:03:49 +0000

    Add package medface3d/0.0.1/medface3d-0.0.1-py3-none-any.whl

---
+++ packages/medface3d/medface3d-0.0.1-py3-none-any.whl/medface3D/reconstruct.py
@@ -0,0 +1,72 @@
+import os
+import cv2
+import argparse
+import numpy as np
+
+from medface3D.worker.face_mesh_reconstruction import FaceMeshGenerator
+from medface3D.utils.save_mesh import save_ply_mesh
+from medface3D.utils.visualize import get_depth_map, save_images_as_gif
+from medface3D.utils.render_mesh import render_mesh, render_rotate_mesh
+from medface3D.utils.transform import get_3D_point_cloud
+
+def main():
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f',  '--folder',      default='data/sample' , help='path to sample folder image')
+    parser.add_argument('-l',  '--max_loop',    type=int, default= 2,   help='max loop to reconstruct 3D face mesh')
+    parser.add_argument('-p',  '--point_cloud', action="store_true",    help='get image result as point cloud')
+    parser.add_argument('-d',  '--depth_scale', action="store_true",    help='depth scale for depth value')
+    parser.add_argument('-sv', '--save_mesh',   action="store_true",    help='save result as .ply file')
+    parser.add_argument('-dm', '--depth_map',   action="store_true",    help='save result as depth map')
+    parser.add_argument('-sf', '--face_dense',  action="store_true",    help='save result as dense map')
+    parser.add_argument('-rm', '--render_mesh',  action="store_true",    help='render 3D mesh map', default=True)
+    parser.add_argument('-mv', '--mesh_view',  action="store_true",    help='render 3D mesh map', default=True)
+    parser.add_argument('-ar', '--axis_rotate',  default='y',    help='render 3D mesh map')
+
+
+    args = parser.parse_args()
+
+    files = os.listdir(args.folder)
+
+    for _, file in enumerate(files):
+        if file.split('.')[-1] not in ['jpg', 'png']:
+            continue
+
+        path_read = f'{args.folder}/{file}'
+        image = cv2.imread(path_read)
+        h, w, c = image.shape
+
+        detector = FaceMeshGenerator(max_loop = args.max_loop)
+        input_image = image.copy()
+        output_image, face_detected, points, depth_list, triangles = detector.generate_face_mesh(input_image, args.point_cloud, args.depth_scale)
+
+        if not face_detected:
+            print(f'No face detected in is image')
+
+        if args.face_dense:
+            cv2.imwrite(f'{args.folder}/{file}_face_dense.jpg', output_image)
+
+        if args.depth_map:
+            depth_map = get_depth_map(input_image, points, triangles.simplices, depth_list)
+            cv2.imwrite(f'{args.folder}/{file}_face_depth.jpg', depth_map)
+
+        if args.save_mesh:
+            points_3d = get_3D_point_cloud(points, depth_list)
+            save_ply_mesh(points_3d,  triangles.simplices, f'{args.folder}/{file}_face_dense.ply')
+
+        if args.render_mesh:
+            point_cloud_data = get_3D_point_cloud(points, depth_list, w)
+            mesh_image = render_mesh(image, point_cloud_data)
+            cv2.imwrite(f'{args.folder}/{file}_face_mesh.jpg', mesh_image)
+
+
+        if args.mesh_view:
+            point_cloud_data = get_3D_point_cloud(points, depth_list, w)
+            list_view = render_rotate_mesh(image, point_cloud_data)
+            save_images_as_gif(list_view, f'{args.folder}/{file}_face_mesh_view.gif')
+
+
+
+if __name__ == '__main__':
+    # Example usage:
+    main()
---
+++ packages/medface3d/medface3d-0.0.1-py3-none-any.whl/medface3D/video_capture.py
@@ -0,0 +1,81 @@
+import os
+import cv2
+import argparse
+import numpy as np
+import traceback
+
+
+from medface3D.worker.face_mesh_reconstruction import FaceMeshGenerator
+from medface3D.worker.face_pose_estimation import FacePoseEstimator
+
+from medface3D.utils.render_mesh import render_mesh
+from medface3D.utils.transform import get_3D_point_cloud
+
+
+def main():
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v',  '--video_path',  default=0 , help='path to sample folder image')
+    parser.add_argument('-l',  '--max_loop',    type=int, default=0 ,   help='max loop to reconstruct 3D face mesh')
+
+    args = parser.parse_args()
+
+    detector = FaceMeshGenerator(max_loop = args.max_loop)
+    pose_estimator = FacePoseEstimator()
+
+    cap = cv2.VideoCapture(args.video_path)
+
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)) * 3, int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+
+    if args.video_path == 0:
+        name_save = 'local.mp4'
+    else: name_save = os.path.basename(args.video_path)
+
+    out = cv2.VideoWriter(f'data/demo/mesh_{name_save}', fourcc, 10, size)
+
+
+    if not cap.isOpened():
+        print("Error: Could not open camera.")
+        exit()
+
+    while True:
+        try:
+            ret, frame = cap.read()
+
+            # Check if the frame was captured successfully
+            if not ret:
+                print("Error: Could not read frame.")
+                break
+
+            input_image = frame
+            output_image, face_detected, points, depth_list, _ = detector.generate_face_mesh(input_image, True, False)
+
+            if face_detected:
+                point_cloud_data = get_3D_point_cloud(points, depth_list)
+
+                face_pose = pose_estimator.get_pose(point_cloud_data)
+                mesh_blank = render_mesh(input_image, point_cloud_data)
+                mesh_map = render_mesh(input_image, point_cloud_data, False)
+                output_image = cv2.hconcat([frame, mesh_blank])
+                output_image = cv2.hconcat([output_image, mesh_map])
+
+            out.write(output_image)
+            # Display the frame in a window
+            cv2.imshow("Video Capture", output_image)
+
+            # Exit the loop when the 'q' key is pressed (or any other key)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        except Exception as e:
+            continue
+
+    # Release the VideoCapture object and close the OpenCV window
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    # Example usage:
+    main()
