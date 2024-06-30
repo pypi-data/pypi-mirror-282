683256e23c7a87e3e0d04dabfc285d9abf1aa780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 14:03:57 +0000

    Add package promptscript/0.3.0/promptscript-0.3.0-py3-none-any.whl

---
+++ packages/promptscript/promptscript-0.3.0-py3-none-any.whl/promptscript/cli.py
@@ -0,0 +1,68 @@
+import os
+import atexit
+import readline
+from promptscript.utils.config import COMMANDS
+from promptscript.executor import PersistentCommandExecutor
+
+
+def setup_readline():
+    history_file = '.promptscript_cli_history'
+    try:
+        readline.read_history_file(history_file)
+    except Exception as e:
+        pass
+    readline.set_history_length(1000)
+
+    atexit.register(readline.write_history_file, history_file)
+
+    def completer(text: str, state: int):
+        options = [cmd for cmd in COMMANDS if cmd.startswith(text.lower())]
+        if state < len(options):
+            if text == '':
+                return '\t'
+            
+            if text.isupper():
+                return options[state].upper()
+            else:
+                return options[state] 
+    
+    readline.parse_and_bind('tab: complete')
+    readline.set_completer(completer)
+
+def run_cli():
+    command_executor = PersistentCommandExecutor()
+    setup_readline()
+    
+    in_buffer = False
+    command_buffer = []
+    while True:
+        try:
+            if not in_buffer:
+                command = input('>>> ')
+            else:
+                command = input('... ')
+            if command.lower() == 'exit':
+                break
+            
+            if command.lower().startswith(('if', 'elif', 'else', 'for', 'while')):
+                in_buffer = True
+            
+            if in_buffer:
+                if command == '':
+                    command = '\n'.join(command_buffer)
+                    in_buffer = False
+                else:
+                    command_buffer.append(command)
+            
+            if not in_buffer:
+                command_executor.run(command)
+
+        except KeyboardInterrupt:
+            print('\nKeyboard Interrupt')
+        
+        except EOFError:
+            print("\n\nExiting...")
+            break
+
+        except Exception as e:
+            print(f'Error: {e}')
