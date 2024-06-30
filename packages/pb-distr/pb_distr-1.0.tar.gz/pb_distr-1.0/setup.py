a32a3a76c24073020ae3506cfc2ab659b424fab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 14:03:51 +0000

    Add package pb-distr/1.0/pb_distr-1.0.tar.gz

---
+++ packages/pb-distr/pb_distr-1.0.tar.gz/pb_distr-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pb_distr",
+    version="1.0",
+    description="Gaussian and Binomial distributions",
+    packages=["pb_distr"],
+    author="Weijie Wang",
+    author_email="wang.weijie0110@gmail.com",
+    zip_safe=False,
+)
