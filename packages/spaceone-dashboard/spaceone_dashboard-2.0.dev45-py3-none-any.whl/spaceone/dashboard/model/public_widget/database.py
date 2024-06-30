fbf5f44aa324a7a460b03d38e325e2292d0c4ba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 03:04:22 +0000

    Add package spaceone-dashboard/2.0.dev45/spaceone_dashboard-2.0.dev45-py3-none-any.whl

---
+++ packages/spaceone-dashboard/spaceone_dashboard-2.0.dev45-py3-none-any.whl/spaceone/dashboard/model/public_widget/database.py
@@ -0,0 +1,58 @@
+from mongoengine import *
+
+from spaceone.core.model.mongo_model import MongoModel
+
+
+class PublicWidget(MongoModel):
+    widget_id = StringField(max_length=40, generate_id="public-widget", unique=True)
+    name = StringField(max_length=100)
+    description = StringField(default=None)
+    widget_type = StringField(max_length=40, default="NONE")
+    size = StringField(default=None, null=True)
+    options = DictField(default=None, null=True)
+    tags = DictField(default=None)
+    data_table_id = StringField(max_length=40, default=None, null=True)
+    dashboard_id = StringField(max_length=40)
+    resource_group = StringField(
+        max_length=40, choices=("DOMAIN", "WORKSPACE", "PROJECT")
+    )
+    project_id = StringField(max_length=40)
+    workspace_id = StringField(max_length=40)
+    domain_id = StringField(max_length=40)
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True)
+
+    meta = {
+        "updatable_fields": [
+            "name",
+            "description",
+            "widget_type",
+            "size",
+            "options",
+            "tags",
+            "data_table_id",
+        ],
+        "minimal_fields": [
+            "widget_id",
+            "name",
+            "widget_type",
+            "data_table_id",
+            "dashboard_id",
+            "resource_group",
+            "project_id",
+            "workspace_id",
+            "domain_id",
+        ],
+        "change_query_keys": {"user_projects": "project_id"},
+        "ordering": ["name"],
+        "indexes": [
+            "name",
+            "widget_type",
+            "data_table_id",
+            "dashboard_id",
+            "resource_group",
+            "project_id",
+            "workspace_id",
+            "domain_id",
+        ],
+    }
