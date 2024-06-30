ff4469517b4aeffb7d5b67996e88b9af9e91a6f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 03:04:12 +0000

    Add package nvmath-python/0.1.0/nvmath_python-0.1.0-cp310-cp310-win_amd64.whl

---
+++ packages/nvmath-python/nvmath_python-0.1.0-cp310-cp310-win_amd64.whl/nvmath/device/experimental/__init__.py
@@ -0,0 +1,7 @@
+#
+# Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from .load_store_numba import fast_copy, fast_copy_2x, fast_copy_4x, load, store
