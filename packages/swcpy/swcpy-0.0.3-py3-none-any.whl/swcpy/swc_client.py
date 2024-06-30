cecdc0fdb8b221af032fe8cd0adf70748c2a2ec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 04:03:56 +0000

    Add package swcpy/0.0.3/swcpy-0.0.3-py3-none-any.whl

---
+++ packages/swcpy/swcpy-0.0.3-py3-none-any.whl/swcpy/swc_client.py
@@ -0,0 +1,16 @@
+# SDK client
+import httpx
+
+
+class SWCClient:
+    HEALTH_CHECK_ENDPOINT = "/"
+
+    def __init__(self, swc_base_url: str):
+        # initial with config values
+        self.swc_base_url = swc_base_url
+
+    # analytics endpoints
+    def get_health_check(self):
+        # make the API call
+        with httpx.Client(base_url=self.swc_base_url) as client:
+            return client.get(self.HEALTH_CHECK_ENDPOINT)
