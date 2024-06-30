300dae578c668860dd932cc9bed9279d0f303508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 04:03:52 +0000

    Add package AbInbevCase/1.0.0/abinbevcase-1.0.0.tar.gz

---
+++ packages/AbInbevCase/abinbevcase-1.0.0.tar.gz/abinbevcase-1.0.0/fuel_efficency/entities/down_hill.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass, field
+
+from fuel_efficency.entities.node import Node
+from fuel_efficency.entities.position import Position
+
+
+@dataclass(slots=True)
+class DownHill:
+    weight: float = float(0.5)
+    position: 'Position' = field(default_factory=Position)
+
+    def __hash__(self):
+        return hash((self.weight, self.position.x, self.position.y))
+
+    def __eq__(self, other):
+        try:
+            return (self.weight, self.position.x, self.position.y) == (other.weight, other.position.x, other.position.y)
+
+        except AttributeError:
+            raise NotImplementedError("Missing `position` or `weight` attribute")
+
+    def __lt__(self, other):
+        try:
+            return self.weight < other.weight
+
+        except AttributeError:
+            raise NotImplementedError("Missing `weight` attribute")
