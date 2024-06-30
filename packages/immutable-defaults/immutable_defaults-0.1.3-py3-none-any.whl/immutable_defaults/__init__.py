48383f330c6afb209667c66281ce95a5c6844c0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 15:03:52 +0000

    Add package immutable-defaults/0.1.3/immutable_defaults-0.1.3-py3-none-any.whl

---
+++ packages/immutable-defaults/immutable_defaults-0.1.3-py3-none-any.whl/immutable_defaults/__init__.py
@@ -0,0 +1,11 @@
+from immutable_defaults.immutable_defaults import (
+    immutable_defaults,
+    class_with_immutable_defaults,
+    ImmutableDefaultsError,
+)
+
+__all__ = [
+    "immutable_defaults",
+    "class_with_immutable_defaults",
+    "ImmutableDefaultsError",
+]
