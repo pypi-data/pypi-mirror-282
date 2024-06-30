f1ab6dba960ca0338ec6c4d5ed8c74134a7b59f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 03:03:31 +0000

    Add package portobello/0.0.10/portobello-0.0.10.tar.gz

---
+++ packages/portobello/portobello-0.0.10.tar.gz/portobello-0.0.10/src/portobello/cli.py
@@ -0,0 +1,42 @@
+import os
+import psutil
+
+from internal.utils import split_quoted_string, load_portobello_config, save_portobello_config
+from netstat.main import main as netstat_main
+
+
+def ldap(cli_strings):
+    pass
+
+
+def cli_help(cli_strings):
+    pass
+
+
+def edit_config(cli_strings):
+    pass
+
+
+def main():
+    """
+    if keepass not in config, ask for keepass path to be entered
+    give list of ldap, netstat, help, or config
+    :return:
+    """
+    portobello_config = load_portobello_config()
+    current_pid = os.getpid()
+    current_process = psutil.Process(current_pid)
+    cli_string = ' '.join(current_process.cmdline())
+    cli_strings = split_quoted_string(cli_string)
+    {
+        'ldap': ldap,
+        'netstat': netstat_main,
+        'ns': netstat_main,
+        'help': cli_help,
+        'config': edit_config
+    }[cli_strings[0]](cli_strings[1:], portobello_config)
+
+    save_portobello_config(portobello_config)
+
+
+main()
