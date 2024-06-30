b0cf6f2c131d18da67af0ec1339720275038fa78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 04:03:17 +0000

    Add package event-plugin-system/0.0.2/event_plugin_system-0.0.2.tar.gz

---
+++ packages/event-plugin-system/event_plugin_system-0.0.2.tar.gz/event_plugin_system-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="event-plugin-system",
+    version="0.0.2",
+    author="Carlos",
+    author_email="chalonga@gmail.com",
+    description="A simple plugin event system",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/chalonga/event-plugin-system",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[],
+)
