f2abbb2be1a7205d8d2d6289271450f6ae655d57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 03:03:48 +0000

    Add package ytb2audiobot/2.215/ytb2audiobot-2.215-py3-none-any.whl

---
+++ packages/ytb2audiobot/ytb2audiobot-2.215-py3-none-any.whl/ytb2audiobot/cron.py
@@ -0,0 +1,26 @@
+import asyncio
+import datetime
+
+from ytb2audiobot.datadir import get_data_dir
+from ytb2audiobot.utils import delete_file_async
+
+data_dir = get_data_dir()
+
+
+async def empty_dir_by_cron(age_seconds):
+    print('♻️ Empty aged items: ')
+    for file in data_dir.iterdir():
+        if datetime.datetime.now().timestamp() - file.stat().st_mtime > age_seconds:
+            print('\t', '🔹🗑', file.name)
+            await delete_file_async(file)
+
+
+async def run_periodically(interval, age, func):
+    while True:
+        await func(age)
+        await asyncio.sleep(interval)
+
+
+async def run_cron():
+    print('⏰  Running cron ... ')
+    await run_periodically(60, 3600, empty_dir_by_cron)
