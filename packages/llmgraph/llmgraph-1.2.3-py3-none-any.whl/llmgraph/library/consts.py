62ca48ba3655c0411b510db09f8a146e8f85dd67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 14:04:16 +0000

    Add package llmgraph/1.2.3/llmgraph-1.2.3-py3-none-any.whl

---
+++ packages/llmgraph/llmgraph-1.2.3-py3-none-any.whl/llmgraph/library/consts.py
@@ -0,0 +1,7 @@
+package_name = "llmgraph"
+version = "1.2.3"
+prompts_yaml_location = "prompts.yaml"
+
+default_llm_model = "gpt-3.5-turbo"
+default_llm_temp = 0.0
+default_output_folder = "./_output/"
