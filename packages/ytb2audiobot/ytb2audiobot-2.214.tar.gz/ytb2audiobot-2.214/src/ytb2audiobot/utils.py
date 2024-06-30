8bdbc1c5b98d7f3e7d3a878c312ce162b5cc87a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 03:03:44 +0000

    Add package ytb2audiobot/2.214/ytb2audiobot-2.214.tar.gz

---
+++ packages/ytb2audiobot/ytb2audiobot-2.214.tar.gz/ytb2audiobot-2.214/src/ytb2audiobot/utils.py
@@ -0,0 +1,52 @@
+import asyncio
+import pathlib
+import re
+from pathlib import Path
+
+import aiofiles
+import aiofiles.os
+
+
+CAPITAL_LETTERS_PERCENT_THRESHOLD = 0.3
+
+
+async def create_directory_async(path):
+    path = pathlib.Path(path)
+    if path.exists():
+        return path
+    try:
+        await aiofiles.os.mkdir(path)
+    except Exception as e:
+        print(f"🔴 Create file async. Error: {e}")
+        return
+
+    return path
+
+
+async def delete_file_async(path: Path):
+    try:
+        async with aiofiles.open(path, 'r'):  # Ensure the file exists
+            pass
+        await asyncio.to_thread(path.unlink)
+    except Exception as e:
+        print(f"🔴 Delete file async. Error: {e}")
+
+
+def capital2lower(text):
+    count_capital = sum(1 for char in text if char.isupper())
+    if count_capital / len(text) < CAPITAL_LETTERS_PERCENT_THRESHOLD:
+        return text
+
+    text = text.lower()
+    text = text[0].upper() + text[1:]
+    return text
+
+
+def filename_m4a(text):
+    name = (re.sub(r'[^\w\s\-\_\(\)\[\]]', ' ', text)
+            .replace('    ', ' ')
+            .replace('   ', ' ')
+            .replace('  ', ' ')
+            .strip())
+    return f'{name}.m4a'
+
