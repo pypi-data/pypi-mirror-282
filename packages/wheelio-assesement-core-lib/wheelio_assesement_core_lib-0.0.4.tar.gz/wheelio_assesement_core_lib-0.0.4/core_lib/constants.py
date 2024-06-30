7c6b784fd1dec22d990da82de0206db0c7cafd85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 03:05:08 +0000

    Add package wheelio-assesement-core-lib/0.0.4/wheelio_assesement_core_lib-0.0.4.tar.gz

---
+++ packages/wheelio-assesement-core-lib/wheelio_assesement_core_lib-0.0.4.tar.gz/wheelio_assesement_core_lib-0.0.4/core_lib/constants.py
@@ -0,0 +1,7 @@
+import os
+
+STATUS_CREATE_QUEUE = os.getenv('STATUS_CREATE_QUEUE', 'status_create')
+STATUS_UPDATE_QUEUE = os.getenv('STATUS_UPDATE_QUEUE', 'status_update')
+NOTIFY_FRIENDS_QUEUE = os.getenv('NOTIFY_FRIENDS_QUEUE', 'notify_friends')
+FRIEND_REQUESTS_QUEUE = os.getenv('FRIEND_REQUESTS_QUEUE', 'friend_requests')
+FRIEND_ACCEPTANCE_QUEUE = os.getenv('FRIEND_ACCEPTANCE_QUEUE', 'friend_acceptance')
