3454d819141de169406b127eeabd9e6efda5684e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 04:04:09 +0000

    Add package electricity-data-fetching-tr/0.1.5/electricity_data_fetching_tr-0.1.5.tar.gz

---
+++ packages/electricity-data-fetching-tr/electricity_data_fetching_tr-0.1.5.tar.gz/electricity_data_fetching_tr-0.1.5/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='electricity_data_fetching_tr',
+    version='0.1.5',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'requests',
+        'pandas',
+        'forex-python',
+        'workalendar',
+    ],
+    author='Rojen Arda Şeşen',
+    author_email='sesen19@itu.edu.tr',
+    description='Uses the EPIAS API to fetch electricity data for Turkey. The package also cleans and structures the data to be used in forecasting models.',
+    license='GPL-3.0',
+    keywords='electricity data_fetching ',
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ],
+    url='https://github.com/rojenarda/electricity_data_fetching_tr',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    project_urls={
+        'Bug Tracker': 'https://github.com/yourusername/electricity_data_fetching_tr/issues',
+        'Source Code': 'https://github.com/yourusername/electricity_data_fetching_tr'
+    }
+)
