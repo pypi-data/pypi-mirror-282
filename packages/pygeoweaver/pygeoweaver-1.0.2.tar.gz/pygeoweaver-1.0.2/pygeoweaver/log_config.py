971b9f3b6bd9db9ceaff1717267100bdb249501c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 14:03:51 +0000

    Add package pygeoweaver/1.0.2/pygeoweaver-1.0.2.tar.gz

---
+++ packages/pygeoweaver/pygeoweaver-1.0.2.tar.gz/pygeoweaver-1.0.2/pygeoweaver/log_config.py
@@ -0,0 +1,38 @@
+import logging
+import os
+
+
+def setup_logging():
+    log_file = '~/geoweaver.log'
+    log_file = os.path.expanduser(log_file)
+
+    # Ensure the directory for the log file exists, create if not
+    log_dir = os.path.dirname(log_file)
+    os.makedirs(log_dir, exist_ok=True)
+    # Get the absolute path to the logging.ini file
+    logging_ini_path = os.path.abspath('logging.ini')
+
+    # Open the logging.ini file
+    with open(logging_ini_path, 'rt') as f:
+        config_str = f.read()
+        config_str = config_str.replace('%(log_file)s', os.path.expanduser(log_file))
+
+    config_file = 'logging_temp.ini'
+    with open(config_file, 'wt') as f:
+        f.write(config_str)
+
+    logging.config.fileConfig(config_file)
+    os.remove(config_file)
+
+
+def get_logger(class_name):
+    """
+    Get a logger with the specified class name.
+    """
+    logger = logging.getLogger(class_name)
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    return logger
