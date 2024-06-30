d3a2febb6c5285438aa7ff1133075c69a298ac24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 03:04:43 +0000

    Add package pysonlib/0.0.2/pysonlib-0.0.2.tar.gz

---
+++ packages/pysonlib/pysonlib-0.0.2.tar.gz/pysonlib-0.0.2/README.md
@@ -0,0 +1,15 @@
+# pysonlib
+- Python Script Object Notation (pyson) is a standard text-based format for expressing structured data using Python object syntax.
+  
+- 파이썬 문법으로 다룰 수 있는 유사 JSON 포맷의 데이터 오브젝트 입니다.
+- 파이썬이 json 을 dict로 변환하기 때문에 마찬가지로 dict를 사용합니다.
+- 데이터 포맷이면서 파이썬 코드이므로 그대로 사용해도 무방합니다.
+
+- 여러가지 편의기능을 지원할 예정입니다.
+  - 주석 기능
+  - 필드명 결합 기능
+  - 읽기/쓰기 권한 기능
+  - 클래스 오브젝트 생성 기능
+  - 오브젝트로서 직렬화/역직렬화 기능
+  - PYSONObject 객체로서의 기능
+  - pyson 구조 기반 파이썬 클래스 작성 기능
---
+++ packages/pysonlib/pysonlib-0.0.2.tar.gz/pysonlib-0.0.2/setup.py
@@ -0,0 +1,31 @@
+#------------------------------------------------------------------------
+# 참조 모듈 목록.
+#------------------------------------------------------------------------
+from setuptools import setup, find_packages
+
+
+#------------------------------------------------------------------------
+# 설치.
+#------------------------------------------------------------------------
+setup(
+	name = "pysonlib",
+	version = "0.0.2",
+	author = "ddukbaek2",
+	author_email = "ddukbaek2@gmail.com",
+	description = "Python Script Object Notation (pysonlib) is a standard text-based format for expressing structured data using Python object syntax.",
+	long_description = open(file = "README.md", mode = "r", encoding = "utf-8").read(),
+	long_description_content_type = "text/markdown",
+	url = "https://github.com/ddukbaek2/pysonlib",
+	packages = find_packages(),
+	include_package_data=True,
+	package_data = {
+		"": ["resources/*"],
+	},
+    install_requires = [],
+	classifiers = [
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires = ">=3.9"
+)
