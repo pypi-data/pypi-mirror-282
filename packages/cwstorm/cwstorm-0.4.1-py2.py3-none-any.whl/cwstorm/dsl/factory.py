9d87ae611c16aa11e0a91751f2bb8a3e9aced48e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 14:03:50 +0000

    Add package cwstorm/0.4.1/cwstorm-0.4.1-py2.py3-none-any.whl

---
+++ packages/cwstorm/cwstorm-0.4.1-py2.py3-none-any.whl/cwstorm/dsl/factory.py
@@ -0,0 +1,21 @@
+
+from cwstorm.dsl.task import Task
+from cwstorm.dsl.upload import Upload
+from cwstorm.dsl.job import Job
+from cwstorm.dsl.slack import Slack
+from cwstorm.dsl.email import Email
+
+CLASSES = {
+    "job": Job,
+    "task": Task,
+    "upload": Upload,
+    "slack": Slack,
+    "email": Email
+}
+
+def get(data):
+    datatype = data.get("type")
+    klass = CLASSES.get(datatype)
+    if klass is None:
+        raise ValueError(f"Class {datatype} not found")
+    return klass(**data)
