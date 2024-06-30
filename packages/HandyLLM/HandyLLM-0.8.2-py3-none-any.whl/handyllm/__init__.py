50fe4550c67cf47c71008b3975c867bc7a4357f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 03:04:43 +0000

    Add package HandyLLM/0.8.2/HandyLLM-0.8.2-py3-none-any.whl

---
+++ packages/HandyLLM/HandyLLM-0.8.2-py3-none-any.whl/handyllm/__init__.py
@@ -0,0 +1,6 @@
+from .openai_client import OpenAIClient, ClientMode
+from .requestor import Requestor
+from .openai_api import OpenAIAPI
+from .endpoint_manager import Endpoint, EndpointManager
+from .prompt_converter import PromptConverter
+from .utils import *
