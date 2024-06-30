15930190d6ac6ce726754b8cd329617bb61b7f2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 03:04:02 +0000

    Add package flask-Captchaify/1.7.6.3/flask_captchaify-1.7.6.3.tar.gz

---
+++ packages/flask-Captchaify/flask_captchaify-1.7.6.3.tar.gz/flask_captchaify-1.7.6.3/setup.py
@@ -0,0 +1,47 @@
+"""
+⚒️ Setup module for flask_Captchaify
+
+https://github.com/tn3w/flask_Captchaify
+Made with 💩 in Germany by TN3W
+"""
+
+from setuptools import setup, find_packages
+from pip._internal.req import parse_requirements
+
+requirements = [str(requirement.requirement)\
+                for requirement in list(parse_requirements("requirements.txt", session=False))]
+
+with open('README.md', 'r', encoding='utf-8') as readable_file:
+    long_description = readable_file.read()
+
+long_description = long_description.split('<h1 align="center">𝐟𝐥𝐚𝐬𝐤_𝐂𝐚𝐩𝐭𝐜𝐡𝐚𝐢𝐟𝐲</h1>')[1]
+long_description = '<h1 align="center">𝐟𝐥𝐚𝐬𝐤_𝐂𝐚𝐩𝐭𝐜𝐡𝐚𝐢𝐟𝐲</h1>' + long_description
+
+setup(
+    name='flask_Captchaify',
+    version='1.7.6.3',
+    description='Protect against bots and DDoS attacks',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='TN3W',
+    author_email='tn3wA8xxfuVMs2@proton.me',
+    url='https://github.com/tn3w/flask_Captchaify',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    package_data={
+        'flask_Captchaify': ['assets/*', 'templates/*', 'datasets/*', 'templates/assets/*']
+    },
+    classifiers=[
+        'Environment :: Web Environment',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    license='GPL-3.0',
+    keywords='flask, Python, Bot, Captcha, DDoS',
+    install_requires=requirements
+)
