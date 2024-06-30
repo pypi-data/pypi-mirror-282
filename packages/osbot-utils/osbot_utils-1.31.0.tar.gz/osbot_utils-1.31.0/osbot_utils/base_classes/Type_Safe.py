496bdf0ac744f66caa3b75b6a51716ff136e16f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 03:04:21 +0000

    Add package osbot-utils/1.31.0/osbot_utils-1.31.0.tar.gz

---
+++ packages/osbot-utils/osbot_utils-1.31.0.tar.gz/osbot_utils-1.31.0/osbot_utils/base_classes/Type_Safe.py
@@ -0,0 +1,6 @@
+from osbot_utils.base_classes.Kwargs_To_Self import Kwargs_To_Self
+
+# todo: refactor all of Kwargs_To_Self into this class (in a way to minimize the side effects, since
+#        Kwargs_To_Self is used in many places in the codebase)
+
+Type_Safe = Kwargs_To_Self
