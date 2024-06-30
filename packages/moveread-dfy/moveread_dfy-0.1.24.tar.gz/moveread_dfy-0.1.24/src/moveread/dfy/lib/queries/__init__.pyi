fd67d346cf1462b81987ceabcf063f4be8174244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 14:04:37 +0000

    Add package moveread-dfy/0.1.24/moveread_dfy-0.1.24.tar.gz

---
+++ packages/moveread-dfy/moveread_dfy-0.1.24.tar.gz/moveread_dfy-0.1.24/src/moveread/dfy/lib/queries/__init__.pyi
@@ -0,0 +1,9 @@
+from .queries import exact_game, exact_round, group_games, round_games, \
+  select_game, select_round, select_rounds, select_tnmt, select_group_games, \
+  select_group
+
+__all__ = [
+  'exact_game', 'exact_round', 'group_games', 'round_games',
+  'select_game', 'select_round', 'select_rounds', 'select_tnmt',
+  'select_group_games', 'select_group',
+]
