48e532d72e05e7482346dd7d8de121be29a7d2c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 14:04:37 +0000

    Add package moveread-dfy/0.1.24/moveread_dfy-0.1.24-py3-none-any.whl

---
+++ packages/moveread-dfy/moveread_dfy-0.1.24-py3-none-any.whl/moveread/dfy/__init__.pyi
@@ -0,0 +1,7 @@
+from .types import Game, GameId, Image, Tournament, PGN, Pairing
+from . import server, doer, lib
+
+__all__ = [
+  'Game', 'GameId', 'Image', 'Tournament', 'PGN', 'Pairing',
+  'server', 'doer', 'lib',
+]
---
+++ packages/moveread-dfy/moveread_dfy-0.1.24-py3-none-any.whl/moveread/dfy/lib/__init__.pyi
@@ -0,0 +1,6 @@
+from .pairings import update_current_pairings
+from . import queries
+
+__all__ = [
+  'queries', 'update_current_pairings',
+]
