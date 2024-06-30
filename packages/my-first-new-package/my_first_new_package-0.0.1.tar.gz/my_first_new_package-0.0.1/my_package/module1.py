9ac584e3be95fefce95f70c36895c5edf2840a50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 03:03:30 +0000

    Add package my-first-new-package/0.0.1/my_first_new_package-0.0.1.tar.gz

---
+++ packages/my-first-new-package/my_first_new_package-0.0.1.tar.gz/my_first_new_package-0.0.1/my_package/module1.py
@@ -0,0 +1,11 @@
+# mypackage/module1.py
+
+def function1():
+    return "Hello from function1 in module1"
+
+class Class1:
+    def __init__(self, name):
+        self.name = name
+    
+    def greet(self):
+        return f"Hello, {self.name}!"
