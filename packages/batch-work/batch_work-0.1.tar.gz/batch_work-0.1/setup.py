449fae24ab54403d02309237a640f4e778105147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-30 04:03:34 +0000

    Add package batch-work/0.1/batch_work-0.1.tar.gz

---
+++ packages/batch-work/batch_work-0.1.tar.gz/batch_work-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="batch_work",
+    version="0.1",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts':[
+            'bw_rename=batch_work.main:main',
+            'bw_sort=batch_work.main:main'
+        ],
+    },
+    install_requires=[
+        "tree",
+    ],
+    author="Ugyen Norbu",
+    description="CLI tool to organize and rename files.",
+    long_description=open("README.md").read(),
+    long_description_content_type='text/markdown',
+)
