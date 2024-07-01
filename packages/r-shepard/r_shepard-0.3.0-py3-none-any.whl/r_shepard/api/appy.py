8e2264ed34cd426f337a1c360f4dd1fa77adffb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 15:04:51 +0000

    Add package r-shepard/0.3.0/r_shepard-0.3.0-py3-none-any.whl

---
+++ packages/r-shepard/r_shepard-0.3.0-py3-none-any.whl/r_shepard/api/appy.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class ApiConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "r_shepard.api"
+    verbose_name = "R Shepard"
