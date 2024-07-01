618bdded0850e556ba492aaaac03cfbfcbf0f512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 15:04:57 +0000

    Add package kameleoon-client-python/3.3.1/kameleoon-client-python-3.3.1.tar.gz

---
+++ packages/kameleoon-client-python/kameleoon-client-python-3.3.1.tar.gz/kameleoon-client-python-3.3.1/kameleoon/configuration/variation_by_exposition.py
@@ -0,0 +1,19 @@
+"""Kameleoon Configuration"""
+
+from typing import Any, List, Dict, Optional
+
+
+class VariationByExposition:
+    """
+    Variation is used for saving variations of feature flags (v2) with rules
+    """
+
+    @staticmethod
+    def from_array(array: List[Dict[str, Any]]) -> List["VariationByExposition"]:
+        """Creates a list of VariationByExposition from array of dictionaries"""
+        return [VariationByExposition(item) for item in array]
+
+    def __init__(self, dict_json: Dict[str, Any]):
+        self.variation_key: str = dict_json.get("variationKey", "")
+        self.variation_id: Optional[int] = dict_json.get("variationId")
+        self.exposition: float = dict_json.get("exposition", 0.0)
