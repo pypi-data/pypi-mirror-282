c9eac4a03dc23b5e7feb80d853fd6035d275a9ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 04:04:51 +0000

    Add package github-changes/1.0.3/github_changes-1.0.3.tar.gz

---
+++ packages/github-changes/github_changes-1.0.3.tar.gz/github_changes-1.0.3/github_changes/GithubFilteredChanges.py
@@ -0,0 +1,36 @@
+from github import Github
+
+
+class GithubFilteredChanges:
+    """
+    Construct a GithubFilteredChanges
+    :param repo: GitHub repository
+    :param path: path of interest in which to track git changes
+    :param hierarchy: file path hierarchy of interest e.g /home/bobo/test.yaml hierarchy 1 will collect the bobo name
+    :param organization: the GitHub organization
+    :param token = token: GitHub token
+    :param commit = commit: the commit to track
+    """
+    def __init__(self, repo: str, path: str, hierarchy: int, organization: str, token: str, commit: str):
+        self.repo = repo
+        self.path = path
+        self.hierarchy = hierarchy
+        self.organization = organization
+        self.token = token
+        self.commit = commit
+
+    @property
+    def session(self):
+        if not self.session:
+            return Github(self.token).get_organization(self.organization).get_repo(self.repo)
+        return self.session
+
+    def _get_changed_files(self):
+        changed_files = []
+        for change in self.session.get_commit(self.commit).files:
+            if change.status != 'removed' and self.path_to_eveluate in change.filename:
+                changed_files.append(change.filename.split('/')[self.hierarchy])
+        return set(changed_files)
+
+    def get_github_session(self):
+        return Github(self.token).get_organization(self.organization).get_repo(self.repo)
