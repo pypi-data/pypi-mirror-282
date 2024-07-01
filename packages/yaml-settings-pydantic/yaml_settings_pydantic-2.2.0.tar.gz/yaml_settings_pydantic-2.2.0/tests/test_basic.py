ae8d98b66e4654abbf9e3b7ec0f4253c054e1f0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 14:03:45 +0000

    Add package yaml-settings-pydantic/2.2.0/yaml_settings_pydantic-2.2.0.tar.gz

---
+++ packages/yaml-settings-pydantic/yaml_settings_pydantic-2.2.0.tar.gz/yaml_settings_pydantic-2.2.0/tests/test_basic.py
@@ -0,0 +1,196 @@
+# import logging
+import os
+import pathlib
+from typing import Annotated, Set, Tuple, Type
+from unittest import mock
+
+import pytest
+import yaml
+from pydantic import BaseModel
+from pydantic.fields import Field
+
+from yaml_settings_pydantic import (
+    DEFAULT_YAML_FILE_CONFIG_DICT,
+    BaseYamlSettings,
+    CreateYamlSettings,
+    YamlFileConfigDict,
+    YamlSettingsConfigDict,
+    resolve_filepaths,
+)
+
+# logger = logging.getLogger(__name__)
+# logging.basicConfig(level=logging.DEBUG)
+
+
+class TestCreateYamlSettings:
+    def test_reload(self, fileDummies):
+        # Test args
+        with pytest.raises(ValueError):
+            CreateYamlSettings(BaseYamlSettings)
+
+        # Make sure it works. Check name of returned learcal
+        def create_settings(reload=None, files=None):
+            return type(
+                "Settings",
+                (BaseYamlSettings,),
+                dict(
+                    __yaml_reload__=reload or False,
+                    __yaml_files__=files or set(fileDummies),
+                ),
+            )
+
+        Settings = create_settings()
+        yaml_settings = CreateYamlSettings(Settings)
+        yaml_settings()
+        assert not yaml_settings.reload
+
+        # NOTE: Malform a file.
+        bad = Settings.__yaml_files__.pop()
+        with open(bad, "w") as file:
+            yaml.dump([], file)
+
+        # # NOTE: Loading should not be an error as the files should not be reloaded.
+        yaml_settings()
+        #
+        # # NOTE: Test reloading with bad file.
+        # #       This could be called without the args as mutation is visible
+        # #       to fn.
+        Settings = create_settings(reload=False)
+        yaml_settings = CreateYamlSettings(Settings)
+
+        with pytest.raises(ValueError) as err:
+            yaml_settings()
+
+        assert str(bad) in str(err.value)
+
+        with open(bad, "w") as file:
+            yaml.dump({}, file)
+
+        yaml_settings()
+
+    def from_model_config(
+        self, **kwargs
+    ) -> Tuple[CreateYamlSettings, Type[BaseYamlSettings]]:
+        Settings = type(
+            "Settings",
+            (BaseYamlSettings,),
+            dict(model_config=YamlSettingsConfigDict(**kwargs)),
+        )
+        return CreateYamlSettings(Settings), Settings
+
+    def test_dunders_have_priority(self) -> None:
+        init_reload = True
+        yaml_settings, Settings = self.from_model_config(
+            yaml_files={"foo-bar.yaml"},
+            yaml_reload=init_reload,
+        )
+
+        default = DEFAULT_YAML_FILE_CONFIG_DICT
+        assert yaml_settings.files == {"foo-bar.yaml": default}
+        assert yaml_settings.reload == init_reload
+
+        final_files: Set[str] = {"spam-eggs.yaml"}
+        OverwriteSettings = type(
+            "OverwriteSettings",
+            (Settings,),
+            dict(__yaml_files__=final_files),
+        )
+        yaml_settings = CreateYamlSettings(OverwriteSettings)
+
+        assert yaml_settings.files == {"spam-eggs.yaml": default}
+        assert yaml_settings.reload == init_reload
+
+    @pytest.mark.parametrize(
+        "yaml_files",
+        [
+            "foo.yaml",
+            {"foo.yaml"},
+            {"foo.yaml": YamlFileConfigDict(required=True, subpath=None)},
+        ],
+    )
+    def test_hydration_yaml_files(self, yaml_files) -> None:
+        make, _ = self.from_model_config(yaml_files=yaml_files)
+
+        assert len(make.files) == 1
+        assert isinstance(make.files, dict)
+        assert (foo := make.files.get("foo.yaml")) is not None
+        assert isinstance(foo, dict)
+        assert foo.get("required") is True
+        assert foo.get("subpath") is None
+
+    def test_yaml_not_required(self) -> None:
+        # Should not raise error
+        make, Settings = self.from_model_config(
+            yaml_files={
+                "foo.yaml": YamlFileConfigDict(
+                    required=False,
+                    subpath=None,
+                )
+            }
+        )
+        assert make.files.get("foo.yaml")
+        make.load()
+
+        # Should raise error
+        make, _ = self.from_model_config(yaml_files="foo.yaml")
+        with pytest.raises(ValueError) as err:
+            make.load()
+
+        assert str(err.value)
+
+    def test_envvar(self, tmp_path: pathlib.Path) -> None:
+
+        # ------------------------------------------------------------------- #
+        # NOTE: Settup
+
+        path_default = str(tmp_path / "default.yaml")
+        path_other = str(tmp_path / "other.yaml")
+
+        make, _Settings = self.from_model_config(
+            yaml_files={
+                path_default: YamlFileConfigDict(
+                    envvar="FOO_PATH",
+                    required=False,
+                    subpath=None,
+                )
+            }
+        )
+        assert set(make.files.keys()) == {path_default}
+
+        class SettingsModel(BaseModel):
+            whatever: Annotated[str, Field(default="whatever")]
+
+        class Settings(SettingsModel, _Settings): ...
+
+        default = SettingsModel(whatever="default")  # type: ignore
+        other = SettingsModel(whatever="other")  # type: ignore
+
+        with open(path_default, "w") as file_default, open(
+            path_other, "w"
+        ) as file_other:
+            yaml.dump(default.model_dump(mode="json"), file_default)
+            yaml.dump(other.model_dump(mode="json"), file_other)
+
+        # ------------------------------------------------------------------- #
+        # NOTE: Actual tests.
+
+        with mock.patch.dict(os.environ, {"FOO_PATH": path_other}, clear=True):
+            filepath_resolved = resolve_filepaths(
+                path_default,
+                make.files[path_default],
+            )
+            assert filepath_resolved == path_other
+
+            # NOTE: Now testing loading.
+            settings = Settings()
+            assert settings.whatever == "other"
+
+        with mock.patch.dict(os.environ, {"FOO_PATH": ""}, clear=True):
+            filepath_resolved = resolve_filepaths(
+                path_default,
+                make.files[path_default],
+            )
+            assert filepath_resolved == path_default
+
+            settings = Settings()
+            assert settings.whatever == "default"
---
+++ packages/yaml-settings-pydantic/yaml_settings_pydantic-2.2.0.tar.gz/yaml_settings_pydantic-2.2.0/tests/test_examples.py
@@ -0,0 +1,120 @@
+import json
+import os
+import subprocess
+from os import path
+from typing import List, Optional
+from unittest import mock
+
+import pytest
+from pydantic_settings import SettingsConfigDict
+
+from .examples import ExplicitSettings, MinimalSettings, SubpathSettings
+
+
+@pytest.mark.parametrize(
+    "Settings", [ExplicitSettings, MinimalSettings, SubpathSettings]
+)
+class TestExampleCanOverWrite:
+    env_extras = dict(
+        MY_SETTINGS_MYFIRSTSETTING="9999",
+        MY_SETTINGS_MYDATABASESETTINGS__HOSTSPEC__HOST="12.34.56.78",
+    )
+
+    def test_init(self, Settings):
+
+        Settings()
+
+        raw = dict(
+            myFirstSetting=1234,
+            myDatabaseSettings=dict(  # type: ignore
+                connectionspec=dict(),
+                hostspec=dict(
+                    username="username",
+                    password="password",
+                ),
+            ),
+        )
+        s = Settings.model_validate(raw)  # type: ignore
+
+        assert s.myFirstSetting == 1234
+        assert (
+            s.myDatabaseSettings.hostspec.username == "username"
+        ), "Failed to load nested configuration."
+
+    @mock.patch.dict(os.environ, **env_extras)
+    def test_envvars(self, Settings):
+        """Environment variables should be able to overwrite YAML
+        configuration."""
+
+        s = Settings()  # type: ignore
+        expected = int(self.env_extras["MY_SETTINGS_MYFIRSTSETTING"])
+        assert s.myFirstSetting == expected
+
+        field = "MY_SETTINGS_MYDATABASESETTINGS__HOSTSPEC__HOST"
+        expected = self.env_extras[field]
+        assert s.myDatabaseSettings.hostspec.host == expected
+
+    @mock.patch.dict(os.environ, **env_extras)
+    def test_envvars_after_init(self, Settings):
+        """Environment variables should take presendence by init."""
+
+        expectedMyFirstSetting = 11111111
+        s = Settings(myFirstSetting=expectedMyFirstSetting)  # type: ignore
+        assert s.myFirstSetting == expectedMyFirstSetting
+
+    def test_dotenv(self, Settings):
+        model_config = SettingsConfigDict(
+            env_prefix="MY_SETTINGS_",
+            env_nested_delimiter="__",
+            env_file=path.join(
+                path.dirname(__file__),
+                "examples",
+                "example.env",
+            ),
+        )
+        namespace = dict(model_config=model_config)
+        SettingsWEnv = type("ExplicitSettingsWEnv", (Settings,), namespace)
+        s = SettingsWEnv()  # type: ignore
+        assert s.myFirstSetting == 8888
+        assert s.myDatabaseSettings.hostspec.host == "5.4.3.2"
+
+    '''
+    def test_file_secret_settings(self) -> None:
+        """Reproduces the functionality described in the
+        `pydantic docs<docs.pydantic.dev/latest/usage/pydantic_settings/>`.
+
+        Will require docker in pipelines.
+        """
+        client: DockerClient = docker.from_env()
+
+        client.containers.run("python:latest", name="ysp-test-container")
+        client.secrets.create(name="ysp-test-secret", data="")
+        '''
+
+
+@pytest.mark.parametrize(
+    "subcommand",
+    [None, "minimal-settings", "explicit-settings", "subpath-settings"],
+)
+def test_example_execution(subcommand: Optional[str]):
+    command = ["python", "-m", "tests.examples"]
+    if subcommand is not None:
+        command.append(subcommand)
+    result = subprocess.run(
+        command,
+        stderr=subprocess.STDOUT,
+        stdout=subprocess.PIPE,
+    )
+    out: str | List[str]
+    assert result.returncode == 0
+    assert (out := result.stdout.decode())
+    assert not result.stderr
+
+    out = out.split("\n")
+    assert "=============" in out[0]
+    assert "=============" in out[-2]
+    assert "Result" in out[1]
+
+    # Verify that the body is valid JSON
+    result = "".join(out[2:-2])
+    result = json.loads(result)
