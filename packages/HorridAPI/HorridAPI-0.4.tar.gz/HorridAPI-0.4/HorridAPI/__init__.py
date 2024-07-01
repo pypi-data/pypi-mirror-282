5a628e8c5d9bc36736ed7ddcffd06245b6b7f10d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 03:03:49 +0000

    Add package HorridAPI/0.4/HorridAPI-0.4.tar.gz

---
+++ packages/HorridAPI/HorridAPI-0.4.tar.gz/HorridAPI-0.4/HorridAPI/__init__.py
@@ -0,0 +1,40 @@
+import requests
+from urllib.parse import quote
+
+__all__ = ["api"]
+
+
+class HorridAPI:
+
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def llama(query: str):
+        """
+        from HorridAPI import api
+
+        K = api.llama("hi")
+        print(K) """
+        prompt = quote(query)
+        api = f'https://horrid-api.onrender.com/llama?query={prompt}'
+        res = requests.get(api).json()
+        k = res['response']
+        return k
+
+    @staticmethod
+    def dare():
+        api = f'https://horrid-api.onrender.com/dare'
+        res = requests.get(api).json()
+        dare = res['dare']
+        return dare
+
+    @staticmethod
+    def truth():
+        api = f'https://horrid-api.onrender.com/truth'
+        res = requests.get(api).json()
+        k = res['truth']
+        return k
+
+
+api = HorridAPI()
