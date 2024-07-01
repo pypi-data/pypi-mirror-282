8367f2d21df949132736807e3ab928fc8ba516da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 03:03:53 +0000

    Add package argrelay/0.7.5.dev2/argrelay-0.7.5.dev2.tar.gz

---
+++ packages/argrelay/argrelay-0.7.5.dev2.tar.gz/argrelay-0.7.5.dev2/src/argrelay/relay_client/RemoteClient.py
@@ -0,0 +1,29 @@
+from argrelay.relay_client.AbstractClient import AbstractClient
+from argrelay.relay_client.RemoteClientCommandFactory import RemoteClientCommandFactory
+from argrelay.runtime_data.ClientConfig import ClientConfig
+
+
+class RemoteClient(AbstractClient):
+    """
+    Client remote to server
+
+    It talks to `CustomFlaskApp` which is remote server in this case.
+
+    See also `LocalClient` and `LocalServer`.
+    """
+
+    def __init__(
+        self,
+        client_config: ClientConfig,
+        is_split_mode: bool,
+        child_pipe_src,
+    ):
+        super().__init__(
+            client_config,
+            RemoteClientCommandFactory(
+                client_config,
+                is_split_mode,
+                child_pipe_src,
+            ),
+        )
+        assert not client_config.use_local_requests
