0f0b2a99cbc50c74c9959e3b740de8dff9bafb3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 16:03:59 +0000

    Add package archive-toolbox/1.0/archive_toolbox-1.0.tar.gz

---
+++ packages/archive-toolbox/archive_toolbox-1.0.tar.gz/archive_toolbox-1.0/archive_toolbox/logic.py
@@ -0,0 +1,54 @@
+import os
+import tarfile
+import time
+from zipfile import ZipFile
+
+from archive_toolbox.utils import get_archive_name, is_zip
+
+
+def extract_archive(filename: str) -> None:
+    archive_name = get_archive_name(filename)
+    
+    if not os.path.exists(archive_name):
+        os.mkdir(archive_name)
+
+    if is_zip(filename):
+        with ZipFile(filename) as archive:
+            for file in archive.namelist():
+                print(f'[+] Extracting {file} from {filename}')
+                archive.extract(file, archive_name)
+    else:
+        with tarfile.open(filename) as archive:
+            for file in archive.getmembers():
+                print(f'[+] Extracting {file.name} from {filename}')
+                archive.extract(file, archive_name)
+
+    print(f"Extracted all files from {filename} to {archive_name}")
+
+
+def show_info_zip(filename: str):
+    with ZipFile(filename) as archive:
+        print(f"Count of files: {len(archive.filelist)}")
+        time.sleep(1)
+        for file in  archive.filelist:
+            print(f'[+] File {file.filename}')
+            print(f'\t- Size {file.file_size}')
+            print(f'\t- Created at {file.date_time[:3]}')
+            print(f'\t- Compress size {file.compress_size}')
+
+
+def show_info_tar(filename: str):
+    with tarfile.open(filename) as archive:
+        print(f"Count of files: {len(archive.getmembers())}")
+        time.sleep(1)
+
+        for file in archive.getmembers():
+            print(f'[+] File {file.name}')
+            print(f'\t- Size {file.size}')
+
+
+def show_info(filename: str):
+    if is_zip(filename):
+        show_info_zip(filename)
+    else:
+        show_info_tar(filename)
---
+++ packages/archive-toolbox/archive_toolbox-1.0.tar.gz/archive_toolbox-1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='archive_toolbox',
+    version='1.0',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'atb = archive_toolbox.main:main',
+        ],
+    },
+    install_requires=[
+        'pyfiglet==1.0.2',
+    ],
+    author='Dementev Maksim',
+    author_email='i@dmaksim.ru',
+    description='A toolbox for managing archives',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/idmaksim/archive_toolbox',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
