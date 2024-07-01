5335d9a93e4b80e8f9127ebed4aed7f6736c6043
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 14:03:44 +0000

    Add package neurocaps/0.13.1/neurocaps-0.13.1.tar.gz

---
+++ packages/neurocaps/neurocaps-0.13.1.tar.gz/neurocaps-0.13.1/neurocaps/_utils/_cap_internals/_cap2statmap.py
@@ -0,0 +1,18 @@
+"""Internal function for turning CAPs into NifTI Statistical Maps"""
+import nibabel as nib, numpy as np
+from nilearn import image
+
+def _cap2statmap(atlas_file, cap_vector, fwhm):
+    atlas = nib.load(atlas_file)
+    atlas_fdata = atlas.get_fdata()
+    # Get array containing all labels in atlas to avoid issue if atlas labels dont start at 1, like Nilearn's AAL map
+    target_array = sorted(np.unique(atlas_fdata))
+    for indx, value in enumerate(cap_vector):
+        actual_indx = indx + 1
+        atlas_fdata[np.where(atlas_fdata == target_array[actual_indx])] = value
+    stat_map = nib.Nifti1Image(atlas_fdata, atlas.affine, atlas.header)
+    # Add smoothing to stat map to help mitigate potential coverage issues
+    if fwhm is not None:
+        stat_map = image.smooth_img(stat_map, fwhm=fwhm)
+
+    return stat_map
