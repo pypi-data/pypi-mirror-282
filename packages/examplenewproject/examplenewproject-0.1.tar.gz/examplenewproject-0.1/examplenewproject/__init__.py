602fbd22de7eaa087cbe976ba33e494eb6670b2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 04:04:44 +0000

    Add package examplenewproject/0.1/examplenewproject-0.1.tar.gz

---
+++ packages/examplenewproject/examplenewproject-0.1.tar.gz/examplenewproject-0.1/examplenewproject/__init__.py
@@ -0,0 +1,5 @@
+# isimproje/__init__.py
+
+from .wrapper import process_files
+
+__all__ = ['process_files']
---
+++ packages/examplenewproject/examplenewproject-0.1.tar.gz/examplenewproject-0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="examplenewproject",
+    version="0.1",
+    packages=find_packages(),
+    description="A Python wrapper for a C++ project",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Vasfi Tataroglu",
+    author_email="testestteestetestes2009@gmail.com",
+    url="https://github.com/brainvs/examplenewproject",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    package_data={
+        'examplenewproject': ['examplenewproject.cpython-311-darwin.so'],
+    },
+)
