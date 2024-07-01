04f1c51d1c8f01aee8b71d7b41a6191b2bce5ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 14:04:19 +0000

    Add package spy-spider/0.1.2/spy_spider-0.1.2.tar.gz

---
+++ packages/spy-spider/spy_spider-0.1.2.tar.gz/spy_spider-0.1.2/README.md
@@ -0,0 +1,14 @@
+#spy_spider
+
+
+~~~
+pip install poetry
+
+poetry init
+
+poetry build
+
+poetry config pypi-token.pypi <your-pypi-token>
+
+poetry publish --build
+~~~
---
+++ packages/spy-spider/spy_spider-0.1.2.tar.gz/spy_spider-0.1.2/spy_spider/__init__.py
@@ -0,0 +1,11 @@
+from spy_spider.spiders import Spider
+from spy_spider.http.request import Request
+# from spy_spider.http.response import Response
+from spy_spider.utils import tools
+
+__all__ = [
+    "Spider",
+    "Request",
+    # "Response",
+    "tools",
+]
