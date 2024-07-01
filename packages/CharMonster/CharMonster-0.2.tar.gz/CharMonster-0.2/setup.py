79d5610fe8e6ea07c3389e616f835904cc7fa3de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 04:05:26 +0000

    Add package CharMonster/0.2/CharMonster-0.2.tar.gz

---
+++ packages/CharMonster/CharMonster-0.2.tar.gz/CharMonster-0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CharMonster',
+    version='0.2',
+    author='James Evans',
+    author_email='joesaysahoy@gmail.com',
+    url='https://github.com/primal-coder/CharMonster',
+    packages=find_packages(),
+    install_requires=[
+        'gridengine_framework',
+        'dicepy',
+        'pymunk',
+        'entyty'
+    ],
+    license='MIT'    
+)
