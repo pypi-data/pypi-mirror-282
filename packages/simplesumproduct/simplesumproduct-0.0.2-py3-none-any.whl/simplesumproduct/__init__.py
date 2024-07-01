89da2504f2415559e52d3a9f879054d8108d07bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 03:03:49 +0000

    Add package simplesumproduct/0.0.2/simplesumproduct-0.0.2-py3-none-any.whl

---
+++ packages/simplesumproduct/simplesumproduct-0.0.2-py3-none-any.whl/simplesumproduct/__init__.py
@@ -0,0 +1,2 @@
+from .sumtool import my_sum, special_sum
+from .producttool import my_product, my_special_product
---
+++ packages/simplesumproduct/simplesumproduct-0.0.2-py3-none-any.whl/simplesumproduct/sumtool.py
@@ -0,0 +1,6 @@
+def my_sum(x,y):
+    return x + y
+
+def special_sum(x,y):
+    return my_sum(x,y) / 10
+
