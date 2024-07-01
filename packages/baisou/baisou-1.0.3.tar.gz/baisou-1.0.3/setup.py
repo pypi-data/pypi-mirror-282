93d91b6af6980597f7db6104ea5ed275599a2b3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 16:04:15 +0000

    Add package baisou/1.0.3/baisou-1.0.3.tar.gz

---
+++ packages/baisou/baisou-1.0.3.tar.gz/baisou-1.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='baisou',
+    version='1.0.3',
+    description='The Baidu Searcher On Python|Python 百度搜索工具',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Python学霸',
+    author_email='python@xueba.com',
+    py_modules=['baisou'],
+    install_requires=['requests'],
+    entry_points={
+        'console_scripts': [
+            'sou=baidou:sou']
+    }
+)
