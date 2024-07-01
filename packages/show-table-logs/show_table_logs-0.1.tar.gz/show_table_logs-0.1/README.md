054e68a2610fbf61b9522dcd504ed00db0d6d9b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 03:03:54 +0000

    Add package show-table-logs/0.1/show_table_logs-0.1.tar.gz

---
+++ packages/show-table-logs/show_table_logs-0.1.tar.gz/show_table_logs-0.1/README.md
@@ -0,0 +1,1 @@
+This is a simple Python library that creates two text files in the root directory of the project. The first file contains "1111" and the second file contains "2222".
---
+++ packages/show-table-logs/show_table_logs-0.1.tar.gz/show_table_logs-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='show_table_logs',
+    version='0.1',
+    packages=find_packages(),
+    description='A library to create two files with specific content',
+    author='SINTT',
+    author_email='bbhh66606@gmail.com',
+    install_requires=[],
+)
