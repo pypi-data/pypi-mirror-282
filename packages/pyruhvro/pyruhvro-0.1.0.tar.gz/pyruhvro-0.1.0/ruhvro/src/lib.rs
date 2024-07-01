4001c99fe4a56fd459f6f6cba432b21233ce9208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 04:05:42 +0000

    Add package pyruhvro/0.1.0/pyruhvro-0.1.0.tar.gz

---
+++ packages/pyruhvro/pyruhvro-0.1.0.tar.gz/pyruhvro-0.1.0/ruhvro/src/lib.rs
@@ -0,0 +1,44 @@
+#![warn(clippy::pedantic)]
+//! Crate to deserialize avro and return arrow.
+//!
+//! This library was made to enable quick efficient transformation of schemaless encoded avro
+//! data into arrow record batches.
+//!
+
+
+/// Converts Avro to Arrow
+/// Decode Avro data returning an Arrow Record Batch.
+/// ## Example
+/// ```
+/// let raw_schema = r#"
+///             {
+///                 "type": "record",
+///                 "name": "test",
+///                 "fields": [
+///                     {"name": "a", "type": "long", "default": 42},
+///                     {"name": "b",
+///                         "type": ["null", "string"],
+///                         "default": null
+///             }
+///                 ]
+///             }
+///         "#;
+///
+/// let parsed_schema = ruhvro::deserialize::parse_schema(&raw_schema).unwrap();
+/// // create a record
+/// let mut record = apache_avro::types::Record::new(&parsed_schema).unwrap();
+/// record.put("a", 27i64);
+/// record.put("b", None::<String>);
+/// // and serialize it
+/// let serialized = apache_avro::to_avro_datum(&parsed_schema, record).unwrap();
+/// // verify deserialization
+/// let result = ruhvro::deserialize::per_datum_deserialize(&vec![&serialized[..]], &parsed_schema);
+/// println!("{:?}", result);
+///```
+pub mod deserialize;
+mod schema_translate;
+mod complex;
+
+#[cfg(test)]
+mod tests {
+}
---
+++ packages/pyruhvro/pyruhvro-0.1.0.tar.gz/pyruhvro-0.1.0/src/lib.rs
@@ -0,0 +1,46 @@
+//! Python extensions for transforming a vector of avro encoded binary data to an
+//! apache arrow record batch
+//!
+use arrow::array::RecordBatch;
+use arrow::pyarrow::PyArrowType;
+use pyo3::prelude::*;
+use pyo3::types::PyList;
+use ruhvro::deserialize;
+
+#[pyfunction]
+fn deserialize_array(list: &PyList, schema: &str) -> PyResult<PyArrowType<RecordBatch>> {
+    let parsed_schema = deserialize::parse_schema(schema).unwrap();
+    let borrow_list = list
+        .iter()
+        .map(|x| x.extract::<&[u8]>().unwrap())
+        .collect::<Vec<_>>();
+    let record_batch = deserialize::per_datum_deserialize(&borrow_list, &parsed_schema);
+    Ok(PyArrowType(record_batch))
+}
+#[pyfunction]
+fn deserialize_array_threaded(
+    list: &PyList,
+    schema: &str,
+    num_chunks: usize,
+) -> PyResult<Vec<PyArrowType<RecordBatch>>> {
+    let parsed_schema = deserialize::parse_schema(schema).unwrap();
+    let borrow_list = list
+        .iter()
+        .map(|x| x.extract::<&[u8]>().unwrap())
+        .collect::<Vec<_>>();
+    let record_batches =
+        deserialize::per_datum_deserialize_arrow_multi(borrow_list, &parsed_schema, num_chunks);
+    let python_typed_batches = record_batches
+        .into_iter()
+        .map(|x| PyArrowType(x))
+        .collect::<Vec<_>>();
+    Ok(python_typed_batches)
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn pyruhvro(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(deserialize_array, m)?)?;
+    m.add_function(wrap_pyfunction!(deserialize_array_threaded, m)?)?;
+    Ok(())
+}
