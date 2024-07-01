a14ba857571143f6f032bc074cde5a8d63563198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 14:03:50 +0000

    Add package fs-rclone/0.1.1/fs_rclone-0.1.1-py3-none-any.whl

---
+++ packages/fs-rclone/fs_rclone-0.1.1-py3-none-any.whl/fs_rclone/__init__.py
@@ -0,0 +1,13 @@
+# fs-pyfilesystem
+# __init__.py
+
+__version__ = "0.1.1"
+__author__ = "Raygan Henley"
+__author_email__ = "raygan@raygan.com"
+__license__ = "MIT"
+
+# 0.0.0
+# package for distribution
+
+# 0.0.1
+# fixed bug in `example.py::add_one`
