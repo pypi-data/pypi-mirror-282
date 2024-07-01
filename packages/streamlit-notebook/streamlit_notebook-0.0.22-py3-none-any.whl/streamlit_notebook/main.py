2504a3dfeee695494442b5b69ef0d1a4bf8b599d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 04:05:00 +0000

    Add package streamlit-notebook/0.0.22/streamlit_notebook-0.0.22-py3-none-any.whl

---
+++ packages/streamlit-notebook/streamlit_notebook-0.0.22-py3-none-any.whl/streamlit_notebook/main.py
@@ -0,0 +1,10 @@
+"""
+Main streamlit script of the notebook app
+"""
+
+import streamlit as st
+from streamlit_notebook import st_notebook
+
+st.set_page_config(layout="centered",initial_sidebar_state="collapsed")
+
+st_notebook()
