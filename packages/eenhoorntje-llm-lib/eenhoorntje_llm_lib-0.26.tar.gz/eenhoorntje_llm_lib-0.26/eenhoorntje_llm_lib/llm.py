aeaf5f9ee488b550ddd7dea4afc97bd2353e6705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 14:04:12 +0000

    Add package eenhoorntje-llm-lib/0.26/eenhoorntje_llm_lib-0.26.tar.gz

---
+++ packages/eenhoorntje-llm-lib/eenhoorntje_llm_lib-0.26.tar.gz/eenhoorntje_llm_lib-0.26/eenhoorntje_llm_lib/llm.py
@@ -0,0 +1,48 @@
+from openai import OpenAI
+import json
+import eenhoorntje_llm_lib.llm_cache
+import eenhoorntje_llm_lib.utils
+import time
+
+
+def clear_cache():
+    eenhoorntje_llm_lib.llm_cache.cache.clear()
+
+
+def call_llm(messages, model="anthropic/claude-3-haiku", temperature=0.0, max_tokens=500):
+    api_key = eenhoorntje_llm_lib.utils.get_key("OPEN_ROUTER_API_KEY")
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key,
+    )
+
+    cache_key = None
+    if temperature == 0.0:
+        cache_key = model + "\n" + json.dumps(messages, ensure_ascii=False, indent=4) + "\nMax tokens: " + str(
+            max_tokens)
+        output = eenhoorntje_llm_lib.llm_cache.cache.query(cache_key)
+        if output:
+            return output
+
+    for i in range(8):
+        try:
+            completion = client.chat.completions.create(
+                model=model,
+                messages=messages,
+                temperature=temperature,
+                max_tokens=max_tokens
+            )
+
+            if completion.choices is None:
+                return None
+
+            res = completion.choices[0].message.content
+
+            if cache_key:
+                eenhoorntje_llm_lib.llm_cache.cache.add(cache_key, res)
+            return res
+        except Exception as e:
+            print(e)
+            print(f"Retrying in {2 ** i} seconds...")
+            time.sleep(2 ** i)
+    return None
