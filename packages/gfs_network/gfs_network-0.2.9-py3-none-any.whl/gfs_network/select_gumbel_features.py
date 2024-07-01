a8e6008d4c359fec886c99711d3271e40bc00cad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 15:04:58 +0000

    Add package gfs_network/0.2.9/gfs_network-0.2.9-py3-none-any.whl

---
+++ packages/gfs_network/gfs_network-0.2.9-py3-none-any.whl/gfs_network/select_gumbel_features.py
@@ -0,0 +1,57 @@
+from .feature_selection_network import FeatureSelectionNetwork, gumbel_sigmoid
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import tqdm
+
+def select_gumbel_features(X_train, y_train, device="cpu", verbose=False, temperature_decay=0.997, epochs=300, batch_size=1, fs_balance=1.0):
+    balance = (y_train.sum(axis=0) / len(y_train)).flip(0)
+
+    network = FeatureSelectionNetwork(X_train.shape[1], 32, len(balance)).to(device)
+
+    trainloader = DataLoader(list(zip(X_train, y_train)), batch_size=batch_size)
+
+    criterion = nn.CrossEntropyLoss(weight=balance.to(torch.float32).to(device))
+    optimizer = optim.Adam([
+        {"params": network.fc1.parameters(), "lr": 4e-3},
+        {"params": network.cont.parameters(), "lr": 3e-4},
+    ])
+    temperature = 2.0
+
+    for epoch in tqdm.trange(epochs, disable=not verbose):
+        total = 0
+        correct = 0
+        epoch_loss = 0
+        for data in trainloader:
+            X_, y_ = data
+            X_, y_ = X_.to(device), y_.to(device).to(torch.float32)
+
+            optimizer.zero_grad()
+            output, selected_no = network(X_.float(), temperature=temperature)
+            loss = criterion(output, y_.argmax(axis=1)) + selected_no * fs_balance
+            loss.backward()
+            optimizer.step()
+            epoch_loss += loss.item()
+
+            predicted = torch.argmax(output, axis=1)
+            real = torch.argmax(y_, axis=1)
+            total += y_.size(0)
+            correct += (predicted == real).sum().item()
+            
+            if epoch > epochs:
+                temperature = 0
+                network.fc1.requires_grad_(False)
+                network.emb.requires_grad_(False)
+        temperature = temperature * temperature_decay
+
+    features_sum = torch.zeros(X_train.shape[1], dtype=torch.int32)
+    for data in trainloader:
+        X_, y_ = data
+        X_, y_ = X_.to(device), y_.to(device).to(torch.float32)
+        features = network.fc1(network.emb)
+        features = gumbel_sigmoid(features, tau=0, hard=True)
+        features = features.cpu().detach().numpy()
+        features_sum += features.sum(axis=0)
+        
+    return features_sum.numpy(), network
