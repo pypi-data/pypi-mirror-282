735c8a33b2351c724e66592e644b7a4e59776df5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 14:04:18 +0000

    Add package ncuphy/0.1.4/ncuphy-0.1.4.tar.gz

---
+++ packages/ncuphy/ncuphy-0.1.4.tar.gz/ncuphy-0.1.4/ncuphy/experiment/example/__init__.py
@@ -0,0 +1,11 @@
+import os
+
+# Create a directory for the example
+print(os.getcwd())
+# if not os.path.exists('examples'):
+#     os.makedirs('examples')
+    
+# # Create a file for the example
+# with open('.stepper_motor.py', 'r') as f:
+#     print(f.read())
+    
