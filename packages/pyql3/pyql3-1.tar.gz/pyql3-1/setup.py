66f99d824fc724c36491ad8160e890abc0ab00b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 04:05:13 +0000

    Add package pyql3/1/pyql3-1.tar.gz

---
+++ packages/pyql3/pyql3-1.tar.gz/pyql3-1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+name = 'pyql3'
+version = '1'
+author = 'GooseG4G'
+email = 'danilanegusev@gmail.com'
+desc = 'The package provides a wrapper for working with SQLite databases.'
+url = 'https://github.com/GooseG4G/pyql3'
+packages = ['pyql3']
+requires = []
+
+setup(
+    name=name,
+    version=version,
+    author=author,
+    author_email=email,
+    description=desc,
+    url=url,
+    packages=packages,
+    install_requires=requires
+)
