e29fc84f8265e7d63bdd3899af603f313f2d5f15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 15:04:52 +0000

    Add package pms-encoder/1.0.6/pms_encoder-1.0.6.tar.gz

---
+++ packages/pms-encoder/pms_encoder-1.0.6.tar.gz/pms_encoder-1.0.6/pms_encoder/common/_redis_client.py
@@ -0,0 +1,15 @@
+import redis
+import os
+
+class RedisClient:
+    _instance = None
+    
+    @staticmethod
+    def get_instance():
+        if RedisClient._instance is None:
+            RedisClient._instance = redis.StrictRedis(
+                host=os.getenv("REDIS_HOST"),
+                password=os.getenv("REDIS_PASSWORD"),
+            )
+        
+        return RedisClient._instance
