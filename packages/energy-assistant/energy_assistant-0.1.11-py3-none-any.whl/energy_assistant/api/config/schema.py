f68b3861a96b5a4e99982633bfbfb5273ca27ba0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 04:05:26 +0000

    Add package energy-assistant/0.1.11/energy_assistant-0.1.11-py3-none-any.whl

---
+++ packages/energy-assistant/energy_assistant-0.1.11-py3-none-any.whl/energy_assistant/api/config/schema.py
@@ -0,0 +1,13 @@
+"""Schemas for home measurement api."""
+
+from pydantic import BaseModel
+
+
+class ConfigModel(BaseModel):
+    """Schema for the configuration."""
+
+    config: dict
+
+
+class ReadConfigResponse(ConfigModel):
+    """API Response for reading the configuration."""
