97a569167c66f9c86359b3a52a2b166640700728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 03:03:52 +0000

    Add package HorridAPI/0.5/HorridAPI-0.5.tar.gz

---
+++ packages/HorridAPI/HorridAPI-0.5.tar.gz/HorridAPI-0.5/HorridAPI/__init__.py
@@ -0,0 +1,55 @@
+import requests
+from urllib.parse import quote
+
+__all__ = ["api"]
+
+
+class HorridAPI:
+
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def llama(query: str):
+        """
+        from HorridAPI import api
+
+        K = api.llama("hi")
+        print(K) """
+        prompt = quote(query)
+        api = f'https://horrid-api.onrender.com/llama?query={prompt}'
+        res = requests.get(api).json()
+        k = res['response']
+        return k
+
+    @staticmethod
+    def dare():
+        api = f'https://horrid-api.onrender.com/dare'
+        res = requests.get(api).json()
+        dare = res['dare']
+        return dare
+
+    @staticmethod
+    def truth():
+        api = f'https://horrid-api.onrender.com/truth'
+        res = requests.get(api).json()
+        k = res['truth']
+        return k
+
+    @staticmethod
+    def gpt(query: str):
+        prompt = quote(query)
+        api = f'https://horrid-api.onrender.com/gpt?query={prompt}'
+        res = requests.get(api).json()
+        k = res['response']
+        return k
+
+    @staticmethod
+    def joke():
+        api = f'https://horrid-api.onrender.com/joke'
+        res = requests.get(api).json()
+        joke = res['joke']
+        return joke
+
+
+api = HorridAPI()
