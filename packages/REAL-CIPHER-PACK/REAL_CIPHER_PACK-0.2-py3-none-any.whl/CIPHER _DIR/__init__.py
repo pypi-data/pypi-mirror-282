607408f45e3725c707714a4ca489c704662f1087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 03:04:03 +0000

    Add package REAL-CIPHER-PACK/0.2/REAL_CIPHER_PACK-0.2-py3-none-any.whl

---
+++ packages/REAL-CIPHER-PACK/REAL_CIPHER_PACK-0.2-py3-none-any.whl/CIPHER _DIR/__init__.py
@@ -0,0 +1,14 @@
+# cipher_package/__init__.py
+
+# Print a message when the package is imported
+print("cipher_package is imported. Use 'from Caesar_Cipher_try.cipher_proj import CaesarCipher' to import the CaesarCipher class.")
+
+# Provide a comment with instructions
+"""
+To use the CaesarCipher class, import it as follows:
+from cipher_package.ceaser import CaesarCipher
+"""
+
+from REAL_CIPHER_PACK.CIPHER _DIR.Real_Cipher import CaesarCipher
+
+__all__ = ['CaesarCipher']
