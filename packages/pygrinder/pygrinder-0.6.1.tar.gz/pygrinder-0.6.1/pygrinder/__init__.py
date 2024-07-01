9f9f21a84408d52c21e179b5e2d83ce256042337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 14:03:46 +0000

    Add package pygrinder/0.6.1/pygrinder-0.6.1.tar.gz

---
+++ packages/pygrinder/pygrinder-0.6.1.tar.gz/pygrinder-0.6.1/pygrinder/__init__.py
@@ -0,0 +1,55 @@
+"""
+PyGrinder: a Python toolkit for grinding data beans into the incomplete.
+"""
+
+# Created by Wenjie Du <wenjay.du@gmail.com>
+# License: BSD-3-Clause
+
+# PyGrinder version
+#
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+# Generic release markers:
+# X.Y
+# X.Y.Z # For bugfix releases
+#
+# Admissible pre-release markers:
+# X.YaN # Alpha release
+# X.YbN # Beta release
+# X.YrcN # Release Candidate
+# X.Y # Final release
+#
+# Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
+# 'X.Y.dev0' is the canonical version of 'X.Y.dev'
+__version__ = "0.6.1"
+
+from .missing_at_random import mar_logistic
+from .missing_completely_at_random import mcar, mcar_little_test
+from .missing_not_at_random import mnar_x, mnar_t
+from .randomly_drop_observations import rdo
+from .sequential_missing import seq_missing
+from .block_missing import block_missing
+from .utils import (
+    calc_missing_rate,
+    masked_fill,
+    fill_and_get_mask,
+    fill_and_get_mask_torch,
+    fill_and_get_mask_numpy,
+)
+
+__all__ = [
+    "__version__",
+    "mcar",
+    "mcar_little_test",
+    "mar_logistic",
+    "mnar_x",
+    "mnar_t",
+    "rdo",
+    "seq_missing",
+    "block_missing",
+    "calc_missing_rate",
+    "masked_fill",
+    "fill_and_get_mask",
+    "fill_and_get_mask_torch",
+    "fill_and_get_mask_numpy",
+]
