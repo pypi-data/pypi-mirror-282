0a8db7f807260ae49422dcb76c3d4a5d4c46499a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 15:04:04 +0000

    Add package split-lang/1.0.1/split_lang-1.0.1-py3-none-any.whl

---
+++ packages/split-lang/split_lang-1.0.1-py3-none-any.whl/tests/test_split_to_substrings.py
@@ -0,0 +1,49 @@
+from split_lang import split_by_lang
+from tests.data.generate_test_json import texts_de_fr_en, texts_zh_jp_ko_en
+from split_lang.split.utils import DEFAULT_THRESHOLD
+
+
+new_lang_map = {
+    "zh": "zh",
+    "zh-cn": "zh",
+    "zh-tw": "x",
+    "ko": "ko",
+    "ja": "ja",
+}
+
+
+def test_split_to_substring():
+    for text in texts_zh_jp_ko_en:
+        substr = split_by_lang(
+            text=text,
+            verbose=False,
+            lang_map=new_lang_map,
+            threshold=DEFAULT_THRESHOLD,
+            default_lang="en",
+            merge_across_punctuation=True,
+        )
+        for index, item in enumerate(substr):
+            print(item)
+            # print(f"{index}|{item.lang}:{item.text}")
+        print("----------------------")
+
+    for text in texts_de_fr_en:
+        substr = split_by_lang(
+            text=text,
+            verbose=False,
+            # lang_map=new_lang_map,
+            threshold=DEFAULT_THRESHOLD,
+            default_lang="en",
+        )
+        for index, item in enumerate(substr):
+            print(item)
+            # print(f"{index}|{item.lang}:{item.text}")
+        print("----------------------")
+
+
+def main():
+    test_split_to_substring()
+
+
+if __name__ == "__main__":
+    main()
