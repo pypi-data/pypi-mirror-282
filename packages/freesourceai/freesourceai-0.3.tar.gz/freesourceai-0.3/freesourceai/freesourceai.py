994740c7de859b6af3aaab2ebed6a2107afd7347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 16:04:04 +0000

    Add package freesourceai/0.3/freesourceai-0.3.tar.gz

---
+++ packages/freesourceai/freesourceai-0.3.tar.gz/freesourceai-0.3/freesourceai/freesourceai.py
@@ -0,0 +1,79 @@
+import aiohttp
+import asyncio
+import json
+
+api_url = 'https://reverse.mubi.tech/v1/chat/completions'
+api_url1 = 'https://reverse.mubi.tech/v1/images/generations'
+
+headers = {
+    'Content-Type': 'application/json',
+    'Origin': 'https://gptcall.net/',
+    'Referer': 'https://gptcall.net/'
+}
+
+headers1 = {
+    'Content-Type': 'application/json',
+}
+
+async def generate_text_on_script_no_command(text):
+    data = {
+        'model': 'gpt-4',
+        'messages': [
+            {
+                'role': "assistant",
+                'content': "Assistant",
+            },
+            {
+                'role': "user",
+                'content': text
+            }
+        ]
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.post(api_url, json=data, headers=headers) as response:
+            json_data = await response.json()
+            result = json_data['choices'][0]['message']['content']
+            return result
+            
+def generate(text):
+    asyncio.run(generate_text_on_script_no_command(text))
+
+async def is_safe_text(text):
+    data = {
+        'model': 'gpt-4',
+        'messages': [
+            {
+                'role': "system",
+                'content': "Ты AI ассистент, который определяет, является ли текст 18+ или нет. Ответь одним словом без символов и букв верхнего регистра 'да' если текст безопасен и 'нет' если текст 18+."
+            },
+            {
+                'role': "user",
+                'content': text
+            }
+        ]
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.post(api_url, json=data, headers=headers) as response:
+            json_data = await response.json()
+            result = json_data['choices'][0]['message']['content']
+            return result.strip().lower() == 'да'
+
+async def generate_image_on_script_no_command(text):
+    data1 = {
+        'model': 'dreamshaper-8',
+        'prompt': text
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.post(api_url1, json=data1, headers=headers1) as response:
+            json_data = await response.json()
+            result = json_data['data'][0]['url']
+            return result
+
+def image(text):
+    safe = asyncio.run(is_safe_text(text))
+    if not safe:
+        raise ValueError("Input text contains explicit content. Image generation aborted.")
+    else:
+	    asyncio.run(generate_image_on_script_no_command(text))
+
+all = ["generate", "image"]
---
+++ packages/freesourceai/freesourceai-0.3.tar.gz/freesourceai-0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup( 
+    name='freesourceai', 
+    version='0.3', 
+    description='FreeSourceAI(FreeGPT-4o) - ChatGPT without API Key.Creator - Telegram: @ruslanfreegpt.', 
+    author='FreeGPT', 
+    author_email='projectsruslans@gmail.com', 
+    packages=['freesourceai'], 
+    install_requires=[ 
+        'aiohttp', 
+    ], 
+) 
