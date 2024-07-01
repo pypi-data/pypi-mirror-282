d5ace7213485832c59df3f241990f3a4873df348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 14:04:16 +0000

    Add package python-logic/0.0.1/python_logic-0.0.1.tar.gz

---
+++ packages/python-logic/python_logic-0.0.1.tar.gz/python_logic-0.0.1/tests/test_fol.py
@@ -0,0 +1,258 @@
+from pylogic.fol import (
+    HornClauseFOL,
+    Predicate,
+    Substitution,
+    Term,
+    TermType,
+    standardize_predicate,
+    standardize_variables,
+    unify,
+    unify_var,
+    fol_fc_ask,
+    compose,
+)
+import pytest
+
+
+def test_predicate():
+    x = Term("x", TermType.VARIABLE)
+    c = Term("John", TermType.CONSTANT)
+    p = Predicate("Admires", (x, c))
+    assert p.__repr__() == (
+        "Admires(Term(x, type=TermType.VARIABLE), "
+        "Term(John, type=TermType.CONSTANT))"
+    )
+    p = Predicate("King", (x,))
+    assert (~p).__repr__() == "¬King(Term(x, type=TermType.VARIABLE))"
+    cp = Term("John", TermType.CONSTANT)
+    assert c == cp
+    p1 = Predicate("Admires", (x, c))
+    p2 = Predicate("Admires", (Term("x", TermType.VARIABLE), cp))
+    assert p1 == p2
+
+
+def test_substitution():
+    x = Term("x", TermType.VARIABLE)
+    term = Term("Diego", TermType.CONSTANT)
+    p = Predicate("King", (x,))
+    sub = Substitution({x: term})
+
+    result = sub.substitute(p)
+    expected = Predicate("King", (term,))
+    assert result == expected
+
+    y = Term("x", TermType.VARIABLE)
+    a = Term("John", TermType.CONSTANT)
+    assert y in sub
+    assert a not in sub
+    assert sub[x] == Term("Diego", TermType.CONSTANT)
+
+    y = Term("y", TermType.VARIABLE)
+    assert Term("y", TermType.VARIABLE) in sub.add_substitutions({y: a})
+    assert sub.add_substitutions({y: a})[Term("y", TermType.VARIABLE)] == Term(
+        "John", TermType.CONSTANT
+    )
+
+
+def test_horn_clause_fol():
+    x = Term("x", TermType.VARIABLE)
+    p1 = Predicate("King", (x,))
+    p2 = Predicate("Greedy", (x,))
+    b = Predicate("Evil", (x,))
+
+    with pytest.raises(HornClauseFOL.BadHornClauseFOL):
+        HornClauseFOL([p1, ~p2], b)
+
+    hc = HornClauseFOL([p1, p2], b)
+    assert repr(hc) == (
+        "Greedy(Term(x, type=TermType.VARIABLE)) ^ "
+        "King(Term(x, type=TermType.VARIABLE)) => "
+        "Evil(Term(x, type=TermType.VARIABLE))"
+    )
+    assert hc == HornClauseFOL([p2, p1], b)
+    hc = HornClauseFOL([p1, p2], ~b)
+    assert hc.consequent is False
+    assert hc == HornClauseFOL([p1, b, p2], None)
+
+
+def test_unify_var():
+    x = Term("x", TermType.VARIABLE)
+    y = Term("y", TermType.VARIABLE)
+    s = Substitution({})
+    expected = Substitution(
+        {Term("x", TermType.VARIABLE): Term("y", TermType.VARIABLE)}
+    )
+    result = unify_var(x, y, s)
+    assert expected == result
+
+
+def test_unify():
+    x = Term("x", TermType.VARIABLE)
+    y = Term("x", TermType.VARIABLE)
+    a = Term("A", TermType.CONSTANT)
+    s1 = Substitution({Term("z", TermType.VARIABLE): Term("A", TermType.CONSTANT)})
+    s2 = Substitution({Term("z", TermType.VARIABLE): a})
+    assert unify(x, y, s1) == s2
+    assert unify(x, y, None) is None
+    assert unify(x, a, Substitution({})) == Substitution({x: a})
+    assert unify(a, y, Substitution({})) == Substitution({y: a})
+    assert unify(x, [a], Substitution({})) is None
+
+    x = Term("x", TermType.VARIABLE)
+    y = Term("y", TermType.VARIABLE)
+    z = Term("z", TermType.VARIABLE)
+    a = Term("A", TermType.CONSTANT)
+    b = Term("B", TermType.CONSTANT)
+
+    expected = Substitution(
+        {
+            x: a,
+            y: b,
+            z: a,
+        }
+    )
+    result = unify([x, b, z], [a, y, x], Substitution({}))
+    assert expected == result
+
+
+# TODO: Fix test once the implementation is correct
+def test_standardize_predicate():
+    x = Term("x", TermType.VARIABLE)
+    j = Term("John", TermType.CONSTANT)
+    e = Term("Elizabeth", TermType.CONSTANT)
+    p1 = Predicate("Knows", [j, x])
+    p2 = Predicate("Knows", [x, e])
+    theta = Substitution({})
+    assert unify(p1.args, p2.args, theta) is None
+
+    p1_new, counter, seen = standardize_predicate(p1, 0, {})
+    p2_new, counter, seen = standardize_predicate(p2, counter, {})
+
+    # Counter should be in one as we replaced two variables
+    assert counter == 2
+
+    assert seen == {"x": 1}
+
+    # Unification should not fail
+    assert unify(p1_new.args, p2_new.args, theta) == Substitution(
+        {Term("x1", type=TermType.VARIABLE): j, Term("x0", type=TermType.VARIABLE): e}
+    )
+
+
+def test_standardize_variables():
+    x = Term("x", TermType.VARIABLE)
+    y = Term("y", TermType.VARIABLE)
+    a = Term("A", TermType.CONSTANT)
+    p1 = Predicate("Knows", [x, y])
+    p2 = Predicate("Friends", [x, y])
+    hc = HornClauseFOL([p1], p2)
+    new_hc, counter = standardize_variables(hc, 0)
+    assert counter == 2
+    assert new_hc == HornClauseFOL(
+        [
+            Predicate(
+                "Knows", [Term("x0", TermType.VARIABLE), Term("y1", TermType.VARIABLE)]
+            )
+        ],
+        Predicate(
+            "Friends", [Term("x0", TermType.VARIABLE), Term("y1", TermType.VARIABLE)]
+        ),
+    )
+
+    p1 = Predicate("Knows", [x, a])
+    p2 = Predicate("Friends", [x, a])
+    hc = HornClauseFOL([p1], p2)
+    new_hc, counter = standardize_variables(hc, 0)
+    assert counter == 1
+    assert new_hc == HornClauseFOL(
+        [Predicate("Knows", [Term("x0", TermType.VARIABLE), a])],
+        Predicate("Friends", [Term("x0", TermType.VARIABLE), a]),
+    )
+
+    p2 = Predicate("Friends", [x, a])
+    hc = HornClauseFOL([p1], False)
+    new_hc, counter = standardize_variables(hc, 0)
+    assert counter == 1
+    assert new_hc == HornClauseFOL(
+        [Predicate("Knows", [Term("x0", TermType.VARIABLE), a])], False
+    )
+
+
+def test_fol_fc_ask():
+    # No function symbols, so this KB is of class Datalog
+    x = Term("x", TermType.VARIABLE)
+    y = Term("y", TermType.VARIABLE)
+    z = Term("z", TermType.VARIABLE)
+
+    nono = Term("Nono", TermType.CONSTANT)
+    west = Term("West", TermType.CONSTANT)
+    m1 = Term("M1", TermType.CONSTANT)
+    america = Term("America", TermType.CONSTANT)
+
+    p1 = Predicate("American", [x])
+    p2 = Predicate("Weapon", [y])
+    p3 = Predicate("Sells", [x, y, z])
+    p4 = Predicate("Hostile", [z])
+    p5 = Predicate("Criminal", [x])
+    p6 = Predicate("Owns", [nono, m1])
+    p7 = Predicate("Missile", [m1])
+    p8 = Predicate("Missile", [x])
+    p9 = Predicate("Owns", [nono, x])
+    p10 = Predicate("Sells", [west, x, nono])
+    p11 = Predicate("Weapon", [x])
+    p12 = Predicate("Enemy", [x, america])
+    p13 = Predicate("American", [west])
+    p14 = Predicate("Enemy", [nono, america])
+
+    hc1 = HornClauseFOL([p1, p2, p3, p4], p5)
+    hc2 = HornClauseFOL([], p6)
+    hc3 = HornClauseFOL([], p7)
+    hc4 = HornClauseFOL([p8, p9], p10)
+    hc5 = HornClauseFOL([p8], p11)
+    hc6 = HornClauseFOL([p12], Predicate("Hostile", [x]))
+    hc7 = HornClauseFOL([], p13)
+    hc8 = HornClauseFOL([], p14)
+
+    kb = [
+        hc1,
+        hc2,
+        hc3,
+        hc4,
+        hc5,
+        hc6,
+        hc7,
+        hc8,
+    ]
+
+    result = fol_fc_ask(kb, Predicate("Criminal", [west]))
+    expected = Substitution(
+        {
+            Term("x0", type=TermType.VARIABLE): Term("West", type=TermType.CONSTANT),
+            Term("y2", type=TermType.VARIABLE): Term("M1", type=TermType.CONSTANT),
+            Term("z1", type=TermType.VARIABLE): Term("Nono", type=TermType.CONSTANT),
+        }
+    )
+    assert expected == result
+
+
+def test_compose():
+    s1 = Substitution(
+        {
+            Term("x0", type=TermType.VARIABLE): Term("West", type=TermType.CONSTANT),
+            Term("y2", type=TermType.VARIABLE): Term("M1", type=TermType.CONSTANT),
+            Term("z1", type=TermType.VARIABLE): Term("Nono", type=TermType.CONSTANT),
+        }
+    )
+    s2 = Substitution(
+        {
+            Term("w3", type=TermType.VARIABLE): Term("M2", type=TermType.CONSTANT),
+        }
+    )
+    assert compose(s1, s2) == Substitution(
+        {
+            Term("x0", type=TermType.VARIABLE): Term("West", type=TermType.CONSTANT),
+            Term("y2", type=TermType.VARIABLE): Term("M1", type=TermType.CONSTANT),
+            Term("z1", type=TermType.VARIABLE): Term("Nono", type=TermType.CONSTANT),
+            Term("w3", type=TermType.VARIABLE): Term("M2", type=TermType.CONSTANT),
+        }
+    )
---
+++ packages/python-logic/python_logic-0.0.1.tar.gz/python_logic-0.0.1/tests/test_propositional.py
@@ -0,0 +1,352 @@
+from functools import reduce
+from typing import List
+from pylogic.propositional import (
+    BadHornClause,
+    BicondClause,
+    Clause,
+    CnfParser,
+    DpllKB,
+    HornClause,
+    ResolutionKB,
+    Variable,
+    dpll_satisfiable,
+    find_clause_symbols,
+    find_pure_symbol,
+    find_unit_clause,
+    pl_fc_entails,
+    to_cnf,
+    CnfClause,
+)
+import pytest
+
+p = Variable("P", True)
+q = Variable("Q", True)
+r = Variable("R", True)
+a = Variable("A", True)
+b = Variable("B", True)
+c = Variable("C", True)
+d = Variable("D", True)
+
+
+def test_to_cnf():
+    assert to_cnf((a & b & c & p) | q) == (a | q) & (b | q) & (c | q) & (p | q)
+    assert to_cnf(~((~p > ~q) & ~r)) == (~p | r) & (q | r)
+    assert to_cnf((a | b) | (~a & ~b & c & p)) == (a | b | ~a) & (a | b | ~b) & (
+        a | b | c
+    ) & (a | b | p)
+    assert to_cnf((a & b & c & p) | q) == (a | q) & (b | q) & (c | q) & (p | q)
+    assert to_cnf((a | b | c | p) & q) == (a | b | c | p) & q
+    assert str(to_cnf(BicondClause(a | b, c & d))) == str(
+        (a | b | ~a)
+        & (a | b | ~b)
+        & (a | b | ~c | ~d)
+        & (c | ~a)
+        & (c | ~b)
+        & (d | ~a)
+        & (d | ~b)
+        & (c | ~c | ~d)
+        & (d | ~c | ~d)
+    )
+
+
+def test_cnf_class():
+    p = Variable("P", True)
+    q = Variable("Q", True)
+    r = Variable("R", True)
+    a = Variable("A", True)
+    c1 = CnfClause({p, q, a})
+    c2 = CnfClause({r, ~q})
+    assert q in c1
+    assert c1.resolve(c2, q) == CnfClause({p, r, a})
+
+    # We don't desire to mutate any of the clauses
+    assert q in c1 and ~q in c2
+
+
+def test_pl_kb():
+    kb = ResolutionKB()
+    assert len(kb.clauses) == 0
+
+    kb.add(p | q | a)
+    assert kb.clauses == {CnfClause({p, q, a})}
+    kb.add([p | q, r])
+    assert kb.clauses == {CnfClause({p, q, a}), CnfClause({p, q}), CnfClause({r})}
+
+
+def test_cnf_parser():
+    B11 = Variable("B11", True)
+    P12 = Variable("P12", True)
+    P21 = Variable("P21", True)
+
+    # Example from AIMA book
+    cnf = to_cnf(BicondClause(B11, (P12 | P21)))
+    parser = CnfParser()
+    assert (
+        len(
+            parser.parse(cnf).intersection(
+                {
+                    CnfClause({B11, ~P21}),
+                    CnfClause({B11, ~P12}),
+                    CnfClause({P21, ~B11, P12}),
+                }
+            )
+        )
+        == 3
+    )
+
+    # Extra checking to confirm hashing works well
+    assert parser.parse(cnf).issubset(
+        {
+            CnfClause({B11, ~P21}),
+            CnfClause({B11, ~P12}),
+            CnfClause({P21, ~B11, P12}),
+        }
+    )
+
+
+def test_pl_resolution():
+    # Example from AIMA
+    B11 = Variable("B11", True)
+    P12 = Variable("P12", True)
+    P21 = Variable("P21", True)
+    cnf = to_cnf(BicondClause(B11, (P12 | P21)) & ~B11)
+    parser = CnfParser()
+    clauses = parser.parse(cnf)
+    kb = ResolutionKB(clauses)
+    assert kb.query(Variable("P12", False))
+
+
+def test_previous_bug_1():
+    parser = CnfParser()
+    result = to_cnf(
+        BicondClause(
+            Variable("B21", True), (Variable("P22", True) | Variable("P31", True))
+        )
+    )
+    r4 = parser.parse(result)
+
+    result = to_cnf(~Variable("B11", True))
+    r2 = parser.parse(result)
+
+    alpha = ~Variable("P12", True)
+
+    kb = ResolutionKB()
+    kb.add(list(r4))
+    kb.add(list(r2))
+    assert kb.query(alpha) is False
+
+
+def test_debug():
+    parser = CnfParser()
+    result = to_cnf(
+        BicondClause(
+            Variable("B13", True),
+            (Variable("P12", True) | Variable("P03", True) | Variable("P32", True)),
+        )
+    )
+    r4 = parser.parse(result)
+
+    result = to_cnf(
+        Variable("P12", True) & Variable("P03", True) & Variable("B13", True)
+    )
+    r2 = parser.parse(result)
+
+    alpha = ~Variable("P32", True)
+
+    kb = ResolutionKB()
+    kb.add(list(r4))
+    kb.add(list(r2))
+    assert kb.query(alpha) is False
+
+
+def test_find_clause_symbols():
+    B11 = Variable("B11", True)
+    P12 = Variable("P12", True)
+    P21 = Variable("P21", True)
+    cnf = to_cnf(BicondClause(B11, (P12 | P21)) & ~B11)
+    parser = CnfParser()
+    clauses = parser.parse(cnf)
+    assert find_clause_symbols(clauses) == {"B11", "P12", "P21"}
+
+
+def test_find_pure_symbol():
+    clauses = {
+        CnfClause(
+            {
+                Variable("A", True, True),
+                Variable("B", False, True),
+                Variable("C", False, False),
+            }
+        ),
+        CnfClause({Variable("A", False, True), Variable("C", True, False)}),
+        CnfClause({Variable("B", True, False)}),
+    }
+    symbols = find_clause_symbols(clauses)
+    p, value = find_pure_symbol(symbols, clauses, {"A": False, "B": False, "C": True})
+    assert (p, value) == (None, None)
+
+    clauses = {
+        CnfClause(
+            {
+                Variable("A", False, True),
+                Variable("B", False, True),
+                Variable("C", False, False),
+            }
+        ),
+        CnfClause({Variable("A", False, True), Variable("C", True, False)}),
+        CnfClause({Variable("B", True, False)}),
+    }
+    symbols = find_clause_symbols(clauses)
+    p, value = find_pure_symbol(symbols, clauses, {"A": False, "B": False, "C": True})
+    assert (p, value) == ("A", False)
+
+    clauses = {
+        CnfClause(
+            {
+                Variable("A", False, True),
+                Variable("B", False, True),
+                Variable("C", True, False),
+            }
+        ),
+        CnfClause({Variable("A", True, True), Variable("C", True, False)}),
+        CnfClause({Variable("B", True, False)}),
+    }
+    symbols = find_clause_symbols(clauses)
+    p, value = find_pure_symbol(symbols, clauses, {"A": False, "B": False, "C": True})
+    assert (p, value) == ("C", True)
+
+
+def test_find_unit_clause():
+    clauses = {
+        CnfClause({Variable("B", True, True), Variable("C", True, False)}),
+    }
+
+    model = {"B": False}
+
+    p, val = find_unit_clause(clauses, model, set())
+    assert p is None and val is None
+
+
+def test_dpll():
+    b11 = Variable("b11", False, None)
+    p12 = Variable("p12", False, None)
+    p21 = Variable("p21", False, None)
+
+    # Simple case
+    assert dpll_satisfiable(b11) is True
+
+    # More complex case
+    clause = (BicondClause(b11, p12 | p21) & ~b11) & ~p12
+    assert dpll_satisfiable(clause) is True
+
+    assert dpll_satisfiable(b11 & ~b11) is False
+
+
+def test_dpll_kb():
+    # Example from AIMA
+    B11 = Variable("B11", True)
+    P12 = Variable("P12", True)
+    P21 = Variable("P21", True)
+    kb = DpllKB([BicondClause(B11, (P12 | P21)), ~B11])
+    assert kb.query(Variable("P12", False))
+
+
+def test_debug_dpll():
+    kb = DpllKB()
+    kb.add(
+        BicondClause(
+            Variable("B13", True),
+            (Variable("P12", True) | Variable("P03", True) | Variable("P32", True)),
+        )
+    )
+
+    kb.add(Variable("P12", True) & Variable("P03", True) & Variable("B13", True))
+    alpha = ~Variable("P32", True)
+    assert kb.query(alpha) is False
+
+
+def _one_wumpus_rule() -> Clause:
+    """There should exist one wumpus."""
+    map_width = 3
+    map_height = 3
+    literals: List[Variable] = []
+    for i in range(map_width):
+        for j in range(map_height):
+            literals.append(Variable(f"W{i}{j}", is_negated=False, truthyness=None))
+
+    return reduce(lambda x, y: x | y, literals)  # type: ignore
+
+
+def _at_most_one_wumpus() -> Clause:
+    """There must be just one Wumpus."""
+    map_width = 4
+    map_height = 4
+    clauses = []
+    for j in range(map_height):
+        for i in range(map_width):
+            if i > 0:
+                clauses.append(
+                    Variable(f"W{i}{j}", is_negated=True, truthyness=None)
+                    | Variable(f"W{i - 1}{j}", is_negated=True, truthyness=None)
+                )
+            if i < map_width - 1:
+                clauses.append(
+                    Variable(f"W{i}{j}", is_negated=True, truthyness=None)
+                    | Variable(f"W{i + 1}{j}", is_negated=True, truthyness=None)
+                )
+            if j > 0:
+                clauses.append(
+                    Variable(f"W{i}{j}", is_negated=True, truthyness=None)
+                    | Variable(f"W{i}{j - 1}", is_negated=True, truthyness=None)
+                )
+
+            if j < map_height - 1:
+                if j > 0:
+                    clauses.append(
+                        Variable(f"W{i}{j}", is_negated=True, truthyness=None)
+                        | Variable(f"W{i}{j + 1}", is_negated=True, truthyness=None)
+                    )
+
+    # type: ignore
+    return reduce(lambda x, y: x & y, clauses)
+
+
+def test_horn_clause():
+    a1 = Variable("a1", is_negated=False)
+    a2 = Variable("a2", is_negated=False)
+    a3 = Variable("a3", is_negated=False)
+    b = Variable("b", is_negated=False)
+
+    with pytest.raises(BadHornClause):
+        HornClause([a1, a2, ~a3], b)
+
+    hc = HornClause([a3, a1, a2], b)
+    assert repr(hc) == "a1 ^ a2 ^ a3 => b"
+    assert hc == HornClause([a1, a2, a3], b)
+    hc = HornClause([a1, a2, a3], ~b)
+    assert hc.consequent is False
+    assert hc == HornClause([a1, a2, a3, b])
+    hc = HornClause([a1, a2, a3], None)
+    assert hc.consequent is False
+
+
+def test_pl_fc_entails():
+    A = Variable("A", is_negated=False, truthyness=True)
+    B = Variable("B", is_negated=False, truthyness=True)
+    L = Variable("L", is_negated=False)
+    P = Variable("P", is_negated=False)
+    M = Variable("M", is_negated=False)
+    Q = Variable("Q", is_negated=False)
+    kb = {
+        HornClause([A]),
+        HornClause([B]),
+        HornClause([A, B], L),
+        HornClause([A, P], L),
+        HornClause([B, L], M),
+        HornClause([L, M], P),
+        HornClause([P], Q),
+    }
+
+    assert pl_fc_entails(kb, Q) is True
+    X = Variable("X", is_negated=False)
+    assert pl_fc_entails(kb, X) is False
