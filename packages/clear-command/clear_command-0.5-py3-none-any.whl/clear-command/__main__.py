311c729789fc1ae476b6e4c52ff142f3251fabf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 14:04:17 +0000

    Add package clear-command/0.5/clear_command-0.5-py3-none-any.whl

---
+++ packages/clear-command/clear_command-0.5-py3-none-any.whl/clear-command/__main__.py
@@ -0,0 +1,8 @@
+from .clear_command import clearCommand
+
+def main():
+    command = clearCommand()
+    print(command)
+
+if __name__ == "__main__":
+    main()
---
+++ packages/clear-command/clear_command-0.5-py3-none-any.whl/clear-command/main.py
@@ -0,0 +1,11 @@
+import platform
+import os
+
+global clearCommand
+
+def defineClearCommand():
+    if platform.system() == "Windows":
+        clearCommand = "cls"
+        
+    else:
+        clearCommand = "clear"    
