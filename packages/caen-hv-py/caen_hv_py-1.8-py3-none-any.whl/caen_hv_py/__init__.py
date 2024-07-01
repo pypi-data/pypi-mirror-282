95c8295cfb257c7600bd36d46def3ba693da3413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 03:03:51 +0000

    Add package caen-hv-py/1.8/caen_hv_py-1.8-py3-none-any.whl

---
+++ packages/caen-hv-py/caen_hv_py-1.8-py3-none-any.whl/caen_hv_py/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on April 26 9:31 AM 2024
+Created in PyCharm
+Created as CAEN_HV_Python/__init__.py
+
+@author: Dylan Neff, Dylan
+"""
+
+
