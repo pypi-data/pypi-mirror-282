65798baea7af509551c36dd44566d813b8821103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 03:04:08 +0000

    Add package narwhals/0.9.20/narwhals-0.9.20.tar.gz

---
+++ packages/narwhals/narwhals-0.9.20.tar.gz/narwhals-0.9.20/tests/frame/drop_nulls_test.py
@@ -0,0 +1,23 @@
+from typing import Any
+
+import pandas as pd
+import polars as pl
+import pytest
+
+import narwhals as nw
+from tests.utils import compare_dicts
+
+data = {
+    "a": [1.0, 2.0, None, 4.0],
+    "b": [None, 3.0, None, 5.0],
+}
+
+
+@pytest.mark.parametrize("constructor", [pd.DataFrame, pl.LazyFrame])
+def test_drop_nulls(constructor: Any) -> None:
+    result = nw.from_native(constructor(data)).drop_nulls()
+    expected = {
+        "a": [2.0, 4.0],
+        "b": [3.0, 5.0],
+    }
+    compare_dicts(result, expected)
