e4313b59cb87bdfde46be217ec46bd1d50d299bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 04:05:39 +0000

    Add package dspedal/0.0.1/dspedal-0.0.1-cp312-abi3-win_amd64.whl

---
+++ packages/dspedal/dspedal-0.0.1-cp312-abi3-win_amd64.whl/dspedal/include/dspedal/dspedal.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "dspedal/context.h"
+#include "dspedal/model.h"
+#include "dspedal/signal.h"
+#include "dspedal/clock.h"
+#include "dspedal/dff.h"
+#include "dspedal/simulator.h"
+#include "dspedal/axis.h"
+#include "dspedal/vmodel.h"
+#include "dspedal/tracer.h"
