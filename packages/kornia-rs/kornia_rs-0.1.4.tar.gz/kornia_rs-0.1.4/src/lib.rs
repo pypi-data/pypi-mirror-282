17c07263559383261f61d9bdb92c1469c8b4552f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 15:05:01 +0000

    Add package kornia-rs/0.1.4/kornia_rs-0.1.4.tar.gz

---
+++ packages/kornia-rs/kornia_rs-0.1.4.tar.gz/kornia_rs-0.1.4/src/lib.rs
@@ -0,0 +1,17 @@
+pub mod calibration;
+pub mod color;
+pub mod core;
+// NOTE: not ready yet
+// pub mod distance_transform;
+pub mod flip;
+pub mod histogram;
+pub mod image;
+pub mod interpolation;
+pub mod io;
+pub mod metrics;
+pub mod normalize;
+pub mod resize;
+// NOTE: not ready yet
+pub mod tensor;
+pub mod threshold;
+pub mod warp;
