5524452359e99769d1bb66312984a837c96c7b12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 04:05:03 +0000

    Add package graphexecutor/0.1.5/graphexecutor-0.1.5-py3-none-any.whl

---
+++ packages/graphexecutor/graphexecutor-0.1.5-py3-none-any.whl/graphexecutor/app.py
@@ -0,0 +1,60 @@
+import inspect
+import importlib.util
+import logging
+import pathlib
+
+import click
+import rich
+from rich.logging import RichHandler
+from rich.traceback import install
+
+from graphexecutor.parser import execute_runcard
+
+FORMAT = "%(message)s"
+logging.basicConfig(
+    level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+)
+
+log = logging.getLogger("rich")
+
+install(show_locals=True)
+
+@click.command()
+@click.argument(
+    "providers",
+    nargs=-1,
+    type=click.Path(exists=True, path_type=pathlib.Path),
+    required=True,
+)
+@click.argument(
+    "runcard",
+    nargs=1,
+    type=click.Path(exists=True, path_type=pathlib.Path),
+    required=True,
+)
+@click.option(
+    "--parallel", is_flag=True, default=False, help="Execute the graph asynchronously"
+)
+def main(providers, runcard, parallel):
+    ld = load_provider_functions(providers)
+
+    if parallel:
+        logging.info("Executing in parallel")
+    sol = execute_runcard(runcard, ld, parallel)
+    rich.print(sol)
+    return sol
+
+
+def load_provider_functions(providers):
+    functions = {}
+
+    for provider in providers:
+        spec = importlib.util.spec_from_file_location(f"prov.{provider.name}", provider)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+
+        provider_mapping = dict(inspect.getmembers(module, inspect.isfunction))
+
+        functions = {**functions, **provider_mapping}
+
+    return functions
---
+++ packages/graphexecutor/graphexecutor-0.1.5-py3-none-any.whl/graphexecutor/checker.py
@@ -0,0 +1,60 @@
+import inspect
+import logging
+
+import networkx as nx
+
+
+from graphexecutor import builder
+from graphexecutor import runner
+
+class CycleError(Exception): pass
+
+
+class MissingParameterError(Exception): pass
+
+
+def format_node_path(node_path_map):
+    msg = "The following keys are missing:\n"
+    for node, path in node_path_map.items():
+        msg += f'- {node} through: '
+        msg += ' -> '.join(path)
+        msg += '\n\n'
+    msg = msg[:-1]
+    return msg
+
+def check_bare_graph_against_runcard(graph, rootns):
+    needed_nodes = [i for i in builder.graph_leaves(graph) if i.default is inspect._empty]
+    needed_keys = set([node.name for node in needed_nodes])
+
+    missing_keys = needed_keys.difference(rootns.keys())
+    missing_nodes = [node for node in needed_nodes if node.name in missing_keys]
+
+    solution_node = builder.find_solution_node(graph)
+
+    node_path_map = {}
+    for node in missing_nodes:
+        # Don't expose solution node to user
+        path = nx.shortest_path(graph, solution_node, node)[1:]
+        path = [node.name for node in path]
+        node_path_map[node.name] = path
+
+    extra_keys = runner.unused_keys(graph, rootns)
+
+    if missing_keys:
+        msg = format_node_path(node_path_map)
+        raise MissingParameterError(msg)
+
+    if extra_keys:
+        logging.warning(f"The following are unused keys: {extra_keys}")
+
+
+def check_for_cycles(graph):
+    cycles = list(nx.simple_cycles(graph))
+    for idx, cycle in enumerate(cycles):
+        cycles[idx] = [node.name for node in cycle]
+    if cycles:
+        raise CycleError(
+            "The following cyclic dependencies exist "
+            f"in the dependency graph {cycles}"
+            )
+    return graph
