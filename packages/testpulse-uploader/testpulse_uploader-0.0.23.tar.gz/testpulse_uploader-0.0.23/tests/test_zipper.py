3458fc48880496dca3491e8a690dd79bc1757d52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 04:04:52 +0000

    Add package testpulse-uploader/0.0.23/testpulse_uploader-0.0.23.tar.gz

---
+++ packages/testpulse-uploader/testpulse_uploader-0.0.23.tar.gz/testpulse_uploader-0.0.23/tests/test_zipper.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+
+from uploader.domain.domain import TestRun
+from uploader.upload import create_parser
+from uploader.utils.zipper import find_files
+
+
+def test_find_files():
+    parser = create_parser()
+    args = parser.parse_args(['-tr', r'.*\.xml'])
+
+    test_run = TestRun(args=args)
+    result = find_files(test_run, Path('tests/fixtures/test_results/'))
+    elements = [
+        'tests/fixtures/test_results/pydriller_testresults.xml',
+        'tests/fixtures/test_results/examples/pydriller_testresults.xml',
+        'tests/fixtures/test_results/examples1/pydriller_testresults1.xml',
+        'tests/fixtures/test_results/examples1/examples2/' +
+        'pydriller_testresults2.xml',
+    ]
+    for el in elements:
+        assert el in result
+    assert len(elements) == len(result)
+
+
+def test_find_files_exclude_dir():
+    parser = create_parser()
+    args = parser.parse_args(['-tr', r'.*\.xml'])
+
+    test_run = TestRun(args=args)
+    result = find_files(test_run, Path('tests/fixtures/test_results_exclude_dirs/'))
+    elements = [
+        'tests/fixtures/test_results_exclude_dirs/pydriller_testresults.xml',
+    ]
+    for el in elements:
+        assert el in result
+    assert len(elements) == len(result)
+
+
+def test_find_files_with_config():
+    parser = create_parser()
+    args = parser.parse_args(['-tr', r'.*\.xml', '-cf', '.testpulse.yaml'])
+
+    test_run = TestRun(args=args)
+    result = find_files(test_run, Path('tests/fixtures/test_results/'))
+    elements = [
+        'tests/fixtures/test_results/.testpulse.yaml',
+        'tests/fixtures/test_results/pydriller_testresults.xml',
+        'tests/fixtures/test_results/examples/pydriller_testresults.xml',
+        'tests/fixtures/test_results/examples1/pydriller_testresults1.xml',
+        'tests/fixtures/test_results/examples1/examples2/' +
+        'pydriller_testresults2.xml',
+    ]
+    for el in elements:
+        assert el in result
+    assert len(elements) == len(result)
