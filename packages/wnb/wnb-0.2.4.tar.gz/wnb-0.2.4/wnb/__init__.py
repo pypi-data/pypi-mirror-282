a3aba3da3baadb8d97244e8e0e40d93a7328926e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 14:03:58 +0000

    Add package wnb/0.2.4/wnb-0.2.4.tar.gz

---
+++ packages/wnb/wnb-0.2.4.tar.gz/wnb-0.2.4/wnb/__init__.py
@@ -0,0 +1,16 @@
+__version__ = "0.2.4"
+__author__ = "Mehdi Samsami"
+
+
+from ._base import ContinuousDistMixin, DiscreteDistMixin
+from .enums import Distribution
+from .gnb import GeneralNB
+from .gwnb import GaussianWNB
+
+__all__ = [
+    "GeneralNB",
+    "GaussianWNB",
+    "Distribution",
+    "ContinuousDistMixin",
+    "DiscreteDistMixin",
+]
