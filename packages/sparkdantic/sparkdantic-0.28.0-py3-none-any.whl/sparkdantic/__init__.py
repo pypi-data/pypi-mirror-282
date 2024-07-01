bb2b4cd202cc0837dfa48e161abb6002df99ad33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 03:03:53 +0000

    Add package sparkdantic/0.28.0/sparkdantic-0.28.0-py3-none-any.whl

---
+++ packages/sparkdantic/sparkdantic-0.28.0-py3-none-any.whl/sparkdantic/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.28.0'
+__author__ = 'Mitchell Lisle'
+__email__ = 'm.lisle90@gmail.com'
+
+from sparkdantic.generation import ColumnGenerationSpec
+from sparkdantic.model import ColumnSpecs, SparkModel
