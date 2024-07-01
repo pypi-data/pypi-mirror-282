0675cd5834ff8cf805b964ba9089fe362f9f6ebb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 04:05:14 +0000

    Add package netbox-config-diff/2.5.0/netbox_config_diff-2.5.0-py3-none-any.whl

---
+++ packages/netbox-config-diff/netbox_config_diff-2.5.0-py3-none-any.whl/netbox_config_diff/__init__.py
@@ -0,0 +1,25 @@
+from extras.plugins import PluginConfig
+
+__author__ = "Artem Kotik"
+__email__ = "miaow2@yandex.ru"
+__version__ = "2.5.0"
+
+
+class ConfigDiffConfig(PluginConfig):
+    name = "netbox_config_diff"
+    verbose_name = "NetBox Config Diff Plugin"
+    description = "Find diff between the intended device configuration and actual."
+    author = __author__
+    author_email = __email__
+    version = __version__
+    base_url = "config-diff"
+    required_settings = ["USERNAME", "PASSWORD"]
+    min_version = "3.5.0"
+    default_settings = {
+        "USER_SECRET_ROLE": "Username",
+        "PASSWORD_SECRET_ROLE": "Password",
+        "SECOND_AUTH_SECRET_ROLE": "Second Auth",
+    }
+
+
+config = ConfigDiffConfig
