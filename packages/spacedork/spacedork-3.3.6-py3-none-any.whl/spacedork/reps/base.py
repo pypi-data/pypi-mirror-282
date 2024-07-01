245cd02093ba1f6fc5f058d86586fbf1c3df2e72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 16:04:16 +0000

    Add package spacedork/3.3.6/spacedork-3.3.6-py3-none-any.whl

---
+++ packages/spacedork/spacedork-3.3.6-py3-none-any.whl/spacedork/reps/base.py
@@ -0,0 +1,23 @@
+import logging
+import typing
+
+logger = logging.getLogger("dork")
+
+
+class SearchBase(object):
+    def __init__(self, fields="url", **kwargs):
+        self.fields = fields
+
+    def _query(self, dork: str, page: int) -> typing.Optional[typing.List[dict]]:
+        ...
+
+    def query(self, dork: str, start_page=1, end_page=1) -> typing.Iterable[typing.Dict]:
+        current_page = 1
+        while True:
+            items = self._query(dork, page=current_page)
+            if not items:
+                break
+            for item in items:
+                yield item
+            if current_page >= end_page:
+                break
