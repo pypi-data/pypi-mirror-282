d7b610befdf13c8e66a4fbdf217567bdccff608e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 03:04:01 +0000

    Add package loe-simp-app-fw/2.0.9/loe_simp_app_fw-2.0.9-py3-none-any.whl

---
+++ packages/loe-simp-app-fw/loe_simp_app_fw-2.0.9-py3-none-any.whl/loe_simp_app_fw/__main__.py
@@ -0,0 +1,63 @@
+from typing import no_type_check
+from .logger import Logger
+from .start import main as init_repo
+
+import os
+from argparse import ArgumentParser
+
+# Setup flags
+isCLI = False
+
+Logger.set_log_level("ERROR")
+
+# Parse CLI or not
+@no_type_check ## Skip type checker here because 
+def _isNotebook():
+    return get_ipython().__class__.__name__ # het_python() is globally available when using jupyter notebook
+
+def isNotebook() -> bool:
+    try:
+        shell = _isNotebook()
+        if shell == 'ZMQInteractiveShell':
+            return True   # Jupyter notebook or qtconsole
+        elif shell == 'TerminalInteractiveShell':
+            return False  # Terminal running IPython
+        else:
+            return False  # Other type (?)
+    except NameError:
+        return False      # Probably standard Python interpreter
+
+# CLI Parser (Only runs once)
+if not isNotebook():
+    # Create the main parser
+    parser = ArgumentParser(description="This app builds a repo template for loe's simple app framework.")
+
+    # Create subparsers for gather and search commands
+    subparsers = parser.add_subparsers(title="subcommands", dest="subcommand",
+                                        help="Choose what to do")
+
+    # Init subparser
+    init_parser = subparsers.add_parser("init-project", help="Create folder structure")
+    init_parser.add_argument("path", help="The path to the project root", default="./")
+
+    # Search subparser
+    # search_parser = subparsers.add_parser("search", help="Search for data")
+    # search_parser.add_argument("query", help="Search query")
+    # search_parser.add_argument("-l", "--limit", type=int, help="Maximum number of results")
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Handle subcommands based on 'subcommand' attribute
+    if args.subcommand == "init-project":
+        init_repo(os.path.abspath(args.path))
+
+    # elif args.subcommand == "search":
+    #     # Handle search arguments here (e.g., call a search function)
+    #     print(f"Searching for: {args.query}")
+    #     if args.limit:
+    #         print(f"Limiting results to {args.limit}")
+    else:
+        parser.print_help()
+else:
+    Logger.info("Jupyter Notebook environment detected")
