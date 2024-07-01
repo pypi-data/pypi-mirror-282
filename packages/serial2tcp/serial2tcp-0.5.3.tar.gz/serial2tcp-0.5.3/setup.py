3b22ea3f9aafa37b1e32a5b7483dcffcbf66e93a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 03:03:50 +0000

    Add package serial2tcp/0.5.3/serial2tcp-0.5.3.tar.gz

---
+++ packages/serial2tcp/serial2tcp-0.5.3.tar.gz/serial2tcp-0.5.3/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+    name='serial2tcp',
+    version='0.5.3',
+    url='https://github.com/mosquito/serial2tcp',
+    download_url='https://github.com/mosquito/serialtcp/archive/master.zip',
+    description='Convert serial device to tcp socket.',
+    license='GNU GPL v3',
+    package_data={'': ['README']},
+    long_description=open('README').read(),
+    author='Dmitry Orlov',
+    author_email='me@mosquito.su',
+    platforms=['linux', 'windows', 'darwin'],
+    scripts=['bin/serial2tcp'],
+    install_requires=[
+        'pyserial',
+    ],
+)
