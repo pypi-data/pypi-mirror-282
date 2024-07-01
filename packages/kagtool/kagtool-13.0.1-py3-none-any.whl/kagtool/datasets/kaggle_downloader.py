4de52149378b39be37276a6c65c49a22d5bc87dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 04:05:35 +0000

    Add package kagtool/13.0.1/kagtool-13.0.1-py3-none-any.whl

---
+++ packages/kagtool/kagtool-13.0.1-py3-none-any.whl/kagtool/datasets/kaggle_downloader.py
@@ -0,0 +1,31 @@
+import warnings
+import os
+from pathlib import Path
+import json
+
+class KaggleDownloader:
+    def __init__(self, dataset, creds_str, is_kaggle_env=os.environ.get('KAGGLE_KERNEL_RUN_TYPE', '')):
+        warnings.warn(
+            "The `__init__` method is deprecated and will be removed in a future version. "
+            "Please use the `from_kaggle_env()` method instead.",
+            DeprecationWarning,
+            stacklevel=2)
+        self.dataset = dataset
+        self.creds = json.loads(creds_str)
+        os.environ['KAGGLE_USERNAME'] = creds_str['username']
+        os.environ['KAGGLE_KEY'] = creds_str['key']
+        self.iskaggle_env = is_kaggle_env
+    
+    @classmethod
+    def build(cls, dataset, username, key, is_kaggle_env=os.environ.get('KAGGLE_KERNEL_RUN_TYPE', '')):
+        return cls(dataset, json.dumps({"username": username, "key": key}), is_kaggle_env)
+
+    def load_or_fetch_kaggle_dataset(self):
+        if self.iskaggle_env:
+            path = '/kaggle/input/' + str(self.dataset)
+        else:
+            path = self.dataset
+            import zipfile,kaggle
+            kaggle.api.competition_download_cli(path)
+            zipfile.ZipFile(f'{path}.zip').extractall(path)
+        return Path(path)
