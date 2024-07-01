c848635785c9ef7bc1c63d2c41b411269ff6ba52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 03:04:38 +0000

    Add package rq-dashboard-fast/0.5.0/rq_dashboard_fast-0.5.0.tar.gz

---
+++ packages/rq-dashboard-fast/rq_dashboard_fast-0.5.0.tar.gz/rq_dashboard_fast-0.5.0/rq_dashboard_fast/tests/test_dashboard.py
@@ -0,0 +1,129 @@
+import threading
+from random import randint
+
+import pytest
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+from redis import Redis
+from rq import Queue, Worker
+
+from rq_dashboard_fast import RedisQueueDashboard
+
+worker_name = "test"
+queue_name = "test_queue"
+
+
+async def example_task() -> int:
+    return randint(0, 100)
+
+
+@pytest.fixture
+def setup_redis():
+    redis = Redis(port=6379, host="redis")
+    yield redis
+
+
+@pytest.fixture
+def client():
+    app = FastAPI()
+    dashboard = RedisQueueDashboard(redis_url="redis://redis:6379", prefix="")
+
+    app.mount("", dashboard)
+    return TestClient(app)
+
+
+@pytest.fixture
+def create_queue(setup_redis):
+    queue = Queue(connection=setup_redis, name=queue_name)
+    return queue
+
+
+@pytest.fixture
+def add_task(create_queue):
+    job = create_queue.enqueue(example_task)
+    return job
+
+
+@pytest.fixture
+def setup_worker(setup_redis):
+    worker = Worker(["default"], connection=setup_redis, name=worker_name)
+    thread = threading.Thread(target=worker.work, daemon=True)
+    thread.start()
+    yield worker
+    worker.teardown()
+
+
+def test_add_job(client, add_task):
+    job_id = add_task.id
+
+    response = client.get("/job/" + job_id)
+    assert job_id in response.text
+
+    response_read_html = client.get("/jobs")
+    assert response_read_html.status_code == 200
+    assert job_id in response_read_html.text
+
+    response_read_json = client.get("/jobs/json")
+    assert response_read_json.status_code == 200
+    assert job_id in response.text
+
+
+def test_get_job_data(client, add_task):
+    job_id = add_task.id
+
+    response = client.get(f"/job/{job_id}")
+    assert response.status_code == 200
+    assert job_id in response.text
+
+
+def test_get_jobs(client, add_task):
+    job_id = add_task.id
+
+    response = client.get("/jobs")
+    assert response.status_code == 200
+    assert job_id in response.text
+
+    response = client.get("/jobs/json")
+    assert response.status_code == 200
+    assert job_id in response.text
+
+
+def test_delete_job(client, add_task):
+    job_id = add_task.id
+
+    response_delete = client.delete(f"/job/{job_id}")
+    assert response_delete.status_code == 200
+
+    response_read_html_after_delete = client.get("/jobs")
+    assert response_read_html_after_delete.status_code == 200
+    assert job_id not in response_read_html_after_delete.text
+
+
+def test_delete_jobs_in_queue(client, add_task):
+    job_id = add_task.id
+
+    response_delete = client.delete(f"/queues/{queue_name}")
+    assert response_delete.status_code == 200
+
+    response = client.get("/jobs/json")
+    assert response.status_code == 200
+    assert job_id not in response.text
+
+
+def test_get_queue_json(client):
+    response_read_json = client.get("/queues/json")
+    assert response_read_json.status_code == 200
+    assert any(queue["queue_name"] == queue_name for queue in response_read_json.json())
+
+
+def test_get_queues(client):
+    response = client.get("/queues")
+
+    assert queue_name in response.text
+
+
+def test_get_workers_json(client, setup_worker):
+    response_read_json = client.get("/workers/json")
+    assert response_read_json.status_code == 200
+
+    assert len(response_read_json.json()) == 1
---
+++ packages/rq-dashboard-fast/rq_dashboard_fast-0.5.0.tar.gz/rq_dashboard_fast-0.5.0/rq_dashboard_fast/tests/test_queues_utils.py
@@ -0,0 +1,139 @@
+import pytest
+from redis import Redis
+from rq import Queue
+import pandas
+from ..utils.queues import delete_jobs_for_queue, get_job_registry_amount, get_queues, convert_queue_data_to_json_dict, convert_queues_dict_to_dataframe, QueueRegistryStats
+
+
+@pytest.fixture
+def setup_redis():
+    redis = Redis(port=6379, host="redis")
+    yield redis
+
+
+def example_task():
+    return "Hello World"
+
+
+def test_get_queues(setup_redis):
+    # Set up test data
+    redis_url = "redis://redis:6379"
+    queue_name = "test_queue"
+    queue = Queue(connection=setup_redis, name=queue_name)
+
+    queues = get_queues(redis_url)
+
+    assert queue in queues
+
+
+def test_get_job_registry_amount(setup_redis):
+    redis_url = "redis://redis:6379"
+    queue_name = "test_queue"
+    queue = Queue(connection=setup_redis, name=queue_name)
+    queue.enqueue(example_task)
+
+    registry_stats = get_job_registry_amount(redis_url)
+
+    assert any(
+        stat.queued == 1 and stat.queue_name == queue_name for stat in registry_stats
+    )
+
+
+def test_delete_jobs_for_queue(setup_redis):
+    redis_url = "redis://redis:6379"
+    queue_name = "test_queue3"
+    queue = Queue(connection=setup_redis, name=queue_name)
+    queue.enqueue(example_task)
+
+    registry_stats = get_job_registry_amount(redis_url)
+
+    assert any(
+        stat.queued == 1 and stat.queue_name == queue_name for stat in registry_stats
+    )
+
+    delete_jobs_for_queue(queue_name, redis_url)
+
+    registry_stats = get_job_registry_amount(redis_url)
+
+    assert any(
+        stat.queued == 0 and stat.queue_name == queue_name for stat in registry_stats
+    )
+    
+def test_convert_queue_data_to_json_dict():
+    queue_stats_1 = QueueRegistryStats(
+        queue_name='Queue 1',
+        queued=10,
+        started=5,
+        failed=2,
+        deferred=3,
+        finished=20
+    )
+    queue_stats_2 = QueueRegistryStats(
+        queue_name='Queue 2',
+        queued=15,
+        started=8,
+        failed=1,
+        deferred=0,
+        finished=25
+    )
+    queue_data = [queue_stats_1, queue_stats_2]
+    result_json = convert_queue_data_to_json_dict(queue_data)
+    expected_json = [
+        {
+            "Queue 1": {
+                "queued": 10,
+                "started": 5,
+                "failed": 2,
+                "deferred": 3,
+                "finished": 20
+            },
+            "Queue 2": {
+                "queued": 15,
+                "started": 8,
+                "failed": 1,
+                "deferred": 0,
+                "finished": 25
+            }
+        }
+    ]
+    
+    assert result_json == expected_json
+
+def test_convert_queues_dict_to_dataframe():
+    input_data =  [
+        {
+            "Queue 1": {
+                "queued": 10,
+                "started": 5,
+                "failed": 2,
+                "deferred": 3,
+                "finished": 20
+            },
+            "Queue 2": {
+                "queued": 15,
+                "started": 8,
+                "failed": 1,
+                "deferred": 0,
+                "finished": 25
+            }
+        }
+    ]
+        
+    expected_columns = ['queue_name', 'status', 'count']
+    expected_data = pandas.DataFrame([
+    {"queue_name": "Queue 1", "status": "queued", "count": 10},
+    {"queue_name": "Queue 1", "status": "started", "count": 5},
+    {"queue_name": "Queue 1", "status": "failed", "count": 2},
+    {"queue_name": "Queue 1", "status": "deferred", "count": 3},
+    {"queue_name": "Queue 1", "status": "finished", "count": 20},
+    {"queue_name": "Queue 2", "status": "queued", "count": 15},
+    {"queue_name": "Queue 2", "status": "started", "count": 8},
+    {"queue_name": "Queue 2", "status": "failed", "count": 1},
+    {"queue_name": "Queue 2", "status": "deferred", "count": 0},
+    {"queue_name": "Queue 2", "status": "finished", "count": 25}
+])
+
+    result_df = convert_queues_dict_to_dataframe(input_data)
+        
+    assert result_df.equals(expected_data)
+    assert list(result_df.columns) == expected_columns
