8839ac51e721188abc58cf0a5d14b29bede30a5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 03:03:55 +0000

    Add package sqladmin-async/1.0/sqladmin_async-1.0.tar.gz

---
+++ packages/sqladmin-async/sqladmin_async-1.0.tar.gz/sqladmin_async-1.0/sqladmin_async/__init__.py
@@ -0,0 +1,12 @@
+from sqladmin_async.application import Admin, action, expose
+from sqladmin_async.models import BaseView, ModelView
+
+__version__ = "0.17.0"
+
+__all__ = [
+    "Admin",
+    "expose",
+    "action",
+    "BaseView",
+    "ModelView",
+]
