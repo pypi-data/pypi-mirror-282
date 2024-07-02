c31625f17ea3fa2bd06b11cf91fc38ffd49d1004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 16:05:44 +0000

    Add package mini-util/0.1.4/mini_util-0.1.4.tar.gz

---
+++ packages/mini-util/mini_util-0.1.4.tar.gz/mini_util-0.1.4/mini_util/do_n.py
@@ -0,0 +1,36 @@
+class DoN:
+    @staticmethod
+    def do_n(n: int):
+        """
+        Do function n times
+
+        Args
+            n (int): the number you want to repeat
+        """
+        def inner(f):
+            def wrapper(*args, **kwargs):
+                ret = [None] * n
+                for i in range(n):
+                    ret[i] = f(*args, **kwargs)
+
+            return wrapper
+
+        return inner
+
+
+def do_n(n: int):
+    """
+    Do function n times
+
+    Args
+        n (int): the number you want to repeat
+    """
+    def inner(f):
+        def wrapper(*args, **kwargs):
+            ret = [None] * n
+            for i in range(n):
+                ret[i] = f(*args, **kwargs)
+
+        return wrapper
+
+    return inner
