cd7938663d9292c1dba32e3086ab837874cb87f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 03:04:08 +0000

    Add package Signature-Classifier/1.0.1/signature_classifier-1.0.1.tar.gz

---
+++ packages/Signature-Classifier/signature_classifier-1.0.1.tar.gz/signature_classifier-1.0.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Signature-Classifier',
+    version='1.0.1',
+    author='Mercurlc',
+    author_email='alexanderarefievrus@gmail.com',
+    description='A package for signature classification with HOG features and KNN classifier, also for forg signatures detection',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Mercurlc/Signature-Classifier',
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'optuna',
+        'pandas',
+        'scikit-image',
+        'scikit-learn',
+        'seaborn',
+        'sphinx',
+        'sphinx',
+        'sphinx-autobuild',
+        'sphinx_rtd_theme'
+    ],
+    project_urls={
+        'Source code': 'https://github.com/Mercurlc/Signature-Classifier',
+        'Documentation': 'https://signature-classifier.readthedocs.io/en/latest/',
+        'Example usage': 'https://github.com/Mercurlc/Signature-Classifier/blob/main/example-usage/example.py'
+      },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.11',
+)
