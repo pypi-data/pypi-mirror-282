4c4ae8ac65b39c99c3d390c30aabf733e4b9e80c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 03:03:54 +0000

    Add package kr8s/0.16.2/kr8s-0.16.2.tar.gz

---
+++ packages/kr8s/kr8s-0.16.2.tar.gz/kr8s-0.16.2/docs/conf.py
@@ -0,0 +1,76 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023-2024, Kr8s Developers (See LICENSE for list)
+# SPDX-License-Identifier: BSD 3-Clause License
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import datetime
+
+project = "kr8s"
+author = "Dask Developers, NVIDIA"
+copyright = f"{datetime.date.today().year}, {author}"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.intersphinx",
+    "sphinx_design",
+    "sphinx_copybutton",
+    "sphinxcontrib.mermaid",
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "autoapi.extension",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- copybutton configuration ---------------------------------------------------
+
+copybutton_prompt_text = r">>> |\.\.\. |\$ |In \[\d*\]: | {2,5}\.\.\.: | {5,8}: "
+copybutton_prompt_is_regexp = True
+
+# -- autoapi configuration ---------------------------------------------------
+
+autodoc_typehints = "signature"  # autoapi respects this
+
+autoapi_type = "python"
+autoapi_dirs = ["../kr8s"]
+autoapi_template_dir = "_templates/autoapi"
+autoapi_options = [
+    "members",
+    "inherited-members",
+    "undoc-members",
+    "show-inheritance",
+    "show-module-summary",
+    "imported-members",
+]
+autoapi_ignore = ["*tests*", "*conftest*", "*asyncio*"]
+# autoapi_python_use_implicit_namespaces = True
+autoapi_keep_files = True
+# autoapi_generate_api_docs = False
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ["_static"]
+
+html_theme_options = {
+    "light_logo": "branding/logo-solo.png",
+    "dark_logo": "branding/logo-solo.png",
+}
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+}
+
+
+def setup(app):
+    app.add_css_file("css/custom.css")
