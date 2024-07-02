34ede54830759b900353dfae998d37e304e79b3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 03:03:56 +0000

    Add package ass-tg/2.0.4/ass_tg-2.0.4-py3-none-any.whl

---
+++ packages/ass-tg/ass_tg-2.0.4-py3-none-any.whl/ass_tg/types/__init__.py
@@ -0,0 +1,33 @@
+from ass_tg.types.aliases import DividedArg
+from ass_tg.types.entities import CutCommandEntitiesArg
+from ass_tg.types.keyvalue import KeyValueArg, KeyValuesArg
+from ass_tg.types.lists import ListArg
+from ass_tg.types.logic import OrArg, OptionalArg, AndArg
+from ass_tg.types.oneword import WordArg, IntArg, BooleanArg
+from ass_tg.types.text import TextArg
+from ass_tg.types.text_rules import StartsWithArg, UntilArg, SurroundedArg
+from ass_tg.types.time_arg import ActionTimeArg
+from ass_tg.types.user import UserArg, UsernameArg, UserIDArg, UserMentionArg
+
+__all__ = [
+    'KeyValueArg',
+    'KeyValuesArg',
+    'ListArg',
+    'OrArg',
+    'OptionalArg',
+    'TextArg',
+    'WordArg',
+    'IntArg',
+    'BooleanArg',
+    'StartsWithArg',
+    'UntilArg',
+    'ActionTimeArg',
+    'DividedArg',
+    'SurroundedArg',
+    'CutCommandEntitiesArg',
+    'UserArg',
+    'UsernameArg',
+    'UserIDArg',
+    'UserMentionArg',
+    'AndArg'
+]
