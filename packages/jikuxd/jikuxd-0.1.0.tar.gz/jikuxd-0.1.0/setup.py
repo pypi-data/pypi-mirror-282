32472dd9f940c7df68c9bcbd0f1530629c59171c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 03:04:14 +0000

    Add package jikuxd/0.1.0/jikuxd-0.1.0.tar.gz

---
+++ packages/jikuxd/jikuxd-0.1.0.tar.gz/jikuxd-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="jikuxd",
+    version="0.1.0",
+    author="Personal Tool",
+    author_email="phinerx749@gmail.com",
+    description="This is my personal tool",
+    long_description_content_type='text/markdown',
+    url="https://github.com/phinerx",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
