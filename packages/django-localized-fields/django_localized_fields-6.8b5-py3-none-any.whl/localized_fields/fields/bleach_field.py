fa11c523a7cc55cac6fcfb7d10c8b98405caa3a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 15:10:16 +0000

    Add package django-localized-fields/6.8b5/django_localized_fields-6.8b5-py3-none-any.whl

---
+++ packages/django-localized-fields/django_localized_fields-6.8b5-py3-none-any.whl/localized_fields/fields/bleach_field.py
@@ -0,0 +1,74 @@
+import html
+
+from django.conf import settings
+
+from .field import LocalizedField
+
+
+class LocalizedBleachField(LocalizedField):
+    """Custom version of :see:BleachField that is actually a
+    :see:LocalizedField."""
+
+    DEFAULT_SHOULD_ESCAPE = True
+
+    def __init__(self, *args, escape=True, **kwargs):
+        """Initializes a new instance of :see:LocalizedBleachField."""
+
+        self.escape = escape
+
+        super().__init__(*args, **kwargs)
+
+    def deconstruct(self):
+        name, path, args, kwargs = super().deconstruct()
+
+        if self.escape != self.DEFAULT_SHOULD_ESCAPE:
+            kwargs["escape"] = self.escape
+
+        return name, path, args, kwargs
+
+    def pre_save(self, instance, add: bool):
+        """Ran just before the model is saved, allows us to built the slug.
+
+        Arguments:
+            instance:
+                The model that is being saved.
+
+            add:
+                Indicates whether this is a new entry
+                to the database or an update.
+        """
+
+        # the bleach library vendors dependencies and the html5lib
+        # dependency is incompatible with python 3.9, until that's
+        # fixed, you cannot use LocalizedBleachField with python 3.9
+        # sympton:
+        #   ImportError: cannot import name 'Mapping' from 'collections'
+        try:
+            import bleach
+
+            from django_bleach.utils import get_bleach_default_options
+        except ImportError:
+            raise UserWarning(
+                "LocalizedBleachField is not compatible with Python 3.9 yet."
+            )
+
+        localized_value = getattr(instance, self.attname)
+        if not localized_value:
+            return None
+
+        for lang_code, _ in settings.LANGUAGES:
+            value = localized_value.get(lang_code)
+            if not value:
+                continue
+
+            cleaned_value = bleach.clean(
+                value if self.escape else html.unescape(value),
+                **get_bleach_default_options()
+            )
+
+            localized_value.set(
+                lang_code,
+                cleaned_value if self.escape else html.unescape(cleaned_value),
+            )
+
+        return localized_value
---
+++ packages/django-localized-fields/django_localized_fields-6.8b5-py3-none-any.whl/localized_fields/lookups.py
@@ -0,0 +1,170 @@
+from django.conf import settings
+from django.contrib.postgres.fields.hstore import KeyTransform
+from django.contrib.postgres.lookups import (
+    SearchLookup,
+    TrigramSimilar,
+    Unaccent,
+)
+from django.db.models import TextField, Transform
+from django.db.models.expressions import Col, Func, Value
+from django.db.models.functions import Coalesce
+from django.db.models.lookups import (
+    Contains,
+    EndsWith,
+    Exact,
+    IContains,
+    IEndsWith,
+    IExact,
+    In,
+    IRegex,
+    IsNull,
+    IStartsWith,
+    Regex,
+    StartsWith,
+)
+from django.utils import translation
+from psqlextra.expressions import HStoreColumn
+
+from .fields import LocalizedField
+
+try:
+    from django.db.models.functions import NullIf
+except ImportError:
+    # for Django < 2.2
+    class NullIf(Func):
+        function = "NULLIF"
+        arity = 2
+
+
+class LocalizedLookupMixin:
+    def process_lhs(self, qn, connection):
+        # If the LHS is already a reference to a specific hstore key, there
+        # is nothing to be done since it already references as specific language.
+        if isinstance(self.lhs, HStoreColumn) or isinstance(
+            self.lhs, KeyTransform
+        ):
+            return super().process_lhs(qn, connection)
+
+        # If this is something custom expression, we don't really know how to
+        # handle that, so we better do nothing.
+        if not isinstance(self.lhs, Col):
+            return super().process_lhs(qn, connection)
+
+        # Select the key for the current language. We do this so that
+        #
+        # myfield__<lookup>=
+        #
+        # Is converted into:
+        #
+        # myfield__<lookup>__<current language>=
+        language = translation.get_language() or settings.LANGUAGE_CODE
+        self.lhs = KeyTransform(language, self.lhs)
+
+        return super().process_lhs(qn, connection)
+
+    def get_prep_lookup(self):
+        # Django 4.0 removed the ability for isnull fields to be something
+        # other than a bool We should NOT convert them to strings
+        if isinstance(self.rhs, bool):
+            return self.rhs
+        return str(self.rhs)
+
+
+class LocalizedSearchLookup(LocalizedLookupMixin, SearchLookup):
+    pass
+
+
+class LocalizedUnaccent(LocalizedLookupMixin, Unaccent):
+    pass
+
+
+class LocalizedTrigramSimilair(LocalizedLookupMixin, TrigramSimilar):
+    pass
+
+
+class LocalizedExact(LocalizedLookupMixin, Exact):
+    pass
+
+
+class LocalizedIExact(LocalizedLookupMixin, IExact):
+    pass
+
+
+class LocalizedIn(LocalizedLookupMixin, In):
+    pass
+
+
+class LocalizedContains(LocalizedLookupMixin, Contains):
+    pass
+
+
+class LocalizedIContains(LocalizedLookupMixin, IContains):
+    pass
+
+
+class LocalizedStartsWith(LocalizedLookupMixin, StartsWith):
+    pass
+
+
+class LocalizedIStartsWith(LocalizedLookupMixin, IStartsWith):
+    pass
+
+
+class LocalizedEndsWith(LocalizedLookupMixin, EndsWith):
+    pass
+
+
+class LocalizedIEndsWith(LocalizedLookupMixin, IEndsWith):
+    pass
+
+
+class LocalizedIsNullWith(LocalizedLookupMixin, IsNull):
+    pass
+
+
+class LocalizedRegexWith(LocalizedLookupMixin, Regex):
+    pass
+
+
+class LocalizedIRegexWith(LocalizedLookupMixin, IRegex):
+    pass
+
+
+@LocalizedField.register_lookup
+class ActiveRefLookup(Transform):
+    output_field = TextField()
+    lookup_name = "active_ref"
+    arity = None
+
+    def as_sql(self, compiler, connection):
+        language = translation.get_language() or settings.LANGUAGE_CODE
+        return KeyTransform(language, self.lhs).as_sql(compiler, connection)
+
+
+@LocalizedField.register_lookup
+class TranslatedRefLookup(Transform):
+    output_field = TextField()
+    lookup_name = "translated_ref"
+    arity = None
+
+    def as_sql(self, compiler, connection):
+        language = translation.get_language()
+        fallback_config = getattr(settings, "LOCALIZED_FIELDS_FALLBACKS", {})
+        target_languages = fallback_config.get(language, [])
+        if not target_languages and language != settings.LANGUAGE_CODE:
+            target_languages.append(settings.LANGUAGE_CODE)
+
+        if language:
+            target_languages.insert(0, language)
+
+        if len(target_languages) > 1:
+            return Coalesce(
+                *[
+                    NullIf(KeyTransform(language, self.lhs), Value(""))
+                    for language in target_languages
+                ]
+            ).as_sql(compiler, connection)
+
+        return KeyTransform(target_languages[0], self.lhs).as_sql(
+            compiler, connection
+        )
