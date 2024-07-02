d5fd851fba8bf51d16737fc89f7dd5727cccb0b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 04:05:45 +0000

    Add package gvtt/0.1.0/gvtt-0.1.0.tar.gz

---
+++ packages/gvtt/gvtt-0.1.0.tar.gz/gvtt-0.1.0/gvtt/components/mmi1x2.py
@@ -0,0 +1,110 @@
+import gdsfactory as gf
+from gdsfactory.component import Component
+from gdsfactory.components.straight import straight as straight_function
+from gdsfactory.components.taper import taper as taper_function
+from gdsfactory.typings import ComponentSpec, CrossSectionSpec
+
+from gvtt.tech import TECH
+
+
+@gf.cell
+def mmi1x2(
+    width: float = TECH.width_strip,
+    width_taper: float = TECH.width_strip,
+    length_taper: float = 1.0,
+    length_mmi: float = 43.25,
+    width_mmi: float = 6.25,
+    gap_mmi=(6.25 - 2 * TECH.width_strip) / 2,
+    taper: ComponentSpec = taper_function,
+    straight: ComponentSpec = straight_function,
+    cross_section: CrossSectionSpec = "xs_sc",
+) -> Component:
+    r"""1x2 MultiMode Interferometer (MMI).
+
+    Args:
+        width: input and output straight width. Defaults to cross_section width.
+        width_taper: interface between input straights and mmi region.
+        length_taper: into the mmi region.
+        length_mmi: in x direction.
+        width_mmi: in y direction.
+        gap_mmi:  gap between tapered wg.
+        taper: taper function.
+        straight: straight function.
+        cross_section: specification (CrossSection, string or dict).
+
+
+    .. code::
+
+               length_mmi
+                <------>
+                ________
+               |        |
+               |         \__
+               |          __  o2
+            __/          /_ _ _ _
+         o1 __          | _ _ _ _| gap_mmi
+              \          \__
+               |          __  o3
+               |         /
+               |________|
+
+             <->
+        length_taper
+
+    """
+    c = Component()
+    gap_mmi = gf.snap.snap_to_grid(gap_mmi, grid_factor=2)
+    x = gf.get_cross_section(cross_section)
+    xs_mmi = gf.get_cross_section(cross_section, width=width_mmi)
+    width = width or x.width
+
+    _taper = gf.get_component(
+        taper,
+        length=length_taper,
+        width1=width,
+        width2=width_taper,
+        cross_section=cross_section,
+    )
+
+    a = gap_mmi / 2 + width_taper / 2
+    _ = c << gf.get_component(straight, length=length_mmi, cross_section=xs_mmi)
+
+    ports = [
+        gf.Port(
+            "o1",
+            orientation=180,
+            center=(0, 0),
+            width=width_taper,
+            layer=x.layer,
+            cross_section=x,
+        ),
+        gf.Port(
+            "o2",
+            orientation=0,
+            center=(+length_mmi, +a),
+            width=width_taper,
+            layer=x.layer,
+            cross_section=x,
+        ),
+        gf.Port(
+            "o3",
+            orientation=0,
+            center=(+length_mmi, -a),
+            width=width_taper,
+            layer=x.layer,
+            cross_section=x,
+        ),
+    ]
+
+    for port in ports:
+        taper_ref = c << _taper
+        taper_ref.connect(port="o2", other=port, allow_width_mismatch=True)
+        c.add_port(name=port.name, port=taper_ref.ports["o1"])
+
+    c.flatten()
+    return c
+
+
+if __name__ == "__main__":
+    c = mmi1x2()
+    c.show()
---
+++ packages/gvtt/gvtt-0.1.0.tar.gz/gvtt-0.1.0/gvtt/tech.py
@@ -0,0 +1,154 @@
+"""technology definitions."""
+
+import sys
+from functools import partial
+
+import gdsfactory as gf
+from gdsfactory.cross_section import (
+    CrossSection,
+    LayerSpec,
+    Section,
+    cross_section,
+    get_cross_sections,
+)
+
+from gvtt.layers import LAYER
+
+
+class Tech:
+    width_strip = 1.874
+
+
+TECH = Tech()
+
+
+def rib(
+    width: float = 2.5,
+    width_trench: float = 10.0,
+    wg_marking_layer: LayerSpec = LAYER.TYPE_RIB,
+    **kwargs,
+) -> CrossSection:
+    """Return CrossSection of rib waveguide defined by trenches.
+
+    Args:
+        width: waveguide width.
+        width_trench: trench width.
+        wg_marking_layer: layer for waveguide marking.
+        kwargs: cross_section arguments.
+    """
+
+    sections = kwargs.pop("sections", [])
+    sections += [
+        Section(width=width, layer="WG_RIBS_SUB", name="WG", simplify=None),
+        Section(
+            width=2.0 * width_trench + width,
+            layer="WG_RIBS_ADD",
+            name="trench",
+            simplify=None,
+        ),
+    ]
+
+    return cross_section(
+        width=width,
+        layer=wg_marking_layer,
+        sections=tuple(sections),
+        port_names=("o1", "o2"),
+        **kwargs,
+    )
+
+
+def strip(
+    width: float = TECH.width_strip,
+    width_trench: float = 10.0,
+    wg_marking_layer: LayerSpec = LAYER.TYPE_STRIP,
+    **kwargs,
+) -> CrossSection:
+    """Return CrossSection of strip waveguide defined by trenches.
+
+    Args:
+        width: waveguide width.
+        width_trench: trench width.
+        wg_marking_layer: layer for waveguide marking.
+        kwargs: cross_section arguments.
+    """
+
+    sections = kwargs.pop("sections", [])
+    sections += [
+        Section(width=width, layer="WG_RIBS_SUB", name="WG", simplify=None),
+        Section(
+            width=2.0 * width_trench + width,
+            layer="WG_RIBS_ADD",
+            name="trench",
+            simplify=None,
+        ),
+        Section(
+            width=2.0 * width_trench + width + 1,
+            layer="WG_MOST_ADD",
+            name="trench_2",
+            simplify=None,
+        ),
+    ]
+
+    return cross_section(
+        width=width,
+        layer=wg_marking_layer,
+        sections=tuple(sections),
+        port_names=("o1", "o2"),
+        **kwargs,
+    )
+
+
+def vttstrip(
+    width: float = TECH.width_strip,
+    width_trench: float = 10.0,
+    wg_marking_layer: LayerSpec | None = LAYER.TYPE_STRIP,
+    **kwargs,
+) -> CrossSection:
+    """Return CrossSection of strip waveguide defined by trenches.
+
+    Args:
+        width: waveguide width.
+        width_trench: trench width.
+        wg_marking_layer: layer for waveguide marking.
+        kwargs: cross_section arguments.
+    """
+
+    sections = kwargs.pop("sections", [])
+    sections += [
+        Section(width=width, layer="WG_RIBS_SUB", name="WG", simplify=None),
+        Section(
+            width=2.0 * width_trench + width,
+            layer="WG_RIBS_ADD",
+            name="trench",
+            simplify=None,
+        ),
+        Section(
+            width=2.0 * width_trench + width + 1,
+            layer="WG_MOST_ADD",
+            name="trench_2",
+            simplify=None,
+        ),
+    ]
+
+    return cross_section(
+        width=width,
+        layer=wg_marking_layer,
+        sections=tuple(sections),
+        port_names=("o1", "o2"),
+        **kwargs,
+    )
+
+
+sm_rib = rib(width=2.5)
+euler_strip = strip(width=TECH.width_strip)
+xs_sc = partial(strip, width=TECH.width_strip)
+
+straight_sc = partial(gf.components.straight, cross_section=xs_sc)
+straight_rib = partial(gf.components.straight, cross_section=sm_rib)
+straight = straight_sc
+
+cross_sections = get_cross_sections(sys.modules[__name__])
+
+if __name__ == "__main__":
+    c = straight()
+    c.show()
