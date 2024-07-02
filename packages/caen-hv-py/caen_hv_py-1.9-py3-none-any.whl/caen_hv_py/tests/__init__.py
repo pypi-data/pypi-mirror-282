0756109d5f2db7d3128384fd5575a70201c01b50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 17:03:57 +0000

    Add package caen-hv-py/1.9/caen_hv_py-1.9-py3-none-any.whl

---
+++ packages/caen-hv-py/caen_hv_py-1.9-py3-none-any.whl/caen_hv_py/tests/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on April 26 1:20 PM 2024
+Created in PyCharm
+Created as CAEN_HV_Python/__init__.py
+
+@author: Dylan Neff, Dylan
+"""
+
---
+++ packages/caen-hv-py/caen_hv_py-1.9-py3-none-any.whl/caen_hv_py/tests/get_set_param_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on July 02 11:05 AM 2024
+Created in PyCharm
+Created as CAEN_HV_Python/get_set_param_test.py
+
+@author: Dylan Neff, Dylan
+"""
+
+from caen_hv_py.CAENHVController import CAENHVController
+
+
+def main():
+    ip_address = '192.168.10.81'
+    username = 'admin'
+    password = 'admin'
+
+    with CAENHVController(ip_address, username, password) as hv_wrapper:
+        print(hv_wrapper.get_ch_param_float(3, 2, 'VMon'))
+
+    print('donzo')
+
+
+if __name__ == '__main__':
+    main()
