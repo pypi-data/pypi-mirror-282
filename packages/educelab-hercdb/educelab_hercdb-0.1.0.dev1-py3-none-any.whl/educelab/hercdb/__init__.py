eb8b6d8e93f1e3187447f999b41eef7719e1661e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 06:04:41 +0000

    Add package educelab-hercdb/0.1.0.dev1/educelab_hercdb-0.1.0.dev1-py3-none-any.whl

---
+++ packages/educelab-hercdb/educelab_hercdb-0.1.0.dev1-py3-none-any.whl/educelab/hercdb/__init__.py
@@ -0,0 +1,6 @@
+from educelab.hercdb import config
+from educelab.hercdb.api import (connect, node_count, disconnect,
+                                 list_cornici_pezzi, find_datasets,
+                                 get_display_name, verify_connection)
+
+from educelab.hercdb.api import (FlatbedScanType, PGSRawType, SpectralRawType)
