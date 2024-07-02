862835667d02fd57ae084968422dc1b7247ec31f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 03:04:00 +0000

    Add package rmrag/0.2.0/rmrag-0.2.0.tar.gz

---
+++ packages/rmrag/rmrag-0.2.0.tar.gz/rmrag-0.2.0/rmrag/pipelines/__init__.py
@@ -0,0 +1,4 @@
+from .directory_into_faiss import directory_into_faiss
+from .get_response import get_response
+
+__all__ = ['directory_into_faiss', 'get_response']
---
+++ packages/rmrag/rmrag-0.2.0.tar.gz/rmrag-0.2.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rmrag',
+    version='0.2.0',
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=[],
+    author='Lasse Tranekjær Leed',
+    author_email='lasseleed@gmail.com',
+    description='RAG functions',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 3 - Alpha', 
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.9']
+)
+
+#0.2 = added tests for all function + get_response into class 
