bb341128fbb933266c443e1717cfe4d0ddbe8fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 03:03:53 +0000

    Add package ishanki-probability-distributions/1.2/ishanki_probability_distributions-1.2.tar.gz

---
+++ packages/ishanki-probability-distributions/ishanki_probability_distributions-1.2.tar.gz/ishanki_probability_distributions-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ishanki_probability_distributions',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['ishanki_probability_distributions'],
+      author = "Ishanki Saxena",
+      author_email = "saxenaishanki@gmail.com",
+      zip_safe=False)
