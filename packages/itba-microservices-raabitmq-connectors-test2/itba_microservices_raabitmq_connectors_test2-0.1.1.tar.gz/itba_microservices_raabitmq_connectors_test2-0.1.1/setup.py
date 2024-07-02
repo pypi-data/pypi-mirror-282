bf5cbc85949a09514922210bb9bfd0a26f62e98d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 14:05:24 +0000

    Add package itba-microservices-raabitmq-connectors-test2/0.1.1/itba_microservices_raabitmq_connectors_test2-0.1.1.tar.gz

---
+++ packages/itba-microservices-raabitmq-connectors-test2/itba_microservices_raabitmq_connectors_test2-0.1.1.tar.gz/itba_microservices_raabitmq_connectors_test2-0.1.1/setup.py
@@ -0,0 +1,24 @@
+# root/artifacts/queues/setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='itba_microservices_raabitmq_connectors_test2',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    author='Gonzalo Beade',
+    author_email='gbeade@itba.edu.ar',
+    description="""
+        A queues utility package for microservices.
+        Consumers, producers and DTOs.
+    """,
+    url='https://gitlab.com/gbeade/microservices',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+)
