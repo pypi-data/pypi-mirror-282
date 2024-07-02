282f6be2cc49674394ece96bd8427d5d48a27d0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 15:09:49 +0000

    Add package fake-bpy-module/20240702/fake_bpy_module-20240702.tar.gz

---
+++ packages/fake-bpy-module/fake_bpy_module-20240702.tar.gz/fake_bpy_module-20240702/_bpy_internal/extensions/__init__.pyi
@@ -0,0 +1,7 @@
+import typing
+import collections.abc
+from . import junction_module
+from . import wheel_manager
+
+GenericType1 = typing.TypeVar("GenericType1")
+GenericType2 = typing.TypeVar("GenericType2")
