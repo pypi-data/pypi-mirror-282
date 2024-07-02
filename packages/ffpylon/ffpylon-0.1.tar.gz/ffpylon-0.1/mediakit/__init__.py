fb46a48a2a2257c5ff53e4c3083b49fddae82228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 16:05:10 +0000

    Add package ffpylon/0.1/ffpylon-0.1.tar.gz

---
+++ packages/ffpylon/ffpylon-0.1.tar.gz/ffpylon-0.1/mediakit/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Author  : nickdecodes
+@Email   : nickdecodes@163.com
+@Usage   :
+@FileName: __init__.py
+@DateTime: 2024/7/2 14:09
+@SoftWare: 
+"""
