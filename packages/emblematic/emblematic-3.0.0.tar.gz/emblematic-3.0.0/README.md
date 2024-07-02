5750def6a7d5e86150f2fea72de3b277a0e6aeb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 04:04:14 +0000

    Add package emblematic/3.0.0/emblematic-3.0.0.tar.gz

---
+++ packages/emblematic/emblematic-3.0.0.tar.gz/emblematic-3.0.0/README.md
@@ -0,0 +1,15 @@
+<div align="center"> 
+
+![](.media/icon-128x128_round.png) 
+
+# Emblematic
+
+Generate emblems from an icon and a background
+
+</div>
+
+## Links
+
+[![PyPI](https://img.shields.io/pypi/v/emblematic)](https://pypi.org/project/emblematic)
+ 
+[![Documentation](https://img.shields.io/readthedocs/emblematic)](https://emblematic.readthedocs.io/en/latest/)
---
+++ packages/emblematic/emblematic-3.0.0.tar.gz/emblematic-3.0.0/emblematic/compose.py
@@ -0,0 +1,44 @@
+"""
+Module containing the functions used to generate ``<svg>`` icons from other ``<svg>`` :class:`bs4.Tag`.
+"""
+
+import bs4
+
+
+def compose_basic(background: bs4.Tag, icon: bs4.Tag, width: int, height: int) -> bs4.Tag:
+    """
+    Create a new and nice ``<svg>`` icon from the given background ``<svg>`` and the given foreground ``<svg>``.
+    """
+
+    if background.name != "svg":
+        raise ValueError("bg is not a <svg> tag.")
+    if icon.name != "svg":
+        raise ValueError("fg is not a <svg> tag.")
+
+    background = background.__copy__()
+    background.attrs["id"] = "emblematic-background"
+    background.attrs["width"] = "100%"
+    background.attrs["height"] = "100%"
+
+    icon = icon.__copy__()
+    icon.attrs["id"] = "emblematic-icon"
+    icon.attrs["width"] = "63%"
+    icon.attrs["height"] = "63%"
+    icon.attrs["preserveAspectRatio"] = "xMidYMid meet"
+    icon.attrs["x"] = str(width * 0.37 / 2)
+    icon.attrs["y"] = str(height * 0.37 / 2)
+
+    doc = bs4.BeautifulSoup(f"""
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 {width} {height}">
+    </svg>
+    """, features="lxml-xml")
+    container: bs4.Tag = doc.svg
+    container.append(background)
+    container.append(icon)
+
+    return doc
+
+
+__all__ = (
+    "compose_basic",
+)
