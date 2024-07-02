b742c7c460ff7ffde4810bbf9d04c96a60b55973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 03:04:21 +0000

    Add package performaddition/0.1.0/performaddition-0.1.0.tar.gz

---
+++ packages/performaddition/performaddition-0.1.0.tar.gz/performaddition-0.1.0/setup.py
@@ -0,0 +1,23 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="performaddition",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # Add your dependencies here
+    ],
+    author="Nandhini",
+    author_email="nandhini67288@gmail.com",
+    description="A short description of your package",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/nandhinikesavan20/hello_world",  # Your package's GitHub repo
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    # python_requires='>=3.10',
+)
