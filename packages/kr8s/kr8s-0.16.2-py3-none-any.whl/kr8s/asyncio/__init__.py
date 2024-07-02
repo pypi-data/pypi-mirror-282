ae132a15bf1b8c033616477b35433efbeef664d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 03:03:54 +0000

    Add package kr8s/0.16.2/kr8s-0.16.2-py3-none-any.whl

---
+++ packages/kr8s/kr8s-0.16.2-py3-none-any.whl/kr8s/asyncio/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023-2024, Kr8s Developers (See LICENSE for list)
+# SPDX-License-Identifier: BSD 3-Clause License
+from kr8s._api import Api  # noqa
+
+from ._api import api  # noqa
+from ._helpers import api_resources, get, version, watch, whoami  # noqa
