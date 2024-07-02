208b45b61283674c87e254e6e9159fee7249f00c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 14:04:33 +0000

    Add package python-flint/0.7.0a2/python_flint-0.7.0a2-cp310-cp310-win_amd64.whl

---
+++ packages/python-flint/python_flint-0.7.0a2-cp310-cp310-win_amd64.whl/flint/__init__.py
@@ -0,0 +1,53 @@
+"""""" # start delvewheel patch
+def _delvewheel_patch_1_7_0():
+    import os
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'python_flint.libs'))
+    if os.path.isdir(libs_dir):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_7_0()
+del _delvewheel_patch_1_7_0
+# end delvewheel patch
+
+from .pyflint import *
+
+from .types.fmpz import *
+from .types.fmpz_poly import *
+from .types.fmpz_mat import *
+from .types.fmpz_series import *
+
+from .types.fmpq import *
+from .types.fmpq_poly import *
+from .types.fmpq_mat import *
+from .types.fmpq_series import *
+
+from .types.nmod import *
+from .types.nmod_poly import *
+from .types.nmod_mat import *
+from .types.nmod_series import *
+
+from .types.fmpz_mpoly import *
+from .types.fmpz_mod import *
+from .types.fmpz_mod_poly import *
+from .types.fmpz_mod_mat import fmpz_mod_mat
+
+from .types.arf import *
+from .types.arb import *
+from .types.arb_poly import *
+from .types.arb_mat import *
+from .types.arb_series import *
+from .types.acb import *
+from .types.acb_poly import *
+from .types.acb_mat import *
+from .types.acb_series import *
+
+from .types.dirichlet import *
+from .functions.showgood import good, showgood
+
+from .flint_base.flint_base import (
+    FLINT_VERSION as __FLINT_VERSION__,
+    FLINT_RELEASE as __FLINT_RELEASE__,
+)
+
+__version__ = '0.7.0a2'
