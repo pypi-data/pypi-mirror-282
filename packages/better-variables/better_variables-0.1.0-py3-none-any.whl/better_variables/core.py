4af8f838bc1663f4830ad51ca4bffbba84345339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 04:04:48 +0000

    Add package better-variables/0.1.0/better_variables-0.1.0-py3-none-any.whl

---
+++ packages/better-variables/better_variables-0.1.0-py3-none-any.whl/better_variables/core.py
@@ -0,0 +1,15 @@
+from typing import Any, Callable
+def get_variable_name(variable: Any, namespace: dict[str, Any] = globals()):
+    return [nombre for nombre in namespace if namespace[nombre] is variable][0]
+
+class BetterVariable:
+    def __init__(self, value, description: str) -> None:
+        self.value = value
+        self.description = description
+    def __str__(self) -> str:
+        return f"""Variable name: {get_variable_name(self)}
+Variable value: {self.value}
+Variable description: {self.description}
+"""
+    def __matmul__(self, function: Callable) -> Any:
+        return function(self.value)
