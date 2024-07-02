db057db3b3310ec52be208954cf6c8ca36d10172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 03:04:31 +0000

    Add package dulu/0.1.3/dulu-0.1.3.tar.gz

---
+++ packages/dulu/dulu-0.1.3.tar.gz/dulu-0.1.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dulu',
+    version='0.1.3',
+    packages=find_packages(), 
+    install_requires=[
+        'numpy',
+        'opencv-python',
+        'transformers',
+        'torch',
+        'timm',
+        'einops'
+    ],
+    author='Krishna Sangeeth',
+    author_email='kskrishnasangeeth@gmail.com',
+    description='A Python library for OCR',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/whiletruelearn/florence_ocr',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
