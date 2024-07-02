8b2b3ddb75c39346ddda605337be0059eb6761ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 17:04:44 +0000

    Add package ragstack-ai-langflow-base/1.0.6.post3/ragstack_ai_langflow_base-1.0.6.post3-py3-none-any.whl

---
+++ packages/ragstack-ai-langflow-base/ragstack_ai_langflow_base-1.0.6.post3-py3-none-any.whl/langflow/frontend/assets/input-sNDUKOHK.js
@@ -0,0 +1,1 @@
+import{r,j as s,aw as p}from"./index-CXIlJsYb.js";const e=r.forwardRef(({className:a,type:t,...n},o)=>s.jsx("input",{"data-testid":"",type:t,className:p("nopan nodelete nodrag noundo nocopy primary-input",a),ref:o,...n}));e.displayName="Input";export{e as I};
