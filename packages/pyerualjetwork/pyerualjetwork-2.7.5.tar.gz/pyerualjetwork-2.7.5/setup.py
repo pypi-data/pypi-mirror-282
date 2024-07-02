ac926dd4f40512ea7777970d8322ce79c55cacca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 04:05:35 +0000

    Add package pyerualjetwork/2.7.5/pyerualjetwork-2.7.5.tar.gz

---
+++ packages/pyerualjetwork/pyerualjetwork-2.7.5.tar.gz/pyerualjetwork-2.7.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+# Setting Up
+
+setup(
+      
+      name = "pyerualjetwork",
+      version = "2.7.5",
+      author = "Hasan Can Beydili",
+      author_email = "tchasancan@gmail.com",
+      description= "Input normalization removed",
+      packages = find_packages(),
+      keywords = ["model evaluation", "classifcation", 'potentiation learning artficial neural networks'],
+
+      
+      )
