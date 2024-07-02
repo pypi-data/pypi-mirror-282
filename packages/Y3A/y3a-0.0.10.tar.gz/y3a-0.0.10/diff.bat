cb163571d722e4082d192f78440be5ead2c74126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 16:05:08 +0000

    Add package Y3A/0.0.10/y3a-0.0.10.tar.gz

---
+++ packages/Y3A/y3a-0.0.10.tar.gz/y3a-0.0.10/diff.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\SourceGear\Common\DiffMerge\sgdm.exe" C:\Users\nummulith\AppData\Roaming\Python\Python312\site-packages\Y3A\Y3A.py C:\Code\OMF\Y3A\src\Y3A\Y3A.py
---
+++ packages/Y3A/y3a-0.0.10.tar.gz/y3a-0.0.10/src/Y3A/__init__.py
@@ -0,0 +1,2 @@
+from ._version import version as __version__
+from .Y3A import Y3A
