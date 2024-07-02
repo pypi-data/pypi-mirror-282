5dd7238be1e1a51cb6ee07873a78689663d70287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 04:04:17 +0000

    Add package odoo12-addon-knowledge-pages-api/12.0.1.0.0/odoo12_addon_knowledge_pages_api-12.0.1.0.0-py3-none-any.whl

---
+++ packages/odoo12-addon-knowledge-pages-api/odoo12_addon_knowledge_pages_api-12.0.1.0.0-py3-none-any.whl/odoo/addons/knowledge_pages_api/models/knowledge_pages_highlight.py
@@ -0,0 +1,16 @@
+from odoo import models, fields
+
+
+class KnowledgePageHighlight(models.Model):
+    _name = 'knowledge.page.highlight'
+    _description = 'Knowledge Page Highlight'
+
+    name = fields.Char(string='Name', required=True, translate=True)
+    code = fields.Char(string='Code', required=True, help="Unique code to identify the highlight.")
+    color = fields.Integer(string='Color Index')
+    active = fields.Boolean(default=True)
+
+    _sql_constraints = [
+        ('unique_name', 'unique(name)', 'Tags name must me unique'),
+        ('unique_code', 'unique(code)', 'Tags code must me unique'),
+    ]
