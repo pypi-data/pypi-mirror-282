b6f01a7ca2e038f00b84afc6ceb11392d500869f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 15:10:19 +0000

    Add package tap-belvo/0.0.1b8/tap_belvo-0.0.1b8-py3-none-any.whl

---
+++ packages/tap-belvo/tap_belvo-0.0.1b8-py3-none-any.whl/tap_belvo/streams/core.py
@@ -0,0 +1,53 @@
+"""Stream type classes for tap-belvo."""
+
+from __future__ import annotations
+
+import typing as t
+
+from tap_belvo.client import BelvoStream
+
+
+class Links(BelvoStream):
+    """Links stream."""
+
+    name = "links"
+    path = "/api/links"
+    primary_keys = ("id",)
+    replication_key = "created_at"
+    openapi_ref = "Link"
+
+    def get_child_context(
+        self,
+        record: dict[str, t.Any],
+        context: dict[t.Any, t.Any] | None,  # noqa: ARG002
+    ) -> dict[t.Any, t.Any]:
+        """Return the child context.
+
+        Args:
+            record: The record to get the child context for.
+            context: The parent context.
+
+        Returns:
+            The child context.
+        """
+        return {"link_id": record["id"]}
+
+
+class Institutions(BelvoStream):
+    """Institutions stream."""
+
+    name = "institutions"
+    path = "/api/institutions"
+    primary_keys = ("id",)
+    replication_key = None
+    openapi_ref = "Institution"
+
+
+class Consents(BelvoStream):
+    """Consents stream."""
+
+    name = "consents"
+    path = "/api/consents"
+    primary_keys = ("id",)
+    replication_key = None
+    openapi_ref = "Consent"
