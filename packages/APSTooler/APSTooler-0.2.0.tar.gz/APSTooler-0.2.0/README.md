ef433aaaa893affcf54793c7463eafb8d002ded7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 16:06:28 +0000

    Add package APSTooler/0.2.0/APSTooler-0.2.0.tar.gz

---
+++ packages/APSTooler/APSTooler-0.2.0.tar.gz/APSTooler-0.2.0/README.md
@@ -0,0 +1,31 @@
+# APSTooler
+
+The APSTooler is a Python toolkit designed to interact with the Autodesk BIM 360 and Autodesk Construction cloud (ACC) platform's API for managing projects, folders, and documents.
+
+## Features
+
+- Authentication using 2-legged OAuth
+- Retrieval of hub, project, folder, and model details
+- Handling of project files and documents
+- Updating dwg custom attributes
+
+## Installation
+
+To use the APSTooler toolkit, follow these steps:
+
+1. install using `%pip install --upgrade APSTooler`
+2. Add a config.json file and define `APS Client ID`  and `APS Client Secret` like example below:
+ 
+     ```
+      {
+            "APS_CLIENT_ID":	    "your aps client id",
+            "APS_CLIENT_SECRET": 	"your aps client secret"
+      }
+      ```
+## Usage
+
+Refer to the documentation for detailed usage instructions and examples.
+
+## Contributing
+
+Contributions are welcome!
---
+++ packages/APSTooler/APSTooler-0.2.0.tar.gz/APSTooler-0.2.0/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md") as f:
+    if f is not None:
+        readme = f.read()
+
+setuptools.setup(
+    name="APSTooler",
+    version="0.2.0",
+    author="Olufemi Akinwumi",
+    author_email="cashimawo@gmail.com",
+    description="A Python toolkit for Autodesk Platform Services",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/01U2/APSTooler.git",
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=["Autodesk", "APS", "forge", "Python"],
+    license= "MIT",
+    package_dir={"": "APSTooler"},
+    packages=setuptools.find_packages(where="APSTooler"),
+    python_requires=">=3.9",
+    install_requires=['requests', 'comtypes'],
+    project_urls={  
+        'Homepage': 'https://github.com/01U2/APSTooler',
+        'Issues': 'https://github.com/01U2/APSTooler/blob/main/.github/ISSUE_TEMPLATE/bug_report.md',
+        'Documentation': 'https://github.com/01U2/APSTooler/blob/main/Documentation.md'
+    }
+)
