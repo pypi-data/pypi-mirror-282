996334c5043d000bf4c5283ed27e46dca2fbfe34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 03:03:38 +0000

    Add package routellm/0.1.0/routellm-0.1.0.tar.gz

---
+++ packages/routellm/routellm-0.1.0.tar.gz/routellm-0.1.0/routellm/calibrate_threshold.py
@@ -0,0 +1,58 @@
+import argparse
+import json
+
+import yaml
+from datasets import Dataset, load_dataset
+from pandarallel import pandarallel
+from tqdm import tqdm
+
+from routellm.routers.routers import ROUTER_CLS
+
+tqdm.pandas()
+pandarallel.initialize(progress_bar=True)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--battles_dataset", type=str, default="lmsys/lmsys-arena-human-preference-55k"
+    )
+    parser.add_argument("--config", type=str, default="config.example.yaml")
+    parser.add_argument(
+        "--routers",
+        nargs="+",
+        type=str,
+        default=["random"],
+        choices=list(ROUTER_CLS.keys()),
+    )
+    parser.add_argument("--strong-model-pct", type=float)
+    parser.add_argument(
+        "--task", type=str, choices=["generate", "calibrate"], default="calibrate"
+    )
+    args = parser.parse_args()
+
+    config = yaml.safe_load(open(args.config, "r"))
+
+    if args.task == "generate":
+        battles_df = load_dataset(args.battles_dataset, split="train").to_pandas()
+        for router in args.routers:
+            router = ROUTER_CLS[router](**config.get(router, {}))
+            if router.NO_PARALLEL:
+                battles_df[str(router)] = battles_df["prompt"].progress_apply(
+                    lambda x: router.calculate_strong_win_rate(json.loads(x)[0])
+                )
+            else:
+                battles_df[str(router)] = battles_df["prompt"].parallel_apply(
+                    lambda x: router.calculate_strong_win_rate(json.loads(x)[0])
+                )
+            Dataset.from_pandas(battles_df).push_to_hub(
+                "routellm/lmsys-arena-human-preference-55k-thresholds"
+            )
+    elif args.task == "calibrate":
+        thresholds_df = load_dataset(
+            "routellm/lmsys-arena-human-preference-55k-thresholds", split="train"
+        ).to_pandas()
+        for router in args.routers:
+            threshold = thresholds_df[router].quantile(q=1 - args.strong_model_pct)
+            print(
+                f"For {args.strong_model_pct * 100}% strong model calls, calibrated threshold for {router}: {threshold}"
+            )
