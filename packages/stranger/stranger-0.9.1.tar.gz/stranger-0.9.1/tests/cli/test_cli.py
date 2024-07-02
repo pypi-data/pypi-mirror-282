656702ac2556e8f207f1350b3ac65eb00ed74a60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 04:04:37 +0000

    Add package stranger/0.9.1/stranger-0.9.1.tar.gz

---
+++ packages/stranger/stranger-0.9.1.tar.gz/stranger-0.9.1/tests/cli/test_cli.py
@@ -0,0 +1,21 @@
+from click.testing import CliRunner
+
+from stranger.cli import cli
+
+
+def test_stranger_cli_version():
+    runner = CliRunner()
+    result = runner.invoke(cli, ["--version"])
+    assert result.exit_code == 0
+
+
+def test_stranger_cli(vcf_path):
+    runner = CliRunner()
+    result = runner.invoke(cli, [vcf_path])
+    assert result.exit_code == 0
+
+
+def test_stranger_cli_zipped(vcf_zipped_path):
+    runner = CliRunner()
+    result = runner.invoke(cli, [vcf_zipped_path])
+    assert result.exit_code == 0
