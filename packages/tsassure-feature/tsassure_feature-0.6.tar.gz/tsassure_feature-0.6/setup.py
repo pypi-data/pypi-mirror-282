c6d00fd87591f82ba8b3bc7e4bba9d379f2dd62d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 04:04:23 +0000

    Add package tsassure-feature/0.6/tsassure_feature-0.6.tar.gz

---
+++ packages/tsassure-feature/tsassure_feature-0.6.tar.gz/tsassure_feature-0.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name='tsassure_feature',
+    version='0.6',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'openpyxl',
+        # Add any additional dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            'tsassure=tsassure_feature.main:main',
+        ],
+    },
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
