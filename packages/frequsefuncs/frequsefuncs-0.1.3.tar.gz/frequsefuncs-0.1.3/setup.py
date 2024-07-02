d0cde76d0353136bbd828ac55661a78f874e71a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 03:03:59 +0000

    Add package frequsefuncs/0.1.3/frequsefuncs-0.1.3.tar.gz

---
+++ packages/frequsefuncs/frequsefuncs-0.1.3.tar.gz/frequsefuncs-0.1.3/setup.py
@@ -0,0 +1,31 @@
+
+
+from setuptools import setup, find_packages
+
+VERSION = '0.1.3' 
+DESCRIPTION = 'python package of frequently used functions'
+LONG_DESCRIPTION = 'python package of frequently used functions'
+
+# Setting up
+setup(
+       # the name must match the folder name 'frequsefuncs'
+        name="frequsefuncs",
+        version=VERSION,
+        author="y",
+        author_email="<y@outlook.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        setup_requires=['wheel'],
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Other Audience",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+        ]
+
+)
+
