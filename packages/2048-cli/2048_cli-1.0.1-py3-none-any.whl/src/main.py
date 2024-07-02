ee4d9bb583b92510391922ed7c51017e92aa5667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 15:08:44 +0000

    Add package 2048-cli/1.0.1/2048_cli-1.0.1-py3-none-any.whl

---
+++ packages/2048-cli/2048_cli-1.0.1-py3-none-any.whl/src/main.py
@@ -0,0 +1,25 @@
+from rich_menu import Menu
+
+from src.ai_playing.montecarlo import MonteCarlo
+from src.game import Game
+
+
+def main() -> None:
+    menu = Menu(
+        "Single Player",
+        "AI Playing",
+        "Exit",
+        title="2048",
+    )
+    while True:
+        match menu.ask():
+            case "Single Player":
+                Game().run()
+            case "AI Playing":
+                MonteCarlo().run()
+            case "Exit":
+                exit()
+
+
+if __name__ == "__main__":
+    main()
