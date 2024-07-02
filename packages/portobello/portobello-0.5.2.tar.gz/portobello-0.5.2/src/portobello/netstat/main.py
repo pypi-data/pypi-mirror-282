a7b1694418a03a9255b59f6c016383446e04a536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 16:05:49 +0000

    Add package portobello/0.5.2/portobello-0.5.2.tar.gz

---
+++ packages/portobello/portobello-0.5.2.tar.gz/portobello-0.5.2/src/portobello/netstat/main.py
@@ -0,0 +1,26 @@
+import socket
+
+from portobello.internal.utils import ask_for_input_or_list_choice
+
+
+def check_port(host, port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.settimeout(1)
+            s.connect((host, port))
+            return True
+        except (socket.timeout, socket.error):
+            return False
+
+
+def main(cli_strings: list, portobello_config: dict):
+    saved_hostnames = portobello_config['netstat']['hostnames']
+    hostname = ask_for_input_or_list_choice(saved_hostnames, 'hostname', 'hostnames', cli_strings=cli_strings)
+
+    if len(cli_strings) >= 2:
+        port_number = int(cli_strings[1])
+    else:
+        port_number = int(input('Please enter a port number to check:\n'))
+
+    print(f"####\nThe port you checked is{[' not', ''][check_port(hostname, port_number)]} open.\n####\n")
+    return [hostname, str(port_number)]
