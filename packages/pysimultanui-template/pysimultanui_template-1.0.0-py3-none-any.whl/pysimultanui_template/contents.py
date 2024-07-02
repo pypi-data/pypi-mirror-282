1423ebf00bd3694e9f015dafa78b1abd60627d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 16:05:36 +0000

    Add package pysimultanui-template/1.0.0/pysimultanui_template-1.0.0-py3-none-any.whl

---
+++ packages/pysimultanui-template/pysimultanui_template-1.0.0-py3-none-any.whl/pysimultanui_template/contents.py
@@ -0,0 +1,24 @@
+from PySimultan2.taxonomy_maps import Content
+
+contents = {}
+
+contents['attr1'] = Content(text_or_key='attr1',
+                            property_name='attr1',
+                            type=None,
+                            unit='',
+                            documentation='',
+                            component_policy='subcomponent')
+
+contents['attr2'] = Content(text_or_key='attr2',
+                            property_name='attr2',
+                            type=None,
+                            unit='',
+                            documentation='',
+                            component_policy='subcomponent')
+
+contents['attr3'] = Content(text_or_key='attr3',
+                            property_name='attr3',
+                            type=None,
+                            unit='',
+                            documentation='',
+                            component_policy='subcomponent')
---
+++ packages/pysimultanui-template/pysimultanui_template-1.0.0-py3-none-any.whl/pysimultanui_template/main.py
@@ -0,0 +1,6 @@
+from pysimultanui import run_ui
+
+from . import *
+
+if __name__ in {"__main__", "__mp_main__"}:
+    run_ui(reload=False)
