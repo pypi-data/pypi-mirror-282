9cf804951495effeddfd33dc4c7e62b8551c4923
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 04:04:45 +0000

    Add package bighummingbird/0.0.2024070101/bighummingbird-0.0.2024070101-py3-none-any.whl

---
+++ packages/bighummingbird/bighummingbird-0.0.2024070101-py3-none-any.whl/lyraml/utils/constants.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+BASE_URL=os.getenv("LYRAML_API_URL") or "https://lyraml-api-2-7c3apmua4a-uc.a.run.app"
+BASE_CLIENT_URL="http://localhost:3000"
+PROJECT_BASE_URL = BASE_URL + "/projects"
+RUN_BASE_URL = BASE_URL + "/runs"
+MODEL_BASE_URL = BASE_URL + "/models"
+DATASET_BASE_URL = BASE_URL + "/datasets"
+JUDGE_BASE_URL = BASE_URL + "/judges"
+EVALUATION_BASE_URL = BASE_URL + "/evaluations"
