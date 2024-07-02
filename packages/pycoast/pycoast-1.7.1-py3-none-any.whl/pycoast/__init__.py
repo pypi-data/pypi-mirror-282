5f117b56f8778713cd3be8d9e8d790c6c6715c59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 04:04:13 +0000

    Add package pycoast/1.7.1/pycoast-1.7.1-py3-none-any.whl

---
+++ packages/pycoast/pycoast-1.7.1-py3-none-any.whl/pycoast/__init__.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Pycoast package for adding geographic-based decorations to images."""
+from pycoast.cw_base import get_resolution_from_area  # noqa
+
+from .cw_agg import ContourWriterAGG  # noqa
+from .cw_pil import ContourWriterPIL
+from .version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
+
+
+class ContourWriter(ContourWriterPIL):
+    """Writer wrapper for deprecation warning.
+
+    .. deprecated:: 1.2.0
+
+        Use :class:`~pycoast.cw_pil.ContourWriterPIL` or :class:`~pycoast.cw_agg.ContourWriterAGG` instead.
+
+    """
+
+    def __init__(self, *args, **kwargs):
+        import warnings
+
+        warnings.warn(
+            "'ContourWriter' has been deprecated please use " "'ContourWriterPIL' or 'ContourWriterAGG' instead",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        super(ContourWriter, self).__init__(*args, **kwargs)
