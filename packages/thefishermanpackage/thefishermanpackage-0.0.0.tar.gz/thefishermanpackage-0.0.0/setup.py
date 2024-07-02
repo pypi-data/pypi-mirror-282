820675f4d6a9a393d6267a3188aa374d902aaed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 04:05:34 +0000

    Add package thefishermanpackage/0.0.0/thefishermanpackage-0.0.0.tar.gz

---
+++ packages/thefishermanpackage/thefishermanpackage-0.0.0.tar.gz/thefishermanpackage-0.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+name='thefishermanpackage',
+author='Fisher Man',
+author_email='thefishermanhacker@gmail.com',
+description='Just a test',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
