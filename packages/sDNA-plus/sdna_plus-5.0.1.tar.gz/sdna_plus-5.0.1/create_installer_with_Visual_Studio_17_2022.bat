4de42832760af30a5e36fadef41f911e8b6ad834
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 05:04:47 +0000

    Add package sDNA-plus/5.0.1/sdna_plus-5.0.1.tar.gz

---
+++ packages/sDNA-plus/sdna_plus-5.0.1.tar.gz/sdna_plus-5.0.1/create_installer_with_Visual_Studio_17_2022.bat
@@ -0,0 +1,24 @@
+setlocal
+
+set THIS_FILE_DIR=%~dp0
+set SRC_DIR=%THIS_FILE_DIR%
+set CONFIG=Release
+
+cmake -G "Visual Studio 17 2022" ^
+      -A Win32 ^
+      -B %THIS_FILE_DIR%\build_output_cmake_Win32 ^
+      -S %SRC_DIR% ^
+      -D USE_ZIG=OFF ^
+      -D BUNDLE_PYSHP=ON && ^ 
+cmake --build %THIS_FILE_DIR%\build_output_cmake_Win32 --config %CONFIG% && ^
+cmake -G "Visual Studio 17 2022" ^
+      -A x64 ^
+      -B %THIS_FILE_DIR%\build_output_cmake_x64 ^
+      -S %SRC_DIR% ^
+      -D USE_ZIG=OFF ^
+      -D BUNDLE_PYSHP=ON && ^
+cmake --build %THIS_FILE_DIR%\build_output_cmake_x64 --config %CONFIG% && ^
+AdvancedInstaller.com /build installerbits\advanced\sdna.aip && ^
+python -u installerbits\rename_version.py installerbits/advanced/output/sdna_setup.msi .
+
+@REM python -m pip install -r %THIS_FILE_DIR%\requirements\base.txt -r %THIS_FILE_DIR%\requirements\R_full.txt --target %THIS_FILE_DIR%\output\%CONFIG% && ^
