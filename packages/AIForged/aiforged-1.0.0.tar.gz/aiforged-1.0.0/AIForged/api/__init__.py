75a0a2650b2d244f60b54a9e6ea34da93255f3e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 15:11:20 +0000

    Add package AIForged/1.0.0/aiforged-1.0.0.tar.gz

---
+++ packages/AIForged/aiforged-1.0.0.tar.gz/aiforged-1.0.0/AIForged/api/__init__.py
@@ -0,0 +1,31 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.account_api import AccountApi
+from swagger_client.api.audit_api import AuditApi
+from swagger_client.api.bundles_api import BundlesApi
+from swagger_client.api.classes_api import ClassesApi
+from swagger_client.api.cloning_api import CloningApi
+from swagger_client.api.contact_api import ContactApi
+from swagger_client.api.data_set_api import DataSetApi
+from swagger_client.api.document_api import DocumentApi
+from swagger_client.api.group_api import GroupApi
+from swagger_client.api.log_api import LogApi
+from swagger_client.api.market_place_api import MarketPlaceApi
+from swagger_client.api.notification_api import NotificationApi
+from swagger_client.api.param_def_api import ParamDefApi
+from swagger_client.api.parameters_api import ParametersApi
+from swagger_client.api.payment_api import PaymentApi
+from swagger_client.api.project_api import ProjectApi
+from swagger_client.api.reports_api import ReportsApi
+from swagger_client.api.roles_api import RolesApi
+from swagger_client.api.service_type_api import ServiceTypeApi
+from swagger_client.api.services_api import ServicesApi
+from swagger_client.api.system_api import SystemApi
+from swagger_client.api.user_preferences_api import UserPreferencesApi
+from swagger_client.api.user_settings_api import UserSettingsApi
+from swagger_client.api.verification_api import VerificationApi
+from swagger_client.api.wizard_api import WizardApi
+from swagger_client.api.work_item_api import WorkItemApi
