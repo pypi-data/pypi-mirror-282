a9fb115207935c999a48d10c92e7a28ff3aa3880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 06:04:50 +0000

    Add package rapid-qc-ms/1.1.0/rapid_qc_ms-1.1.0.tar.gz

---
+++ packages/rapid-qc-ms/rapid_qc_ms-1.1.0.tar.gz/rapid_qc_ms-1.1.0/src/rapidqcms/__main__.py
@@ -0,0 +1,21 @@
+from rapidqcms.DashWebApp import *
+
+def main():
+
+    """
+    Opens web browser and starts Flask server for Dash app
+    """
+    logging.basicConfig(filename="rapid-qc-ms.log", level=logging.INFO)
+    # Opens localhost:8050 in Google Chrome
+    if sys.platform == "win32":
+        chrome_path = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+        webbrowser.register("chrome", None, webbrowser.BackgroundBrowser(chrome_path))
+        webbrowser.get("chrome").open("http://127.0.0.1:8050/")
+    elif sys.platform == "darwin":
+        webbrowser.get("chrome").open("http://127.0.0.1:8050/", new=1)
+
+    # Start Dash app on port 8050
+    app.run_server(threaded=False, debug=False, port=8050)
+
+if __name__ == "__main__":
+    main()
