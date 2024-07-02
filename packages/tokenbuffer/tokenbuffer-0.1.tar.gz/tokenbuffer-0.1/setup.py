91b8038076b1d152650cc598a3870e76793d33df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 15:10:19 +0000

    Add package tokenbuffer/0.1/tokenbuffer-0.1.tar.gz

---
+++ packages/tokenbuffer/tokenbuffer-0.1.tar.gz/tokenbuffer-0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+with open('readme.md', 'r') as readme:
+    long_description = readme.read()
+
+setup(
+    name = 'tokenbuffer',
+    version = '0.1',
+    description = 'A tokenizer with file position tracking and backtracking suitable for making parsers.',
+    package_dir = {"": "src"},
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    author = 'pawkw',
+    author_email = 'pawkw@users.github.com',
+    license = 'MIT',
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10',
+        'Operating System :: OS Independent'
+    ],
+    python_requires = '>=3.10'
+)
