96afbc89b8df59b09983de9f4b1f7f39ce63e051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 14:05:29 +0000

    Add package summawise/0.2.2/summawise-0.2.2-py3-none-any.whl

---
+++ packages/summawise/summawise-0.2.2-py3-none-any.whl/summawise/utils.py
@@ -0,0 +1,24 @@
+import tempfile
+from pathlib import Path
+
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+def get_summawise_dir() -> Path:
+    temp_dir = Path(tempfile.gettempdir())
+    return temp_dir / "summawise"
+
+def fp(file_path: Path) -> Path:
+    """
+    Patch a given 'Path' object in a specific scenario:
+    The correct path is the same exact location, but with a .gz suffix, denoting gzip compression.
+    """
+    if not file_path.exists():
+        gz_path = file_path.with_suffix(file_path.suffix + ".gz")
+        if gz_path.exists():
+            return gz_path
+    return file_path
