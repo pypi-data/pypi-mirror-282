9110c463ade6b69d15e1376ddb7b9b95bc8f7d83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 06:04:53 +0000

    Add package HIDDENLIB/1.0.0/HIDDENLIB-1.0.0.tar.gz

---
+++ packages/HIDDENLIB/HIDDENLIB-1.0.0.tar.gz/HIDDENLIB-1.0.0/hiddenlib/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hiddelib",
+    version="1.0",
+    packages=find_packages(),
+    description="HiddenTool",
+    author="@agenthidden",
+    author_email="doesnts@example.com",
+    url="https://github.com/yourusername/softhidden",
+    install_requires=[],
+)
---
+++ packages/HIDDENLIB/HIDDENLIB-1.0.0.tar.gz/HIDDENLIB-1.0.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+with open(r'D:\scripts\hiddenlib\hiddenlib\README.md', 'r', encoding='utf-8') as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name='HIDDENLIB',
+	version='1.0.0',
+	author='agenthiddenoriginal',
+	author_email='tejajat421@joeroc.com',
+	description='HiddenTool',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	url='https://github.com/SuperZombi/Pypi-uploader',
+	packages=['hiddenlib'],
+	include_package_data=True,
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
