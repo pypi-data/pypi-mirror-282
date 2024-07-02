cdcc6fe7b5a43c6512ca1cc81bc250b4831b69c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 04:05:12 +0000

    Add package paketlib/0.0.1/paketlib-0.0.1.tar.gz

---
+++ packages/paketlib/paketlib-0.0.1.tar.gz/paketlib-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='paketlib',
+    version='0.0.1',
+    description='PaketSoftware lib',
+    author='PaketSoftware',
+    packages=find_packages(), 
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
