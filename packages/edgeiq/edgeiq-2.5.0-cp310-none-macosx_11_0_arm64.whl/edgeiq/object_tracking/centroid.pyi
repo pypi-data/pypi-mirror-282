7419b1c9028ba56b580146e06ab14fc3aea08a9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 05:04:45 +0000

    Add package edgeiq/2.5.0/edgeiq-2.5.0-cp310-none-macosx_11_0_arm64.whl

---
+++ packages/edgeiq/edgeiq-2.5.0-cp310-none-macosx_11_0_arm64.whl/edgeiq/object_tracking/centroid.pyi
@@ -0,0 +1,6 @@
+from .matchers import match_greedy as match_greedy
+from .object_tracking import DEFAULT_DEREGISTER_FRAMES as DEFAULT_DEREGISTER_FRAMES, DEFAULT_HISTORY_LENGTH as DEFAULT_HISTORY_LENGTH, DEFAULT_MAX_DISTANCE as DEFAULT_MAX_DISTANCE, DEFAULT_MIN_INERTIA as DEFAULT_MIN_INERTIA, TrackerAlgorithm as TrackerAlgorithm
+from edgeiq.object_tracking.trackable_prediction import PredictionT as PredictionT, TrackablePrediction as TrackablePrediction, TrackerCbT as TrackerCbT
+
+class CentroidTracker(TrackerAlgorithm[TrackablePrediction]):
+    def __init__(self, deregister_frames: int = ..., max_distance: int = ..., min_inertia: int = ..., history_length: int = ..., enter_cb: TrackerCbT | None = None, exit_cb: TrackerCbT | None = None) -> None: ...
---
+++ packages/edgeiq/edgeiq-2.5.0-cp310-none-macosx_11_0_arm64.whl/edgeiq/runtime/onnx_rt.pyi
@@ -0,0 +1,14 @@
+import numpy as np
+from .runtime import Runtime
+from _typeshed import Incomplete
+from edgeiq.model_config import ModelConfig
+
+__all__ = ['OnnxRT']
+
+class OnnxRT(Runtime):
+    net: Incomplete
+    input_name: Incomplete
+    layer_names: Incomplete
+    def __init__(self, model_config: ModelConfig) -> None: ...
+    def forward(self): ...
+    def set_input(self, image: np.ndarray): ...
