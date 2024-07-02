0de0af755045eecec9f41207dc471351281ef7f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 16:05:39 +0000

    Add package AdsbAnomalyDetector/0.5.2/AdsbAnomalyDetector-0.5.2.tar.gz

---
+++ packages/AdsbAnomalyDetector/AdsbAnomalyDetector-0.5.2.tar.gz/AdsbAnomalyDetector-0.5.2/AdsbAnomalyDetector/B_Model_ReplaySolver_HASH.py
@@ -0,0 +1,120 @@
+from ._Utils_numpy import np, ax
+from ._Utils_os_wrapper import os
+
+from .B_Model_AbstractModel import Model as AbstactModel
+
+from ._Utils_ProgressBar import ProgressBar
+from . import B_Model_ReplaySolver_Utils_hashing as hashing
+
+
+# |====================================================================================================================
+# | GLOBAL VARIABLES
+# |====================================================================================================================
+
+
+PBM_NAME = os.path.dirname(os.path.abspath(__file__)).split("/")[-1]
+BAR = ProgressBar()
+
+
+
+class Model(AbstactModel):
+
+    name = "HASH"
+
+    hashtable:"dict[int, list[str]]"
+
+# |====================================================================================================================
+# |    INITIALIZATION
+# |====================================================================================================================
+
+    def __init__(self, CTX:dict) -> None:
+        # Public attributes
+        self.CTX = CTX
+        hashing.init_hash(CTX["HISTORY"])
+        self.hashtable = {}
+
+
+
+# |====================================================================================================================
+# |    PREDICTION
+# |====================================================================================================================
+
+
+    def predict(self, x:np.float64_3d[ax.sample, ax.time, ax.feature]) -> "list[list[str]]":
+
+        if (x.shape[1] != self.CTX["HISTORY"]):
+            raise ValueError("The input shape is not correct !")
+
+        y_ = []
+        for b in range(len(x)):
+            fps = hashing.sub_fingerprint(x[b, :, 0])
+            hashes = hashing.hash(fps)
+            matches = hashing.match(hashes, self.hashtable)
+            y_.append(matches)
+
+        return y_
+
+
+
+    def compute_loss(self, x:np.float64_3d[ax.sample, ax.time, ax.feature], y:np.str_1d[ax.sample]) -> """
+            tuple[float, list[list[str]]]""":
+
+        y_ = self.predict(x)
+
+        accuracy, size = 0, 0
+        for s in range(len(y_)):
+            if (len(y_[s]) == 0):
+                y_[s].append("unknown")
+
+            if (y[s] == np.nan):
+                continue
+
+            nb_correct = 0
+            for pred in y_[s]:
+                if (pred == y[s]):
+                    nb_correct += 1
+
+            if (len(y_[s]) > 0):
+                accuracy += nb_correct / len(y_[s])
+            size += 1
+
+        return accuracy / size, y_
+
+
+
+    def training_step(self, x:np.float64_3d[ax.sample, ax.time, ax.feature], y:np.str_1d[ax.sample]) -> None:
+        for b in range(len(x)):
+            if (y[b] == np.nan):
+                continue
+
+            fps = hashing.sub_fingerprint(x[b, :, 0])
+            hashes = hashing.hash(fps)
+
+            for h in range(len(hashes)):
+                matches = self.hashtable.get(hashes[h], None)
+                if (matches is None):
+                    self.hashtable[hashes[h]] = set([y[b]])
+                else:
+                    self.hashtable[hashes[h]].add(y[b])
+
+
+
+
+    def visualize(self, save_path="./_Artifacts/"):
+        """
+        Generate a visualization of the model's architecture
+        """
+
+
+    def get_variables(self):
+        """
+        Return the variables of the model
+        """
+        return self.hashtable
+
+
+    def set_variables(self, variables):
+        """
+        Set the variables of the model
+        """
+        self.hashtable = variables
