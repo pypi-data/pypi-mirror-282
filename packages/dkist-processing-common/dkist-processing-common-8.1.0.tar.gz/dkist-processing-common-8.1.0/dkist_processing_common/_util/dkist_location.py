f67f8735daeb3e153ac7166e1c0e2f94f5c99bfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 04:06:11 +0000

    Add package dkist-processing-common/8.1.0/dkist-processing-common-8.1.0.tar.gz

---
+++ packages/dkist-processing-common/dkist-processing-common-8.1.0.tar.gz/dkist-processing-common-8.1.0/dkist_processing_common/_util/dkist_location.py
@@ -0,0 +1,30 @@
+"""Fault tolerant retrieval of the dkist earth location."""
+from astropy import units as u
+from astropy.coordinates import EarthLocation
+
+
+def location_of_dkist() -> EarthLocation:
+    """Return hard-coded EarthLocation of the DKIST.
+
+    Cartesian geocentric coordinates of DKIST on Earth as retrieved from
+    https://github.com/astropy/astropy-data/blob/gh-pages/coordinates/sites.json#L838
+    """
+    _dkist_site_info = {
+        "aliases": ["DKIST", "ATST"],
+        "name": "Daniel K. Inouye Solar Telescope",
+        "elevation": 3067,
+        "elevation_unit": "meter",
+        "latitude": 20.7067,
+        "latitude_unit": "degree",
+        "longitude": 203.7436,
+        "longitude_unit": "degree",
+        "timezone": "US/Hawaii",
+        "source": "DKIST website: https://www.nso.edu/telescopes/dki-solar-telescope/",
+    }
+    location_of_dkist = EarthLocation.from_geodetic(
+        _dkist_site_info["longitude"] * u.Unit(_dkist_site_info["longitude_unit"]),
+        _dkist_site_info["latitude"] * u.Unit(_dkist_site_info["latitude_unit"]),
+        _dkist_site_info["elevation"] * u.Unit(_dkist_site_info["elevation_unit"]),
+    )
+
+    return location_of_dkist
