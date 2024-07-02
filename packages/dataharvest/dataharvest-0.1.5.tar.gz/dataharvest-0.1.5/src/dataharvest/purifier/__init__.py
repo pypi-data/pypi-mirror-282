f2b64c2c7217bff51cf0cbcde43a8d46ce239d78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 16:05:25 +0000

    Add package dataharvest/0.1.5/dataharvest-0.1.5.tar.gz

---
+++ packages/dataharvest/dataharvest-0.1.5.tar.gz/dataharvest-0.1.5/src/dataharvest/purifier/__init__.py
@@ -0,0 +1,12 @@
+from dataharvest.purifier.baidu_purifier import BaiduBaikePurifier
+from dataharvest.purifier.common_purifier import CommonPurifier
+from dataharvest.purifier.sohu_purifier import SohuPurifier
+from dataharvest.purifier.sll_purifier import SllPurifier
+from dataharvest.purifier.baijiahao_purifier import BaiJiaHaoPurifier
+from dataharvest.purifier.wangyi_purifier import WangYiPurifier
+from dataharvest.purifier.purifier import AutoPurifier, BasePurifier
+
+__all__ = ["BasePurifier", "CommonPurifier", "AutoPurifier", "BaiduBaikePurifier",
+           "SohuPurifier", "SllPurifier", "BaiJiaHaoPurifier", "WangYiPurifier"]
+
+
---
+++ packages/dataharvest/dataharvest-0.1.5.tar.gz/dataharvest-0.1.5/tests/test_purifiter.py
@@ -0,0 +1,47 @@
+from dataharvest.purifier.purifier import AutoPurifier
+from dataharvest.spider.spider import AutoSpider
+
+
+def test_auto_purifier():
+    url = "https://zhuanlan.zhihu.com/p/369984873"
+    auto_spider = AutoSpider()
+    doc = auto_spider.crawl(url)
+    auto_purifier = AutoPurifier()
+    doc = auto_purifier.purify(doc)
+    print(doc)
+
+
+def test_auto_purifier_sohu():
+    url = "https://www.sohu.com/a/325718406_120013344"
+    auto_spider = AutoSpider()
+    doc = auto_spider.crawl(url)
+    auto_purifier = AutoPurifier()
+    doc = auto_purifier.purify(doc)
+    print(doc)
+
+
+def test_auto_purifier_sll():
+    url = "http://www.360doc.com/content/23/0613/11/72042116_1084562587.shtml"
+    auto_spider = AutoSpider()
+    doc = auto_spider.crawl(url)
+    auto_purifier = AutoPurifier()
+    doc = auto_purifier.purify(doc)
+    print(doc)
+
+
+def test_auto_purifier_baijiahao():
+    url = "https://baijiahao.baidu.com/s?id=1800439094856373024"
+    auto_spider = AutoSpider()
+    doc = auto_spider.crawl(url)
+    auto_purifier = AutoPurifier()
+    doc = auto_purifier.purify(doc)
+    print(doc)
+
+
+def test_auto_purifier_wangyi():
+    url = "https://www.163.com/auto/article/J6361L350008856R.html?clickfrom=w_lb_4_big"
+    auto_spider = AutoSpider()
+    doc = auto_spider.crawl(url)
+    auto_purifier = AutoPurifier()
+    doc = auto_purifier.purify(doc)
+    print(doc)
