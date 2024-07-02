3ab213eb61039b035f5d7e9184865349828f3fe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 03:03:43 +0000

    Add package Dalood/2024.5/dalood-2024.5-py3-none-any.whl

---
+++ packages/Dalood/dalood-2024.5-py3-none-any.whl/dalood/data_wrapper.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+Data wrapper class.
+"""
+
+import datetime
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+
+class DataWrapper:
+    """
+    Wrapper around loaded data that stores that data and a loader reference, and
+    tracks loading and access times.
+    """
+
+    def __init__(self, loader, src):
+        self.loader = loader
+        self.src = src
+        self.data = None
+        self.load_time = None
+        self.access_time = None
+
+    def load(self):
+        """
+        Load the data for the given source.
+
+        Args:
+            src:
+                The data source (e.g. a file or URI, or whatever else the
+                matching loader can handle).
+        """
+        self.data = self.loader.load(self.src)
+        self.load_time = datetime.datetime.now(datetime.UTC)
+        self.access_time = self.load_time
+
+    def refresh(self):
+        """
+        Reload data if the loader reports that the data has changed since it was
+        last loaded.
+        """
+        mtime = self.loader.get_mtime(self.src)
+        if mtime is not None and mtime > self.load_time:
+            LOGGER.debug("Refreshing %s", self.src)
+            self.load()
+
+    def access(self):
+        """
+        Access the data. It will be loaded first if necessary.
+        """
+        if self.load_time is None:
+            self.load()
+        self.access_time = datetime.datetime.now(datetime.UTC)
+        return self.data
