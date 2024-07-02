87af9195ebf9c669a540db96c8c2816a1efd6736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 15:08:44 +0000

    Add package examplemul/0.1.1/examplemul-0.1.1.tar.gz

---
+++ packages/examplemul/examplemul-0.1.1.tar.gz/examplemul-0.1.1/setup.py
@@ -0,0 +1,21 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='examplemul',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[],  # List your dependencies here
+    author='Nandhini',
+    author_email='nandhini67288@gmail.com',
+    description='A simple example library to perform multiplication',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/nandhinikesavan20/hello_world',  # Update with your URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
