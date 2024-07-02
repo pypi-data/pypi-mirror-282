e7b64a5f979e213374614539767af16105df414d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 15:10:19 +0000

    Add package dataharvest/0.1.4/dataharvest-0.1.4.tar.gz

---
+++ packages/dataharvest/dataharvest-0.1.4.tar.gz/dataharvest-0.1.4/tests/test_purifiter.py
@@ -0,0 +1,20 @@
+from dataharvest.purifier.purifier import AutoPurifier
+from dataharvest.spider.spider import AutoSpider
+
+
+def test_auto_purifier():
+    url = "https://zhuanlan.zhihu.com/p/369984873"
+    auto_spider = AutoSpider()
+    doc = auto_spider.crawl(url)
+    auto_purifier = AutoPurifier()
+    doc = auto_purifier.purify(doc)
+    print(doc)
+
+
+def test_auto_purifier_sohu():
+    url = "https://www.sohu.com/a/325718406_120013344"
+    auto_spider = AutoSpider()
+    doc = auto_spider.crawl(url)
+    auto_purifier = AutoPurifier()
+    doc = auto_purifier.purify(doc)
+    print(doc)
