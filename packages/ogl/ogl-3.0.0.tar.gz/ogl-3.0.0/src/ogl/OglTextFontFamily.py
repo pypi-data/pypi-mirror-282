eba41f7a944fef77e2e3c2156284e1d8d4d8280a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 04:06:03 +0000

    Add package ogl/3.0.0/ogl-3.0.0.tar.gz

---
+++ packages/ogl/ogl-3.0.0.tar.gz/ogl-3.0.0/src/ogl/OglTextFontFamily.py
@@ -0,0 +1,13 @@
+
+from enum import Enum
+
+
+class OglTextFontFamily(Enum):
+    """
+    How we specify fonts for Pyut
+    """
+    SWISS    = 'Swiss'
+    MODERN   = 'Modern'
+    ROMAN    = 'Roman'
+    SCRIPT   = 'Script'
+    TELETYPE = 'Teletype'
