8f34b3a3f1be9d30e98e077c09183243467e29ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 05:04:45 +0000

    Add package edgeiq/2.5.0/edgeiq-2.5.0-cp310-none-macosx_10_9_x86_64.whl

---
+++ packages/edgeiq/edgeiq-2.5.0-cp310-none-macosx_10_9_x86_64.whl/edgeiq/runtime/__init__.pyi
@@ -0,0 +1,8 @@
+from .hailo_rt import HailoRT as HailoRT
+from .ocv import OpenCV as OpenCV
+from .onnx_rt import OnnxRT as OnnxRT
+from .qaic_rt import QaicRT as QaicRT
+from .runtime import Runtime as Runtime
+from .trt import TensorRT as TensorRT
+
+__all__ = ['Runtime', 'TensorRT', 'OpenCV', 'HailoRT', 'QaicRT', 'OnnxRT']
