0909202e8a7cee7d671ca42195f36d3c3fc2f362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 03:04:33 +0000

    Add package HorridAPI/1.4/horridapi-1.4.tar.gz

---
+++ packages/HorridAPI/horridapi-1.4.tar.gz/horridapi-1.4/HorridAPI/__init__.py
@@ -0,0 +1,38 @@
+import requests
+from urllib.parse import quote
+
+__version__ = "1.4"
+
+
+class HorridAPI:
+
+    def __init__(self) -> None:
+        pass
+
+    def joke():
+        api = f'https://horrid-api.onrender.com/joke'
+        res = requests.get(api).json()
+        joke = res['joke']
+        return joke
+        
+    def dare():
+        api = f'https://horrid-api.onrender.com/dare'
+        res = requests.get(api).json()
+        dare = res['dare']
+        return dare
+  
+    def truth():
+        api = f'https://horrid-api.onrender.com/truth'
+        res = requests.get(api).json()
+        k = res['truth']
+        return k
+
+    def gpt(query: str):
+        prompt = quote(query)
+        api = f'https://horrid-api.onrender.com/gpt?query={prompt}'
+        res = requests.get(api).json()
+        k = res['response']
+        return k    
+
+
+api = HorridAPI()
