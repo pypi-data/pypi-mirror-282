ec451e16f3182dd5ed8ca4435fb7a4c81863b5b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 16:05:41 +0000

    Add package tortreinador/0.0.10/tortreinador-0.0.10-py3-none-any.whl

---
+++ packages/tortreinador/tortreinador-0.0.10-py3-none-any.whl/tortreinador/utils/Recorder.py
@@ -0,0 +1,35 @@
+import torch
+
+
+class Recorder:
+    def __init__(self, device):
+        super().__init__()
+        self.device = device
+        self.val = torch.Tensor([]).to(self.device)
+        self.is_check = False
+
+    def update(self, val):
+        if not self.is_check:
+            if not _check_type(self.val, val):
+                self.val.to(val.dtype)
+                self.is_check = True
+
+            else:
+                self.is_check = True
+
+        val = val.unsqueeze(0)
+        self.val = torch.cat((self.val, val), 0)
+
+    def avg(self):
+        return torch.mean(self.val.mean(), dim=0).unsqueeze(0)
+
+    def reset(self):
+        self.val = torch.Tensor([]).to(self.device)
+
+
+def _check_type(x, y):
+    if isinstance(x, torch.Tensor) and isinstance(y, torch.Tensor):
+        return x.dtype == y.dtype
+
+    else:
+        return False
