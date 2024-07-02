9e2d6c95efc62e9990f6e75c3c7081c99d9830d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 03:04:03 +0000

    Add package jupyter-scatter/0.17.1/jupyter_scatter-0.17.1-py2.py3-none-any.whl

---
+++ packages/jupyter-scatter/jupyter_scatter-0.17.1-py2.py3-none-any.whl/jscatter/__init__.py
@@ -0,0 +1,13 @@
+try:
+    from importlib.metadata import PackageNotFoundError, version
+except ImportError:
+    from importlib_metadata import PackageNotFoundError, version  # type: ignore
+
+try:
+    __version__ = version("jupyter-scatter")
+except PackageNotFoundError:
+    __version__ = "uninstalled"
+
+from .jscatter import Scatter, plot
+from .compose import compose, link
+from .color_maps import okabe_ito, glasbey_light, glasbey_dark
