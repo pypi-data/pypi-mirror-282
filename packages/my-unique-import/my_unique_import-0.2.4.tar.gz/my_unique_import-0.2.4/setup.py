5da983c3e4687a08f749a33d94d6c1e757f0a2b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 04:05:07 +0000

    Add package my-unique-import/0.2.4/my_unique_import-0.2.4.tar.gz

---
+++ packages/my-unique-import/my_unique_import-0.2.4.tar.gz/my_unique_import-0.2.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my_unique_import",
+    version="0.2.4",
+    packages=find_packages(),
+    include_package_data=True,
+    description="A custom importer package",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="http://example.com",
+    author="Jingyuan Chen",
+    author_email="jchensteve@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'qgit=quick_git:main',  # 格式：命令名=模块路径:函数名
+        ],
+    },
+    python_requires='>=3.7',
+)
