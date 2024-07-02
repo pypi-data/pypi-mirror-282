64765cd860376d63aefe7fea8168718bb39514be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 04:06:14 +0000

    Add package paketlib/0.0.0.3/paketlib-0.0.0.3-py3-none-any.whl

---
+++ packages/paketlib/paketlib-0.0.0.3-py3-none-any.whl/paketlib/__init__.py
@@ -0,0 +1,10 @@
+import paketlib.console
+import paketlib.api
+import paketlib.paket
+import paketlib.path
+import paketlib.pkcrypt
+import paketlib.proto
+import paketlib.search
+
+if __name__ == '__main__':
+    print('ну тут скора чота будет наверна')
