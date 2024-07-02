c89ee209a1f0f7a57d263439271917bf75d33b5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 14:05:17 +0000

    Add package dialog-lib/0.0.2.0a1/dialog_lib-0.0.2.0a1-py3-none-any.whl

---
+++ packages/dialog-lib/dialog_lib-0.0.2.0a1-py3-none-any.whl/dialog_lib/agents/llama_cpp.py
@@ -0,0 +1,18 @@
+from .abstract import AbstractDialog
+from langchain_openai.chat_models.base import ChatOpenAI
+
+
+class DialogOpenAI(AbstractDialog):
+    def __init__(self, *args, **kwargs):
+        model = kwargs.pop("model", "gpt-3.5-turbo")
+        temperature = kwargs.pop("temperature", 0.1)
+
+        kwargs["model_class"] = ChatOpenAI(
+            model=model,
+            temperature=temperature,
+            openai_api_key=kwargs.get("llm_api_key"),
+        )
+        super().__init__(*args, **kwargs)
+
+    def postprocess(self, output):
+        return output.get("text")
