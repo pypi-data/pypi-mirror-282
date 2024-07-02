c2067fbdff07756b2b7ff973404ac362aee6305a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 16:05:32 +0000

    Add package iwopy/0.2.2/iwopy-0.2.2-py3-none-any.whl

---
+++ packages/iwopy/iwopy-0.2.2-py3-none-any.whl/iwopy/__init__.py
@@ -0,0 +1,28 @@
+"""
+Fraunhofer IWES optimization tools in Python
+    
+"""
+
+from .core import Problem, Objective, Constraint, Memory
+from .wrappers import (
+    ProblemWrapper,
+    DiscretizeRegGrid,
+    LocalFD,
+    SimpleProblem,
+    SimpleObjective,
+    SimpleConstraint,
+)
+
+from . import utils
+from . import interfaces
+from . import benchmarks
+from . import optimizers
+
+try:
+    from importlib.resources import files
+
+    __version__ = files(__package__).joinpath("VERSION").read_text()
+except ImportError:
+    from importlib.resources import read_text
+
+    __version__ = read_text(__package__, "VERSION")
