0ca99938ed012051b000b06d093e4feb8a4e3d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 15:10:33 +0000

    Add package linkml-store/0.1.10/linkml_store-0.1.10-py3-none-any.whl

---
+++ packages/linkml-store/linkml_store-0.1.10-py3-none-any.whl/linkml_store/api/config.py
@@ -0,0 +1,113 @@
+from typing import Any, Dict, List, Optional
+
+from pydantic import BaseModel, Field
+
+
+class ConfiguredBaseModel(BaseModel, extra="forbid"):
+    pass
+
+
+class CollectionConfig(ConfiguredBaseModel):
+    name: Optional[str] = Field(
+        default=None,
+        description="An optional name for the collection",
+    )
+    alias: Optional[str] = Field(
+        default=None,
+        description="An optional alias for the collection",
+    )
+    type: Optional[str] = Field(
+        default=None,
+        description="The type of object in the collection. TODO; use this instead of name",
+    )
+    additional_properties: Optional[Dict] = Field(
+        default=None,
+        description="Optional metadata for the collection",
+    )
+    attributes: Optional[Dict[str, Dict]] = Field(
+        default=None,
+        description="Optional attributes for the collection, following LinkML schema",
+    )
+    indexers: Optional[Dict[str, Dict]] = Field(
+        default=None,
+        description="Optional configuration for indexers",
+    )
+    hidden: Optional[bool] = Field(
+        default=False,
+        description="Whether the collection is hidden",
+    )
+    is_prepopulated: Optional[bool] = Field(
+        default=False,
+        description="Whether the collection is prepopulated",
+    )
+    source_location: Optional[str] = Field(
+        default=None,
+        description="Filesystem or remote URL that stores the data",
+    )
+
+
+class DatabaseConfig(ConfiguredBaseModel):
+    handle: str = Field(
+        default="duckdb:///:memory:",
+        description="The database handle, e.g., 'duckdb:///:memory:' or 'mongodb://localhost:27017'",
+    )
+    alias: Optional[str] = Field(
+        default=None,
+        description="An optional alias for the database",
+    )
+    schema_location: Optional[str] = Field(
+        default=None,
+        description="The location of the schema file, either a path on disk or URL",
+    )
+    schema_dict: Optional[Dict[str, Any]] = Field(
+        default=None,
+        description="The LinkML schema as a dictionary",
+    )
+    collections: Optional[Dict[str, CollectionConfig]] = Field(
+        default={},
+        description="A dictionary of collection configurations",
+    )
+    recreate_if_exists: bool = Field(
+        default=False,
+        description="Whether to recreate the database if it already exists",
+    )
+    collection_type_slot: Optional[str] = Field(
+        default=None,
+        description=(
+            "For databases that combine multiple collections into a single space, this field"
+            "specifies the field that contains the collection type. An example of this is a Solr"
+            "index that does not use cores for collections, and instead uses a single global"
+            "document space; if this has a field 'document_type', then this field should be set"
+        ),
+    )
+    searchable_slots: Optional[List[str]] = Field(
+        default=None,
+        description="Optional configuration for search fields",
+    )
+    ensure_referential_integrity: bool = Field(
+        default=False,
+        description="Whether to ensure referential integrity",
+    )
+
+
+class ClientConfig(ConfiguredBaseModel):
+    handle: Optional[str] = Field(
+        default=None,
+        description="The client handle",
+    )
+    databases: Dict[str, DatabaseConfig] = Field(
+        default={},
+        description="A dictionary of database configurations",
+    )
+    default_database: Optional[str] = Field(
+        default=None,
+        description="The default database",
+    )
+    schema_path: Optional[str] = Field(
+        default=None,
+        description="The path to the LinkML schema file",
+    )
+    base_dir: Optional[str] = Field(
+        default=None,
+        description="The base directory for the client",
+    )
---
+++ packages/linkml-store/linkml_store-0.1.10-py3-none-any.whl/linkml_store/index/__init__.py
@@ -0,0 +1,39 @@
+from typing import Type
+
+from linkml_store.index.implementations.llm_indexer import LLMIndexer
+from linkml_store.index.implementations.simple_indexer import SimpleIndexer
+from linkml_store.index.indexer import Indexer
+
+INDEXER_CLASSES = {
+    "simple": SimpleIndexer,
+    "llm": LLMIndexer,
+}
+
+
+def get_indexer_class(name: str) -> Type[Indexer]:
+    """
+    Get an indexer class by name.
+
+    :param name: the name of the indexer
+    :return: the indexer class
+    """
+    if name not in INDEXER_CLASSES:
+        raise ValueError(f"Unknown indexer class: {name}")
+    return INDEXER_CLASSES[name]
+
+
+def get_indexer(index_type: str, **kwargs) -> Indexer:
+    """
+    Get an indexer by name.
+
+    :param name: the name of the indexer
+    :param kwargs: additional arguments to pass to the indexer
+    :return: the indexer
+    """
+    kwargs = {k: v for k, v in kwargs.items() if v is not None}
+    cls = get_indexer_class(index_type)
+    kwargs["index_type"] = index_type
+    indexer = cls(**kwargs)
+    if not indexer.name:
+        indexer.name = index_type
+    return indexer
