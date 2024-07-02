7580d359d4e29c7e99f43873d0b48e4b55df311a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-02 03:04:18 +0000

    Add package azaux/0.1.1/azaux-0.1.1.tar.gz

---
+++ packages/azaux/azaux-0.1.1.tar.gz/azaux-0.1.1/azaux/__init__.py
@@ -0,0 +1,9 @@
+from azaux.container_manager import ContainerManager
+from azaux.queue_manager import QueueManager
+from azaux.table_manager import TableManager
+
+__all__ = [
+    "ContainerManager",
+    "QueueManager",
+    "TableManager",
+]
