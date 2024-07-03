8efdb39b5ea106014b45562230658b173c3b17af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 15:05:07 +0000

    Add package cloudzeta-sdk/0.4.2/cloudzeta_sdk-0.4.2.tar.gz

---
+++ packages/cloudzeta-sdk/cloudzeta_sdk-0.4.2.tar.gz/cloudzeta_sdk-0.4.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="cloudzeta-sdk",
+    packages=["zeta.sdk", "zeta.utils"],
+    author="Zeta Computing, Inc.",
+    author_email="dev@cloudzeta.com",
+)
