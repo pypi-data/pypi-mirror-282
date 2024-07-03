d6ab270d2146f3966826feeccde929d6a3688b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 16:05:38 +0000

    Add package emtechstack/0.0.96/emtechstack-0.0.96.tar.gz

---
+++ packages/emtechstack/emtechstack-0.0.96.tar.gz/emtechstack-0.0.96/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="emtechstack",
+    version="0.0.96",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "click",
+        "black",
+        "PyYAML",
+        "requests",
+        "tabulate",
+        "termcolor",
+    ],
+    entry_points={
+        "console_scripts": [
+            "emtechstack=emtechstack.cli:cli",
+            "e9k=emtechstack.cli:cli",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
