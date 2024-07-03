27c9a3ec86c1c808b62e5eb6825fd1ef30551094
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 05:04:44 +0000

    Add package jgtml/0.0.81/jgtml-0.0.81.tar.gz

---
+++ packages/jgtml/jgtml-0.0.81.tar.gz/jgtml-0.0.81/jgtml/__init__.py
@@ -0,0 +1,24 @@
+# jgtml
+__version__ = "0.0.81"
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
+from jtc import (
+    calculate_target_variable_min_max as calc_target_from_df,
+    pto_target_calculation as calc_target_to_file,
+    readMXFile as read
+)
+
+from jplt import (an_biv_plt2ds as plot_an_biv_plt2ds, an_bivariate_plot00 as plot_an_bivariate_plot00)
+
+import jdash as jdb
+
+# from jgtpy import JGTPDS as pds,JGTADS as ads,JGTPDSP as pds
+
+def __init__():
+    """
+    Initialize the jgtml module.
+    """
+    pass
