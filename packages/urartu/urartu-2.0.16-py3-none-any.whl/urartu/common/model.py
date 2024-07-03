16633641a3db6ec6f12981eae15a9146b74ffbb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 03:05:36 +0000

    Add package urartu/2.0.16/urartu-2.0.16-py3-none-any.whl

---
+++ packages/urartu/urartu-2.0.16-py3-none-any.whl/urartu/common/model.py
@@ -0,0 +1,40 @@
+from typing import Any, Dict, List
+
+import hydra
+
+from .device import Device
+
+
+class Model:
+    def __init__(self, cfg: List[Dict[str, Any]]):
+        self.cfg = cfg
+        self.model = None
+        self.tokenizer = None
+        self._load_model()
+
+    @staticmethod
+    def get_model(cfg):
+        return hydra.utils.instantiate(cfg.type, cfg)
+
+    def _load_model(self):
+        raise NotImplementedError("method '_load_model' is not implemented")
+
+    # def __call__(self, args):
+    #     return self.model(**args)
+
+    def generate(self, prompt):
+        raise NotImplementedError("method 'generate' is not implemented")
+
+    @staticmethod
+    def collate_tokenize(data, tokenizer, input_key):
+        input_batch = []
+        for element in data:
+            if isinstance(element[input_key], list):
+                input_text = " ".join(element[input_key])
+            else:
+                input_text = element[input_key]
+            input_batch.append(input_text)
+        tokenized = tokenizer(
+            input_batch, padding="longest", truncation=True, return_tensors="pt"
+        ).to(Device.get_device())
+        return tokenized
