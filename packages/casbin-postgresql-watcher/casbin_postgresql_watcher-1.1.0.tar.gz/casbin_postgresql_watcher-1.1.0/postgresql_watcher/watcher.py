e5129440441c643f3bc175bb0ff6bc8d2bc3d7f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 15:05:27 +0000

    Add package casbin-postgresql-watcher/1.1.0/casbin_postgresql_watcher-1.1.0.tar.gz

---
+++ packages/casbin-postgresql-watcher/casbin_postgresql_watcher-1.1.0.tar.gz/casbin_postgresql_watcher-1.1.0/postgresql_watcher/watcher.py
@@ -0,0 +1,158 @@
+from typing import Optional, Callable
+from psycopg2 import connect, extensions
+from multiprocessing import Process, Pipe
+import time
+from select import select
+from logging import Logger, getLogger
+
+
+POSTGRESQL_CHANNEL_NAME = "casbin_role_watcher"
+
+
+def casbin_subscription(
+    process_conn: Pipe,
+    logger: Logger,
+    host: str,
+    user: str,
+    password: str,
+    port: Optional[int] = 5432,
+    dbname: Optional[str] = "postgres",
+    delay: Optional[int] = 2,
+    channel_name: Optional[str] = POSTGRESQL_CHANNEL_NAME,
+    sslmode: Optional[str] = None,
+    sslrootcert: Optional[str] = None,
+    sslcert: Optional[str] = None,
+    sslkey: Optional[str] = None,
+):
+    # delay connecting to postgresql (postgresql connection failure)
+    time.sleep(delay)
+    conn = connect(
+        host=host,
+        port=port,
+        user=user,
+        password=password,
+        dbname=dbname,
+        sslmode=sslmode,
+        sslrootcert=sslrootcert,
+        sslcert=sslcert,
+        sslkey=sslkey
+    )
+    # Can only receive notifications when not in transaction, set this for easier usage
+    conn.set_isolation_level(extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+    curs = conn.cursor()
+    curs.execute(f"LISTEN {channel_name};")
+    logger.debug("Waiting for casbin policy update")
+    while True and not curs.closed:
+        if not select([conn], [], [], 5) == ([], [], []):
+            logger.debug("Casbin policy update identified..")
+            conn.poll()
+            while conn.notifies:
+                notify = conn.notifies.pop(0)
+                logger.debug(f"Notify: {notify.payload}")
+                process_conn.send(notify.payload)
+
+
+class PostgresqlWatcher(object):
+    def __init__(
+        self,
+        host: str,
+        user: str,
+        password: str,
+        port: Optional[int] = 5432,
+        dbname: Optional[str] = "postgres",
+        channel_name: Optional[str] = POSTGRESQL_CHANNEL_NAME,
+        start_process: Optional[bool] = True,
+        sslmode: Optional[str] = None,
+        sslrootcert: Optional[str] = None,
+        sslcert: Optional[str] = None,
+        sslkey: Optional[str] = None,
+        logger: Optional[Logger] = None,
+    ):
+        self.update_callback = None
+        self.parent_conn = None
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.dbname = dbname
+        self.channel_name = channel_name
+        self.sslmode = sslmode
+        self.sslrootcert = sslrootcert
+        self.sslcert = sslcert
+        self.sslkey = sslkey
+        if logger is None:
+            logger = getLogger()
+        self.logger = logger
+        self.subscribed_process = self.create_subscriber_process(start_process)
+
+    def create_subscriber_process(
+        self,
+        start_process: Optional[bool] = True,
+        delay: Optional[int] = 2,
+    ):
+        parent_conn, child_conn = Pipe()
+        if not self.parent_conn:
+            self.parent_conn = parent_conn
+        p = Process(
+            target=casbin_subscription,
+            args=(
+                child_conn,
+                self.logger,
+                self.host,
+                self.user,
+                self.password,
+                self.port,
+                self.dbname,
+                delay,
+                self.channel_name,
+                self.sslmode,
+                self.sslrootcert,
+                self.sslcert,
+                self.sslkey,
+            ),
+            daemon=True,
+        )
+        if start_process:
+            p.start()
+        return p
+
+    def set_update_callback(self, fn_name: Callable):
+        self.logger.debug(f"runtime is set update callback {fn_name}")
+        self.update_callback = fn_name
+
+    def update(self):
+        conn = connect(
+            host=self.host,
+            port=self.port,
+            user=self.user,
+            password=self.password,
+            dbname=self.dbname,
+            sslmode=self.sslmode,
+            sslrootcert=self.sslrootcert,
+            sslcert=self.sslcert,
+            sslkey=self.sslkey
+        )
+        # Can only receive notifications when not in transaction, set this for easier usage
+        conn.set_isolation_level(extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+        curs = conn.cursor()
+        curs.execute(
+            f"NOTIFY {self.channel_name},'casbin policy update at {time.time()}'"
+        )
+        conn.close()
+        return True
+
+    def should_reload(self):
+        try:
+            if self.parent_conn.poll(None):
+                message = self.parent_conn.recv()
+                self.logger.debug(f"message:{message}")
+                return True
+        except EOFError:
+            self.logger.warning(
+                "Child casbin-watcher subscribe process has stopped, "
+                "attempting to recreate the process in 10 seconds..."
+            )
+            self.subscribed_process, self.parent_conn = self.create_subscriber_process(
+                delay=10
+            )
+            return False
---
+++ packages/casbin-postgresql-watcher/casbin_postgresql_watcher-1.1.0.tar.gz/casbin_postgresql_watcher-1.1.0/tests/test_postgresql_watcher.py
@@ -0,0 +1,54 @@
+import sys
+import unittest
+from multiprocessing.connection import Pipe
+
+from postgresql_watcher import PostgresqlWatcher
+from multiprocessing import connection, context
+
+# Warning!!! , Please setup yourself config
+HOST = "127.0.0.1"
+PORT = 5432
+USER = "postgres"
+PASSWORD = "123456"
+DBNAME = "postgres"
+
+
+def get_watcher():
+    return PostgresqlWatcher(host=HOST, port=PORT, user=USER, password=PASSWORD, dbname=DBNAME)
+
+
+pg_watcher = get_watcher()
+
+try:
+    import _winapi
+    from _winapi import WAIT_OBJECT_0, WAIT_ABANDONED_0, WAIT_TIMEOUT, INFINITE
+except ImportError:
+    if sys.platform == 'win32':
+        raise
+    _winapi = None
+
+
+class TestConfig(unittest.TestCase):
+    def test_pg_watcher_init(self):
+        if _winapi:
+            assert isinstance(pg_watcher.parent_conn, connection.PipeConnection)
+        else:
+            assert isinstance(pg_watcher.parent_conn, connection.Connection)
+        assert isinstance(pg_watcher.subscribed_process, context.Process)
+
+    def test_update_pg_watcher(self):
+        assert pg_watcher.update() is True
+
+    def test_default_update_callback(self):
+        assert pg_watcher.update_callback() is None
+
+    def test_add_update_callback(self):
+        def _test_callback():
+            pass
+
+        pg_watcher.set_update_callback(_test_callback)
+        assert pg_watcher.update_callback == _test_callback
+
+
+if __name__ == "__main__":
+    unittest.main()
