b2579923449c0bf9bd3e075dfaa292c09158730f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 15:05:15 +0000

    Add package linkmailLensApi/0.6/linkmailLensApi-0.6-py3-none-any.whl

---
+++ packages/linkmailLensApi/linkmailLensApi-0.6-py3-none-any.whl/linkmailLensApi/formatter2.py
@@ -0,0 +1,38 @@
+import json
+import re
+def formatter22(ok):
+ def eliminar_cadena_generic(te):
+    pattern = r'",null,null,null,"generic".*'
+    te = re.sub(pattern, '', te)
+
+ def eliminar_cadena_generic2(te):
+    pattern2 = r',true,null,null,.*'
+    te = re.sub(pattern2, ']', te)
+    
+
+ with open("datos.json", "r") as file:
+    te = file.read()
+
+ te = te.replace('\\"', '"')
+ te = te.replace('\\\\\\\\"', "'")
+ te = te.replace('"[[', "[[")
+ te = te.replace('activado"]]"', 'activado"]]]]]]]]')
+
+ te = eliminar_cadena_generic(te)
+ te = eliminar_cadena_generic2(te)
+
+ te = eliminar_cadena_generic2(te)
+
+ with open("datos.json", "w") as file:
+    file.write(te)
+ with open("datos.json", "r") as file:
+    te2 = file.read()
+    te2 = eliminar_cadena_generic2(te2)
+ with open("datos.json", "w") as file:
+    file.write(te2)
+
+
+ try:
+     json_data = json.loads(te)
+ except json.JSONDecodeError as e:
+     print(f"Error al decodificar JSON: {e}")
