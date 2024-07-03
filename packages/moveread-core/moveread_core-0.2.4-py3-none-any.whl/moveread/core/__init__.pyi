427405efc4027eddab09491717a6f7916f705455
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 04:05:53 +0000

    Add package moveread-core/0.2.4/moveread_core-0.2.4-py3-none-any.whl

---
+++ packages/moveread-core/moveread_core-0.2.4-py3-none-any.whl/moveread/core/__init__.pyi
@@ -0,0 +1,10 @@
+from ._types import Image, Sheet, Player, Game, Rectangle, Vec2, Corners
+from .labels import StylesNA
+from .core import Core
+from . import cli
+
+__all__ = [
+  'Image', 'Sheet', 'Player', 'Game',
+  'StylesNA', 'Rectangle', 'Vec2', 'Corners',
+  'Core', 'cli'
+]
