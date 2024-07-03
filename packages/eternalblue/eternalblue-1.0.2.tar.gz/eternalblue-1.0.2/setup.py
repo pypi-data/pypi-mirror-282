0e193ba22547335a79399e50f100e23b12596015
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 05:04:17 +0000

    Add package eternalblue/1.0.2/eternalblue-1.0.2.tar.gz

---
+++ packages/eternalblue/eternalblue-1.0.2.tar.gz/eternalblue-1.0.2/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='eternalblue',
+    version='1.0.2',
+    description='Um pacote de diarização',
+    url='https://github.com/rhaymisonbetini/eternalblue',
+    author='Rhaymison Betini',
+    author_email='rhaymisoncristian@gmail.com',
+    license='Apache 2.0',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'eternalblue': ['public/**/*'],
+    },
+    install_requires=[
+        'transformers~=4.38.2',
+        'pyannote.audio',
+        'torch~=2.1.2',
+        'torchaudio~=2.1.2',
+        'torchvision',
+        'pydub~=0.25.1',
+        'accelerate',
+        'noisereduce~=3.0.2',
+        'librosa~=0.10.1',
+        'python-dotenv',
+        'soundfile~=0.12.1',
+        'bitsandbytes',
+        'insanely-fast-whisper',
+        'optimum',
+        'openai'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+    ],
+    python_requires='>=3.8',
+)
