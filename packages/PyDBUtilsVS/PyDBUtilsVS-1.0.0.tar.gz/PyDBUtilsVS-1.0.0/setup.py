374f8b469ca71bba17f4458f8cf73941cae15256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 06:05:48 +0000

    Add package PyDBUtilsVS/1.0.0/PyDBUtilsVS-1.0.0.tar.gz

---
+++ packages/PyDBUtilsVS/PyDBUtilsVS-1.0.0.tar.gz/PyDBUtilsVS-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='PyDBUtilsVS',
+    version='1.0.0',
+    description='Python Data Base Utils',
+    author='dwashere',
+    author_email='den.langeto45@yandex.ru',
+    packages=[''],  # список пакетов в вашей библиотеке
+    install_requires=['pyodbc', 'Pillow'],  # зависимости
+)
