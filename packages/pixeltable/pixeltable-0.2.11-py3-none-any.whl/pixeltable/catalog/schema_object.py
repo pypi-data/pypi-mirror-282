1100f10b9de51ea7eca8eaab75ad7845edce4e06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 07:05:16 +0000

    Add package pixeltable/0.2.11/pixeltable-0.2.11-py3-none-any.whl

---
+++ packages/pixeltable/pixeltable-0.2.11-py3-none-any.whl/pixeltable/catalog/schema_object.py
@@ -0,0 +1,34 @@
+from abc import abstractmethod
+from typing import Optional
+from uuid import UUID
+
+
+class SchemaObject:
+    """
+    Base class of all addressable objects within a Db.
+    Each object has an id, a name and a parent directory.
+    """
+    def __init__(self, obj_id: UUID, name: str, dir_id: Optional[UUID]):
+        # make these private so they don't collide with column names (id and name are fairly common)
+        self._id = obj_id
+        self._name = name
+        self._dir_id = dir_id
+
+    def _get_id(self) -> UUID:
+        return self._id
+
+    def get_name(self) -> str:
+        return self._name
+
+    @classmethod
+    @abstractmethod
+    def display_name(cls) -> str:
+        """
+        Return name displayed in error messages.
+        """
+        pass
+
+    def _move(self, new_name: str, new_dir_id: UUID) -> None:
+        """Subclasses need to override this to make the change persistent"""
+        self._name = new_name
+        self._dir_id = new_dir_id
