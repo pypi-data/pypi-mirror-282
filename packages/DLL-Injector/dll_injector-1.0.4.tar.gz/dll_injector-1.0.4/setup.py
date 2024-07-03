61a2b712ebcbd4ea67db27540a19408998aae795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 04:06:07 +0000

    Add package DLL-Injector/1.0.4/dll_injector-1.0.4.tar.gz

---
+++ packages/DLL-Injector/dll_injector-1.0.4.tar.gz/dll_injector-1.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DLL-Injector",
+    version="1.0.4",
+    author="LixNew",
+    author_email="lixnew2@gmail.com",
+    description="DLL-Injector is a library for injecting DLLs into processes using Python.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/LixNew2/DLL-Injector",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    package_data={
+        "dll_injector": ["libs/*.dll"]        
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "psutil"
+    ],
+)
