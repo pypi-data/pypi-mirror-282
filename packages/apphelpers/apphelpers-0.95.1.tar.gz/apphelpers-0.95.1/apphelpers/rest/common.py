a660031135fe64092647034afbadd22a088c7c2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 15:05:29 +0000

    Add package apphelpers/0.95.1/apphelpers-0.95.1.tar.gz

---
+++ packages/apphelpers/apphelpers-0.95.1.tar.gz/apphelpers-0.95.1/apphelpers/rest/common.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from dataclasses import (
+    asdict,
+    dataclass,
+    field,
+)
+from typing import (
+    Dict,
+    List,
+    Optional,
+)
+
+
+def phony(f):
+    return f
+
+
+@dataclass
+class User:
+    sid: Optional[str] = None
+    id: Optional[int] = None
+    name: Optional[str] = None
+    groups: List[str] = field(default_factory=list)
+    email: Optional[str] = None
+    mobile: Optional[str] = None
+    site_groups: Dict[int, int] = field(default_factory=dict)
+    site_ctx: Optional[int] = None
+
+    def to_dict(self):
+        return asdict(self)
+
+    def __bool__(self):
+        return self.id is not None
