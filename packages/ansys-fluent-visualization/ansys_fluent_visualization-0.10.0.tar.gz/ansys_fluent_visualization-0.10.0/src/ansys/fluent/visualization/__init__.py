9bd08eac58c6c5eaeb8139d618ce3ae6fe95efa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 05:05:02 +0000

    Add package ansys-fluent-visualization/0.10.0/ansys_fluent_visualization-0.10.0.tar.gz

---
+++ packages/ansys-fluent-visualization/ansys_fluent_visualization-0.10.0.tar.gz/ansys_fluent_visualization-0.10.0/src/ansys/fluent/visualization/__init__.py
@@ -0,0 +1,25 @@
+"""Python post processing integrations for the Fluent solver."""
+
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+_VERSION_INFO = "Build date: July 02, 2024 18:51 UTC ShaID: ae36f97"
+__version__ = importlib_metadata.version(__name__.replace(".", "-"))
+
+
+def version_info() -> str:
+    """Method returning the version of PyFluent being used.
+    Returns
+    -------
+    str
+        The PyFluent version being used.
+    Notes
+    -------
+    Only available in packaged versions. Otherwise it will return __version__.
+    """
+    return _VERSION_INFO if _VERSION_INFO is not None else __version__
+
+
+from ansys.fluent.visualization._config import get_config, set_config  # noqa: F401
