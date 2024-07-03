91f75d8159ef7567bd9ee0436973aa418323a704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 16:05:59 +0000

    Add package lidarwind/1.0.0/lidarwind-1.0.0.tar.gz

---
+++ packages/lidarwind/lidarwind-1.0.0.tar.gz/lidarwind-1.0.0/tests/test_windPropRetrieval.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pytest
+import xarray as xr
+
+import lidarwind as lst
+
+
+def test_wind_prop_retrieval_ffWindPropRet_doppler_obs():
+
+    with pytest.raises(TypeError):
+        lst.FourierTransfWindMethod(doppler_obs=np.array([0, 1]))
+
+
+def test_wind_prop_retrieval_GetWindProperties5Beam_data():
+
+    with pytest.raises(TypeError):
+        lst.GetWindProperties5Beam(data=xr.DataArray(np.array([0, 1])))
+
+
+def test_wind_prop_retrieval_RetriveWindFFT_tranfdData():
+
+    with pytest.raises(TypeError):
+        lst.RetriveWindFFT(transfd_data=np.array([0, 1]))
