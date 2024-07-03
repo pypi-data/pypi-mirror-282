36b86c3f7e507cc684e2bf4e387a8face64ab2b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 15:05:14 +0000

    Add package linkmailLensApi/0.5/linkmaillensapi-0.5.tar.gz

---
+++ packages/linkmailLensApi/linkmaillensapi-0.5.tar.gz/linkmaillensapi-0.5/linkmailLensApi/formatter2.py
@@ -0,0 +1,25 @@
+import json
+import re
+def formatter22(ok):
+ def eliminar_cadena_generic(te):
+    pattern = r'",null,null,null,"generic".*'
+    te = re.sub(pattern, '', te)
+    return te
+
+ with open("datos.json", "r") as file:
+    te = file.read()
+
+ te = te.replace('\\"', '"')
+ te = te.replace('\\\\\\\\"', "'")
+ te = te.replace('"[[', "[[")
+ te = te.replace('activado"]]"', 'activado"]]]]]]]]')
+
+ te = eliminar_cadena_generic(te)
+
+ with open("datos.json", "w") as file:
+     file.write(te)
+
+ try:
+     json_data = json.loads(te)
+ except json.JSONDecodeError as e:
+     print(f"Error al decodificar JSON: {e}")
