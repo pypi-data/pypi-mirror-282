d8481833caf2340f34c0726456c773b9b8a303eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 04:05:46 +0000

    Add package spond-classes/0.10.1/spond_classes-0.10.1.tar.gz

---
+++ packages/spond-classes/spond_classes-0.10.1.tar.gz/spond_classes-0.10.1/spond_classes/__init__.py
@@ -0,0 +1,9 @@
+"""Explicitly import all classes and functions into the package namespace."""
+
+# `import x as x` pattern used here for explicit re-export for Mypy
+
+from .event import Event as Event
+from .group import Group as Group
+from .member import Member as Member
+from .role import Role as Role
+from .subgroup import Subgroup as Subgroup
