dce81e03dc2882455c6f232ac37130337c69e47d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 16:05:48 +0000

    Add package pyverless/0.0.54/pyverless-0.0.54.tar.gz

---
+++ packages/pyverless/pyverless-0.0.54.tar.gz/pyverless-0.0.54/pyverless/api_gateway_handler/api_gateway_handler_standalone.py
@@ -0,0 +1,56 @@
+import json
+from abc import ABC
+from typing import Dict
+
+from pyverless.config import settings
+
+from pyverless.api_gateway_handler.api_gateway_handler import (
+    ApiGatewayHandler,
+    ApiGatewayWSHandler,
+)
+
+
+class ApiGatewayHandlerStandalone(ApiGatewayHandler, ABC):
+    headers: Dict = {}
+
+    def render_response(self):
+        headers = {
+            "Access-Control-Allow-Origin": settings.CORS_ORIGIN,
+            "Access-Control-Allow-Headers": settings.CORS_HEADERS,
+            "Access-Control-Allow-Methods": "*",
+        }
+        if self.headers:
+            headers = {**headers, **self.headers}
+        return {
+            "statusCode": self.response.status_code,
+            "body": json.dumps(self.response.body),
+            "headers": headers,
+        }
+
+
+class ApiGatewayWSHandlerStandalone(ApiGatewayWSHandler, ABC):
+    headers: Dict = {}
+
+    def render_response(self):
+        return {
+            "statusCode": self.response.status_code,
+            "body": json.dumps(self.response.body),
+        }
+
+
+class ApiGatewayStreamingHandlerStandalone(ApiGatewayHandler, ABC):
+    headers: Dict = {}
+
+    def render_response(self):
+        headers = {
+            "Access-Control-Allow-Origin": settings.CORS_ORIGIN,
+            "Access-Control-Allow-Headers": settings.CORS_HEADERS,
+            "Access-Control-Allow-Methods": "*",
+        }
+        if self.headers:
+            headers = {**headers, **self.headers}
+        return {
+            "statusCode": self.response.status_code,
+            "body": self.response.body,
+            "headers": headers,
+        }
