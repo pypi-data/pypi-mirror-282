e238e5ec3bea25697aadceb147aeafe5990fc540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 03:04:40 +0000

    Add package InstantLight-sdk/0.1.0/InstantLight_sdk-0.1.0.tar.gz

---
+++ packages/InstantLight-sdk/InstantLight_sdk-0.1.0.tar.gz/InstantLight_sdk-0.1.0/README.md
@@ -0,0 +1,9 @@
+# InstantLight-sdk
+
+## Installation
+
+Install the SDK using pip:
+
+```bash
+pip install your_sdk
+
---
+++ packages/InstantLight-sdk/InstantLight_sdk-0.1.0.tar.gz/InstantLight_sdk-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="InstantLight_sdk",
+    version="0.1.0",
+    description="A Python SDK for your API",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="Your Name",
+    author_email="your.email@example.com",
+    url="https://github.com/yourusername/your_sdk",
+    packages=find_packages(),
+    install_requires=[
+        "requests"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
