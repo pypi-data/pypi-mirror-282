52a6621c146e06cfc7314a7f2b723db191d96eba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 04:05:51 +0000

    Add package jatarag/0.1.3/jatarag-0.1.3-py3-none-any.whl

---
+++ packages/jatarag/jatarag-0.1.3-py3-none-any.whl/jatarag/embedder/interface.py
@@ -0,0 +1,8 @@
+from abc import ABC, abstractmethod
+import numpy as np
+
+
+class Embedder(ABC):
+    """abstract class for embedding a list of paragraphs into a matrix"""
+    @abstractmethod
+    def embed_paragraphs(self, paragraphs: list[str]) -> np.ndarray: ...
