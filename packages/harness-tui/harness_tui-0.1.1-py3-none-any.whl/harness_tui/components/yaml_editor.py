feff2b6707dbfbc1da7241b86e97284233f1033c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 15:05:14 +0000

    Add package harness-tui/0.1.1/harness_tui-0.1.1-py3-none-any.whl

---
+++ packages/harness-tui/harness_tui-0.1.1-py3-none-any.whl/harness_tui/components/yaml_editor.py
@@ -0,0 +1,114 @@
+"""Define the YAML editor component."""
+
+from __future__ import annotations
+
+import asyncio
+import typing as t
+
+import jsonschema
+import referencing
+import requests
+import yaml
+from referencing.jsonschema import DRAFT202012
+from textual import work
+from textual.app import ComposeResult
+from textual.containers import Horizontal
+from textual.message import Message
+from textual.reactive import reactive
+from textual.widgets import Button, Static, TextArea
+
+REGISTRY = "https://raw.githubusercontent.com/harness/harness-schema/main/v0"
+PIPELINE_SCHEMA = f"{REGISTRY}/pipeline.json"
+
+
+class YamlEditor(Static):
+    """Component that displays the YAML editor for a specific pipeline."""
+
+    class SavePipelineRequest(Message):
+        def __init__(self, yaml: str, obj: dict) -> None:
+            self.yaml = yaml
+            self.obj = obj
+            super().__init__()
+
+    base_content = reactive("", recompose=True)
+
+    def __init__(self, *args: t.Any, **kwargs: t.Any):
+        super().__init__(*args, **kwargs)
+        self.validator = None
+
+    def compose(self) -> ComposeResult:
+        yield TextArea.code_editor(
+            self.base_content,
+            id="yaml-editor",
+            theme="css",
+            language="yaml",
+            soft_wrap=False,
+            show_line_numbers=True,
+            tab_behavior="indent",
+        )  # TODO: set the border to red if the yaml is invalid
+        with Horizontal():
+            yield Button("Save", id="save-button", variant="success")
+            yield Button("Validate", id="validate-button", variant="default")
+            yield Button("Reset", id="reset-button", variant="error")
+
+    def on_mount(self) -> None:
+        """Load the pipeline YAML into the text area."""
+        self.get_schema()
+        self.query_one("#save-button", Button).disabled = True
+
+    async def on_button_pressed(self, event: Button.Pressed):
+        """Handle button presses."""
+        if event.button.id == "validate-button":
+            valid = self.validate_yaml()
+            self.query_one("#save-button", Button).disabled = not valid
+        elif event.button.id == "save-button":
+            text = self.query_one("#yaml-editor", TextArea).text
+            obj = yaml.safe_load(text)
+            if self.validator:
+                self.validator.validate(obj)
+            self.post_message(self.SavePipelineRequest(text, obj))
+        elif event.button.id == "reset-button":
+            await self.recompose()
+            self.notify("YAML reset to original state", severity="information")
+
+    @work(group="fetch_schema", exclusive=True)
+    async def get_schema(self) -> None:
+        """Get the schema for the pipeline."""
+        try:
+            schema = (await asyncio.to_thread(requests.get, PIPELINE_SCHEMA)).json()
+            resource = referencing.Resource(contents=schema, specification=DRAFT202012)
+            registry = referencing.Registry().with_resource(
+                "https://raw.githubusercontent.com/harness/harness-schema/main/v0",
+                resource,
+            )
+            self.validator = jsonschema.Draft202012Validator(
+                schema=schema, registry=registry
+            )
+        except Exception as e:
+            self.notify(
+                f"Could not fetch jsonschema for pipeline YAML: {e}", severity="warning"
+            )
+
+    def on_text_area_changed(self, event: TextArea.Changed) -> None:
+        self.query_one("#save-button", Button).disabled = True
+
+    def validate_yaml(self) -> bool:
+        """Update the text area with the new text."""
+        text = self.query_one("#yaml-editor", TextArea).text
+        try:
+            obj = yaml.safe_load(text)
+        except yaml.YAMLError as e:
+            self.notify(f"YAML error: {e}", severity="error")
+            return False
+
+        if self.validator:
+            try:
+                self.validator.validate(obj)
+            except jsonschema.ValidationError as e:
+                self.notify(
+                    f"Validation error at {e.json_path}: {e.message}", severity="error"
+                )
+                return False
+
+        self.notify("YAML is valid!", severity="information")
+        return True
---
+++ packages/harness-tui/harness_tui-0.1.1-py3-none-any.whl/harness_tui/vectordb/__init__.py
@@ -0,0 +1,69 @@
+"""A simple agent for answering questions based on log files."""
+
+from __future__ import annotations
+
+import random
+import typing as t
+from pathlib import Path
+
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.document_loaders import TextLoader
+from langchain_community.vectorstores import LanceDB
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain_openai.llms import OpenAI
+
+EMBEDDINGS = OpenAIEmbeddings()
+PROMPT = ChatPromptTemplate.from_template(
+    """Answer the following question based on the provided log file information:
+
+<context>
+{context}
+</context>
+
+Question: {input}"""
+)
+CHAIN = create_stuff_documents_chain(OpenAI(), PROMPT)
+
+FILTERED_STEPS = [
+    "save-cache-harness",
+    "restore-cache-harness",
+    "liteEngineTask",
+]
+
+SAMPLE_SIZE = 100
+"""The number of log files to sample when loading the logs."""
+
+
+def predicate(path: Path) -> bool:
+    return not any(step.lower() in path.stem.lower() for step in FILTERED_STEPS)
+
+
+class LogAgent:
+    """A simple agent for answering questions based on log files."""
+
+    def __init__(self, directory: t.Union[str, Path]):
+        self.directory = Path(directory)
+        self.responder = None
+
+    def load(self):
+        """Load the logs from the provided directory and return a responder."""
+        documents = []
+        paths = list(filter(predicate, Path(self.directory).rglob("*.log")))
+        random.shuffle(paths)
+        sample_size = min(SAMPLE_SIZE, len(paths))
+        if paths:
+            for path in paths[:sample_size]:
+                loader = TextLoader(path)
+                docs = loader.load()
+                documents.extend(docs)
+        vector = LanceDB.from_documents(documents, EMBEDDINGS)
+        retriever = vector.as_retriever()
+        self.responder = create_retrieval_chain(retriever, CHAIN)
+
+    def answer(self, question: str) -> str:
+        if not self.responder:
+            raise ValueError("You must load the logs before asking a question.")
+        response = self.responder.invoke({"input": question})
+        return response["answer"]
