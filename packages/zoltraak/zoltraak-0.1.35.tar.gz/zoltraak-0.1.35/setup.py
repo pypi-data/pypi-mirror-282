d5c92545f4183e4c495f66416b75fcc30290e70b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 05:04:40 +0000

    Add package zoltraak/0.1.35/zoltraak-0.1.35.tar.gz

---
+++ packages/zoltraak/zoltraak-0.1.35.tar.gz/zoltraak-0.1.35/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zoltraak",
+    version="0.1.35",
+    packages=find_packages(),
+    # package_dir={'': '.'},  # ここでベースディレクトリを指定
+    install_requires=[
+        "openai",
+        "anthropic",
+        "groq",
+        "python-dotenv",
+        "pyperclip",
+        "wheel",
+        "diagrams",
+        "google-api-python-client",  # Google APIクライアントライブラリを追加
+        "google-generativeai",  # Google の生成 AI ライブラリを追加
+    ],
+    package_data={
+        '': ['*.txt', '*.md', '*.json', '*.csv', '*.yaml', '*.yml'],
+        'zoltraak': ['llms/*','utils/*', 'grimoires/**/*'],
+    },
+    entry_points={
+        "console_scripts": [
+            "zoltraak=zoltraak.cli:main",
+        ],
+    },
+)
