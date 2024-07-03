12d72f51c556d5347a4bdaac7742ebb91f51d7c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 07:05:15 +0000

    Add package dyno-dino/1.0.8/dyno_dino-1.0.8.tar.gz

---
+++ packages/dyno-dino/dyno_dino-1.0.8.tar.gz/dyno_dino-1.0.8/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dyno_dino',
+    version='1.0.8',
+    packages=find_packages(),
+    install_requires=[
+        
+    ],
+    author='João Victor Godoi Bernardino',
+    author_email='joaogodoi1010@gmail.com',
+    description='Set of useful methods to speed up conversion of objects to AWS services',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/JoaoGodoi/dyno-dino',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
