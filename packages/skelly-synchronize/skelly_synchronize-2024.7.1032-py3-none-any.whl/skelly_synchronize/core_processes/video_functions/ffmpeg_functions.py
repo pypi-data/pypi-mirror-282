8e1f42062aa0cbee7847a014d6590f0905c1394d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 06:05:25 +0000

    Add package skelly-synchronize/2024.7.1032/skelly_synchronize-2024.7.1032-py3-none-any.whl

---
+++ packages/skelly-synchronize/skelly_synchronize-2024.7.1032-py3-none-any.whl/skelly_synchronize/core_processes/video_functions/ffmpeg_functions.py
@@ -0,0 +1,183 @@
+import subprocess
+from pathlib import Path
+
+
+def extract_audio_from_video_ffmpeg(
+    file_pathstring: str,
+    file_name: str,
+    output_folder_path: Path,
+    output_extension="wav",
+):
+    """Run a subprocess call to extract the audio from a video file using ffmpeg"""
+
+    subprocess.run(
+        [
+            "ffmpeg",
+            "-y",
+            "-i",
+            file_pathstring,
+            f"{output_folder_path}/{file_name}.{output_extension}",
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    )
+
+
+def extract_video_duration_ffmpeg(file_pathstring: str):
+    """Run a subprocess call to get the duration from a video file using ffmpeg"""
+
+    extract_duration_subprocess = subprocess.run(
+        [
+            "ffprobe",
+            "-v",
+            "error",
+            "-show_entries",
+            "format=duration",
+            "-of",
+            "default=noprint_wrappers=1:nokey=1",
+            file_pathstring,
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    )
+    video_duration = float(extract_duration_subprocess.stdout)
+
+    return video_duration
+
+
+def extract_video_fps_ffmpeg(file_pathstring: str):
+    """Run a subprocess call to get the fps of a video file using ffmpeg"""
+
+    extract_fps_subprocess = subprocess.run(
+        [
+            "ffprobe",
+            "-v",
+            "error",
+            "-select_streams",
+            "v:0",
+            "-show_entries",
+            "stream=r_frame_rate",
+            "-of",
+            "default=noprint_wrappers=1:nokey=1",
+            file_pathstring,
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    )
+    # get the results, then remove the excess characters to get something like '####/###'
+    cleaned_stdout = str(extract_fps_subprocess.stdout).split("'")[1].split("\\")[0]
+    # separate out numerator and denominator to calculate the fraction
+    numerator, denominator = cleaned_stdout.split("/")
+    video_fps = float(int(numerator) / int(denominator))
+
+    return video_fps
+
+
+def extract_audio_sample_rate_ffmpeg(file_pathstring: str):
+    """Run a subprocess call to get the audio sample rate of a video file using ffmpeg"""
+
+    extract_sample_rate_subprocess = subprocess.run(
+        [
+            "ffprobe",
+            "-v",
+            "error",
+            "-select_streams",
+            "a:0",
+            "-show_entries",
+            "stream=sample_rate",
+            "-of",
+            "default=noprint_wrappers=1:nokey=1",
+            file_pathstring,
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    )
+    # get the result, then remove the excess characters to get something like '####'
+    cleaned_stdout = (
+        str(extract_sample_rate_subprocess.stdout).split("'")[1].split("\\")[0]
+    )
+    # convert to float
+    audio_sample_rate = float(cleaned_stdout)
+
+    return audio_sample_rate
+
+
+def normalize_framerates_in_video_ffmpeg(
+    input_video_pathstring: str,
+    output_video_pathstring: str,
+    desired_fps: float = 30,
+    desired_sample_rate: int = 44100,
+):
+    """Run a subprocess call to normalize the framerate and audio sample rate of a video file using ffmpeg"""
+
+    normalize_framerates_subprocess = subprocess.run(
+        [
+            "ffmpeg",
+            "-i",
+            f"{input_video_pathstring}",
+            "-r",
+            f"{desired_fps}",
+            "-ar",
+            f"{desired_sample_rate}",
+            "-y",
+            f"{output_video_pathstring}",
+        ]
+    )
+
+    if normalize_framerates_subprocess.returncode != 0:
+        raise Exception(
+            f"ffmpeg returned code {normalize_framerates_subprocess.returncode}"
+        )
+
+
+def trim_single_video_ffmpeg(
+    input_video_pathstring: str,
+    start_time: float,
+    desired_duration: float,
+    output_video_pathstring: str,
+):
+    """Run a subprocess call to trim a video from start time to last as long as the desired duration"""
+
+    subprocess.run(
+        [
+            "ffmpeg",
+            "-i",
+            f"{input_video_pathstring}",
+            "-ss",
+            f"{start_time}",
+            "-t",
+            f"{desired_duration}",
+            "-y",
+            f"{output_video_pathstring}",
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    )
+
+
+def attach_audio_to_video_ffmpeg(
+    input_video_pathstring: str,
+    audio_file_pathstring: str,
+    output_video_pathstring: str,
+):
+    """Run a subprocess call to attach audio file back to the video"""
+
+    attach_audio_subprocess = subprocess.run(
+        [
+            "ffmpeg",
+            "-i",
+            f"{input_video_pathstring}",
+            "-i",
+            f"{audio_file_pathstring}",
+            "-c:v",
+            "copy",
+            "-c:a",
+            "aac",
+            f"{output_video_pathstring}",
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+
+    if attach_audio_subprocess.returncode != 0:
+        print(f"Error occurred: {attach_audio_subprocess.stderr.decode('utf-8')}")
---
+++ packages/skelly-synchronize/skelly_synchronize-2024.7.1032-py3-none-any.whl/skelly_synchronize/utils/path_handling_utilities.py
@@ -0,0 +1,32 @@
+import logging
+from pathlib import Path
+
+from skelly_synchronize.system.paths_and_file_names import SYNCED_VIDEO_PRECURSOR
+
+logging.basicConfig(level=logging.INFO)
+
+
+def create_directory(parent_directory: Path, directory_name: str) -> Path:
+    """Create a new directory under the specified parent directory."""
+    parent_directory = Path(parent_directory)
+    new_directory_path = parent_directory / directory_name
+
+    try:
+        new_directory_path.mkdir(parents=True, exist_ok=True)
+        logging.info(f"Created directory: {new_directory_path}")
+    except Exception as e:
+        logging.error(f"Error creating directory: {new_directory_path}. Exception: {e}")
+        raise
+
+    return new_directory_path
+
+
+def name_synced_video(raw_video_filename: str) -> str:
+    """Take a raw video filename, remove the raw prefix if its there, and return the synced video filename"""
+    raw_video_filename = str(raw_video_filename)
+    if raw_video_filename.split("_")[0] == "raw":
+        synced_video_name = SYNCED_VIDEO_PRECURSOR + raw_video_filename[4:] + ".mp4"
+    else:
+        synced_video_name = SYNCED_VIDEO_PRECURSOR + raw_video_filename + ".mp4"
+
+    return synced_video_name
