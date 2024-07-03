383c1be89d976274002903fb7e5c970c8dc2ef42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 15:05:40 +0000

    Add package fb-api-wrapper/1.0.5/fb_api_wrapper-1.0.5.tar.gz

---
+++ packages/fb-api-wrapper/fb_api_wrapper-1.0.5.tar.gz/fb_api_wrapper-1.0.5/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="fb_api_wrapper",
+    version="1.0.5",
+    author="NoCtrlZ",
+    author_email="noctrlz1305@gmail.com",
+    description="Sort description",
+    long_description="Full description",
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
