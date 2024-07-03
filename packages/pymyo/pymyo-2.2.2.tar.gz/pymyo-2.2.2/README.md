ef0fdb96695bad56c6236d9176a3f1f37b9da4fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 04:04:57 +0000

    Add package pymyo/2.2.2/pymyo-2.2.2.tar.gz

---
+++ packages/pymyo/pymyo-2.2.2.tar.gz/pymyo-2.2.2/README.md
@@ -0,0 +1,38 @@
+pymyo
+=====
+
+[![PyPI](https://img.shields.io/pypi/v/pymyo)](https://pypi.org/project/pymyo/)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/pymyo)](https://pypi.org/project/pymyo/)
+[![GitHub](https://img.shields.io/github/license/Crimson-Crow/pymyo)]((https://github.com/Crimson-Crow/pymyo/blob/main/LICENSE.txt))
+[![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg)](https://makeapullrequest.com)
+
+`pymyo` is a platform-agnostic Python module that allows you to configure or retrieve data from a Thalmic Labs Myo armband through BLE **without** requiring the original dongle or the Myo SDK.
+
+`pymyo` follows [Semantic Versioning](https://semver.org/).
+
+Installation
+------------
+
+`pymyo` can be installed using [`pip`](https://pip.pypa.io/en/stable/):
+
+    $ pip install pymyo
+
+Usage
+-----
+
+To learn how to use `pymyo`, visit the [`/examples`](https://github.com/Crimson-Crow/pymyo/tree/master/examples) directory. For additional features not covered in the examples, the library offers comprehensive docstrings for all available methods and properties.
+
+Issue tracker
+-------------
+
+Please report any bugs and enhancement ideas using the [issue tracker](https://github.com/Crimson-Crow/pymyo/issues).
+
+Acknowledgments
+---------------
+
+The development of this project was made possible by professor François Nougarou.
+
+License
+-------
+
+`pymyo` is licensed under the terms of the [MIT License](https://opensource.org/licenses/MIT) (see [LICENSE.txt](https://github.com/Crimson-Crow/pymyo/blob/master/LICENSE.txt)).
---
+++ packages/pymyo/pymyo-2.2.2.tar.gz/pymyo-2.2.2/examples/example.py
@@ -0,0 +1,57 @@
+# ruff: noqa: T201, INP001
+from __future__ import annotations
+
+import asyncio
+
+from bleak import BleakScanner
+
+from pymyo import Myo
+from pymyo.types import EmgMode, EmgValue, ImuMode, SleepMode, UnsupportedFeatureError
+
+# Put your own Myo Bluetooth address here or device UUID if you're on macOS.
+MYO_ADDRESS = "D7:91:D9:1C:C3:EB"
+
+
+async def main() -> None:
+    # You can use an asynchronous context manager to manage connection/disconnection
+    myo_device = await BleakScanner.find_device_by_address(MYO_ADDRESS)
+    async with Myo(myo_device) as myo:
+        # Access information using awaitable properties
+        print("Device name:", await myo.name)
+        print("Battery level:", await myo.battery)
+        print("Firmware version:", await myo.firmware_version)
+        print("Firmware info:", await myo.info)
+
+        await myo.vibrate2((250, 255), (250, 128), (250, 255))
+
+        # Register an event listener
+        def on_tap(direction: int, count: int) -> None:
+            print(f"Tap: direction: {direction} count: {count}")
+
+        myo.on_tap(on_tap)
+
+        # Register an event listener using a decorator
+        @myo.on_emg
+        def on_emg(emg: tuple[EmgValue, EmgValue]) -> None:
+            print(emg)
+
+        # Enable the optional battery notifications
+        @myo.on_battery
+        def battery_callback(level: int) -> None:
+            print(f"Battery level: {level}")
+
+        try:
+            await myo.enable_battery_notifications()
+        except UnsupportedFeatureError as e:
+            print(e)
+
+        await myo.set_sleep_mode(SleepMode.NEVER_SLEEP)
+        await asyncio.sleep(1)
+        await myo.set_mode(emg_mode=EmgMode.EMG, imu_mode=ImuMode.EVENTS)
+
+        while True:
+            await asyncio.sleep(1)  # Do other stuff
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
