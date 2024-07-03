6db9668c507ed279d9c2c9ac0cca15478c646b9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 16:05:23 +0000

    Add package greenhub/0.1.1/greenhub-0.1.1.tar.gz

---
+++ packages/greenhub/greenhub-0.1.1.tar.gz/greenhub-0.1.1/tests/run.py
@@ -0,0 +1,27 @@
+import greenhub as gh
+
+def run(year: int):
+
+    country = 'AR'
+    state = 'SANTA FE'
+
+
+
+    # get feature data
+
+
+
+    # load model
+
+
+    # run model
+
+
+    # format output
+
+
+    return
+
+
+
+
