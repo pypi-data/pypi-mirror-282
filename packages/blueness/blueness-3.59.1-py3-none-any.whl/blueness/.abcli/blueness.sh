18ce6bfefc46499d3d33135138674b533d621ae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 15:05:36 +0000

    Add package blueness/3.59.1/blueness-3.59.1-py3-none-any.whl

---
+++ packages/blueness/blueness-3.59.1-py3-none-any.whl/blueness/.abcli/blueness.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+function blueness() {
+    local task=$(abcli_unpack_keyword $1 help)
+
+    if [ $task == "help" ]; then
+        blueness version
+        return
+    fi
+
+    abcli_generic_task \
+        plugin=blueness,task=$task \
+        "${@:2}"
+}
+
+abcli_log $(blueness version --show_icon 1)
