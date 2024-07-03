70c3ba3fc33b8404687978b95996fa3c736d8ed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 05:04:38 +0000

    Add package draw-eye-diagram/0.1.0/draw_eye_diagram-0.1.0.tar.gz

---
+++ packages/draw-eye-diagram/draw_eye_diagram-0.1.0.tar.gz/draw_eye_diagram-0.1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="draw_eye_diagram",
+    version="0.1.0",
+    description="A library designed to generate and visualize eye diagrams for digital communication signals. ",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="K ABHISHEK MENON",
+    author_email="kabhishekmenon@gmail.com",
+    url="https://github.com/yourusername/eye_diagram",
+    packages=find_packages(),
+    install_requires=[
+        "numpy>=1.18.0",
+        "matplotlib>=3.1.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+    ],
+    python_requires='>=3.6',
+)
