21e4531668ea14e79453f7a1943b2fd74a04c9ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 14:05:10 +0000

    Add package posting/1.0.0b33/posting-1.0.0b33-py3-none-any.whl

---
+++ packages/posting/posting-1.0.0b33-py3-none-any.whl/posting/config.py
@@ -0,0 +1,79 @@
+from contextvars import ContextVar
+from typing import Type
+from pydantic import BaseModel, Field
+from pydantic_settings import (
+    BaseSettings,
+    PydanticBaseSettingsSource,
+    SettingsConfigDict,
+    YamlConfigSettingsSource,
+)
+from textual.types import AnimationLevel
+import yaml
+
+from posting.locations import config_file
+
+from posting.types import PostingLayout
+
+
+class HeadingSettings(BaseModel):
+    visible: bool = Field(default=True)
+    """Whether this widget should be displayed or not."""
+    show_host: bool = Field(default=True)
+    """Whether or not to show the hostname in the app header."""
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        env_prefix="posting_",
+        env_nested_delimiter="__",
+        env_ignore_empty=True,
+        extra="allow",
+    )
+
+    theme: str = Field(default="posting")
+    layout: PostingLayout = Field(default="vertical")
+    heading: HeadingSettings = Field(default_factory=HeadingSettings)
+    animation: AnimationLevel = Field(default="none")
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: Type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
+        conf_file = config_file()
+        default_sources = (
+            init_settings,
+            env_settings,
+            dotenv_settings,
+            file_secret_settings,
+        )
+
+        # TODO - this is working around a crash in pydantic-settings
+        # where the yaml settings source seems to crash if the file
+        # is empty.
+        # This workaround conditionally loads the yaml config file.
+        # If it's empty, we don't use it.
+        # https://github.com/pydantic/pydantic-settings/issues/329
+        try:
+            yaml_config = yaml.load(conf_file.read_bytes(), Loader=yaml.Loader)
+        except yaml.YAMLError:
+            return default_sources
+
+        if conf_file.exists() and yaml_config:
+            return (
+                init_settings,
+                YamlConfigSettingsSource(settings_cls, conf_file),
+                env_settings,
+                dotenv_settings,
+                file_secret_settings,
+            )
+        return default_sources
+
+
+SETTINGS: ContextVar[Settings] = ContextVar("settings")
