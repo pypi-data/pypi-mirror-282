4309ba8f56eeeb84099d2ac92cbdfa4cfcaf8f73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 06:05:21 +0000

    Add package PyTextForm/1.0.0/PyTextForm-1.0.0.tar.gz

---
+++ packages/PyTextForm/PyTextForm-1.0.0.tar.gz/PyTextForm-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='PyTextForm',
+    version='1.0.0',
+    description='Python Data Base Utils',
+    author='qrobless',
+    author_email='den.langeto45@yandex.ru',
+    packages=[''],  # список пакетов в вашей библиотеке
+    install_requires=['pyodbc', 'Pillow'],  # зависимости
+)
