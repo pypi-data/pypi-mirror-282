ac9ce9874b7efd8bd6d31a1e6f6f0e9f4d0d8968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 06:05:32 +0000

    Add package siliconcompiler/0.23.4/siliconcompiler-0.23.4-cp38-cp38-macosx_10_15_x86_64.whl

---
+++ packages/siliconcompiler/siliconcompiler-0.23.4-cp38-cp38-macosx_10_15_x86_64.whl/siliconcompiler/flows/lintflow.py
@@ -0,0 +1,25 @@
+import siliconcompiler
+
+from siliconcompiler.tools.verilator import lint
+
+
+###########################################################################
+# Flowgraph Setup
+############################################################################
+def setup(chip):
+    '''
+    An RTL linting flow.
+    '''
+
+    flowname = 'lintflow'
+    flow = siliconcompiler.Flow(chip, flowname)
+
+    flow.node(flowname, 'lint', lint)
+
+    return flow
+
+
+##################################################
+if __name__ == "__main__":
+    flow = setup(siliconcompiler.Chip('<flow>'))
+    flow.write_flowgraph(f"{flow.top()}.png", flow=flow.top())
---
+++ packages/siliconcompiler/siliconcompiler-0.23.4-cp38-cp38-macosx_10_15_x86_64.whl/siliconcompiler/tools/vivado/route.py
@@ -0,0 +1,20 @@
+from siliconcompiler.tools import vivado
+from siliconcompiler.tools.vivado import tool
+
+
+def setup(chip):
+    '''Performs routing.'''
+    step = chip.get('arg', 'step')
+    index = chip.get('arg', 'index')
+    task = chip._get_task(step, index)
+    vivado.setup_task(chip, task)
+
+    design = chip.top()
+    chip.set('tool', tool, 'task', task, 'input', f'{design}_checkpoint.dcp',
+             step=step, index=index)
+    chip.set('tool', tool, 'task', task, 'output', f'{design}_checkpoint.dcp',
+             step=step, index=index)
+
+
+def post_process(chip):
+    vivado.post_process(chip)
