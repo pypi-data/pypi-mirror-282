1f5dcb0cf818128a2c0f8776be7801063136f716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 15:05:28 +0000

    Add package libterraform/0.7.0/libterraform-0.7.0-cp312-cp312-manylinux_2_31_x86_64.whl

---
+++ packages/libterraform/libterraform-0.7.0-cp312-cp312-manylinux_2_31_x86_64.whl/libterraform/__init__.py
@@ -0,0 +1,17 @@
+import os
+from ctypes import cdll, c_void_p
+from libterraform.common import WINDOWS
+
+__version__ = '0.7.0'
+
+root = os.path.dirname(os.path.abspath(__file__))
+_lib_filename = 'libterraform.dll' if WINDOWS else 'libterraform.so'
+_lib_tf = cdll.LoadLibrary(os.path.join(root, _lib_filename))
+
+_free = _lib_tf.Free
+_free.argtypes = [c_void_p]
+
+from .cli import TerraformCommand
+from .config import TerraformConfig
+
+__all__ = ['TerraformCommand', 'TerraformConfig']
