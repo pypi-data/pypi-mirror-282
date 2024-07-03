39a109ce0aef4e304484193c587f678d36179999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 15:05:08 +0000

    Add package composer/0.23.5/composer-0.23.5-py3-none-any.whl

---
+++ packages/composer/composer-0.23.5-py3-none-any.whl/composer/utils/object_store/__init__.py
@@ -0,0 +1,31 @@
+# Copyright 2022 MosaicML Composer authors
+# SPDX-License-Identifier: Apache-2.0
+
+"""Object store base class and implementations."""
+
+from composer.utils.object_store.gcs_object_store import GCSObjectStore
+from composer.utils.object_store.libcloud_object_store import LibcloudObjectStore
+from composer.utils.object_store.mlflow_object_store import (
+    MLFLOW_EXPERIMENT_ID_FORMAT_KEY,
+    MLFLOW_RUN_ID_FORMAT_KEY,
+    MLFlowObjectStore,
+)
+from composer.utils.object_store.object_store import ObjectStore, ObjectStoreTransientError
+from composer.utils.object_store.oci_object_store import OCIObjectStore
+from composer.utils.object_store.s3_object_store import S3ObjectStore
+from composer.utils.object_store.sftp_object_store import SFTPObjectStore
+from composer.utils.object_store.uc_object_store import UCObjectStore
+
+__all__ = [
+    'ObjectStore',
+    'ObjectStoreTransientError',
+    'LibcloudObjectStore',
+    'MLFlowObjectStore',
+    'S3ObjectStore',
+    'SFTPObjectStore',
+    'OCIObjectStore',
+    'GCSObjectStore',
+    'UCObjectStore',
+    'MLFLOW_EXPERIMENT_ID_FORMAT_KEY',
+    'MLFLOW_RUN_ID_FORMAT_KEY',
+]
