3eadb371c8896c947ac0104dcd9f5d027a2709fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 04:04:54 +0000

    Add package Delta-Bypasser/0.1.0/delta_bypasser-0.1.0.tar.gz

---
+++ packages/Delta-Bypasser/delta_bypasser-0.1.0.tar.gz/delta_bypasser-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Delta_Bypasser',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'requests'
+    ],
+    description='A module to bypass delta',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='iwcool.gg',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
