b735c5714f92d9e487741729a6718a85443da1c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 05:04:26 +0000

    Add package paketlib/0.0.81/paketlib-0.0.81-py3-none-any.whl

---
+++ packages/paketlib/paketlib-0.0.81-py3-none-any.whl/paketlib/__main__.py
@@ -0,0 +1,33 @@
+import pprint
+from . import api, console, paket, path, pkcrypt, proto, search
+
+shell = console.Console()
+
+while True:
+    shell.path = '~/paketlib/'
+    shell.input()
+    t_input = shell.inputstr.strip()
+    if t_input == 'help':
+        print('help menu:\n\tip [ip] - ip search\n\tuserbox [authtoken] [query] - userbox search\n\tleakosint [token] [query] - leakosint search\n\tdbsearch [database] [query] - search in database\n\tgetmessage - get message from paketapi servers\n\tgetbootcode - get boot code from paketapi servers\n\thelp - show this menu')
+
+    elif t_input.startswith('ip'):
+        t_ip = t_input.split(' ')[-1].strip()
+        for a, b in search.ipLookup(t_ip):
+            print(f'[+] {a}: {b}')
+
+    elif t_input.startswith('userbox'):
+        pprint.pprint(search.SearchUserBox(t_input.split(' ', 2)[2], t_input.split(' ', 2)[1]))
+
+    elif t_input.startswith('leakosint'):
+        search.SearchLeak(t_input.split(' ', 2)[2], t_input.split(' ', 2)[1])
+
+    elif t_input.startswith('dbsearch'):
+        search.dbsearch(t_input.split(' ', 2)[1], t_input.split(' ', 2)[2])
+
+    elif t_input == 'getbootcode':
+        print(api.getbootcode())
+
+    elif t_input == 'getmessage':
+        print(api.getmessage())
+
+        
