7311ffb5752a92960328d751772a567f5fe10e44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 03:05:38 +0000

    Add package django-google-cloud-tasks/2.16.3/django_google_cloud_tasks-2.16.3.tar.gz

---
+++ packages/django-google-cloud-tasks/django_google_cloud_tasks-2.16.3.tar.gz/django_google_cloud_tasks-2.16.3/django_cloud_tasks/exceptions.py
@@ -0,0 +1,13 @@
+class GoogleCredentialsException(Exception):
+    def __init__(self):
+        message = "GCP_JSON or GCP_B64 env variable not set properly"
+        super().__init__(message)
+
+
+class TaskNotFound(Exception):
+    def __init__(self, name: str):
+        message = f"Task {name} not registered."
+        super().__init__(message)
+
+
+class DiscardTaskException(Exception): ...
