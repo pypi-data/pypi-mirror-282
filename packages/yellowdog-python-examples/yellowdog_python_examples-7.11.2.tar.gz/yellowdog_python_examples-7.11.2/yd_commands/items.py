0862c10575eeb8360d0fcf444a5e4132fd37d710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 04:04:47 +0000

    Add package yellowdog-python-examples/7.11.2/yellowdog_python_examples-7.11.2.tar.gz

---
+++ packages/yellowdog-python-examples/yellowdog_python_examples-7.11.2.tar.gz/yellowdog_python_examples-7.11.2/yd_commands/items.py
@@ -0,0 +1,49 @@
+"""
+Utility class for YellowDog item types.
+"""
+
+from typing import TypeVar
+
+from yellowdog_client.model import (
+    Allowance,
+    ComputeRequirement,
+    ComputeRequirementTemplateSummary,
+    ComputeSourceTemplate,
+    ComputeSourceTemplateSummary,
+    ConfiguredWorkerPool,
+    Instance,
+    KeyringSummary,
+    MachineImageFamilySummary,
+    NamespacePolicy,
+    NamespaceStorageConfiguration,
+    ObjectPath,
+    ProvisionedWorkerPool,
+    Task,
+    TaskGroup,
+    WorkerPoolSummary,
+    WorkRequirementSummary,
+)
+
+from yd_commands.cloudwizard_aws_types import AWSAvailabilityZone
+
+Item = TypeVar(
+    "Item",
+    AWSAvailabilityZone,
+    Allowance,
+    ComputeRequirement,
+    ComputeRequirementTemplateSummary,
+    ComputeSourceTemplate,
+    ComputeSourceTemplateSummary,
+    ConfiguredWorkerPool,
+    Instance,
+    KeyringSummary,
+    MachineImageFamilySummary,
+    NamespacePolicy,
+    NamespaceStorageConfiguration,
+    ObjectPath,
+    ProvisionedWorkerPool,
+    Task,
+    TaskGroup,
+    WorkRequirementSummary,
+    WorkerPoolSummary,
+)
