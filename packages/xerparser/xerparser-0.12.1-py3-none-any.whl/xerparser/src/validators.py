f8b578cb0b89ef3d0a378b1b0773e266a096d0f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 15:05:17 +0000

    Add package xerparser/0.12.1/xerparser-0.12.1-py3-none-any.whl

---
+++ packages/xerparser/xerparser-0.12.1-py3-none-any.whl/xerparser/src/validators.py
@@ -0,0 +1,41 @@
+# xerparser
+# validators.py
+# Functions to validate data during object initialization
+
+from datetime import datetime
+
+date_format = "%Y-%m-%d %H:%M"
+
+
+def optional_date(value: str) -> datetime | None:
+    if value == "" or value is None:
+        return None
+    return datetime.strptime(value, date_format)
+
+
+def optional_float(value: str) -> float | None:
+    if value == "" or value is None:
+        return None
+    return float(value.replace(",", "."))
+
+
+def float_or_zero(value: str) -> float:
+    if value == "" or value is None:
+        return 0.0
+    return float(value.replace(",", "."))
+
+
+def optional_int(value: str) -> int | None:
+    if value == "" or value is None:
+        return None
+    return int(value)
+
+
+def int_or_zero(value: str) -> int:
+    if value == "" or value is None:
+        return 0
+    return int(value)
+
+
+def optional_str(value: str) -> str | None:
+    return (value, None)[value == ""]
