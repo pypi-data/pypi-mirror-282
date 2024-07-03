6c9f21a29f4d35b1aeec0492bcfe62faa28c8759
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 05:04:32 +0000

    Add package math-spec-mapping/0.3.8/math_spec_mapping-0.3.8-py3-none-any.whl

---
+++ packages/math-spec-mapping/math_spec_mapping-0.3.8-py3-none-any.whl/math_spec_mapping/Reports/state.py
@@ -0,0 +1,90 @@
+def write_state_variable_table(target_state, links=False):
+    table = """<table>
+      <tr>
+        <th>Name</th>
+        <th>Description</th>
+        <th>Type</th>
+        <th>Symbol</th>
+        <th>Domain</th>
+      </tr>"""
+
+    for var in target_state.variables:
+        table_vars = [
+            var.name,
+            var.description,
+            var.type.name,
+            var.symbol,
+            var.domain,
+        ]
+        table += "<tr>"
+        for i, tv in enumerate(table_vars):
+            table += "<td>"
+            if tv:
+                if links and i == 2:
+                    table += "[[{}]]".format(tv).replace("<", "&lt").replace(">", "&gt")
+                else:
+                    table += "{}".format(tv).replace("<", "&lt").replace(">", "&gt")
+
+            table += "</td>"
+
+        table += "</tr>"
+
+    table += "</table>"
+    return table
+
+
+def write_state_variable_table_markdown(target_state, links=False):
+    table = """| Name | Description | Type | Symbol | Domain |
+| --- | --- | --- | --- | --- |
+"""
+
+    for var in target_state.variables:
+        table_vars = [
+            var.name,
+            var.description,
+            var.type.name,
+            var.symbol,
+            var.domain,
+        ]
+        table += "|"
+        for i, tv in enumerate(table_vars):
+            if tv:
+                if links and i == 2:
+                    table += "[[{}]]".format(tv)
+                elif links and i == 0:
+                    table += "[[{}-{}\|{}]]".format(target_state.name, tv, tv)
+                else:
+                    table += "{}".format(tv)
+            table += "|"
+
+        table += "\n"
+
+    return table
+
+
+def write_global_state_variable_table(state):
+    out = ""
+    out += "<h3>Global State</h3>"
+    out += write_state_variable_table(state)
+    return out
+
+
+def write_local_state_variable_tables(states):
+    out = ""
+    out += "<h3>Local States</h3>"
+    for state in states:
+        out += "<h4>{}</h4>".format(state.name)
+        out += write_state_variable_table(state)
+    return out
+
+
+def write_state_section(state, links=False):
+    out = ""
+    out += "### Notes"
+    out += "\n"
+    out += state.notes
+    out += "\n"
+    out += "### Variable Table"
+    out += "\n"
+    out += write_state_variable_table_markdown(state, links=links)
+    return out
---
+++ packages/math-spec-mapping/math_spec_mapping-0.3.8-py3-none-any.whl/math_spec_mapping/__init__.py
@@ -0,0 +1,31 @@
+from .Load import load_from_json
+from .Reports import (
+    create_action_chains_graph,
+    write_out_boundary_actions,
+    write_out_policies,
+    write_out_mechanisms,
+    load_svg_graphviz,
+    write_basic_report_full,
+    write_action_chain_reports,
+    write_spec_tree,
+    create_parameter_impact_table,
+    write_entity_reports,
+    write_wiring_report,
+    write_overview,
+    write_entity_markdown_report,
+    write_state_markdown_report,
+    write_types_markdown_report,
+    write_boundary_action_markdown_report,
+    write_policy_markdown_report,
+    write_mechanism_markdown_report,
+    write_space_markdown_report,
+    write_control_action_markdown_report,
+    write_wiring_markdown_report,
+    write_parameter_markdown_report,
+    write_stateful_metrics_markdown_report,
+    write_all_markdown_reports,
+)
+from .schema import schema
+from .Convenience import remove_dummy_repo_components
+
+# from .Convenience import write_top_level_json_description
