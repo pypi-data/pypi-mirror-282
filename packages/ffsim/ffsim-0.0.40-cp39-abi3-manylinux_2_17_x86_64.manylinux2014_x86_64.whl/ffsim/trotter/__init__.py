ee8deadf400276e3226668679b8d5be7b1aa042b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 03:04:41 +0000

    Add package ffsim/0.0.40/ffsim-0.0.40-cp39-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ffsim/ffsim-0.0.40-cp39-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/ffsim/trotter/__init__.py
@@ -0,0 +1,19 @@
+# (C) Copyright IBM 2023.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Hamiltonian simulation via Trotter-Suzuki formulas."""
+
+from ffsim.trotter.qdrift import simulate_qdrift_double_factorized
+from ffsim.trotter.trotter import simulate_trotter_double_factorized
+
+__all__ = [
+    "simulate_qdrift_double_factorized",
+    "simulate_trotter_double_factorized",
+]
