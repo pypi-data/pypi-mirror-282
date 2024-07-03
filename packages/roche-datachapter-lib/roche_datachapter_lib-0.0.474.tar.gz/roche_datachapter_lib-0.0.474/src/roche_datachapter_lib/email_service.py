49533f26fcf3e18d5f2cccafb286ecebedc13542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 05:04:44 +0000

    Add package roche-datachapter-lib/0.0.474/roche_datachapter_lib-0.0.474.tar.gz

---
+++ packages/roche-datachapter-lib/roche_datachapter_lib-0.0.474.tar.gz/roche_datachapter_lib-0.0.474/src/roche_datachapter_lib/email_service.py
@@ -0,0 +1,16 @@
+"""Módulo que contiene la clase EmailDeliverySistem que se encarga de crear un registro de correo electrónico para ser enviado por Appsheet"""
+from .google_services import GoogleServices
+
+class EmailDeliverySystem():
+
+    @classmethod
+    def create_email_record(cls, to, subject, body, attachment, attachment_name, appsheet_mails_folder_id):
+        """Crea un registro de correo electrónico para ser enviado por Appsheet"""
+        folder_name = GoogleServices.get_file_name_by_file_id(appsheet_mails_folder_id)
+        return [{
+            "destinatario/s": to,
+            "asunto": subject,
+            "cuerpo": body,
+            "file": f"{folder_name}/{attachment}",
+            "tabla": attachment_name
+        }]
