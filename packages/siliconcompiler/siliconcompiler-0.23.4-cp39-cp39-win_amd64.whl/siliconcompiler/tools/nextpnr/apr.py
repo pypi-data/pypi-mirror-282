aac589ca0253196f084dcd8c64cb83ae68404994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 06:05:35 +0000

    Add package siliconcompiler/0.23.4/siliconcompiler-0.23.4-cp39-cp39-win_amd64.whl

---
+++ packages/siliconcompiler/siliconcompiler-0.23.4-cp39-cp39-win_amd64.whl/siliconcompiler/tools/nextpnr/apr.py
@@ -0,0 +1,22 @@
+
+def setup(chip):
+    '''
+    Perform automated place and route on FPGAs
+    '''
+
+    tool = 'nextpnr'
+    step = chip.get('arg', 'step')
+    index = chip.get('arg', 'index')
+    task = chip._get_task(step, index)
+
+    topmodule = chip.top()
+
+    clobber = False
+    chip.set('tool', tool, 'exe', 'nextpnr-ice40')
+    chip.set('tool', tool, 'vswitch', '--version')
+    chip.set('tool', tool, 'version', '>=0.2', clobber=clobber)
+
+    chip.set('tool', tool, 'task', task, 'option', "", step=step, index=index, clobber=clobber)
+    chip.set('tool', tool, 'task', task, 'input', f'{topmodule}.netlist.json',
+             step=step, index=index)
+    chip.set('tool', tool, 'task', task, 'output', f'{topmodule}.asc', step=step, index=index)
