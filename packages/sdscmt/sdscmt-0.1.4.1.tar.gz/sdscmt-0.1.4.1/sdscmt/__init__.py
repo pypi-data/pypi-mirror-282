0f0d86adcbcc4e4ce603347881dc8cb236b3243e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 15:05:53 +0000

    Add package sdscmt/0.1.4.1/sdscmt-0.1.4.1.tar.gz

---
+++ packages/sdscmt/sdscmt-0.1.4.1.tar.gz/sdscmt-0.1.4.1/sdscmt/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 17 18:15:53 2024
+
+@author: Subham Divakar
+"""
+
+# sdscmt/__init__.py
+
+from .sdscmt import main
+
+if __name__ == "__main__":
+    main()
