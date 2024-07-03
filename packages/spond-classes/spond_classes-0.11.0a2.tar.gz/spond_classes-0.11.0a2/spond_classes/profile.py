a43416390e3b80f3c194a9b06bd21f5ea1e85571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 04:04:52 +0000

    Add package spond-classes/0.11.0a2/spond_classes-0.11.0a2.tar.gz

---
+++ packages/spond-classes/spond_classes-0.11.0a2.tar.gz/spond_classes-0.11.0a2/spond_classes/profile.py
@@ -0,0 +1,19 @@
+"""Module for `Profile` class."""
+
+from pydantic import BaseModel, Field
+
+
+class Profile(BaseModel):
+    """Represents a profile in the Spond system.
+
+    A `Profile` is nested within a `Member`.
+
+    Attributes
+    ----------
+    uid : str
+        id of the `Profile`.
+
+        `id` in API, but that's a reserved term and the `spond` package uses `uid`.
+    """
+
+    uid: str = Field(alias="id")
