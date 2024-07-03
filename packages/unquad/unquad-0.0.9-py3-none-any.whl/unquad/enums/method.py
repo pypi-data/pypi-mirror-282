be61ab7010951dab6050a738bb09cfa649366f4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 03:04:41 +0000

    Add package unquad/0.0.9/unquad-0.0.9-py3-none-any.whl

---
+++ packages/unquad/unquad-0.0.9-py3-none-any.whl/unquad/enums/method.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class Method(Enum):
+    """
+    Enumerators of conformal methods for anomaly detection.
+    """
+
+    NAIVE: str = "NAIVE"  # reference
+
+    SPLIT_CONFORMAL: str = "SC"
+    CV: str = "CV"
+    CV_PLUS: str = "CV+"
+    JACKKNIFE: str = "J"
+    JACKKNIFE_PLUS: str = "J+"
+    JACKKNIFE_AFTER_BOOTSTRAP: str = "JaB"
+    JACKKNIFE_PLUS_AFTER_BOOTSTRAP: str = "J+aB"
