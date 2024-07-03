817127451013fc618222ddb5619469c9727d2ef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 04:06:09 +0000

    Add package datafog-instructor/0.1.0b4/datafog_instructor-0.1.0b4.tar.gz

---
+++ packages/datafog-instructor/datafog_instructor-0.1.0b4.tar.gz/datafog_instructor-0.1.0b4/setup.py
@@ -0,0 +1,54 @@
+from setuptools import find_packages, setup
+
+# Read README for the long description
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
+def __version__():
+    return "0.1.0b4"
+
+project_urls = {
+    "Homepage": "https://datafog.ai",
+    "Documentation": "https://docs.datafog.ai",
+    "Discord": "https://discord.gg/bzDth394R4",
+    "Twitter": "https://twitter.com/datafoginc",
+    "GitHub": "https://github.com/datafog/datafog-python",
+}
+
+
+setup(
+    name="datafog-instructor",
+    version=__version__(),
+    author="Sid Mohan",
+    author_email="sid@datafog.ai",
+    description="Scan, redact, and manage PII in your documents before they get uploaded to a Retrieval Augmented Generation (RAG) system.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "pydantic",
+        "ollama",
+        "ollama-instructor",
+        "python-dotenv",
+    ],
+    python_requires=">=3.10",
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Framework :: tox",
+        "Framework :: Pytest",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+    ],
+    keywords="pii, redaction, nlp, rag, retrieval augmented generation",
+    maintainer="DataFog",
+    maintainer_email="hi@datafog.ai",
+    url="https://datafog.ai",
+    project_urls=project_urls,
+    license="MIT",
+)
