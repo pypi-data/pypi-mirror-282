426b273f5b9deee9f022584ff70165ea34a6679e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 04:04:47 +0000

    Add package syngen/0.9.12/syngen-0.9.12.tar.gz

---
+++ packages/syngen/syngen-0.9.12.tar.gz/syngen-0.9.12/src/syngen/ml/data_loaders/__init__.py
@@ -0,0 +1,10 @@
+from syngen.ml.data_loaders.data_loaders import (  # noqa: F401;
+    CSVLoader,
+    AvroLoader,
+    DataLoader,
+    MetadataLoader,
+    YAMLLoader,
+    BaseDataLoader,
+    BinaryLoader,
+    ExcelLoader,
+)
