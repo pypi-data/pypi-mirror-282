d536e5fc40349b718c2efcdfdb23332c6b70cd31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 07:05:15 +0000

    Add package cyto-dl/0.1.8/cyto-dl-0.1.8.tar.gz

---
+++ packages/cyto-dl/cyto-dl-0.1.8.tar.gz/cyto-dl-0.1.8/cyto_dl/nn/losses/__init__.py
@@ -0,0 +1,19 @@
+from .adversarial_loss import AdversarialLoss
+from .chamfer_loss import ChamferLoss
+from .continuous_bernoulli import CBLogLoss
+from .cosine_loss import CosineLoss
+from .gan_loss import GANLoss, Pix2PixHD
+from .gaussian_nll_loss import GaussianNLLLoss
+from .threshold_loss import ThresholdLoss
+from .weibull import WeibullLogLoss
+from .weighted_mse_loss import WeightedMSELoss
+
+try:
+    from .spharm_loss import SpharmLoss
+except (ModuleNotFoundError, ImportError):
+    SpharmLoss = None
+
+try:
+    from .geomloss import GeomLoss
+except (ModuleNotFoundError, ImportError):
+    GeomLoss = None
