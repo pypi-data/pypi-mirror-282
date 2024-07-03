322daa4bab8fac487bc55ae511c6d87893532c80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 05:04:32 +0000

    Add package pylfit/0.4.0/pylfit-0.4.0.tar.gz

---
+++ packages/pylfit/pylfit-0.4.0.tar.gz/pylfit-0.4.0/pylfit/algorithms/algorithm.py
@@ -0,0 +1,12 @@
+#-----------------------
+# @author: Tony Ribeiro
+# @created: 2020/12/14
+# @updated: 2023/12/27
+#
+# @desc: LFIT algorithm interface class, define common methods.
+#-----------------------
+
+class Algorithm:
+    """
+    Define the common methods to all pylfit algorithms.
+    """
