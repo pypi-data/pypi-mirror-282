ba72286873ea15e50bb0d8af5f460e53b2c66bec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 05:04:56 +0000

    Add package cool-seq-tool/0.4.1/cool_seq_tool-0.4.1-py3-none-any.whl

---
+++ packages/cool-seq-tool/cool_seq_tool-0.4.1-py3-none-any.whl/cool_seq_tool/mappers/__init__.py
@@ -0,0 +1,8 @@
+"""Module for mapping data"""
+
+from .alignment import AlignmentMapper  # noqa: I001
+from .mane_transcript import ManeTranscript
+from .exon_genomic_coords import ExonGenomicCoordsMapper
+
+
+__all__ = ["AlignmentMapper", "ManeTranscript", "ExonGenomicCoordsMapper"]
