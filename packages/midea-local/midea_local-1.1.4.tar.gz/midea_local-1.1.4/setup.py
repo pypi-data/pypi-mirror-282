a5425784bb81632e9dec57fe80e35a3da3a53f0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 03:05:09 +0000

    Add package midea-local/1.1.4/midea_local-1.1.4.tar.gz

---
+++ packages/midea-local/midea_local-1.1.4.tar.gz/midea_local-1.1.4/setup.py
@@ -0,0 +1,36 @@
+"""setup midea-local."""
+
+from pathlib import Path
+
+import setuptools
+
+readme = Path("README.md")
+with readme.open(encoding="utf-8") as fh:
+    long_description = fh.read()
+
+requirements = Path("requirements.txt")
+with requirements.open(encoding="utf-8") as fp:
+    requires = fp.read().splitlines()
+
+setuptools.setup(
+    name="midea-local",
+    version="1.1.4",
+    author="rokam",
+    author_email="lucas@mindello.com.br",
+    description="Control your Midea M-Smart appliances via local area network",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rokam/midea-local",
+    install_requires=requires,
+    packages=setuptools.find_packages(
+        include=["midealocal", "midealocal.*"],
+        exclude=["tests", "tests.*"],
+    ),
+    python_requires=">=3.11",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
