6b7f8e3c9af441cc4efe38e4f330c7960e266f15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 14:04:52 +0000

    Add package saraApi/0.0.1.5/saraApi-0.0.1.5-py3-none-any.whl

---
+++ packages/saraApi/saraApi-0.0.1.5-py3-none-any.whl/sara/rolplayNsfw.py
@@ -0,0 +1,22 @@
+from .req import get
+
+def anal():
+    return get("anal")
+
+def fuck():
+    return get("fuck")
+
+def blowjob():
+    return get('blowjob')
+
+def kuni():
+    return get("kuni")
+
+def spank():
+    return get("spank")
+
+def suck():
+    return get("suck")
+
+def feetjob():
+    return get("feetjob")
