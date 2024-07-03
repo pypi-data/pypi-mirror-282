08b15a320ed308a5b7b76bec8158b16adc990f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 14:05:01 +0000

    Add package combo-llm/0.1.2/combo_llm-0.1.2.tar.gz

---
+++ packages/combo-llm/combo_llm-0.1.2.tar.gz/combo_llm-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="combo_llm",
+    version="0.1.2",
+    author="Jared Hao",
+    author_email="9190632@qq.com",
+    description="combo Chinese llms",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/my_package",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
