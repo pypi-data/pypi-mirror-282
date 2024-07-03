b304996df9a9b181fc1c33956ce8903b46f4e932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 04:05:09 +0000

    Add package Kenar/0.5.5/Kenar-0.5.5-py3-none-any.whl

---
+++ packages/Kenar/Kenar-0.5.5-py3-none-any.whl/samples/sample_app.py
@@ -0,0 +1,19 @@
+import logging
+import os
+
+from dotenv import load_dotenv
+
+from kenar import AppConfig, KenarApp
+
+load_dotenv()
+
+logging.basicConfig(level=logging.INFO)
+
+app_conf = AppConfig(
+    app_slug=os.environ.get("KENAR_APP_SLUG"),
+    api_key=os.environ.get("KENAR_API_KEY"),
+    oauth_secret=os.environ.get("KENAR_OAUTH_SECRET"),
+    oauth_redirect_url=os.environ.get("KENAR_OAUTH_REDIRECT_URL"),
+)
+
+app = KenarApp(app_conf)
