7b757a1bab642c48e78c194b1b134db026c7b762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 03:04:42 +0000

    Add package IntegralElimination/0.1.1/IntegralElimination-0.1.1.tar.gz

---
+++ packages/IntegralElimination/IntegralElimination-0.1.1.tar.gz/IntegralElimination-0.1.1/IntegralElimination/__init__.py
@@ -0,0 +1,9 @@
+from .IntegralAlgebra import *
+from .IntegralMonomial import *
+from .IntegralPolynomial import * 
+from .convert import *
+from .io_equations import (
+    get_IO_equations,
+    replace_exp_in_IO_eqs,
+    simplify_IO_equations
+)
