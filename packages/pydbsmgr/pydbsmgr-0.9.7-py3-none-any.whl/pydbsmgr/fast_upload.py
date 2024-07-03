c07a7ac5f7a15873691364f743c8e596dbee3de9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 16:05:14 +0000

    Add package pydbsmgr/0.9.7/pydbsmgr-0.9.7-py3-none-any.whl

---
+++ packages/pydbsmgr/pydbsmgr-0.9.7-py3-none-any.whl/pydbsmgr/fast_upload.py
@@ -0,0 +1,302 @@
+import os
+import pickle
+import re
+
+import numpy as np
+import pandas as pd
+import pyodbc
+from pandas.core.frame import DataFrame
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+
+from pydbsmgr.utils.tools import ColumnsCheck
+
+
+class DataFrameToSQL(ColumnsCheck):
+    """Allows you to create a table from a dataframe"""
+
+    __slots__ = ["_connection_string", "_con", "_cur"]
+
+    def __init__(self, connection_string: str) -> None:
+        """Set the connection with the database"""
+        self._connection_string = connection_string
+        self._con = pyodbc.connect(self._connection_string, autocommit=True)
+        self._cur = self._con.cursor()
+
+    def import_table(
+        self,
+        df: DataFrame,
+        table_name: str,
+        overwrite: bool = True,
+        char_length: int = 512,
+        override_length: bool = True,
+        close_cursor: bool = True,
+        verbose: bool = False,
+    ) -> None:
+        """Process for importing the dataframe into the database"""
+
+        """Check if the current connection is active. If it is not, create a new connection"""
+
+        super().__init__(df)
+        df = self.get_frame()
+        df = df.replace(" ", None)
+        df = df.replace("<NA>", None)
+        df = df.replace(np.datetime64("NaT"), None)
+
+        if self._con.closed:
+            self._con = pyodbc.connect(self._connection_string, autocommit=True)
+            self._cur = self._con.cursor()
+
+        try:
+            """Create table"""
+            self._cur.execute(
+                self._create_table_query(table_name, df, char_length, override_length)
+            )
+        except pyodbc.Error as e:
+            if overwrite:
+                """If the table exists, it will be deleted and recreated"""
+                self._cur.execute("DROP TABLE %s" % (table_name))
+                self._cur.execute(
+                    self._create_table_query(table_name, df, char_length, override_length)
+                )
+            else:
+                warning_type = "UserWarning"
+                msg = "It was not possible to create the table {%s}" % table_name
+                msg += "Error: {%s}" % e
+                print(f"{warning_type}: {msg}")
+
+        """Insert data"""
+        self._cur.fast_executemany = True
+        self._cur.executemany(
+            self._insert_table_query(table_name, df),
+            [
+                [None if (isinstance(value, float) and np.isnan(value)) else value for value in row]
+                for row in df.values.tolist()
+            ],
+        )
+        if close_cursor:
+            self._con.close()
+
+        if verbose:
+            msg = "Table {%s}, successfully imported!" % table_name
+            print(f"{msg}")
+
+    def upload_table(self, df: DataFrame, table_name: str, verbose: bool = False) -> None:
+        """Access to update data from a dataframe to a database"""
+
+        """Check if the current connection is active. If it is not, create a new connection"""
+
+        super().__init__(df)
+        df = self.get_frame()
+        df = df.replace(" ", None)
+        df = df.replace("<NA>", None)
+        df = df.replace(np.datetime64("NaT"), None)
+
+        if self._con.closed:
+            self._con = pyodbc.connect(self._connection_string, autocommit=True)
+            self._cur = self._con.cursor()
+
+        try:
+            """Insert data"""
+            self._cur.fast_executemany = True
+            self._cur.executemany(self._insert_table_query(table_name, df), df.values.tolist())
+            self._con.close()
+        except pyodbc.Error as e:
+            print(e)
+            warning_type = "UserWarning"
+            msg = "It was not possible to create the table {%s}" % table_name
+            msg += "Error: {%s}" % e
+            print(f"{warning_type}: {msg}")
+
+        if verbose:
+            msg = "Table {%s}, successfully uploaded!" % table_name
+            print(f"{msg}")
+
+    def _create_table_query(
+        self, table_name: str, df: DataFrame, char_length: int, override_length: bool
+    ) -> str:
+        """Build the query that will be used to create the table"""
+        query = "CREATE TABLE " + table_name + "("
+        for j, column in enumerate(df.columns):
+            matches = re.findall(r"([^']*)", str(df.iloc[:, j].dtype))
+            dtype = self._infer_schema_query(matches[0])
+            if dtype == "VARCHAR(MAX)":
+                element = max(list(df[column].astype(str)), key=len)
+                max_string_length = len(element)
+                if max_string_length == 0 or override_length:
+                    max_string_length = char_length
+                dtype = dtype.replace("MAX", str(max_string_length))
+            query += column + " " + dtype + ", "
+
+        query = query[:-2]
+        query += ")"
+        return query
+
+    def _insert_table_query(self, table_name: str, df: DataFrame) -> str:
+        """Build the query to insert all rows found in the dataframe"""
+        query = "INSERT INTO %s({0}) values ({1})" % (table_name)
+        query = query.format(",".join(df.columns), ",".join("?" * len(df.columns)))
+        return query
+
+    def _infer_schema_query(self, datatype: str) -> str:
+        """Infer schema from a given datatype string"""
+        datatype = datatype.lower()
+        if datatype.find("float") != -1:
+            return "FLOAT"
+        elif datatype.find("int") != -1:
+            if datatype.find("64") != -1:
+                return "BIGINT"
+            else:
+                return "INT"
+        elif datatype.find("datetime") != -1:
+            return "DATE"
+        elif datatype.find("object") != -1:
+            return "VARCHAR(MAX)"
+        elif datatype.find("category") != -1:
+            return "VARCHAR(MAX)"
+        elif datatype.find("bool") != -1:
+            return "BIT"
+        else:
+            raise ValueError("Data type could not be inferred!")
+
+
+class UploadToSQL(DataFrameToSQL):
+    """It allows you to import/update a table from a `DataFrame` in an efficient way using the `DataFrameToSQL` class."""
+
+    def __init__(self, connection_string: str) -> None:
+        """Establish the connection to the database using the `DataFrameToSQL` class."""
+        super().__init__(connection_string)
+
+    def execute(
+        self,
+        df: DataFrame,
+        table_name: str,
+        chunk_size: int,
+        method: str = "override",  # or append
+        char_length: int = 512,
+        override_length: bool = True,
+        close_cursor: bool = True,
+        auto_resolve: bool = True,
+        frac: float = 0.01,
+        verbose: bool = False,
+    ) -> None:
+        """Checks if the number of chunks corresponds to the `DataFrame`."""
+        assert (
+            len(df) > chunk_size
+        ), "'chunk_size' cannot be greater than the length of the 'DataFrame', change the 'chunk_size'"
+
+        """Get chunks of `DataFrame`"""
+        if auto_resolve:
+            if len(df) >= 0.5e6:
+                n = int((df).shape[0] * frac)
+                df_chunks = [(df)[i : i + n] for i in range(0, (df).shape[0], n)]
+            else:
+                df_chunks = np.array_split(df, chunk_size)
+        else:
+            df_chunks = np.array_split(df, chunk_size)
+
+        if method == "override":
+            if self._check_table_exists(table_name):
+                print("Table exists, executing OVERRIDE...")
+                self._drop_table(table_name)
+
+                self.import_table(
+                    df=df_chunks[0],
+                    table_name=table_name,
+                    overwrite=True,
+                    char_length=char_length,
+                    override_length=override_length,
+                    close_cursor=close_cursor,
+                    verbose=verbose,
+                )
+
+            else:
+                print("Table does not exist, proceeding with CREATE TABLE.")
+
+                """Inserting the first chunk"""
+                self.import_table(
+                    df_chunks[0],
+                    table_name,
+                    True,
+                    char_length,
+                    override_length,
+                    close_cursor,
+                    verbose,
+                )
+
+            """Inserting the rest of the chunks"""
+            for i in range(1, len(df_chunks)):
+                self.upload_table(df_chunks[i], table_name, False)
+        elif method == "append":
+            if self._check_table_exists(table_name):
+                for data in df_chunks:
+                    self.upload_table(data, table_name, False)
+            else:
+                raise ValueError("Method 'append' requires an existing table.")
+        else:
+            raise ValueError(
+                'Invalid value for argument "method". Choose from ["override","append"].'
+            )
+
+    def _drop_table(self, table_name: str) -> None:
+        query = f"DROP TABLE IF EXISTS {table_name}"
+        _con = pyodbc.connect(self._connection_string, autocommit=True)
+        cursor = _con.cursor()
+        try:
+            cursor.execute(query)
+        except Exception as e:
+            print(f"Failed to drop table '{table_name}'. Error message:\n{str(e)}")
+        cursor.close()
+        _con.close()
+
+    def _check_table_exists(self, table_name: str) -> bool:
+        query = f"SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME='{table_name}'"
+        result = self._execute_query(query)
+        return bool(result["data"][0][0])
+
+    def _execute_query(self, query: str):
+        _con = pyodbc.connect(self._connection_string, autocommit=True)
+        cursor = _con.cursor()
+        try:
+            cursor.execute(query)
+            results = {"columns": [desc[0] for desc in cursor.description]}
+            results["data"] = cursor.fetchall()
+            results["count"] = [len(results["data"])]
+        except Exception as e:
+            print("Error executing SQL Query")
+            raise ValueError(f"Query Error: {str(e)}")
+        cursor.close()
+        _con.close()
+        return results
+
+
+########################################################################################
+
+if __name__ == "__main__":
+    connection_string = os.getenv("conn_string")
+    # Create a DataFrame
+    data = {"Name": ["John", "Alice", "Bob"], "Age": [25, 30, 35]}
+    df = pd.DataFrame(data)
+    table_name = "test_table"
+
+    if connection_string is None:
+        raise ValueError("Connection string not found.")
+
+    upload_from_df = UploadToSQL(connection_string)
+    upload_from_df.execute(
+        df=df,
+        table_name=table_name,
+        chunk_size=2,
+        method="override",
+    )
+
+    # Update the table
+    data = {"Name": ["Alexis", "Ivan", "Cordero"], "Age": [27, 27, 28]}
+    df = pd.DataFrame(data)
+
+    upload_from_df.execute(
+        df=df,
+        table_name=table_name,
+        chunk_size=2,
+        method="append",
+    )
---
+++ packages/pydbsmgr/pydbsmgr-0.9.7-py3-none-any.whl/pydbsmgr/utils/tools.py
@@ -0,0 +1,405 @@
+import concurrent.futures
+import glob
+import os
+import random
+import re
+import sys
+from collections import Counter
+from typing import Callable, List, Tuple
+
+import numpy as np
+import pandas as pd
+import psutil
+import pyarrow as pa
+import pyarrow.parquet as pq
+import yaml
+from numpy import datetime64
+from pandas.core.frame import DataFrame
+from pandas.errors import IntCastingNaNError
+from pyarrow import Table
+
+from pydbsmgr.main import check_if_contains_dates, is_number_regex
+from pydbsmgr.utils.config import load_config, parse_config
+
+
+def disableprints(func: Callable) -> Callable:
+    """Decorator to temporarily suppress print statements in a function"""
+
+    def wrapper(*args, **kwargs):
+        sys.stdout = open(os.devnull, "w")
+        result = func(*args, **kwargs)
+        sys.stdout = sys.__stdout__
+        return result
+
+    # Preserve the function's docstring if it has one
+    if func.__doc__ is not None:
+        wrapper.__doc__ = func.__doc__
+        return wrapper
+    else:
+        return func
+
+
+def most_repeated_item(items: list, two_most_common: bool = False) -> Tuple[str, str | None]:
+    """Returns a `Tuple` with the most common elements of a `list`.
+
+    Parameters
+    ----------
+    items : `list`
+        the `list` containing the items to be evaluated.
+    two_most_common : `bool`, `optional`
+        If `False`, returns only one element. Defaults to `False`.
+
+    Returns
+    -------
+    Tuple[`str`, `str` | `None`]
+        The two most common elements.
+    """
+    # Use Counter to count occurrences of each item in the list
+    counter = Counter(items)
+
+    # Find the two most common items and its count
+    most_common = counter.most_common(2)
+
+    if two_most_common:
+        if len(most_common) == 2:
+            item1, _ = most_common[0]
+            item2, _ = most_common[1]
+            return item1, item2
+        else:
+            item, _ = most_common[0]
+            return item, None
+    else:
+        item, _ = most_common[0]
+        return item, None
+
+
+def generate_secure_password(pass_len: int = 24) -> str:
+    """
+    Generate a secure password with the length specified
+    """
+    config = load_config("./pydbsmgr/utils/config.ini")
+    config = parse_config(config)
+    password = ""
+    char_matrix = config["security"]["char_matrix"]
+    for _ in range(pass_len):
+        password = password + random.choice(char_matrix)
+
+    return password
+
+
+class ColumnsCheck:
+    """Performs the relevant checks on the columns of the `DataFrame`"""
+
+    def __init__(self, df: DataFrame):
+        self.df = df
+
+    def get_frame(self, **kwargs) -> DataFrame:
+        self.df = self._process_columns(**kwargs)
+        return self.df
+
+    def _process_columns(self, surrounding: bool = True) -> DataFrame:
+        df = (self.df).copy()
+        df.columns = df.columns.str.lower()
+        df.columns = df.columns.str.replace(".", "", regex=False)
+        df.columns = df.columns.str.replace(",", "", regex=False)
+        df.columns = df.columns.str.replace(r"[^a-zA-Z0-9ñáéíóú_]", "_", regex=True)
+
+        df.columns = df.columns.str.replace("_+", "_", regex=True)
+        df.columns = df.columns.str.strip()
+        df.columns = df.columns.str.strip("_")
+        if surrounding:
+            df.columns = [f"[{col}]" for col in df.columns]
+
+        return df
+
+
+def coerce_datetime(x: str) -> datetime64:
+    try:
+        x = x.replace("-", "")
+        return pd.to_datetime(x, format="%Y%m%d")
+    except:
+        return np.datetime64("NaT")
+
+
+class ControllerFeatures:
+    def __init__(self, _container_client):
+        self._container_client = _container_client
+
+    def write_pyarrow(
+        self,
+        directory_name: str,
+        pytables: List[Table],
+        names: List[str],
+        overwrite: bool = True,
+    ) -> None:
+        """Write pyarrow table as `parquet` format"""
+        format_type = "parquet"
+        files_not_loaded = []
+        for table, blob_name in zip(pytables, names):
+            if table != None:
+                buf = pa.BufferOutputStream()
+                pq.write_table(table, buf)
+                parquet_data = buf.getvalue().to_pybytes()
+                blob_path_name = directory_name + "/" + blob_name
+                self._container_client.upload_blob(
+                    name=blob_path_name + "." + format_type,
+                    data=parquet_data,
+                    overwrite=overwrite,
+                )
+            else:
+                files_not_loaded.append(blob_name)
+        if len(files_not_loaded) > 0:
+            return files_not_loaded
+
+    def write_parquet(
+        self,
+        directory_name: str,
+        dfs: List[DataFrame],
+        names: List[str],
+        overwrite: bool = True,
+        upload: bool = True,
+    ) -> None:
+        """Write dataframes as `parquet` format by converting them first into `bytes`."""
+        files = []
+        format_type = "parquet"
+        files_not_loaded = []
+        for data, blob_name in zip(dfs, names):
+            if data != None:
+                table = pa.Table.from_pandas(data)
+                buf = pa.BufferOutputStream()
+                pq.write_table(table, buf)
+                parquet_data = buf.getvalue().to_pybytes()
+                blob_path_name = directory_name + "/" + blob_name
+                if upload:
+                    self._container_client.upload_blob(
+                        name=blob_path_name + "." + format_type,
+                        data=parquet_data,
+                        overwrite=overwrite,
+                    )
+                else:
+                    files.append(parquet_data)
+            else:
+                files_not_loaded.append(blob_name)
+        if len(files_not_loaded) > 0:
+            return files_not_loaded
+
+        if not upload:
+            return files
+
+
+def column_coincidence(df1: DataFrame, df2: DataFrame) -> float:
+    """Return the percentage of coincident columns between two pandas dataframes."""
+    if not isinstance(df1, pd.DataFrame) or not isinstance(df2, pd.DataFrame):
+        raise ValueError("Both inputs should be pandas DataFrames")
+
+    column_names1 = set(df1.columns)
+    column_names2 = set(df2.columns)
+
+    common_columns = column_names1.intersection(column_names2)
+    total_columns = column_names1.union(column_names2)
+
+    coincidence_percentage = len(common_columns) / len(total_columns)
+    return coincidence_percentage
+
+
+def merge_by_coincidence(df1: DataFrame, df2: DataFrame, tol: float = 0.9) -> DataFrame:
+    """Merge two pandas dataframes by finding the most similar columns based on their names."""
+    percentage = column_coincidence(df1, df2)
+    total_columns = set(df1.columns).union(set(df2.columns))
+    num_col1 = len(df1.columns)
+    num_col2 = len(df2.columns)
+    if num_col1 < num_col2:
+        min_cols = set(df1.columns)
+        min_cols = list(min_cols.intersection(set(df2.columns)))
+    else:
+        min_cols = set(df2.columns)
+        min_cols = list(min_cols.intersection(set(df1.columns)))
+
+    df2 = (df2[min_cols]).copy()
+    df1 = (df1[min_cols]).copy()
+    diff = total_columns.difference(set(min_cols))
+
+    df = pd.concat([df1, df2], ignore_index=True)
+    if percentage > tol:
+        print("The following columns were lost : ", diff)
+    else:
+        print(
+            f"The following columns were missed with a match percentage of {percentage*100:.2f}% : ",
+            diff,
+        )
+    return df
+
+
+def terminate_process(file_path: str) -> None:
+    """Terminate the process holding the file."""
+    for proc in psutil.process_iter(["pid", "open_files"]):
+        try:
+            if any(file_path in file_info.path for file_info in proc.open_files()):
+                print(f"Terminating process {proc.pid} holding the file.")
+                proc.terminate()
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+
+
+def erase_files(format: str = "log") -> None:
+    """Erase all files with the given format."""
+    for filename in glob.glob("*." + format):
+        try:
+            os.remove(filename)
+        except:
+            terminate_process(filename)
+            os.remove(filename)
+
+
+def get_extraction_date(
+    filename: str | List[str], REGEX_PATTERN: str = r"\d{4}-\d{2}-\d{2}"
+) -> str:
+    """Allows to extract the date of extraction according to the directory within the storage account.
+
+    Parameters
+    ----------
+    filename : Union[str, List[str]]
+        file path inside the storage account
+    REGEX_PATTERN : `str`, `optional`
+        regular expression pattern to extract the date.
+
+    Returns
+    -------
+    `str`
+        the date that was extracted if found in the file path.
+    """
+
+    def sub_extraction_date(filename: str, REGEX_PATTERN: str) -> str:
+        extraction_date = re.findall(REGEX_PATTERN, filename)
+        if len(extraction_date) > 0:
+            _date = extraction_date[0]
+        else:
+            _date = ""
+        return _date
+
+    if type(filename) == str:
+        return sub_extraction_date(filename, REGEX_PATTERN)
+    elif isinstance(filename, list):
+        dates = []
+        for name in filename:
+            dates.append(sub_extraction_date(name, REGEX_PATTERN))
+        return dates
+
+
+class ColumnsDtypes:
+    """Convert all columns to specified dtype."""
+
+    def __init__(self, df_: DataFrame):
+        self.df = df_.copy()
+
+    def correct(
+        self,
+        drop_values: bool = False,
+        drop_rows: bool = False,
+        sample_frac: float = 0.1,
+    ) -> DataFrame:
+        self._check_int_float(drop_values, drop_rows)
+        self._check_datetime(sample_frac)
+        return self.df
+
+    def get_frame(self) -> DataFrame:
+        return self.df
+
+    def _check_int_float(self, drop_values: bool = False, drop_rows: bool = False) -> None:
+        """Check and correct the data types of columns in a `DataFrame`."""
+
+        def check_float(x):
+            if isinstance(x, str):
+                try:
+                    return float(x)
+                except:
+                    return np.nan
+            else:
+                return x
+
+        def check_int(x):
+            if isinstance(x, str):
+                try:
+                    return int(x)
+                except:
+                    return ""
+            else:
+                return x
+
+        df_ = (self.df).copy()
+        if drop_values:
+            if len(df_) < 1e5:
+                for col in df_.columns:
+                    value = str(df_[col].iloc[0])
+                    val_dtype = None
+                    if is_number_regex(value):
+                        if type(check_float(value)).__name__ == "float":
+                            with concurrent.futures.ThreadPoolExecutor() as executor:
+                                df_[col] = list(executor.map(check_float, df_[col]))
+                            df_[col] = df_[col].astype("float64")
+                            val_dtype = "float64"
+                            print("Checking {%s} for column {%s}" % (val_dtype, col))
+
+                        if type(check_int(value)).__name__ == "int" and val_dtype == None:
+                            with concurrent.futures.ThreadPoolExecutor() as executor:
+                                df_[col] = list(executor.map(check_int, df_[col]))
+                            if drop_rows:
+                                df_ = df_.loc[df_[col].notnull()]
+                            try:
+                                df_[col] = df_[col].astype("int64")
+                                val_dtype = "int64"
+                                print("Checking {%s} for column {%s}" % (val_dtype, col))
+                            except IntCastingNaNError as e:
+                                df_[col] = df_[col].astype("object")
+                                val_dtype = "object"
+                                print("Checking {%s} for column {%s}" % (val_dtype, col))
+
+                        print(f"Successfully transformed the '{col}' column into {val_dtype}.")
+        self.df = df_
+
+    def _check_datetime(self, sample_frac: float) -> None:
+        """Check and convert date-time string columns to `datetime` objects."""
+        df_ = self.df
+        cols = df_.columns
+        df_sample = df_.sample(frac=sample_frac)
+        for column_index, datatype in enumerate(df_.dtypes):
+            col = cols[column_index]
+            if datatype == "object":
+                datetype_column = (df_sample[col].apply(check_if_contains_dates)).isin([True]).any()
+                if datetype_column:
+                    try:
+                        with concurrent.futures.ThreadPoolExecutor() as executor:
+                            df_[col] = list(
+                                executor.map(lambda date: date.replace("-", ""), df_[col])
+                            )
+                        df_[col] = pd.to_datetime(df_[col], format="%Y%m%d").dt.normalize()
+                        print(f"Successfully transformed the '{col}' column into datetime64[ns].")
+                    except:
+                        with concurrent.futures.ThreadPoolExecutor() as executor:
+                            df_[col] = list(executor.map(coerce_datetime, df_[col]))
+                        df_[col] = pd.to_datetime(df_[col], format="%Y%m%d", errors="coerce")
+                        print(f"Successfully transformed the '{col}' column into datetime64[ns].")
+            elif datatype == "datetime64[us]" or datatype == "datetime64[ns]":
+                df_[col] = df_[col].astype("datetime64[ns]")
+                df_[col] = df_[col].dt.normalize()
+                print(f"Successfully transformed the '{col}' column into datetime64[ns].")
+
+        self.df = df_
+
+
+def create_directory(data, parent_path=""):
+    """Creates the directory tree from a `yaml` file."""
+    for key, value in data.items():
+        path = os.path.join(parent_path, key)
+        if isinstance(value, dict):
+            os.makedirs(path, exist_ok=True)
+            create_directory(value, path)
+        else:
+            os.makedirs(path, exist_ok=True)
+
+
+def create_directories_from_yaml(yaml_file):
+    """Reads a `yaml` file and creates directories based on its content."""
+    with open(yaml_file, "r") as file:
+        data = yaml.safe_load(file)
+        create_directory(data)
