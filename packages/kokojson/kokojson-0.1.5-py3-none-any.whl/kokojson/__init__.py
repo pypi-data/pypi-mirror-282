8d66ea5830908e7e5a8b3b3d0fe10b19c3a60329
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 07:05:17 +0000

    Add package kokojson/0.1.5/kokojson-0.1.5-py3-none-any.whl

---
+++ packages/kokojson/kokojson-0.1.5-py3-none-any.whl/kokojson/__init__.py
@@ -0,0 +1,82 @@
+import json
+
+def load(data):
+    """
+    Load JSON data and return a dictionary-like object that allows safe access to keys.
+
+    Args:
+        data (str): The JSON data as a string.
+
+    Returns:
+        SafeDict: A dictionary-like object that allows safe access to keys.
+    """
+    json_data = json.loads(data)
+    return SafeDict(json_data)
+
+def wild_append(json_data):
+    """
+    Prepares a JSON-like data structure for versatile appending.
+    Ensures that all keys in the structure can handle both dictionary and list operations.
+    
+    :param json_data: The JSON-like data structure (dictionary)
+    :return: A proxy object that allows versatile appending
+    """
+    class VersatileAppendProxy:
+        def __init__(self, data):
+            self.data = data
+
+        def __getitem__(self, key):
+            if key not in self.data:
+                self.data[key] = {}
+            return VersatileAppendProxy(self.data[key])
+
+        def __setitem__(self, key, value):
+            self.data[key] = value
+
+        def append(self, value):
+            if not isinstance(self.data, list):
+                self.data = [self.data] if self.data else []
+            self.data.append(value)
+
+        def update(self, value):
+            if isinstance(self.data, dict):
+                self.data.update(value)
+            else:
+                self.data = value
+
+        def get_data(self):
+            return self.data
+
+    return VersatileAppendProxy(json_data)
+
+class SafeDict(dict):
+    """
+    A dictionary-like object that allows safe access to keys.
+
+    If a key doesn't exist, it returns None instead of raising a KeyError.
+    """
+
+    def __missing__(self, key):
+        """
+        Called when a key is not found in the dictionary.
+
+        Args:
+            key (str): The key being accessed.
+
+        Returns:
+            None
+        """
+        return None
+
+    def get(self, key, default=None):
+        """
+        Get the value of a key, or return a default value if the key doesn't exist.
+
+        Args:
+            key (str): The key to access.
+            default (Any, optional): The default value to return if the key doesn't exist.
+
+        Returns:
+            Any: The value of the key, or the default value if the key doesn't exist.
+        """
+        return super().get(key, default)
