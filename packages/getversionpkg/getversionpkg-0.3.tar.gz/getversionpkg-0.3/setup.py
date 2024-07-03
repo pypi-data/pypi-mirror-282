34d2089ec27b06e43b1f787540e6b3606f1c9555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 15:05:42 +0000

    Add package getversionpkg/0.3/getversionpkg-0.3.tar.gz

---
+++ packages/getversionpkg/getversionpkg-0.3.tar.gz/getversionpkg-0.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+setup(
+    name='getversionpkg',
+    version='0.3',
+    description="This is a tesp package",
+    packages=find_packages(),
+    python_requires='>=3.6'
+    )
