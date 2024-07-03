00b850de96e82ca9d139da3232b923874179f67c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 06:05:59 +0000

    Add package alchemy-cat/0.0.4/alchemy_cat-0.0.4-py3-none-any.whl

---
+++ packages/alchemy-cat/alchemy_cat-0.0.4-py3-none-any.whl/alchemy_cat/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: Xiaobo Yang
+@contact: hal_42@zju.edu.cn
+@software: PyCharm
+@file: __init__.py
+@time: 2020/1/8 0:15
+@desc:
+"""
---
+++ packages/alchemy-cat/alchemy_cat-0.0.4-py3-none-any.whl/alchemy_cat/py_tools/cat_head.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author  : Xiaobo Yang
+@Contact : hal_42@zju.edu.cn
+@Time    : 2022/5/28 20:37
+@File    : cat_head.py
+@Software: PyCharm
+@Desc    : 
+"""
+from typing import Optional, Union, TYPE_CHECKING
+
+from dataclasses import dataclass
+
+if TYPE_CHECKING:
+    from alchemy_cat.dl_config.config import Config
+
+__all__ = ['喵', 'meow']
+
+
+@dataclass
+class CatHead(object):
+    """该类型实例「喵」可作为全局变量使用。"""
+    cfg: Optional['Config'] = None
+    is_debug: bool = False
+    rand_seed_final: Union[str, int, None] = None
+
+
+喵 = meow = CatHead()
