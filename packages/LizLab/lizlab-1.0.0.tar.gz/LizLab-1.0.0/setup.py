f3f1e29dc5e9651cf6802af97ff306b6aaf4db48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 06:06:00 +0000

    Add package LizLab/1.0.0/lizlab-1.0.0.tar.gz

---
+++ packages/LizLab/lizlab-1.0.0.tar.gz/LizLab-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='LizLab',
+    version='1.0.0',
+    description='Liz Lab',
+    author='qrobless',
+    author_email='robles.b2002@mail.ru',
+    packages=[''],  # список пакетов в вашей библиотеке
+    install_requires=['pyodbc', 'Pillow'],  # зависимости
+)
