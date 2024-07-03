8b9e957adc991fa70c338c3577c36c100192254d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 14:04:57 +0000

    Add package pyproject-fmt-rust/1.1.5/pyproject_fmt_rust-1.1.5.tar.gz

---
+++ packages/pyproject-fmt-rust/pyproject_fmt_rust-1.1.5.tar.gz/pyproject_fmt_rust-1.1.5/tests/test_main.py
@@ -0,0 +1,51 @@
+from __future__ import annotations
+
+from textwrap import dedent
+
+from pyproject_fmt_rust import Settings, format_toml
+
+
+def test_format_toml() -> None:
+    txt = """
+    [project]
+    keywords = [
+      "A",
+    ]
+    classifiers = [
+      "Programming Language :: Python :: 3 :: Only",
+    ]
+    dynamic = [
+      "B",
+    ]
+    dependencies = [
+      "requests>=2.0",
+    ]
+    """
+
+    settings = Settings(
+        column_width=120,
+        indent=4,
+        keep_full_version=True,
+        min_supported_python=(3, 7),
+        max_supported_python=(3, 8),
+    )
+    res = format_toml(dedent(txt), settings)
+
+    expected = """\
+    [project]
+    keywords = [
+        "A",
+    ]
+    classifiers = [
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ]
+    dynamic = [
+        "B",
+    ]
+    dependencies = [
+        "requests>=2.0",
+    ]
+    """
+    assert res == dedent(expected)
