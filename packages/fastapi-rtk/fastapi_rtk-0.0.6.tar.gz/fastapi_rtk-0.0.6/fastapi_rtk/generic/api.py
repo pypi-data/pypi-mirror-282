e524fe1324e432e3855b07412ca08b9ede028666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 03:05:04 +0000

    Add package fastapi-rtk/0.0.6/fastapi_rtk-0.0.6.tar.gz

---
+++ packages/fastapi-rtk/fastapi_rtk-0.0.6.tar.gz/fastapi_rtk-0.0.6/fastapi_rtk/generic/api.py
@@ -0,0 +1,18 @@
+from typing import Callable
+
+from ..api import ModelRestApi
+from ..dependencies import get_query_manager
+from .db import GenericQueryManager
+from .interface import GenericInterface
+
+
+class GenericApi(ModelRestApi):
+    datamodel: GenericInterface
+
+    @property
+    def get_db(self):
+        return lambda: self.datamodel.session
+
+    @property
+    def get_query_manager(self) -> Callable[..., GenericQueryManager]:
+        return get_query_manager(self.datamodel, True)
