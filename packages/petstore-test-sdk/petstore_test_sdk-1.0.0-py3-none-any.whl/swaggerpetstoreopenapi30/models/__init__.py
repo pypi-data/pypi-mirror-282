710b95f4cf70b3f9b1176ff482a1e2aa4501ed2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 04:05:19 +0000

    Add package petstore-test-sdk/1.0.0/petstore_test_sdk-1.0.0-py3-none-any.whl

---
+++ packages/petstore-test-sdk/petstore_test_sdk-1.0.0-py3-none-any.whl/swaggerpetstoreopenapi30/models/__init__.py
@@ -0,0 +1,13 @@
+__all__ = [
+    'address',
+    'pet',
+    'customer',
+    'category',
+    'user',
+    'order',
+    'tag',
+    'pet_image',
+    'order_status_enum',
+    'status_enum',
+    'pet_status_enum',
+]
