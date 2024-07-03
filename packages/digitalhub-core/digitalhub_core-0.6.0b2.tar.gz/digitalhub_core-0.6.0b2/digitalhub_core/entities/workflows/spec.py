3a09dd0e3ec4e430644ee1543a4db0390a13255a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 03:05:21 +0000

    Add package digitalhub-core/0.6.0b2/digitalhub_core-0.6.0b2.tar.gz

---
+++ packages/digitalhub-core/digitalhub_core-0.6.0b2.tar.gz/digitalhub_core-0.6.0b2/digitalhub_core/entities/workflows/spec.py
@@ -0,0 +1,18 @@
+"""
+Workflow base specification module.
+"""
+from __future__ import annotations
+
+from digitalhub_core.entities._base.spec import Spec, SpecParams
+
+
+class WorkflowSpec(Spec):
+    """
+    Workflow specifications.
+    """
+
+
+class WorkflowParams(SpecParams):
+    """
+    Workflow parameters.
+    """
---
+++ packages/digitalhub-core/digitalhub_core-0.6.0b2.tar.gz/digitalhub_core-0.6.0b2/digitalhub_core/utils/exceptions.py
@@ -0,0 +1,22 @@
+"""
+Exceptions module.
+"""
+from __future__ import annotations
+
+
+class StoreError(Exception):
+    """
+    Raised when incontered errors on stores.
+    """
+
+
+class BackendError(Exception):
+    """
+    Raised when incontered errors from backend.
+    """
+
+
+class EntityError(Exception):
+    """
+    Raised when incontered errors on entities.
+    """
