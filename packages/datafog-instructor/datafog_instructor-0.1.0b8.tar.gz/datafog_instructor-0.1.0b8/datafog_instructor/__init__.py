3bde3f60235e1fb671c95e0a6e3fcc321aca2ccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-03 05:04:51 +0000

    Add package datafog-instructor/0.1.0b8/datafog_instructor-0.1.0b8.tar.gz

---
+++ packages/datafog-instructor/datafog_instructor-0.1.0b8.tar.gz/datafog_instructor-0.1.0b8/datafog_instructor/__init__.py
@@ -0,0 +1,4 @@
+from .core import DataFog, OllamaLLM, LLMInterface
+from .models import EntityType, DetectedEntity, DetectedEntities
+
+__all__ = ['DataFog', 'OllamaLLM', 'LLMInterface', 'EntityType', 'DetectedEntity', 'DetectedEntities']
