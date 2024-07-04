791ef919789a67dfe903a4acc7473a6de8104cf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 07:04:45 +0000

    Add package vec64/0.0.6/vec64-0.0.6-cp312-cp312-win32.whl

---
+++ packages/vec64/vec64-0.0.6-cp312-cp312-win32.whl/vec64/__init__.py
@@ -0,0 +1,10 @@
+from .ctype import CharType, CT
+from .span import Span
+from .wrappers import base64_symbol_indexes, split
+
+__all__ = [
+    "CharType",
+    "Span",
+    "base64_symbol_indexes",
+    "split",
+]
