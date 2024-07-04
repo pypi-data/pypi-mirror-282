be36b80512cd7e210180d2a4bf106b847dccdb48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 16:05:35 +0000

    Add package SinhalaTools/0.0.4/SinhalaTools-0.0.4.tar.gz

---
+++ packages/SinhalaTools/SinhalaTools-0.0.4.tar.gz/SinhalaTools-0.0.4/SinhalaTools/__init__.py
@@ -0,0 +1,2 @@
+from .main import sinhala_to_roman, roman_to_sinhala
+from .input_tools import inputTools
---
+++ packages/SinhalaTools/SinhalaTools-0.0.4.tar.gz/SinhalaTools-0.0.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SinhalaTools',
+    version='0.0.4',
+    packages=find_packages(),
+    install_requires=[
+        'requests'
+        # Add dependencies here.
+        # e.g. 'numpy>=1.11.1'
+    ],
+    description='Sinhala Tools for converting Sinhala text to Roman and vice versa.',
+    author='Tharindu Madhusanka Wimalasena',
+    author_email='tharindu.20@cse.mrt.ac.lk',
+    url='https://github.com/TharinduMadhusanka',
+)
