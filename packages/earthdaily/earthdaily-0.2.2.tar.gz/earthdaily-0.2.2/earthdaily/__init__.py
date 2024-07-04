4330b1f2ab2d9d680812bb5ee881e5e9498bd885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 04:04:56 +0000

    Add package earthdaily/0.2.2/earthdaily-0.2.2.tar.gz

---
+++ packages/earthdaily/earthdaily-0.2.2.tar.gz/earthdaily-0.2.2/earthdaily/__init__.py
@@ -0,0 +1,8 @@
+from . import earthdatastore, datasets
+from .accessor import EarthDailyAccessorDataArray, EarthDailyAccessorDataset
+# import warnings
+
+# to hide warnings from rioxarray or nano seconds conversion
+# warnings.filterwarnings("ignore")
+
+__version__ = "0.2.2"
