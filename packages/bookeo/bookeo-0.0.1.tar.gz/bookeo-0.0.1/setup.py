a64c3469f5897b86791260d040c0c408cf1e4f98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 03:06:11 +0000

    Add package bookeo/0.0.1/bookeo-0.0.1.tar.gz

---
+++ packages/bookeo/bookeo-0.0.1.tar.gz/bookeo-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.1"
+DESCRIPTION = "My first Python package"
+LONG_DESCRIPTION = "My first Python package with a slightly longer description"
+
+# Setting up
+setup(
+    name="bookeo",
+    version=VERSION,
+    author="Nolan Welch",
+    author_email="nolanwelch@outlook.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],  # add any additional packages that
+    # needs to be installed along with your package. Eg: 'caer'
+    keywords=["python", "Bookeo"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
