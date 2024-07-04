2b199f28e6aa9764fbdedc66ba9f8a572a64ff26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 04:05:49 +0000

    Add package phylo-converge/0.1.1/phylo_converge-0.1.1.tar.gz

---
+++ packages/phylo-converge/phylo_converge-0.1.1.tar.gz/phylo_converge-0.1.1/phylo_converge/__init__.py
@@ -0,0 +1,5 @@
+from .alignment import read_sequances, align_sequances
+from .phylogeny import construct_phylogenetic_tree
+from .visualization import draw_phylogenetic_tree
+
+__all__ = ["read_sequances", "align_sequances", "construct_phylogenetic_tree", "draw_phylogenetic_tree"]
---
+++ packages/phylo-converge/phylo_converge-0.1.1.tar.gz/phylo_converge-0.1.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="phylo_converge",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[
+        "biopython",
+        "matplotlib",
+    ],
+    entry_points={
+        "console_scripts": [
+            "phylo_converge=phylo_converge:main",
+        ],
+    },
+    author="Ahmed Yassin",
+    author_email="Ahmedyassin300@outlook.com",
+    description="A package to calculate genetic convergence and draw phylogenetic trees.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ahmedyassin300/phylo_converge",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
