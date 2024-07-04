5fecd8c700db7a16bf51b183a3a033f28b4da2d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 04:05:01 +0000

    Add package krystof2h-test/0.0.2/krystof2h_test-0.0.2.tar.gz

---
+++ packages/krystof2h-test/krystof2h_test-0.0.2.tar.gz/krystof2h_test-0.0.2/README.md
@@ -0,0 +1,3 @@
+# Package python krystof2h-test
+
+* https://pypi.org/project/krystof2h-test/0.0.2/
---
+++ packages/krystof2h-test/krystof2h_test-0.0.2.tar.gz/krystof2h_test-0.0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="krystof2h-test",
+    version="0.0.2",
+    install_requires=[
+        "requests",
+    ],
+)
