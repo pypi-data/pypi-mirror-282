e39996ee0f3cd3d650fa295f51e8788204fa7d0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 03:06:30 +0000

    Add package timpypi/1.1.0/timpypi-1.1.0.tar.gz

---
+++ packages/timpypi/timpypi-1.1.0.tar.gz/timpypi-1.1.0/timpypi/time.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+import logging
+from common import exception
+
+_logger = logging.getLogger(__name__)
+
+
+
+@exception
+def timestamp_to_datetime(timestamp) -> datetime:
+    if not (isinstance(timestamp, float) or isinstance(timestamp, int)):
+        raise ValueError("Timestamp must be an integer or float")
+    timestamp = float(timestamp)
+    try:
+        return datetime.fromtimestamp(timestamp)
+    except ValueError as e:
+        return datetime.fromtimestamp(timestamp / 1000)
