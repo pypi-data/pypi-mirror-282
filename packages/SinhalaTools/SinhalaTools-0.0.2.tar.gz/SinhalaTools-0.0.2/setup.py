c8d323c5236a0e36947a3242b14b8ef46aaccc6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 16:05:34 +0000

    Add package SinhalaTools/0.0.2/SinhalaTools-0.0.2.tar.gz

---
+++ packages/SinhalaTools/SinhalaTools-0.0.2.tar.gz/SinhalaTools-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SinhalaTools',
+    version='0.0.2',
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies here.
+        # e.g. 'numpy>=1.11.1'
+    ],
+)
