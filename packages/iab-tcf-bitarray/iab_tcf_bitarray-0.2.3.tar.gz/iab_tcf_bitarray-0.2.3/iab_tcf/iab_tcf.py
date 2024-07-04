64c991bccea909a836b12251f3e1e869bbae6ea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 04:05:33 +0000

    Add package iab-tcf-bitarray/0.2.3/iab_tcf_bitarray-0.2.3.tar.gz

---
+++ packages/iab-tcf-bitarray/iab_tcf_bitarray-0.2.3.tar.gz/iab_tcf_bitarray-0.2.3/iab_tcf/iab_tcf.py
@@ -0,0 +1,23 @@
+import base64
+from typing import List
+
+from .bits import Reader
+
+
+def segments(consent: str) -> List[str]:
+    """Helper to split the core and non core consents."""
+    return consent.split(".")
+
+
+def base64_decode(segment: str) -> bytes:
+    """Helper to decode the IAB TCF segments encoded."""
+    padding = "=" * (-len(segment) % 4)
+    return base64.urlsafe_b64decode(segment + padding)
+
+
+def version(consent: bytes) -> int:
+    """Helper to extract the version from a consent without having
+    to wait for the full decoding.
+    """
+    reader = Reader(consent)
+    return reader.read_int(6)
---
+++ packages/iab-tcf-bitarray/iab_tcf_bitarray-0.2.3.tar.gz/iab_tcf_bitarray-0.2.3/tests/conftest.py
@@ -0,0 +1,27 @@
+import json
+import os
+from pathlib import Path
+from typing import Dict, List
+
+
+def load_seed(file):
+    current_folder = os.path.dirname(__file__)
+    data = Path(os.path.join(current_folder, file)).read_text()
+    return json.loads(data)
+
+
+def mapbit(length: int, trues: List = [], falses: List = []):
+    def _value(key: int):
+        if trues:
+            return key in trues
+        if falses:
+            return key not in falses
+        return True
+
+    return {i + 1: _value(i + 1) for i in range(length)}
+
+
+def mapbit_from_dict(length: int, trues: Dict) -> Dict[int, bool]:
+    trues = [int(i) for i, _ in trues.items()]
+    falses = [i + 1 for i in range(length)]
+    return mapbit(length or len(trues), trues=trues, falses=falses)
