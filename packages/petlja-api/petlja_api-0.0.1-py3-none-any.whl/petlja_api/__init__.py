675aa8c532bc33344b7cc8685e117a57c9ae5067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 05:05:53 +0000

    Add package petlja-api/0.0.1/petlja_api-0.0.1-py3-none-any.whl

---
+++ packages/petlja-api/petlja_api-0.0.1-py3-none-any.whl/petlja_api/__init__.py
@@ -0,0 +1,4 @@
+from .auth import login
+from .competition import create_competition, get_competition_id, get_added_problem_ids, add_problem, upload_scoring, add_language
+from .problem import get_problem_id, get_problem_name, create_problem, upload_testcases, upload_statement
+from .submit import submit_solution
---
+++ packages/petlja-api/petlja_api-0.0.1-py3-none-any.whl/petlja_api/urls.py
@@ -0,0 +1,4 @@
+ARENA_URL = "https://arena.petlja.org"
+PETLJA_URL = "https://petlja.org"
+CPANEL_URL = f"{PETLJA_URL}/sr-Latn-RS/cpanel"
+PROBLEMS_URL = f"{PETLJA_URL}/sr-Latn-RS/problems"
