d55df9e15a1463916533253d36debdbac99cf1f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 05:05:36 +0000

    Add package shapelysmooth/0.2.1/shapelysmooth-0.2.1.tar.gz

---
+++ packages/shapelysmooth/shapelysmooth-0.2.1.tar.gz/shapelysmooth-0.2.1/setup.py
@@ -0,0 +1,19 @@
+from glob import glob
+
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import setup
+
+ext_modules = [
+    Pybind11Extension(
+        "_shapelysmooth",
+        include_dirs=["./src"],
+        sources=sorted(glob("src/*.c*")),
+        define_macros=[("_shapelysmooth", "_shapelysmooth")],
+        extra_compile_args=["-O3", "-Wall"],
+    ),
+]
+
+setup(
+    cmdclass={"build_ext": build_ext},
+    ext_modules=ext_modules,
+)
