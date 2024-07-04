44634e49e3ead1470faed011050465f173aae251
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 04:06:47 +0000

    Add package python-seamless/0.8.0/python_seamless-0.8.0-py3-none-any.whl

---
+++ packages/python-seamless/python_seamless-0.8.0-py3-none-any.whl/seamless/__init__.py
@@ -0,0 +1,5 @@
+from .components import Component
+from .element import Element
+from .html import *
+from .styling import Style
+from .rendering.html import render
---
+++ packages/python-seamless/python_seamless-0.8.0-py3-none-any.whl/seamless/styling/__init__.py
@@ -0,0 +1,5 @@
+from .color import Color
+from .style import Style
+from .css_modules import CSS
+
+__all__ = ["Color", "Style", "CSS"]
