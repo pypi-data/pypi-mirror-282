797306dffc04f9b08690a50c7d5982b574795efc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 05:05:09 +0000

    Add package AuthoRex/1.0.0/authorex-1.0.0.tar.gz

---
+++ packages/AuthoRex/authorex-1.0.0.tar.gz/authorex-1.0.0/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+# Package metadata
+NAME = "AuthoRex"
+VERSION = "1.0.0"
+DESCRIPTION = "A package for generating and authenticating OTPs using MongoDB."
+URL = "https://github.com/TraxDinosaur/AuthoRex"
+AUTHOR = "TraxDinosaur"
+AUTHOR_CONTACT = "https://traxdinosaur.github.io"
+LICENSE = "CC-BY-SA 4.0"
+KEYWORDS = ["OTP", "authentication", "MongoDB", "OTP generator", "OTP authenticator"]
+
+# Read long description from README.md
+with open("README.md", "r", encoding="utf-8") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+# Packages required by the project
+REQUIRED_PACKAGES = [
+    "pymongo"
+]
+
+setuptools.setup(
+    name=NAME,
+    version=VERSION,
+    author=AUTHOR,
+    author_contact=AUTHOR_CONTACT,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url=URL,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved",
+        "Operating System :: OS Independent",
+    ],
+    keywords=KEYWORDS,
+    install_requires=REQUIRED_PACKAGES,
+    python_requires=">=3.6",
+)
