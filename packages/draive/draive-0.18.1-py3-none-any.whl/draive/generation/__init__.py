c11441fb7345476737d4a3fce32bde87e0731330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 04:06:08 +0000

    Add package draive/0.18.1/draive-0.18.1-py3-none-any.whl

---
+++ packages/draive/draive-0.18.1-py3-none-any.whl/draive/generation/__init__.py
@@ -0,0 +1,15 @@
+from draive.generation.image import ImageGeneration, ImageGenerator, generate_image
+from draive.generation.model import ModelGeneration, ModelGenerator, generate_model
+from draive.generation.text import TextGeneration, TextGenerator, generate_text
+
+__all__ = [
+    "ModelGenerator",
+    "ModelGeneration",
+    "generate_model",
+    "TextGenerator",
+    "TextGeneration",
+    "generate_text",
+    "ImageGeneration",
+    "ImageGenerator",
+    "generate_image",
+]
