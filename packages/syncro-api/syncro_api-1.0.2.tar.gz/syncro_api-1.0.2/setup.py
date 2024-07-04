fbf0d1ab042e74516b9f722c7c9c908c44007ea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 14:05:06 +0000

    Add package syncro-api/1.0.2/syncro_api-1.0.2.tar.gz

---
+++ packages/syncro-api/syncro_api-1.0.2.tar.gz/syncro_api-1.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='syncro_api',  # Replace with your desired package name
+    version='1.0.2',            # Replace with your package version
+    py_modules=['syncro_api'], # List all .py files here (without .py extension)
+    description='Some python functions to interact with Syncro API',
+    author='Dan S',
+    author_email='dan@cybertek.systems',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
