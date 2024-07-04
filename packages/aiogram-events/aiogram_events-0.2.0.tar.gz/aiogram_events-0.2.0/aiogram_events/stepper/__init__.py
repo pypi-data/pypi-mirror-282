1ea2820cf83ef72bb96f6d8fce42f5bde7e7925e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 14:04:52 +0000

    Add package aiogram-events/0.2.0/aiogram_events-0.2.0.tar.gz

---
+++ packages/aiogram-events/aiogram_events-0.2.0.tar.gz/aiogram_events-0.2.0/aiogram_events/stepper/__init__.py
@@ -0,0 +1,11 @@
+# pylint: disable=missing-module-docstring
+from aiogram_events.stepper.entry import (  # FileEntry,
+    DateEntry,
+    Entry,
+    FileEntry,
+    NumberEntry,
+    OneOfEntry,
+    TextEntry,
+    UrlEntry,
+)
+from aiogram_events.stepper.stepper import router
