bfe285cc8686f80e4d6091e8c1ffca6206656d3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 04:05:06 +0000

    Add package tabnetviz/1.3/tabnetviz-1.3.tar.gz

---
+++ packages/tabnetviz/tabnetviz-1.3.tar.gz/tabnetviz-1.3/tabnetviz/__init__.py
@@ -0,0 +1,13 @@
+'''tabnetviz - table-based network visualizer'''
+
+# Copyright 2023 Andras Szilagyi
+# Distributed under the GNU General Public License v3
+# See https://www.gnu.org/licenses/gpl-3.0.html
+
+__author__ = "Andras Szilagyi"
+__copyright__ = "Copyright 2024 Andras Szilagyi"
+__license__ = "GPLv3"
+__version__ = "1.3"
+__maintainer__ = "Andras Szilagyi"
+__email__ = "szilagyi.andras@ttk.hu"
+__status__ = "Production"
