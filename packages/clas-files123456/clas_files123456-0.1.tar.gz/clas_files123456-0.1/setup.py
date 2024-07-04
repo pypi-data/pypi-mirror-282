2c861322f0affc064624005f367d47f3a7435e72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 06:04:43 +0000

    Add package clas-files123456/0.1/clas_files123456-0.1.tar.gz

---
+++ packages/clas-files123456/clas_files123456-0.1.tar.gz/clas_files123456-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='clas___files123456',
+      version='0.1',
+      description='Удобный класс для работы с файлами',
+      packages=['class_files'],
+      author_email='nikVCS@yandex.by',
+      zip_safe=False)
