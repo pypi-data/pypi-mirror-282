15882992fdebcc095a4f10d93bf6d0ba844a765a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 06:04:43 +0000

    Add package class-files-rt56/0.4/class_files_rt56-0.4.tar.gz

---
+++ packages/class-files-rt56/class_files_rt56-0.4.tar.gz/class_files_rt56-0.4/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='class_files_rt56',
+      version='0.4',
+      description='Удобный класс для работы с файлами.',
+      packages=['class_files'],
+      author_email='BusinessJson6@gmail.com',
+      zip_safe=False)
