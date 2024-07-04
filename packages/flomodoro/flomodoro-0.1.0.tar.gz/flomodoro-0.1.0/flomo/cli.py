bfa8e56f5690dbba616586f283732c01afb59ad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 03:05:27 +0000

    Add package flomodoro/0.1.0/flomodoro-0.1.0.tar.gz

---
+++ packages/flomodoro/flomodoro-0.1.0.tar.gz/flomodoro-0.1.0/flomo/cli.py
@@ -0,0 +1,26 @@
+import click
+import click_aliases
+
+import flomo.ui as ui
+
+
+@click.group(cls=click_aliases.ClickAliasedGroup)
+def flomo():
+    """
+    A Flowmodoro CLI for productivity enthusiasts.
+    """
+    pass
+
+
+@flomo.command(aliases=["s"])
+@click.option("-t", "--tag", default="Default", help="Session tag name.")
+@click.option("-n", "--name", default="Work", help="Session Name")
+def start(tag: str, name: str):
+    """
+    Start a Flowmodoro session.
+    """
+    ui.main(tag.lower(), name)
+
+
+if __name__ == "__main__":
+    flomo()
