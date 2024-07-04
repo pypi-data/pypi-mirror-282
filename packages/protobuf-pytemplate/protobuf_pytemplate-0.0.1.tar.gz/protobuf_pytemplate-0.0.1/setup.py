a5c8b7ee1079543b2760c7d7d89098ef43127967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 15:05:30 +0000

    Add package protobuf-pytemplate/0.0.1/protobuf_pytemplate-0.0.1.tar.gz

---
+++ packages/protobuf-pytemplate/protobuf_pytemplate-0.0.1.tar.gz/protobuf_pytemplate-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='protobuf_pytemplate',  # Update with your package name
+    version='0.0.1',  # Update with your package version
+    author='Mayur',  # Update with the author's name
+    author_email='mdjakate@gmail.com',  # Update with the author's email
+    url='https://github.com/mayurjakate/protobuf-contracts',  # Update with your repository URL
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
