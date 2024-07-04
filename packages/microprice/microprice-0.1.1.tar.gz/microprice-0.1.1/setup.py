a1bc235f9056a7566fd72af342bda121f2f0c798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 05:06:11 +0000

    Add package microprice/0.1.1/microprice-0.1.1.tar.gz

---
+++ packages/microprice/microprice-0.1.1.tar.gz/microprice-0.1.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="microprice",
+    version="0.1.1",
+    author="Alexandre Brilhante",
+    author_email="alexandre.brilhante@gmail.com",
+    description="A Jax implementation of Micro-Price: A High Frequency Estimator of Future Prices.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alexandrebrilhante/quantnet",
+    project_urls={
+        "Bug Tracker": "https://github.com/alexandrebrilhante/microprice/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.8",
+)
