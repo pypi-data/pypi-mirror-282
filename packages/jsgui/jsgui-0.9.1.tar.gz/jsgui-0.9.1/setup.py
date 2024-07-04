ea3b9e50ff476b1c3f16648fa952c2c08be2890a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 04:04:51 +0000

    Add package jsgui/0.9.1/jsgui-0.9.1.tar.gz

---
+++ packages/jsgui/jsgui-0.9.1.tar.gz/jsgui-0.9.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jsgui',
+    version='0.9.1',
+    packages=find_packages(),
+    description='jsgui provides gui to edit json files based on a schema file',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',  # for Markdown
+    author='TrueKenji',
+    url='https://github.com/truekenji/jsgui',
+    install_requires=[
+        'numpy',
+        'jsonschema',
+        'decomply',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    keywords=['GUI', 'JSON Schema', 'JSON', 'tkinter', 'decomply', 'Visualisation']
+)
