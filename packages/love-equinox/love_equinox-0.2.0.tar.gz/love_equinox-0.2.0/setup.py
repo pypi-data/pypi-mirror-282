185e62814ffbb312339c3c4106f9a98d89a75147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 14:05:04 +0000

    Add package love-equinox/0.2.0/love_equinox-0.2.0.tar.gz

---
+++ packages/love-equinox/love_equinox-0.2.0.tar.gz/love_equinox-0.2.0/setup.py
@@ -0,0 +1,20 @@
+import pathlib
+
+import setuptools
+
+setuptools.setup(
+    name="love_equinox",
+    version="0.2.0",
+    description="Module to calculate love equinox",
+    long_description=pathlib.Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/normthenord/love_equinox",
+    author="Normthenord",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+    ],
+    python_requires =">=3.10, <3.14",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+)
