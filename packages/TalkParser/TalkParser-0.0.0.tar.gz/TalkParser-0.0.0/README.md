ffeed4e52207194eee8f2e60b46b9b1c415bcc72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 03:05:36 +0000

    Add package TalkParser/0.0.0/TalkParser-0.0.0.tar.gz

---
+++ packages/TalkParser/TalkParser-0.0.0.tar.gz/TalkParser-0.0.0/README.md
@@ -0,0 +1,8 @@
+<p align="center">
+    <img  alt="TalkParser" src="https://github.com/Q07K/TalkParser/assets/103545286/127f0a6a-cccc-4e7c-ad6c-19cb1e7742c3">
+</p>
+
+# TalkParser: organize, and analyze text histories efficiently.
+
+![pypi](https://img.shields.io/badge/pypi-v0.0.0-blue.svg)
+<!-- ![Python 3.10](https://img.shields.io/badge/python-3.10-blue.svg) -->
---
+++ packages/TalkParser/TalkParser-0.0.0.tar.gz/TalkParser-0.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='TalkParser',
+    version='0.0.0',
+    description='TalkParser: A tool for organizing and analyzing extracted text histories.',
+    author='K_Gyu',
+    author_email='kgh0730@gmail.com',
+    url='https://github.com/Q07K/TalkParser',
+    # install_requires=['tqdm', 'pandas', 'scikit-learn',],
+    # packages=find_packages(exclude=[]),
+    keywords=['kakaotalk', 'talk', 'parsing', 'parser', 'pypi', 'talkparser'],
+    # python_requires='>=3.6',
+    # package_data={},
+    # zip_safe=False,
+    # classifiers=[
+    #     'Programming Language :: Python :: 3.6',
+    #     'Programming Language :: Python :: 3.7',
+    #     'Programming Language :: Python :: 3.8',
+    #     'Programming Language :: Python :: 3.9',
+    # ],
+)
