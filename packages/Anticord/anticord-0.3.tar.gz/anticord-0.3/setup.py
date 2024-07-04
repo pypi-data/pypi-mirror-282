b67d8bed839df3adbbe5ec05226cb9be3ef05947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 16:06:11 +0000

    Add package Anticord/0.3/anticord-0.3.tar.gz

---
+++ packages/Anticord/anticord-0.3.tar.gz/anticord-0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup,find_packages
+
+with open("readme.md","r") as f:
+    descriptionL = f.read()
+
+setup(
+        name="Anticord",
+        version="0.3",
+        packages=find_packages(),
+        long_description=descriptionL,
+        description="Discord api wrapper",
+        author="Spiderman from earth 69",
+        author_email="spidermanfromearth69@gmail.com",
+        install_requires=[
+            'websocket',
+            'requests',
+        ],
+        long_description_content_type = 'text/markdown'
+)
+
+# Upload the new version on pypl cuz i like to torture myself
+
