d500055831bd742b160b6a797746a1e9d0ae4429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 15:04:52 +0000

    Add package artemis-py/0.1.0/artemis_py-0.1.0.tar.gz

---
+++ packages/artemis-py/artemis_py-0.1.0.tar.gz/artemis_py-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="artemis_py",
+    version="0.1.0",
+    description="A package to interact with the Artemis API. Blame ChatGPT for errors.",
+    author="Matt Maximo",
+    author_email="matt@pioneerdigital.org",
+    url="https://github.com/MattMaximo/PyArtemis",  
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "requests",
+        "plotly"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
