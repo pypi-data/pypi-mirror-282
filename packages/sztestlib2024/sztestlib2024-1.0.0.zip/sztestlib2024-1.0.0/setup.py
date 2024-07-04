a435b4f6e6a60eb564dbaa73b8290d4db825a1d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 16:06:14 +0000

    Add package sztestlib2024/1.0.0/sztestlib2024-1.0.0.zip

---
+++ packages/sztestlib2024/sztestlib2024-1.0.0.zip/sztestlib2024-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+# def readme_file():
+#       with open("README.rst",encoding="utf-8") as rf:
+#             return rf.read()
+
+setup(name="sztestlib2024", version="1.0.0", description="this is a niubi lib",
+      packages=["sztestlib"], py_modules=["Tool"], author="Sz", author_email="wangshu10@chinaunicom.cn",
+      license="MIT")
