9b7f11605abaa8024eb451924f11c10496ea0a5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 05:05:06 +0000

    Add package jupyter-idle/0.1.1/jupyter_idle-0.1.1.tar.gz

---
+++ packages/jupyter-idle/jupyter_idle-0.1.1.tar.gz/jupyter_idle-0.1.1/setup.py
@@ -0,0 +1,45 @@
+import os
+from setuptools import setup, find_packages
+
+package_name_prefix = os.environ.get("PACKAGE_NAME_PREFIX", "")
+
+setup_args = dict(
+    name=f"{package_name_prefix}jupyter-idle",
+    version="0.1.1",
+    url="https://aws.amazon.com/sagemaker/",
+    author="Amazon",
+    description="Jupyter Server Extension for checking last activity time",
+    license="Amazon Software License",
+    packages=find_packages(),
+    python_requires=">=3.9",
+    install_requires=[
+        "jupyter_server >= 2.0.0",
+        "tornado",
+    ],
+    include_package_data=True,
+    data_files=[
+        (
+            "etc/jupyter/jupyter_server_config.d",
+            ["jupyter-config/jupyter_server_config.d/jupyter_idle.json"],
+        ),
+    ],
+    platforms="Linux, Mac OS X, Windows",
+    keywords=["Jupyter", "JupyterLab"],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Framework :: Jupyter",
+    ],
+    extras_require={
+        "dev": [
+            "pytest >= 6",
+            "pytest-cov",
+            "black",
+            "pytest_jupyter",
+        ]
+    },
+)
+
+if __name__ == "__main__":
+    setup(**setup_args)
