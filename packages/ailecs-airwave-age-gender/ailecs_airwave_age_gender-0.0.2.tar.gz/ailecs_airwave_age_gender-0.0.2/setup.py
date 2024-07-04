3285dd20c9e83847c2ed3d2c8bb7764aa71b1aa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 15:05:12 +0000

    Add package ailecs-airwave-age-gender/0.0.2/ailecs_airwave_age_gender-0.0.2.tar.gz

---
+++ packages/ailecs-airwave-age-gender/ailecs_airwave_age_gender-0.0.2.tar.gz/ailecs_airwave_age_gender-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ailecs',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://ailecs.org/',
+    author='AiLECS',
+    author_email='ailecs-lab-l@monash.edu',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
