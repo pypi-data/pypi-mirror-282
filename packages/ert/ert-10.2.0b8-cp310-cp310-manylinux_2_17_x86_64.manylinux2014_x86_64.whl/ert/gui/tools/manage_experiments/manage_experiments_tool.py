b053502594cde2ffad484c3973a38d26f80f66f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 03:05:26 +0000

    Add package ert/10.2.0b8/ert-10.2.0b8-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ert/ert-10.2.0b8-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/ert/gui/tools/manage_experiments/manage_experiments_tool.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Optional
+
+from qtpy.QtCore import Qt
+from qtpy.QtGui import QIcon
+
+from ert.gui.tools import Tool
+from ert.gui.tools.manage_experiments.ensemble_init_configuration import (
+    EnsembleInitializationConfigurationPanel,
+)
+
+if TYPE_CHECKING:
+    from ert.config import ErtConfig
+    from ert.gui.ertnotifier import ErtNotifier
+
+
+class ManageExperimentsTool(Tool):
+    def __init__(self, config: ErtConfig, notifier: ErtNotifier, ensemble_size: int):
+        super().__init__("Manage experiments", QIcon("img:build_wrench.svg"))
+        self.notifier = notifier
+        self.ert_config = config
+        self.ensemble_size = ensemble_size
+        self._ensemble_management_widget: Optional[
+            EnsembleInitializationConfigurationPanel
+        ] = None
+
+    def trigger(self) -> None:
+        if not self._ensemble_management_widget:
+            self._ensemble_management_widget = EnsembleInitializationConfigurationPanel(
+                self.ert_config, self.notifier, self.ensemble_size
+            )
+            self._ensemble_management_widget.setWindowModality(
+                Qt.WindowModality.ApplicationModal
+            )
+
+        self._ensemble_management_widget.show()
