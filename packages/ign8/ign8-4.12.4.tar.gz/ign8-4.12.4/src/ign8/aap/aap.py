d52d8de074672e36fce4fef7c8b482b5214cf14e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 15:05:02 +0000

    Add package ign8/4.12.4/ign8-4.12.4.tar.gz

---
+++ packages/ign8/ign8-4.12.4.tar.gz/ign8-4.12.4/src/ign8/aap/aap.py
@@ -0,0 +1,32 @@
+import requests
+import os
+import urllib3
+import curses
+
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+def default_secrets():
+    return {
+        "client_id": os.getenv("AAP_CLIENT_ID"),
+        "client_secret": os.getenv("AAP_CLIENT_SECRET"),
+        "username": os.getenv("AAP_USERNAME"),
+        "password": os.getenv("AAP_PASSWORD"),
+    }
+
+
+
+def status():
+    #We need to create an ascii menu that shows the status of the AAP
+    #We need to show the status of the AAP
+
+    print("AAP Status")
+    print("==========")
+    print("Checking AAP Status")
+    print("Please wait...")
+    print(default_secrets())
+    print("....")
+
+
+    
