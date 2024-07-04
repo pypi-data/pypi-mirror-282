cef867b0be0f4488b6b913150adef1cfad9ddaed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 16:05:32 +0000

    Add package BLTrade/0.26/bltrade-0.26.tar.gz

---
+++ packages/BLTrade/bltrade-0.26.tar.gz/bltrade-0.26/BLTrade/BLTradeVersion.py
@@ -0,0 +1,25 @@
+# BLTrade Version
+import subprocess
+
+# Git版本提交次数
+def GetGitPushCount():
+    
+    gitpushcount=subprocess.getoutput('git rev-list --count HEAD')
+    return gitpushcount
+
+# 获取版本号
+def GetVersion():
+    try:
+        BLTradeVersion=GetGitPushCount()
+        #BLTradeVersion=subprocess.getoutput('gitx')
+        return f"BLTrade Engine Version: {int(BLTradeVersion)/1000}"
+    except ValueError:
+        print("Excepted: git命令执行失败!!!")
+        BLTradeVersion=269
+        return f"BLTrade Engine Version: {int(BLTradeVersion)/1000}"
+
+
+        
+    
+
+#print(GetVersion())
---
+++ packages/BLTrade/bltrade-0.26.tar.gz/bltrade-0.26/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import BLTrade.BLTradeVersion as bv
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+#long_description = "BLTrade"
+long_description = bv.GetVersion()
+print(long_description)
+#with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+#    long_description = f.read()
+ 
+setup(name='BLTrade', # 包名称
+      #packages=['BLTrade'], # 需要处理的包目录
+      version='0.26', # 版本
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python', 'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.12',
+      ],
+      install_requires=['backtrader'],
+      #entry_points={'console_scripts': ['pmm=pimm.pimm_module:main']},
+      package_data={'': ['*.txt','*.csv']},
+      author='Jiawenze', # 作者
+      author_email='s.gua@163.com', # 作者邮箱
+      description='BLTrade', # 介绍
+      long_description=long_description, # 长介绍，在pypi项目页显示
+      long_description_content_type='text/markdown', # 长介绍使用的类型，我使用的是md
+      url='', # 包主页，一般是github项目主页
+      license='MIT', # 协议
+      keywords='BLTrade',
+      packages=["BLTrade","BLStrategy","test","BLBack","BLNetwork","datas"],
+      ) # 关键字 搜索用
