871021f56b1f8deb20051c0f54c649c7d6d80827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 04:05:24 +0000

    Add package fst-lookup/2024.7.3/fst_lookup-2024.7.3.tar.gz

---
+++ packages/fst-lookup/fst_lookup-2024.7.3.tar.gz/fst_lookup-2024.7.3/fst_lookup/__init__.py
@@ -0,0 +1,8 @@
+"""
+Parse and evaluate FOMA FSTs.
+"""
+
+from .__version__ import __version__
+from .fst import FST
+
+__all__ = ["FST"]
---
+++ packages/fst-lookup/fst_lookup-2024.7.3.tar.gz/fst_lookup-2024.7.3/fst_lookup/typedefs.py
@@ -0,0 +1,3 @@
+from typing import NewType
+
+StateID = NewType("StateID", int)
