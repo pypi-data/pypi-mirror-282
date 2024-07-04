93b77e5796f3cacee714e91226ac1d17e2f7282e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 05:06:24 +0000

    Add package expert-ceylon/2.0.0a2/expert_ceylon-2.0.0a2-py3-none-any.whl

---
+++ packages/expert-ceylon/expert_ceylon-2.0.0a2-py3-none-any.whl/expert_ceylon/matchers/rete/mixins.py
@@ -0,0 +1,45 @@
+"""Mixing classes for the RETE nodes."""
+from collections import namedtuple
+
+try:
+    from collections.abc import Callable  # noqa
+except ImportError:
+    from collections import Callable  # noqa
+
+
+#: Used to store node/callback pair in nodes children set.
+ChildNode = namedtuple('ChildNode', ['node', 'callback'])
+
+
+class NoMemory:
+    """The node has no memory so we have nothing to do."""
+
+    def _reset(self):
+        pass
+
+
+class AnyChild:
+    """This node allow any kind of node as a child."""
+
+    def add_child(self, node, callback):
+        """Add node and callback to the children set."""
+        if node not in self.children:
+            self.children.append(ChildNode(node, callback))
+
+
+class HasMatcher:
+    """This node need a match callable as parameter."""
+
+    def __init__(self, matcher):
+        """If `matcher` is a callable, assign to `self.matcher`."""
+        try:
+            assert isinstance(matcher, Callable)
+        except AssertionError as exc:
+            raise TypeError(exc) from exc
+        else:
+            self.matcher = matcher
+
+        super().__init__()
+
+    def __str__(self):  # pragma: no cover
+        return "%s: %s" % (self.__class__.__name__, self.matcher)
