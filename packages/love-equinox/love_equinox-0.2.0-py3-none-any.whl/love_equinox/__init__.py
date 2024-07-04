984b60d036bbbb3e4e8af50ebf8d812bd2b5e96c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 14:05:04 +0000

    Add package love-equinox/0.2.0/love_equinox-0.2.0-py3-none-any.whl

---
+++ packages/love-equinox/love_equinox-0.2.0-py3-none-any.whl/love_equinox/__init__.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+
+def calculate_le(first_date: datetime, birthday: datetime):
+    '''Takes two datetime objects and returns the love equinox associated with them.'''
+    if first_date < birthday:
+        return None
+
+    diff = birthday - first_date
+    equinox = first_date - diff
+
+    return equinox
+
+def print_le(first_date: datetime, birthday: datetime):
+    '''Takes two datetime objects and prints the love equinox associated with them.'''
+    if first_date < birthday:
+        return None
+
+    diff = birthday - first_date
+    equinox = first_date - diff
+
+    print(f"Your love equinox is {equinox.strftime("%m/%d/%Y")}")
