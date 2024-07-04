472ec23fc7f429c5b0a4450364c69d1a4fc2e426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 15:05:27 +0000

    Add package atom/0.10.5/atom-0.10.5-cp38-cp38-macosx_10_9_universal2.whl

---
+++ packages/atom/atom-0.10.5-cp38-cp38-macosx_10_9_universal2.whl/atom/datastructures/api.py
@@ -0,0 +1,10 @@
+# --------------------------------------------------------------------------------------
+# Copyright (c) 2013-2023, Nucleic Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# --------------------------------------------------------------------------------------
+from .sortedmap import sortedmap
+
+__all__ = ["sortedmap"]
