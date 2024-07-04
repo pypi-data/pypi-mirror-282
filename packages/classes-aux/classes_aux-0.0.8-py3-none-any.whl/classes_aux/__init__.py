4fbe4de6ff2ff9305ddd68f601b1feccc6343161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 04:05:15 +0000

    Add package classes-aux/0.0.8/classes_aux-0.0.8-py3-none-any.whl

---
+++ packages/classes-aux/classes_aux-0.0.8-py3-none-any.whl/classes_aux/__init__.py
@@ -0,0 +1,46 @@
+# =====================================================================================================================
+# VERSION = (0, 0, 1)   # use import EXACT_OBJECTS! not *
+#   from .main import *                 # INcorrect
+#   from .main import EXACT_OBJECTS     # CORRECT
+
+
+# =====================================================================================================================
+# TEMPLATE
+# from .main import (
+#     # BASE
+#     EXACT_OBJECTS,
+#
+#     # AUX
+#
+#     # TYPES
+#
+#     # EXX
+# )
+# ---------------------------------------------------------------------------------------------------------------------
+from .cmp import (
+    # BASE
+    CmpInst,
+    # AUX
+    # TYPES
+    # EXX
+)
+from .getattr import (
+    # BASE
+    GetattrPrefixInst,
+    GetattrPrefixInst_RaiseIf,
+    # AUX
+    GetattrCls_Meta__Template,
+    # TYPES
+    # EXX
+    Exx__GetattrPrefix,
+    Exx__GetattrPrefix_RaiseIf,
+)
+from .middle_group import (
+    # BASE
+    ClsMiddleGroup,
+    # AUX
+    # TYPES
+    # EXX
+)
+
+# =====================================================================================================================
---
+++ packages/classes-aux/classes_aux-0.0.8-py3-none-any.whl/classes_aux/middle_group.py
@@ -0,0 +1,51 @@
+from typing import *
+from object_info import *
+
+
+# =====================================================================================================================
+class ClsMiddleGroup:
+    """
+    if you need to separate instances into several groups.
+    main ide is manipulating with grouped methods? not just to keep name in attribute.
+
+    USAGE
+    -----
+    !. dont use this class as final! only middle!
+    1. create some middle classes which will define groups.
+    2. do nesting your final classes by Groups
+
+    CREATED SPECIALLY FOR
+    ---------------------
+    module testplans.
+    need to handle testcase classes as some groups!
+    there was not enough separating process just by startup_cls and startup_inst!!!
+    """
+
+    MIDDLE_GROUP_NAME: str | None = None  # this is not for comparing! just for short clear name instead of not useful ClsName
+
+    @staticmethod
+    def middle_group_name__check_exists(other: Any) -> bool:
+        return hasattr(other, "middle_group_name__get")
+
+    @classmethod
+    def middle_group_name__get(cls) -> str:
+        for cls_i in cls.__mro__[1:]:
+            if hasattr(cls_i, "middle_group_name__get"):
+                return cls_i.__name__
+
+    @classmethod
+    def middle_group_name__check_equal(cls, other: Any) -> bool | None:
+        if not ClsMiddleGroup.middle_group_name__check_exists(other):
+            return
+
+        return cls.middle_group_name__get() == other.middle_group_name__get()
+
+    # -----------------------------------------------------------------------------------------------------------------
+    # EXAMPLE for TCS
+    # def startup__group(self):
+    #     pass
+    # def teardown__group(self):
+    #     pass
+
+
+# =====================================================================================================================
