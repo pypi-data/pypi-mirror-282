390d264f1855a8d1bebdb15fcd21fbbaf0c1486e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 16:05:37 +0000

    Add package guanwai-tools/1.0.1/guanwai-tools-1.0.1.tar.gz

---
+++ packages/guanwai-tools/guanwai-tools-1.0.1.tar.gz/guanwai-tools-1.0.1/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='guanwai-tools',
+    version='1.0.1',
+    packages=find_packages(),
+    description='A simple example package',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    # url='http://github.com/yourusername/my_package',
+    author='gaunwai',
+    author_email='171391909@qq.com',
+    license='MIT',
+    install_requires=[
+        "pymongo",
+    ],
+    classifiers=[
+    ]
+)
