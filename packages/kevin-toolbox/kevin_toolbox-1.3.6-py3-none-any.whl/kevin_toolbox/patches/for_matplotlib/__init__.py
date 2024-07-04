7203fadf2862315431041215a6b6e885510b8785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 03:06:17 +0000

    Add package kevin-toolbox/1.3.6/kevin_toolbox-1.3.6-py3-none-any.whl

---
+++ packages/kevin-toolbox/kevin_toolbox-1.3.6-py3-none-any.whl/kevin_toolbox/patches/for_matplotlib/__init__.py
@@ -0,0 +1,4 @@
+from .arrow3d import Arrow3D
+from .add_trajectory_2d import add_trajectory_2d
+from .add_trajectory_3d import add_trajectory_3d
+from .generate_color_list import generate_color_list
