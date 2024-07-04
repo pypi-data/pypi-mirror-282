e022da70fd12f9ccb5c3f979d7bcf17f1bfb4424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 05:05:20 +0000

    Add package bookio-fetchfox/2.8.1/bookio_fetchfox-2.8.1-py3-none-any.whl

---
+++ packages/bookio-fetchfox/bookio_fetchfox-2.8.1-py3-none-any.whl/fetchfox/constants/currencies.py
@@ -0,0 +1,21 @@
+ALGO = "ALGO"
+ADA = "ADA"
+BEARD = "BEARD"
+BOOK = "BOOK"
+ETH = "ETH"
+MATIC = "MATIC"
+
+USD = "USD"
+
+CRYPTOS = [
+    ALGO,
+    ADA,
+    BEARD,
+    BOOK,
+    ETH,
+    MATIC,
+]
+
+FIATS = [
+    USD,
+]
