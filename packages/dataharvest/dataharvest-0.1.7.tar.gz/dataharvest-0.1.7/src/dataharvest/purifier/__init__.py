8a418e2840806136c407695d01965c9a98324e1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 16:05:53 +0000

    Add package dataharvest/0.1.7/dataharvest-0.1.7.tar.gz

---
+++ packages/dataharvest/dataharvest-0.1.7.tar.gz/dataharvest-0.1.7/src/dataharvest/purifier/__init__.py
@@ -0,0 +1,21 @@
+from dataharvest.purifier.baidu_purifier import BaiduBaikePurifier
+from dataharvest.purifier.common_purifier import CommonPurifier
+from dataharvest.purifier.sohu_purifier import SohuPurifier
+from dataharvest.purifier.sll_purifier import SllPurifier
+from dataharvest.purifier.baidu_purifier import BaiJiaHaoPurifier
+from dataharvest.purifier.wangyi_purifier import WangYiPurifier
+from dataharvest.purifier.sogou_purifier import SogouBaiKePurifier
+from dataharvest.purifier.zhihu_purifier import ZhihuPurifier
+from dataharvest.purifier.bilibili_purifier import BilibiliPurifier
+from dataharvest.purifier.qqnew_purifier import QQNewPurifier
+from dataharvest.purifier.sobaike_purifier import SoBaikePurifier
+from dataharvest.purifier.toutiao_purifier import ToutiaoPurifier
+from dataharvest.purifier.wechat_purifier import WechatPurifier
+from dataharvest.purifier.purifier import AutoPurifier, BasePurifier
+
+__all__ = ["BasePurifier", "CommonPurifier", "AutoPurifier", "BaiduBaikePurifier",
+           "SohuPurifier", "SllPurifier", "BaiJiaHaoPurifier", "WangYiPurifier",
+           "SogouBaiKePurifier", "ZhihuPurifier", "BilibiliPurifier", "QQNewPurifier",
+           "SoBaikePurifier", "ToutiaoPurifier", "WechatPurifier"]
+
+
