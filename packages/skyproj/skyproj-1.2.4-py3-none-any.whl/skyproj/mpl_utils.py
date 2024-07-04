8290a6a8aa259bcfd3cd2be186f9b13ac1566885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 06:04:47 +0000

    Add package skyproj/1.2.4/skyproj-1.2.4-py3-none-any.whl

---
+++ packages/skyproj/skyproj-1.2.4-py3-none-any.whl/skyproj/mpl_utils.py
@@ -0,0 +1,227 @@
+import numpy as np
+
+from mpl_toolkits.axisartist.grid_finder import ExtremeFinderSimple
+import mpl_toolkits.axisartist.angle_helper as angle_helper
+from .vendor.mpl_toolkit_functions import GridHelperCurveLinear
+
+from .utils import wrap_values
+
+
+__all__ = ['WrappedFormatterDMS', 'ExtremeFinderWrapped', 'GridHelperSkyproj']
+
+
+class WrappedFormatterDMS(angle_helper.FormatterDMS):
+    """A tick formatter that handles longitude wrapping.
+
+    Parameters
+    ----------
+    wrap : `float`
+        Angle (degrees) which should be wrapped (subtract 360).
+    longitude_ticks : `str`
+        Type of longitude ticks, either ``positive`` (0 to 360)
+        or ``symmetric`` (-180 to 180).
+    """
+    def __init__(self, wrap, longitude_ticks):
+        self._wrap = wrap
+        if longitude_ticks == 'positive':
+            self._longitude_ticks = 1
+        elif longitude_ticks == 'symmetric':
+            self._longitude_ticks = -1
+        else:
+            raise ValueError("longitude_ticks must be `positive` or `symmetric`.")
+        super().__init__()
+
+    def _wrap_values(self, factor, values):
+        """Wrap the values according to the wrap angle.
+
+        Parameters
+        ----------
+        factor : `float`
+            Scaling factor for input values
+        values : `list`
+            List of values to format
+
+        Returns
+        -------
+        wrapped_values : `np.ndarray`
+            Array of wrapped values, scaled by factor.
+        """
+        _values = np.atleast_1d(values)/factor
+        _values = wrap_values(_values, wrap=self._wrap)
+        if self._longitude_ticks == 1:
+            # Values should all be positive, 0 to 360
+            _values %= 360.0
+        else:
+            # Additional test to set -180 to positive 180
+            _values[np.isclose(_values, -180.0)] = 180.0
+        return factor*_values
+
+    def __call__(self, direction, factor, values):
+        return super().__call__(direction, factor, self._wrap_values(factor, values))
+
+
+class ExtremeFinderWrapped(ExtremeFinderSimple):
+    """
+    Find extremes with configurable wrap angle and correct limits.
+
+    Parameters
+    ----------
+    nx : `int`
+        Number of samples in x direction.
+    ny : `int`
+        Number of samples in y direction.
+    wrap_angle : `float`
+        Angle at which the 360-degree cycle should be wrapped.
+    """
+    def __init__(self, nx, ny, wrap_angle):
+        self.nx, self.ny = nx, ny
+        self._wrap = wrap_angle
+        self._eps = 1e-5
+
+    def __call__(self, transform_xy, x1, y1, x2, y2):
+        # docstring inherited
+        x, y = np.meshgrid(
+            np.linspace(x1, x2, self.nx), np.linspace(y1, y2, self.ny))
+        lon, lat = transform_xy(np.ravel(x), np.ravel(y))
+
+        with np.errstate(invalid='ignore'):
+            lon = wrap_values(lon, wrap=self._wrap)
+
+        lon_min, lon_max = np.nanmin(lon), np.nanmax(lon)
+        lat_min, lat_max = np.nanmin(lat), np.nanmax(lat)
+
+        lon_min, lon_max, lat_min, lat_max = \
+            self._add_pad(lon_min, lon_max, lat_min, lat_max)
+
+        lat_min = np.clip(lat_min, -90.0 + self._eps, 90.0 - self._eps)
+        lat_max = np.clip(lat_max, -90.0 + self._eps, 90.0 - self._eps)
+
+        lon_min = np.clip(lon_min, self._wrap - 360. + self._eps, self._wrap - self._eps)
+        lon_max = np.clip(lon_max, self._wrap - 360. + self._eps, self._wrap - self._eps)
+
+        return lon_min, lon_max, lat_min, lat_max
+
+
+class GridHelperSkyproj(GridHelperCurveLinear):
+    """GridHelperCurveLinear with tick overlap protection.
+
+    Parameters
+    ----------
+    *args : `list`
+        Arguments for ``GridHelperCurveLinear``.
+    celestial : `bool`, optional
+        Plot is celestial, and angles should be 0 to 360.  Otherwise -180 to 180.
+    equatorial_labels : `bool`, optional
+        Longitude labels are marked on the equator instead of edges.
+    delta_cut : `float`, optional
+        Gridline step (degrees) to signify a jump around a wrapped edge.
+    min_lon_ticklabel_delta : `float`, optional
+        Minimum relative spacing between longitude tick labels (relative to width
+        of axis). Smaller values yield closer tick labels (and potential for clashes)
+        and larger values yield more spacing between tick labels.
+    **kwargs : `dict`, optional
+        Additional kwargs for ``GridHelperCurveLinear``.
+    """
+    def __init__(self, *args, celestial=True, equatorial_labels=False, delta_cut=80.0,
+                 min_lon_ticklabel_delta=0.1, **kwargs):
+        self._celestial = celestial
+        self._equatorial_labels = equatorial_labels
+        self._delta_cut = delta_cut
+        self._min_lon_ticklabel_delta = min_lon_ticklabel_delta
+
+        super().__init__(*args, **kwargs)
+
+    def get_gridlines(self, which="major", axis="both"):
+        # docstring inherited
+        try:
+            _grid_info = self._grid_info
+        except AttributeError:
+            _grid_info = self.grid_info
+
+        grid_lines = []
+        if axis in ["both", "x"]:
+            for gl in _grid_info["lon"]["lines"]:
+                grid_lines.extend(self._cut_grid_line_jumps(gl))
+        if axis in ["both", "y"]:
+            for gl in _grid_info["lat"]["lines"]:
+                grid_lines.extend(self._cut_grid_line_jumps(gl))
+        return grid_lines
+
+    def _cut_grid_line_jumps(self, gl):
+        """Check for jumps and cut gridlines into multiple sections.
+
+        Parameters
+        ----------
+        gl : `list` [`tuple`]
+            Input gridlines.  List of tuples of numpy arrays.
+
+        Returns
+        -------
+        gl_new : `list` [`tuple`]
+            New gridlines.  Jumps have been replaced with `np.nan`
+            values to ensure lines are not connected around edges.
+        """
+        dx = gl[0][0][1:] - gl[0][0][: -1]
+        dy = gl[0][1][1:] - gl[0][1][: -1]
+
+        split, = (np.hypot(dx, dy) > self._delta_cut).nonzero()
+
+        if split.size == 0:
+            return gl
+
+        gl_new = [(np.insert(gl[0][0], split + 1, np.nan),
+                   np.insert(gl[0][1], split + 1, np.nan))]
+
+        return gl_new
+
+    def get_tick_iterator(self, nth_coord, axis_side, minor=False):
+        # docstring inherited
+        try:
+            _grid_info = self._grid_info
+        except AttributeError:
+            _grid_info = self.grid_info
+
+        angle_tangent = dict(left=90, right=90, bottom=0, top=0)[axis_side]
+        lon_or_lat = ["lon", "lat"][nth_coord]
+        if lon_or_lat == "lon":
+            # Need to compute maximum extent in the x direction
+            x_min = 1e100
+            x_max = -1e100
+            for line in _grid_info['lon_lines']:
+                x_min = min((x_min, np.min(line[0])))
+                x_max = max((x_max, np.max(line[0])))
+            delta_x = x_max - x_min
+        if not minor:  # major ticks
+            tick_locs = _grid_info[lon_or_lat]["tick_locs"][axis_side]
+            tick_labels = _grid_info[lon_or_lat]["tick_labels"][axis_side]
+
+            if lon_or_lat == "lon" and self._equatorial_labels:
+                min_lat = np.min(_grid_info["lat"]["levels"])
+                max_lat = np.max(_grid_info["lat"]["levels"])
+                if (min_lat < -89.0 and axis_side == "bottom") \
+                   or (max_lat > 89.0 and axis_side == "top"):
+                    tick_locs = []
+                    tick_labels = []
+
+            prev_xy = None
+            for ctr, ((xy, a), l) in enumerate(zip(tick_locs, tick_labels)):
+                if self._celestial:
+                    angle_normal = 360.0 - a
+                else:
+                    angle_normal = a
+
+                if ctr > 0 and lon_or_lat == 'lon':
+                    # Check if this is too close to the last label.
+                    if abs(xy[0] - prev_xy[0])/delta_x < self._min_lon_ticklabel_delta:
+                        continue
+                prev_xy = xy
+                yield xy, angle_normal, angle_tangent, l
+        else:
+            for (xy, a), l in zip(
+                    _grid_info[lon_or_lat]["tick_locs"][axis_side],
+                    _grid_info[lon_or_lat]["tick_labels"][axis_side]):
+                if self._celestial:
+                    angle_normal = 360.0 - a
+                else:
+                    angle_normal = a
+                yield xy, angle_normal, angle_tangent, ""
---
+++ packages/skyproj/skyproj-1.2.4-py3-none-any.whl/skyproj/skyaxes.py
@@ -0,0 +1,345 @@
+import functools
+import numpy as np
+import warnings
+
+import matplotlib.axes
+from pyproj import Geod
+
+from .skycrs import PlateCarreeCRS
+from .utils import wrap_values
+
+__all__ = ["SkyAxes"]
+
+
+def _add_lonlat(func):
+    """Decorator to add lonlat option."""
+    @functools.wraps(func)
+    def wrapper(self, *args, **kwargs):
+        if kwargs.pop('lonlat', True):
+            kwargs['transform'] = self.projection
+        return func(self, *args, **kwargs)
+    return wrapper
+
+
+class SkyAxes(matplotlib.axes.Axes):
+    # docstring inherited
+    def __init__(self, *args, **kwargs):
+        self.projection = kwargs.pop("sky_crs")
+
+        self.plate_carree = PlateCarreeCRS()
+
+        self._artist = None
+
+        super().__init__(*args, **kwargs)
+
+    def clear(self):
+        """Clear the current axes."""
+        result = super().clear()
+        self.xaxis.set_visible(False)
+        self.yaxis.set_visible(False)
+
+        self.set_frame_on(False)
+
+        # Always equal aspect ratio.
+        self.set_aspect('equal')
+
+        return result
+
+    def set_extent(self, extent, lonlat=True):
+        """Set the extent of the axes.
+
+        Parameters
+        ----------
+        extent : `tuple` [`float`]
+            Set the extent by [lon0, lon1, lat0, lat1] or [x0, x1, y0, y1].
+        lonlat : `bool`, optional
+            Extent is specified in lon/lat coordinates?  Otherwise native.
+        """
+        if not lonlat:
+            x0, x1, y0, y1 = extent
+        else:
+            # Need to do transformation.
+            lon0, lon1, lat0, lat1 = extent
+
+            # Check if the longitude range is the full sphere, and special case that.
+            if np.isclose(np.abs(lon1 - lon0), 360.0):
+                lat_steps = [lat0, lat1]
+                if lat0 < 0.0 and lat1 > 0.0:
+                    # Make sure we have the equator included
+                    lat_steps.append(0.0)
+                lon, lat = np.meshgrid(np.linspace(0, 360.0, 360), lat_steps)
+                xy = self.projection.transform_points(self.plate_carree, lon.ravel(), lat.ravel())
+                # Need to offset this by some small amount to ensure we don't get
+                # out-of-bounds transformations.
+                eps = 1e-5
+                x0 = (1 - eps)*np.min(xy[:, 0])
+                x1 = (1 - eps)*np.max(xy[:, 0])
+                y0 = (1 - eps)*np.min(xy[:, 1])
+                y1 = (1 - eps)*np.max(xy[:, 1])
+            else:
+                # Make a ring of points and check their extent.
+                npt = 100
+                lon_pts = np.linspace(lon0, lon1, npt)
+                lat_pts = np.linspace(lat0, lat1, npt)
+                lon = np.concatenate((lon_pts, lon_pts, np.repeat(lon0, npt), np.repeat(lon1, npt)))
+                lat = np.concatenate((np.repeat(lat0, npt), np.repeat(lat1, npt), lat_pts, lat_pts))
+                xy = self.projection.transform_points(self.plate_carree, lon, lat)
+                # FIXME NOTE NEED TO KNOW LON_0/WRAP OF PROJECTION...
+                x0 = np.min(xy[:, 0])
+                x1 = np.max(xy[:, 0])
+                y0 = np.min(xy[:, 1])
+                y1 = np.max(xy[:, 1])
+
+        self.set_xlim([x0, x1])
+        self.set_ylim([y0, y1])
+
+    def get_extent(self, lonlat=True):
+        """Get the extent of the axes.
+
+        Parameters
+        ----------
+        lonlat : `bool`, optional
+            Return extent in lon/lat coordinates?  Otherwise native.
+        """
+        x0, x1 = self.get_xlim()
+        y0, y1 = self.get_ylim()
+
+        if not lonlat:
+            extent = (x0, x1, y0, y1)
+        else:
+            # Make a ring of points and check their extent.
+            npt = 500
+            x_pts = np.linspace(x0, x1, npt)
+            y_pts = np.linspace(y0, y1, npt)
+            x = np.concatenate((x_pts, x_pts, np.repeat(x0, npt), np.repeat(x1, npt)))
+            y = np.concatenate((np.repeat(y0, npt), np.repeat(y1, npt), y_pts, y_pts))
+            lonlat = self.plate_carree.transform_points(self.projection, x, y)
+
+            # Check for out-of-bounds by reverse-projecting
+            xy = self.projection.transform_points(self.plate_carree, lonlat[:, 0], lonlat[:, 1])
+            bad = ((~np.isclose(xy[:, 0], x)) | (~np.isclose(xy[:, 1], y)))
+            lonlat[bad, :] = np.nan
+
+            # We need to wrap values to get the correct range
+            wrap = (self.projection.lon_0 + 180.) % 360.
+            with warnings.catch_warnings():
+                # Some longitude values may be nan, so we filter these expected warnings.
+                warnings.simplefilter("ignore")
+                lon_wrap = wrap_values(lonlat[:, 0], wrap)
+
+            if np.all(np.isnan(lon_wrap)):
+                lon0 = -180.0 + 1e-5
+                lon1 = 180.0 - 1e-5
+            else:
+                lon0 = np.nanmin(lon_wrap)
+                lon1 = np.nanmax(lon_wrap)
+
+            if np.all(np.isnan(lonlat[:, 1])):
+                lat0 = -90.0 + 1e-5
+                lat1 = 90.0 - 1e-5
+            else:
+                lat0 = np.nanmin(lonlat[:, 1])
+                lat1 = np.nanmax(lonlat[:, 1])
+
+            if self.xaxis_inverted():
+                extent = (lon1, lon0, lat0, lat1)
+            else:
+                extent = (lon0, lon1, lat0, lat1)
+
+        return extent
+
+    @_add_lonlat
+    def plot(self, *args, **kwargs):
+        # docstring inherited
+
+        # The transformation code will automatically plot geodesics
+        # and split line segements that cross the wrapping boundary.
+        result = super().plot(*args, **kwargs)
+
+        return result
+
+    @_add_lonlat
+    def scatter(self, *args, **kwargs):
+        # docstring inherited
+        result = super().scatter(*args, **kwargs)
+
+        return result
+
+    @_add_lonlat
+    def pcolormesh(self, X, Y, C, **kwargs):
+        # docstring inherited
+        C_temp = C.copy()
+
+        if kwargs.get('lonlat', True):
+            # Check for wrapping by projecting and looking for jumps.
+            proj_xy = self.projection.transform_points(self.plate_carree, X, Y)
+            X_proj = proj_xy[..., 0]
+            Y_proj = proj_xy[..., 1]
+
+            dist = np.hypot(X_proj[1:, 1:] - X_proj[0: -1, 0: -1],
+                            Y_proj[1:, 1:] - Y_proj[0: -1, 0: -1])
+
+            # If we have a jump of 10% of the radius, assume it's bad,
+            # except if we are using PlateCarree which doesn't use the radius.
+            if self.projection == self.plate_carree:
+                max_dist = 90.0
+            else:
+                max_dist = 0.1*self.projection.radius
+
+            split = (dist > max_dist).nonzero()
+
+            # By marking these jumps as masked then pcolormesh works just fine.
+            C_temp.mask[split] = True
+
+        result = super().pcolormesh(X, Y, C_temp, **kwargs)
+
+        return result
+
+    @_add_lonlat
+    def fill(self, *args, **kwargs):
+        # docstring inherited
+        result = super().fill(*args, **kwargs)
+
+        return result
+
+    @_add_lonlat
+    def text(self, *args, **kwargs):
+        # docstring inherited
+        result = super().text(*args, **kwargs)
+
+        return result
+
+    @_add_lonlat
+    def circle(self, lon, lat, radius, nsamp=100, fill=False, **kwargs):
+        """Draw a geodesic circle centered at given position.
+
+        Parameters
+        ----------
+        lon : `float`
+            Longitude of center of circle (degrees).
+        lat : `float`
+            Latitude of center of circle (degrees).
+        radius : `float`
+            Radius of circle (degrees).
+        nsamp : `int`, optional
+            Number of points to sample.
+        fill : `bool`, optional
+            Draw filled circle?
+        **kwargs : `dict`
+            Extra plotting kwargs.
+        """
+        geod = Geod(a=self.projection.radius)
+
+        # We need the radius in meters
+        radius_m = self.projection.radius*np.deg2rad(radius)
+
+        az = np.linspace(360.0, 0.0, nsamp)
+        lons, lats, _ = geod.fwd(
+            np.full(nsamp, lon, dtype=np.float64),
+            np.full(nsamp, lat, dtype=np.float64),
+            az,
+            np.full(nsamp, radius_m)
+        )
+        if fill:
+            return self.fill(lons, lats, **kwargs)
+        else:
+            return self.plot(lons, lats, **kwargs)
+
+    @_add_lonlat
+    def ellipse(self, lon, lat, a, b, theta, nsamp=100, fill=False, **kwargs):
+        """Draw a geodesic ellipse centered at given position.
+
+        Parameters
+        ----------
+        lon : `float`
+            Longitude of center of ellipse (degrees).
+        lat : `float`
+            Latitude of center of ellipse (degrees).
+        a : `float`
+            Semi-major axis of ellipse (degrees).
+        b : `float`
+            Semi-minor axis of ellipse (degrees).
+        theta : `float`
+            Position angle of ellipse.  Degrees East of North.
+        nsamp : `int`, optional
+            Number of points to sample.
+        fill : `bool`, optional
+            Draw filled ellipse?
+        **kwargs : `dict`
+            Extra plotting kwargs.
+        """
+        geod = Geod(a=self.projection.radius)
+
+        # We need the radius in meters
+        a_m = self.projection.radius * np.deg2rad(a)
+        b_m = self.projection.radius * np.deg2rad(b)
+
+        az = np.linspace(360.0, 0.0, nsamp)
+
+        phase_rad = np.deg2rad(az - theta)
+
+        # Position Angle is defined as degrees East from North
+        denom = np.sqrt((b_m * np.cos(phase_rad))**2 + (a_m * np.sin(phase_rad))**2)
+        dist = a_m * b_m / denom
+
+        lons, lats, _ = geod.fwd(
+            np.full(nsamp, lon, dtype=np.float64),
+            np.full(nsamp, lat, dtype=np.float64),
+            az,
+            dist,
+        )
+        if fill:
+            return self.fill(lons, lats, **kwargs)
+        else:
+            return self.plot(lons, lats, **kwargs)
+
+    @property
+    def lon_0(self):
+        return self.projection.lon_0
+
+    @property
+    def lat_0(self):
+        return self.projection.lat_0
+
+    def set_xlabel(self, text, side='bottom', **kwargs):
+        """Set the label on the x axis.
+
+        Parameters
+        ----------
+        text : `str`
+            x label string.
+        side : `str`, optional
+            Side to set the label.  Can be ``bottom`` or ``top``.
+        **kwargs : `dict`
+            Additional keyword arguments accepted by ax.set_xlabel().
+        """
+        if self._artist is None:
+            raise RuntimeError("set_xlabel run without initialization of map.")
+
+        return self._artist.axis[side].label.set(text=text, **kwargs)
+
+    def set_ylabel(self, text, side='left', **kwargs):
+        """Set the label on the y axis.
+
+        Parameters
+        ----------
+        text : `str`
+            x label string.
+        side : `str`, optional
+            Side to set the label.  Can be ``left`` or ``right``.
+        **kwargs : `dict`
+            Additional keyword arguments accepted by ax.set_xlabel().
+        """
+        if self._artist is None:
+            raise RuntimeError("set_xlabel run without initialization of map.")
+
+        return self._artist.axis[side].label.set(text=text, **kwargs)
+
+    def update_projection(self, crs_new):
+        """Update the projection central coordinate.
+
+        Parameters
+        ----------
+        crs_new : `skyproj.SkyCRS`
+        """
+        self.projection = crs_new
